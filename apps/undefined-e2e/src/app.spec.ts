58f542fb6ab8c3f333ff05f1f3f8876f2df441c4
Davit-BetterAngels <davit@betterangels.la>
2023-08-09 21:53:51 -0700

    running nx with yarn

--- apps/undefined-e2e/src/app.spec.ts
+++ apps/undefined-e2e/src/app.spec.ts
@@ -1,11 +1,13 @@
-import { device, element, by, expect } from 'detox';
+import {by, device, element, expect} from 'detox';
 
 describe('Undefined', () => {
-  beforeEach(async () => {
-    await device.reloadReactNative();
-  });
+	beforeEach(async () => {
+		await device.reloadReactNative();
+	});
 
-  it('should display welcome message', async () => {
-    await expect(element(by.id('heading'))).toHaveText('Welcome Undefined 👋');
-  });
+	it('should display welcome message', async () => {
+		await expect(element(by.id('heading'))).toHaveText(
+			'Welcome Undefined 👋'
+		);
+	});
 });
