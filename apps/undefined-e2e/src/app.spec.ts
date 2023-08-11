66d43e6a0c013668e1d21196b05de3b44fd1a0f9
DavidManukyan <Davidmanookyan@gmail.com>
2023-08-10 21:07:21 -0700

    stable version V0.0.1

---
+++ apps/undefined-e2e/src/app.spec.ts
@@ -0,0 +1,13 @@
+import {by, device, element, expect} from 'detox';
+
+describe('Undefined', () => {
+	beforeEach(async () => {
+		await device.reloadReactNative();
+	});
+
+	it('should display welcome message', async () => {
+		await expect(element(by.id('heading'))).toHaveText(
+			'Welcome Undefined 👋'
+		);
+	});
+});
