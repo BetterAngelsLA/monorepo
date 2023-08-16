6307cd293558c59c2a092d836f2e3cbc1e353dc9
Paul Vecchio <paul@betterangels.la>
2023-08-16 13:09:35 -0700

    noqa settings line

--- apps/betterangels-backend/betterangels_backend/settings.py
+++ apps/betterangels-backend/betterangels_backend/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",  # noqa: B950
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
