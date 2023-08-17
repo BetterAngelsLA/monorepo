56a824e0a5ef1b2a897ec9ce64bae5706973b48e
Paul Vecchio <paul@betterangels.la>
2023-08-17 14:14:28 -0700

    this should work??

--- apps/outreach/index.js
+++ apps/outreach/index.js
@@ -1,6 +1,11 @@
 import { registerRootComponent } from 'expo';
+import { ExpoRoot } from 'expo-router';
 
-import App from './src/app/App';
+// Must be exported or Fast Refresh won't update the context
+export function App() {
+  const ctx = require.context('./src/app');
+  return <ExpoRoot context={ctx} />;
+}
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
