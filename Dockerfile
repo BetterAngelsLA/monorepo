2b704c59be79e7b49907d1561e99c4574f91049e
Anthony <antkim003@gmail.com>
2023-08-17 12:58:37 -0700

    Correct nx configurations

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM nikolaik/python-nodejs:python3.11-nodejs20-bullseye
 
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
 # TODO: This is only for dev so we can likely make this a dev target instead of global
 RUN mkdir -p /workspace/node_modules /workspace/.venv \
     && chown -R pn:pn /workspace/node_modules /workspace/.venv
--- apps/betterangels-backend/betterangels_backend/settings.py
+++ apps/betterangels-backend/betterangels_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,8 +76,12 @@ WSGI_APPLICATION = "betterangels_backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_NAME"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
