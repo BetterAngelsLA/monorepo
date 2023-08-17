3c3bbb504d757abff8388fc58de426e0b5d47ef1
Anthony <antkim003@gmail.com>
2023-08-17 16:05:57 -0700

    updating version fix

--- apps/betterangels-backend/betterangels_backend/settings.py
+++ apps/betterangels-backend/betterangels_backend/settings.py
@@ -23,6 +23,7 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
