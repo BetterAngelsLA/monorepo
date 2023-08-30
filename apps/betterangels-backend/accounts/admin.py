60074e9368fdfaa77b73e713765e19c1295e4b47
Anthony <antkim003@gmail.com>
2023-08-29 17:19:51 -0700

    Fix type for base user admin type

--- apps/betterangels-backend/accounts/admin.py
+++ apps/betterangels-backend/accounts/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .forms import UserChangeForm, UserCreationForm
 from .models import User
 
 
-class UserAdmin(UserAdmin):
+class UserAdmin(BaseUserAdmin):
     add_form = UserCreationForm
     form = UserChangeForm
     model = User
