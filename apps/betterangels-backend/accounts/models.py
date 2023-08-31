0c3cba465367cbe5231d68857b883e9a2e958bd4
Paul Vecchio <paul@betterangels.la>
2023-08-31 13:36:19 -0700

    Switch to mypy (#21)

--- apps/betterangels-backend/accounts/models.py
+++ apps/betterangels-backend/accounts/models.py
@@ -1,16 +1,21 @@
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from typing import List
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
 from .managers import UserManager
 
 
-class User(AbstractUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    is_superuser = models.BooleanField()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS: List[str] = []
 
-    objects: UserManager["User"] = UserManager()
+    objects = UserManager()
 
-    def __str__(self):
+    def __str__(self: "User") -> str:
         return self.email
