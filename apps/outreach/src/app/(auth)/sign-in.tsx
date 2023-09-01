0328c1441859206efed5817befc74154b02a742a
Davit-BetterAngels <davit@betterangels.la>
2023-08-31 20:05:20 -0700

    font awesome icon setup

--- apps/outreach/src/app/(auth)/sign-in.tsx
+++ apps/outreach/src/app/(auth)/sign-in.tsx
@@ -1,6 +1,8 @@
+import { faHouse } from '@fortawesome/pro-regular-svg-icons/faHouse';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { Button } from '@monorepo/expo/ui-components';
 import { Stack } from 'expo-router';
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 
 export default function SignIn() {
   return (
@@ -9,6 +11,18 @@ export default function SignIn() {
       <View>
         <Text>This is sign in</Text>
         <Button title="Sign In" onPress={() => console.log('signed in')} />
+        <FontAwesomeIcon
+          icon="square-check" // not working
+        />
+        <Pressable>
+          {({ pressed }) => (
+            <FontAwesomeIcon
+              icon={faHouse}
+              size={25}
+              style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }} // not working
+            />
+          )}
+        </Pressable>
       </View>
     </>
   );
