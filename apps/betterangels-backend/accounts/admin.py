8733555003a5c69baf85bd798ed71da78495cb60
Anthony Kim <antkim003@users.noreply.github.com>
2023-09-06 11:33:25 -0700

    Django all auth implementation (#25)

--- apps/betterangels-backend/accounts/admin.py
+++ apps/betterangels-backend/accounts/admin.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib import admin
+from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .forms import UserChangeForm, UserCreationForm
@@ -15,3 +17,5 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(User, UserAdmin)
+
+admin.site.login = staff_member_required(admin.site.login, login_url=settings.LOGIN_URL)
