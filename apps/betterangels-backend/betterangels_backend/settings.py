dd21edb7be1407d11733dd7acbf62e47265c7666
Anthony <antkim003@gmail.com>
2023-09-05 19:22:51 -0700

    adding login for admin pagee

--- apps/betterangels-backend/betterangels_backend/settings.py
+++ apps/betterangels-backend/betterangels_backend/settings.py
@@ -173,7 +173,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
-LOGIN_URL = "accounts/login"
+LOGIN_URL = "/accounts/login/"
 
 
 # ALL AUTH SETTINGS
