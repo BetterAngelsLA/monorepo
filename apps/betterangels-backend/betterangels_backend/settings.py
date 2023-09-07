adaa5d2a061fe1a4a0374cf99c0285875f2ab4f1
Anthony <antkim003@gmail.com>
2023-09-07 09:35:48 -0700

    Adding back env and changing to offline

--- apps/betterangels-backend/betterangels_backend/settings.py
+++ apps/betterangels-backend/betterangels_backend/settings.py
@@ -87,7 +87,7 @@ SOCIALACCOUNT_PROVIDERS = {
             "key": "",
         },
         "AUTH_PARAMS": {
-            "access_type": "online",
+            "access_type": "offline",
         },
     }
 }
