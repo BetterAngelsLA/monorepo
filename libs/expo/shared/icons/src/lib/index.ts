d097c67ebaa1c17b96d96c002c90e4d83aac931d
Anthony <antkim003@gmail.com>
2023-09-11 12:21:54 -0700

    Simplifying folder structure

--- libs/expo/shared/icons/src/lib/index.ts
+++ libs/expo/shared/icons/src/lib/index.ts
@@ -1,195 +1,193 @@
-export { default as HomeIcon } from './HomeIcon';
+export { default as ArrowDownToLineIcon } from './components/ArrowDownToLineIcon';
 
-export { default as ArrowDownToLineIcon } from './icon/ArrowDownToLineIcon';
+export { default as ArrowDownIcon } from './components/ArrowDownIcon';
 
-export { default as ArrowDownIcon } from './icon/ArrowDownIcon';
+export { default as ArrowLeftIcon } from './components/ArrowLeftIcon';
 
-export { default as ArrowLeftIcon } from './icon/ArrowLeftIcon';
+export { default as ArrowRightArrowLeftIcon } from './components/ArrowRightArrowLeftIcon';
 
-export { default as ArrowRightArrowLeftIcon } from './icon/ArrowRightArrowLeftIcon';
+export { default as ArrowRightIcon } from './components/ArrowRightIcon';
 
-export { default as ArrowRightIcon } from './icon/ArrowRightIcon';
+export { default as ArrowTrendUpIcon } from './components/ArrowTrendUpIcon';
 
-export { default as ArrowTrendUpIcon } from './icon/ArrowTrendUpIcon';
+export { default as ArrowUpIcon } from './components/ArrowUpIcon';
 
-export { default as ArrowUpIcon } from './icon/ArrowUpIcon';
+export { default as BarsIcon } from './components/BarsIcon';
 
-export { default as BarsIcon } from './icon/BarsIcon';
+export { default as BellIcon } from './components/BellIcon';
 
-export { default as BellIcon } from './icon/BellIcon';
+export { default as BlanketIcon } from './components/BlanketIcon';
 
-export { default as BlanketIcon } from './icon/BlanketIcon';
+export { default as BookOpenIcon } from './components/BookOpenIcon';
 
-export { default as BookOpenIcon } from './icon/BookOpenIcon';
+export { default as BottleWaterIcon } from './components/BottleWaterIcon';
 
-export { default as BottleWaterIcon } from './icon/BottleWaterIcon';
+export { default as BriefcaseMedicalIcon } from './components/BriefcaseMedicalIcon';
 
-export { default as BriefcaseMedicalIcon } from './icon/BriefcaseMedicalIcon';
+export { default as BuildingUserIcon } from './components/BuildingUserIcon';
 
-export { default as BuildingUserIcon } from './icon/BuildingUserIcon';
+export { default as BurgerSodaIcon } from './components/BurgerSodaIcon';
 
-export { default as BurgerSodaIcon } from './icon/BurgerSodaIcon';
+export { default as ArrowRightFromArcIcon } from './components/ArrowRightFromArcIcon';
 
-export { default as ArrowRightFromArcIcon } from './icon/ArrowRightFromArcIcon';
+export { default as CalendarIcon } from './components/CalendarIcon';
 
-export { default as CalendarIcon } from './icon/CalendarIcon';
+export { default as CarIcon } from './components/CarIcon';
 
-export { default as CarIcon } from './icon/CarIcon';
+export { default as CheckIcon } from './components/CheckIcon';
 
-export { default as CheckIcon } from './icon/CheckIcon';
+export { default as CircleXmarkIcon } from './components/CircleXmarkIcon';
 
-export { default as CircleXmarkIcon } from './icon/CircleXmarkIcon';
+export { default as EyeIcon } from './components/EyeIcon';
 
-export { default as EyeIcon } from './icon/EyeIcon';
+export { default as FaceAngryIcon } from './components/FaceAngryIcon';
 
-export { default as FaceAngryIcon } from './icon/FaceAngryIcon';
+export { default as FaceAnxiousSweatIcon } from './components/FaceAnxiousSweatIcon';
 
-export { default as FaceAnxiousSweatIcon } from './icon/FaceAnxiousSweatIcon';
+export { default as FaceAstonishedIcon } from './components/FaceAstonishedIcon';
 
-export { default as FaceAstonishedIcon } from './icon/FaceAstonishedIcon';
+export { default as FaceAwesomeIcon } from './components/FaceAwesomeIcon';
 
-export { default as FaceAwesomeIcon } from './icon/FaceAwesomeIcon';
+export { default as FaceCloudsIcon } from './components/FaceCloudsIcon';
 
-export { default as FaceCloudsIcon } from './icon/FaceCloudsIcon';
+export { default as FaceDiagonalMouthIcon } from './components/FaceDiagonalMouthIcon';
 
-export { default as FaceDiagonalMouthIcon } from './icon/FaceDiagonalMouthIcon';
+export { default as FaceDisappointedIcon } from './components/FaceDisappointedIcon';
 
-export { default as FaceDisappointedIcon } from './icon/FaceDisappointedIcon';
+export { default as ArrowLeftToArcIcon } from './components/ArrowLeftToArcIcon';
 
-export { default as ArrowLeftToArcIcon } from './icon/ArrowLeftToArcIcon';
+export { default as FaceEyesXmarksIcon } from './components/FaceEyesXmarksIcon';
 
-export { default as FaceEyesXmarksIcon } from './icon/FaceEyesXmarksIcon';
+export { default as FaceFlushedIcon } from './components/FaceFlushedIcon';
 
-export { default as FaceFlushedIcon } from './icon/FaceFlushedIcon';
+export { default as FaceFrownIcon } from './components/FaceFrownIcon';
 
-export { default as FaceFrownIcon } from './icon/FaceFrownIcon';
+export { default as FaceHandYawnIcon } from './components/FaceHandYawnIcon';
 
-export { default as FaceHandYawnIcon } from './icon/FaceHandYawnIcon';
+export { default as ArrowProgressIcon } from './components/ArrowProgressIcon';
 
-export { default as ArrowProgressIcon } from './icon/ArrowProgressIcon';
+export { default as FaceLaughBeamIcon } from './components/FaceLaughBeamIcon';
 
-export { default as FaceLaughBeamIcon } from './icon/FaceLaughBeamIcon';
+export { default as FaceLaughSquintIcon } from './components/FaceLaughSquintIcon';
 
-export { default as FaceLaughSquintIcon } from './icon/FaceLaughSquintIcon';
+export { default as FaceMaskIcon } from './components/FaceMaskIcon';
 
-export { default as FaceMaskIcon } from './icon/FaceMaskIcon';
+export { default as FaceMehIcon } from './components/FaceMehIcon';
 
-export { default as FaceMehIcon } from './icon/FaceMehIcon';
+export { default as FaceMeltingIcon } from './components/FaceMeltingIcon';
 
-export { default as FaceMeltingIcon } from './icon/FaceMeltingIcon';
+export { default as FaceMonocleIcon } from './components/FaceMonocleIcon';
 
-export { default as FaceMonocleIcon } from './icon/FaceMonocleIcon';
+export { default as FaceNauseatedIcon } from './components/FaceNauseatedIcon';
 
-export { default as FaceNauseatedIcon } from './icon/FaceNauseatedIcon';
+export { default as FaceNoseSteamIcon } from './components/FaceNoseSteamIcon';
 
-export { default as FaceNoseSteamIcon } from './icon/FaceNoseSteamIcon';
+export { default as FacePerseveringIcon } from './components/FacePerseveringIcon';
 
-export { default as FacePerseveringIcon } from './icon/FacePerseveringIcon';
+export { default as FaceRelievedIcon } from './components/FaceRelievedIcon';
 
-export { default as FaceRelievedIcon } from './icon/FaceRelievedIcon';
+export { default as FaceSadCryIcon } from './components/FaceSadCryIcon';
 
-export { default as FaceSadCryIcon } from './icon/FaceSadCryIcon';
+export { default as FaceSadTearIcon } from './components/FaceSadTearIcon';
 
-export { default as FaceSadTearIcon } from './icon/FaceSadTearIcon';
+export { default as FaceScreamIcon } from './components/FaceScreamIcon';
 
-export { default as FaceScreamIcon } from './icon/FaceScreamIcon';
+export { default as FaceSleepyIcon } from './components/FaceSleepyIcon';
 
-export { default as FaceSleepyIcon } from './icon/FaceSleepyIcon';
+export { default as FaceSmileHeartsIcon } from './components/FaceSmileHeartsIcon';
 
-export { default as FaceSmileHeartsIcon } from './icon/FaceSmileHeartsIcon';
+export { default as FaceExplodeIcon } from './components/FaceExplodeIcon';
 
-export { default as FaceExplodeIcon } from './icon/FaceExplodeIcon';
+export { default as FaceSmileIcon } from './components/FaceSmileIcon';
 
-export { default as FaceSmileIcon } from './icon/FaceSmileIcon';
+export { default as FaceSpiralEyesIcon } from './components/FaceSpiralEyesIcon';
 
-export { default as FaceSpiralEyesIcon } from './icon/FaceSpiralEyesIcon';
+export { default as FaceSunglassesIcon } from './components/FaceSunglassesIcon';
 
-export { default as FaceSunglassesIcon } from './icon/FaceSunglassesIcon';
+export { default as FaceSwearIcon } from './components/FaceSwearIcon';
 
-export { default as FaceSwearIcon } from './icon/FaceSwearIcon';
+export { default as FaceHeadBandageIcon } from './components/FaceHeadBandageIcon';
 
-export { default as FaceHeadBandageIcon } from './icon/FaceHeadBandageIcon';
+export { default as FaceTiredIcon } from './components/FaceTiredIcon';
 
-export { default as FaceTiredIcon } from './icon/FaceTiredIcon';
+export { default as FaceThermometerIcon } from './components/FaceThermometerIcon';
 
-export { default as FaceThermometerIcon } from './icon/FaceThermometerIcon';
+export { default as FaceTongueSweatIcon } from './components/FaceTongueSweatIcon';
 
-export { default as FaceTongueSweatIcon } from './icon/FaceTongueSweatIcon';
+export { default as FaceUnamusedIcon } from './components/FaceUnamusedIcon';
 
-export { default as FaceUnamusedIcon } from './icon/FaceUnamusedIcon';
+export { default as FaceTissueIcon } from './components/FaceTissueIcon';
 
-export { default as FaceTissueIcon } from './icon/FaceTissueIcon';
+export { default as FaceWearyIcon } from './components/FaceWearyIcon';
 
-export { default as FaceWearyIcon } from './icon/FaceWearyIcon';
+export { default as FaceZipperIcon } from './components/FaceZipperIcon';
 
-export { default as FaceZipperIcon } from './icon/FaceZipperIcon';
+export { default as FileArrowDownIcon } from './components/FileArrowDownIcon';
 
-export { default as FileArrowDownIcon } from './icon/FileArrowDownIcon';
+export { default as FileAudioIcon } from './components/FileAudioIcon';
 
-export { default as FileAudioIcon } from './icon/FileAudioIcon';
+export { default as FaceSmileTearIcon } from './components/FaceSmileTearIcon';
 
-export { default as FaceSmileTearIcon } from './icon/FaceSmileTearIcon';
+export { default as FaceVomitIcon } from './components/FaceVomitIcon';
 
-export { default as FaceVomitIcon } from './icon/FaceVomitIcon';
+export { default as FlagSwallowtailIcon } from './components/FlagSwallowtailIcon';
 
-export { default as FlagSwallowtailIcon } from './icon/FlagSwallowtailIcon';
+export { default as GearIcon } from './components/GearIcon';
 
-export { default as GearIcon } from './icon/GearIcon';
+export { default as HeadSideBrainIcon } from './components/HeadSideBrainIcon';
 
-export { default as HeadSideBrainIcon } from './icon/HeadSideBrainIcon';
+export { default as HouseBlankIcon } from './components/HouseBlankIcon';
 
-export { default as HouseBlankIcon } from './icon/HouseBlankIcon';
+export { default as FilePdfIcon } from './components/FilePdfIcon';
 
-export { default as FilePdfIcon } from './icon/FilePdfIcon';
+export { default as ImageIcon } from './components/ImageIcon';
 
-export { default as ImageIcon } from './icon/ImageIcon';
+export { default as CalendarDaysIcon } from './components/CalendarDaysIcon';
 
-export { default as CalendarDaysIcon } from './icon/CalendarDaysIcon';
+export { default as HouseIcon } from './components/HouseIcon';
 
-export { default as HouseIcon } from './icon/HouseIcon';
+export { default as MapIcon } from './components/MapIcon';
 
-export { default as MapIcon } from './icon/MapIcon';
+export { default as MicrophoneIcon } from './components/MicrophoneIcon';
 
-export { default as MicrophoneIcon } from './icon/MicrophoneIcon';
+export { default as NoteIcon } from './components/NoteIcon';
 
-export { default as NoteIcon } from './icon/NoteIcon';
+export { default as MagnifyingGlassIcon } from './components/MagnifyingGlassIcon';
 
-export { default as MagnifyingGlassIcon } from './icon/MagnifyingGlassIcon';
+export { default as FileIcon } from './components/FileIcon';
 
-export { default as FileIcon } from './icon/FileIcon';
+export { default as PlusIcon } from './components/PlusIcon';
 
-export { default as PlusIcon } from './icon/PlusIcon';
+export { default as PrescriptionIcon } from './components/PrescriptionIcon';
 
-export { default as PrescriptionIcon } from './icon/PrescriptionIcon';
+export { default as ShirtIcon } from './components/ShirtIcon';
 
-export { default as ShirtIcon } from './icon/ShirtIcon';
+export { default as PawIcon } from './components/PawIcon';
 
-export { default as PawIcon } from './icon/PawIcon';
+export { default as ShowerIcon } from './components/ShowerIcon';
 
-export { default as ShowerIcon } from './icon/ShowerIcon';
+export { default as SquareCheckIcon } from './components/SquareCheckIcon';
 
-export { default as SquareCheckIcon } from './icon/SquareCheckIcon';
+export { default as StethoscopeIcon } from './components/StethoscopeIcon';
 
-export { default as StethoscopeIcon } from './icon/StethoscopeIcon';
+export { default as SyringeIcon } from './components/SyringeIcon';
 
-export { default as SyringeIcon } from './icon/SyringeIcon';
+export { default as ToothIcon } from './components/ToothIcon';
 
-export { default as ToothIcon } from './icon/ToothIcon';
+export { default as ToothbrushIcon } from './components/ToothbrushIcon';
 
-export { default as ToothbrushIcon } from './icon/ToothbrushIcon';
+export { default as UserDoctorHairIcon } from './components/UserDoctorHairIcon';
 
-export { default as UserDoctorHairIcon } from './icon/UserDoctorHairIcon';
+export { default as UserIcon } from './components/UserIcon';
 
-export { default as UserIcon } from './icon/UserIcon';
+export { default as VideoIcon } from './components/VideoIcon';
 
-export { default as VideoIcon } from './icon/VideoIcon';
+export { default as WarehouseIcon } from './components/WarehouseIcon';
 
-export { default as WarehouseIcon } from './icon/WarehouseIcon';
+export { default as XmarkIcon } from './components/XmarkIcon';
 
-export { default as XmarkIcon } from './icon/XmarkIcon';
+export { default as PaperclipIcon } from './components/PaperclipIcon';
 
-export { default as PaperclipIcon } from './icon/PaperclipIcon';
+export { default as LocationDotIcon } from './components/LocationDotIcon';
 
-export { default as LocationDotIcon } from './icon/LocationDotIcon';
-
-export { default as ShoePrintsIcon } from './icon/ShoePrintsIcon';
+export { default as ShoePrintsIcon } from './components/ShoePrintsIcon';
