50cd7251c4cc4a436d51d9079ad1bf94154f8319
Anthony <antkim003@gmail.com>
2023-09-12 13:11:32 -0700

    Adding WIP backend w/ upgraded packages

--- apps/betterangels-backend/accounts/views.py
+++ apps/betterangels-backend/accounts/views.py
@@ -1,4 +1,5 @@
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
 from django.db import models
 from django.urls import reverse_lazy
@@ -15,3 +16,5 @@ class SignUpView(CreateView[models.Model, UserCreationForm]):
 
 class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
+    callback_url = "http://127.0.0.1:8000/"
+    client_class = OAuth2Client
