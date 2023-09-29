bc272b7513d67568e743a00ca60681c54f7c7098
Davit-BetterAngels <davit@betterangels.la>
2023-09-29 03:22:07 +0000

    config cleanup

--- apps/betterangels/config.ts
+++ apps/betterangels/config.ts
@@ -1,20 +1,9 @@
 import { makeRedirectUri } from 'expo-auth-session';
 import { Platform } from 'react-native';
 
-function getRequiredEnvVar(name: string): string {
-  const value = process.env[name];
-  if (!value) {
-    throw new Error(`${name} is not defined!`);
-  }
-  return value;
-}
-
 export const clientId = process.env.EXPO_PUBLIC_CLIENT_ID;
-// getRequiredEnvVar('EXPO_PUBLIC_CLIENT_ID');
 export const redirectUri =
   Platform.OS === 'web'
     ? makeRedirectUri()
     : process.env.EXPO_PUBLIC_REDIRECT_URL;
-// getRequiredEnvVar('EXPO_PUBLIC_REDIRECT_URL');
 export const apiUrl = process.env.EXPO_PUBLIC_API_URL;
-// getRequiredEnvVar('EXPO_PUBLIC_API_URL');
