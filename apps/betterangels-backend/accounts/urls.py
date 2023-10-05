74401043548038269d30b10dc3affb948225e163
Paul Vecchio <paul@betterangels.la>
2023-10-05 12:00:32 -0700

    Fix social auth

--- apps/betterangels-backend/accounts/urls.py
+++ apps/betterangels-backend/accounts/urls.py
@@ -4,6 +4,6 @@ from .views import AuthRedirectView, GoogleLogin, SignUpView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
-    path("rest-auth/google/", GoogleLogin.as_view(), name="google_login"),
+    path("rest-auth/google/", GoogleLogin.as_view(), name="api_google_login"),
     path("auth-redirect", AuthRedirectView.as_view(), name="auth_redirect"),
 ]
--- apps/betterangels-backend/betterangels_backend/settings.py
+++ apps/betterangels-backend/betterangels_backend/settings.py
@@ -100,6 +100,7 @@ SOCIALACCOUNT_PROVIDERS = {
         "AUTH_PARAMS": {
             "access_type": "online",
         },
+        "OAUTH_PKCE_ENABLED": True,
     }
 }
 
