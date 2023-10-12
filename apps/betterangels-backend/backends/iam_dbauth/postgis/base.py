eb823d6c84f178b9386d1787eaf82989cb42b4a3
Paul Vecchio <paul@betterangels.la>
2023-10-12 10:39:26 -0700

    Harden for dev deployment: Add SSL, nginx, postgis, RDS IAM Auth (#40)
    * Adding whitenoise
    
    * Updating with allowed hosts
    
    * Add nginx project
    
    * newline
    
    * nx building of image
    
    * add notes
    
    * adding postgres host
    
    * update docker image
    
    * Adding postgressql-client
    
    * Add IAM DB Auth
    
    * fix local dev
    
    * Don't use resolve cnames
    
    * Fix social auth
    
    * handle https in proxy and also for links hopefully
    
    * rename TRUSTED_ORIGINS -> CSRF_TRUSTED_ORIGINS
    
    * CORS_ALLOWED_ORIGINS
    
    * Fix fonts
    
    * Add some session settings for security
    
    * Add HSTS
    
    * CSRF httponly cookie
    
    * Add language cookie httponly
    
    * More cookie secure
    
    * Fix HTTPS forwarding
    
    * I think this is proper whitenoise
    
    * Just use my own IAM Auth
    
    * Fix IAM auth creds
    
    * Add conmax age
    
    * Add conn_max_age and also print all dev db statements
    
    ---------
    
    Co-authored-by: Anthony <antkim003@gmail.com>

---
+++ apps/betterangels-backend/backends/iam_dbauth/postgis/base.py
@@ -0,0 +1,34 @@
+from typing import Any, Dict
+
+import boto3
+from django.contrib.gis.db.backends.postgis.base import (
+    DatabaseWrapper as PostGISDatabaseWrapper,
+)
+
+
+class DatabaseWrapper(PostGISDatabaseWrapper):
+    def get_connection_params(self) -> Dict[str, Any]:
+        # Original connection parameters
+        conn_params: Dict[str, Any] = super().get_connection_params()
+        iam_settings = self.settings_dict.get("IAM_SETTINGS", {})
+
+        # If IAM authentication is enabled in settings, generate an authentication token
+        if iam_settings.get("ENABLED", False):
+            region_name: str = iam_settings["REGION_NAME"]
+            db_hostname: str = self.settings_dict["HOST"]
+            db_port: str = self.settings_dict["PORT"]
+            db_username: str = self.settings_dict["USER"]
+
+            # Generate IAM auth token using boto3
+            rds_client = boto3.client("rds", region_name=region_name)
+            token = rds_client.generate_db_auth_token(
+                DBHostname=db_hostname,
+                Port=db_port,
+                DBUsername=db_username,
+                Region=region_name,
+            )
+
+            # Update connection parameters with the generated token as the password
+            conn_params["password"] = token
+
+        return conn_params
