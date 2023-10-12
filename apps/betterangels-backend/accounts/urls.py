97d1f0b5043d2ba6e6648527e293c9433926888e
Paul Vecchio <paul@betterangels.la>
2023-10-12 10:39:26 -0700

    Harden for dev deployment: Add SSL, nginx, postgis, RDS IAM Auth (#40)
    * Adding whitenoise
    
    * Updating with allowed hosts
    
    * Add nginx project
    
    * newline
    
    * nx building of image
    
    * add notes
    
    * adding postgres host
    
    * update docker image
    
    * Adding postgressql-client
    
    * Add IAM DB Auth
    
    * fix local dev
    
    * Don't use resolve cnames
    
    * Fix social auth
    
    * handle https in proxy and also for links hopefully
    
    * rename TRUSTED_ORIGINS -> CSRF_TRUSTED_ORIGINS
    
    * CORS_ALLOWED_ORIGINS
    
    * Fix fonts
    
    * Add some session settings for security
    
    * Add HSTS
    
    * CSRF httponly cookie
    
    * Add language cookie httponly
    
    * More cookie secure
    
    * Fix HTTPS forwarding
    
    * I think this is proper whitenoise
    
    * Just use my own IAM Auth
    
    * Fix IAM auth creds
    
    * Add conmax age
    
    * Add conn_max_age and also print all dev db statements
    
    ---------
    
    Co-authored-by: Anthony <antkim003@gmail.com>

--- apps/betterangels-backend/accounts/urls.py
+++ apps/betterangels-backend/accounts/urls.py
@@ -4,7 +4,7 @@ from .views import AuthRedirectView, GoogleLogin, SignUpView, current_user
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
-    path("rest-auth/google/", GoogleLogin.as_view(), name="google_login"),
+    path("rest-auth/google/", GoogleLogin.as_view(), name="api_google_login"),
     path("auth-redirect", AuthRedirectView.as_view(), name="auth_redirect"),
     path("current-user/", current_user, name="current-user"),
 ]
