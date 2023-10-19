45241b7abbfc9c9c857900ea857ff669091062c2
Paul Vecchio <paul@betterangels.la>
2023-10-19 14:47:47 -0700

    Switche away from proj

--- apps/betterangels-backend/betterangels_backend/celery.py
+++ apps/betterangels-backend/betterangels_backend/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "betterangels_backend.settings")
 
-app = Celery("proj")
+app = Celery("betterangels-backend")
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
