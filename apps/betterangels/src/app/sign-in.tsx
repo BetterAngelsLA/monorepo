367d52059d3c0c83065a62b57032b61f25742fa5
Davit-BetterAngels <davit@betterangels.la>
2023-10-20 07:04:12 +0000

    sign in screen, sign up screen and iconButton

--- apps/betterangels/src/app/sign-in.tsx
+++ apps/betterangels/src/app/sign-in.tsx
@@ -4,16 +4,45 @@ import {
   useStore,
   useUser,
 } from '@monorepo/expo/betterangels';
+import { GoogleIcon, Windowsicon } from '@monorepo/expo/shared/icons';
+import { colors } from '@monorepo/expo/shared/static';
 import { Button } from '@monorepo/expo/shared/ui-components';
 import { Buffer } from 'buffer';
 import * as AuthSession from 'expo-auth-session';
 import * as Crypto from 'expo-crypto';
-import { router } from 'expo-router';
+import { router, useLocalSearchParams } from 'expo-router';
 import * as WebBrowser from 'expo-web-browser';
 import React, { useCallback, useEffect, useState } from 'react';
-import { Linking, SafeAreaView, Text, View } from 'react-native';
+import { Linking, StyleSheet, Text, View } from 'react-native';
 import { apiUrl, clientId, redirectUri } from '../../config';
-import Logo from './assets/images/logo.svg';
+
+type TAuthFLow = {
+  [key in 'sign-in' | 'sign-up']: {
+    welcome: string;
+    title: string;
+    message: string | null;
+    question: string;
+    link: string;
+  };
+};
+
+const FLOW: TAuthFLow = {
+  'sign-in': {
+    welcome: 'WELCOME BACK!',
+    title: 'SIGN IN',
+    message: null,
+    question: "Don't have an account?",
+    link: 'Sign up',
+  },
+  'sign-up': {
+    welcome: 'WELCOME!',
+    title: 'SIGN UP',
+    message:
+      'If you work as a case manager or health professional, please use your work email to sign up the first time.',
+    question: 'Already have an account?',
+    link: 'Sign in',
+  },
+};
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -39,9 +68,11 @@ export default function SignIn() {
   const [generatedState, setGeneratedState] = useState<string | undefined>(
     undefined
   );
+  const [flow, setFlow] = useState<'sign-in' | 'sign-up'>('sign-in');
   const discovery = AuthSession.useAutoDiscovery(discoveryUrl);
   const { saveStore } = useStore();
   const { setUser } = useUser();
+  const { type } = useLocalSearchParams();
 
   useEffect(() => {
     setGeneratedState(generateStatePayload());
@@ -49,6 +80,10 @@ export default function SignIn() {
 
   if (!clientId || !redirectUri || !apiUrl) {
     throw new Error('env required');
+  }
+
+  if (type !== 'sign-up' && type !== 'sign-in') {
+    throw new Error('auth param is incorrect');
   }
 
   const [request, response, promptAsync] = AuthSession.useAuthRequest(
@@ -152,41 +187,92 @@ export default function SignIn() {
     void Linking.getInitialURL().then(async (url) => handleDeepLinking(url));
   }, [response, handleDeepLinking]);
 
+  useEffect(() => {
+    setFlow(type);
+  }, [type]);
+
   if (!generatedState) {
     return <Text>Loading...</Text>;
   }
 
   return (
     <AuthContainer imageSource={require('./assets/images/auth-background.png')}>
-      <SafeAreaView
-        style={{
-          flex: 1,
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <View />
-        <Logo width={200} height={30} />
-        <View style={{ width: '100%', paddingBottom: 113 }}>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>{FLOW[flow].welcome}</Text>
+        <Text style={styles.title}>{FLOW[flow].title}</Text>
+        {FLOW[flow].message && (
+          <Text style={styles.message}>{FLOW[flow].message}</Text>
+        )}
+        <View style={{ width: '100%', marginBottom: 24 }}>
           <Button
             style={{ marginBottom: 8 }}
-            title="Sign in with Google"
+            title={`${FLOW[flow].link} with Microsoft`}
+            disabled
+            icon={<Windowsicon size="sm" />}
+            fontFamily="IBM-bold"
             size="full"
             variant="dark"
             align="flex-start"
             onPress={() => promptAsync({ showInRecents: false })}
-            disabled={!generatedState && !request}
           />
           <Button
             size="full"
-            title="Sign in with Google"
+            title={`${FLOW[flow].link} with Google`}
             align="flex-start"
+            icon={<GoogleIcon size="sm" />}
+            fontFamily="IBM-bold"
             variant="dark"
             onPress={() => promptAsync({ showInRecents: false })}
             disabled={!generatedState && !request}
           />
         </View>
-      </SafeAreaView>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Text style={styles.question}>{FLOW[flow].question}</Text>
+          <Text
+            style={styles.link}
+            onPress={() => setFlow(flow === 'sign-in' ? 'sign-up' : 'sign-in')}
+          >
+            {' '}
+            {FLOW[flow].link}
+          </Text>
+        </View>
+      </View>
     </AuthContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 170,
+  },
+  welcome: {
+    fontFamily: 'IBM-semibold',
+    fontSize: 16,
+    textTransform: 'uppercase',
+    color: colors.brandYellow,
+    marginBottom: 8,
+  },
+  title: {
+    fontFamily: 'IBM-semibold',
+    fontSize: 32,
+    textTransform: 'uppercase',
+    color: colors.brandAngelBlue,
+    marginBottom: 56,
+  },
+  message: {
+    fontFamily: 'Pragmatica-medium',
+    color: colors.white,
+    fontSize: 16,
+    marginBottom: 24,
+  },
+  question: {
+    fontFamily: 'Pragmatica-medium',
+    color: colors.white,
+    fontSize: 16,
+  },
+  link: {
+    fontFamily: 'Pragmatica-bold',
+    textDecorationLine: 'underline',
+    color: colors.brandLightBlue,
+  },
+});
--- libs/expo/shared/static/src/lib/colors.ts
+++ libs/expo/shared/static/src/lib/colors.ts
@@ -1,7 +1,26 @@
+interface IColors {
+  brandAngelBlue: '#F2FAFC';
+  brandLightBlue: '#9CDCED';
+  brandBlue: '#375C76';
+  brandDarkBlue: '#1E3342';
+  brandYellow: '#FFF82E';
+  blue: '#00B3EB';
+  darkBlue: '#102C55';
+  white: '#ffffff';
+  lightGray: '#F1F3F3';
+  gray: '#9FAAB2';
+  darkGray: '#697A87';
+  disabled: '#cbd0d7';
+  red: '#CB0808';
+  borderRed: '#DE2F2F';
+}
+
 export default {
+  brandAngelBlue: '#F2FAFC',
   brandLightBlue: '#9CDCED',
   brandBlue: '#375C76',
   brandDarkBlue: '#1E3342',
+  brandYellow: '#FFF82E',
   blue: '#00B3EB',
   darkBlue: '#102C55',
   white: '#ffffff',
@@ -11,4 +30,4 @@ export default {
   disabled: '#cbd0d7',
   red: '#CB0808',
   borderRed: '#DE2F2F',
-};
+} as IColors;
