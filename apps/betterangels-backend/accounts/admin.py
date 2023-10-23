6b55c353481e2e754a9d6a02a2589f147b0b1327
Paul Vecchio <paul@betterangels.la>
2023-10-23 13:45:39 -0700

    Remove unused import

--- apps/betterangels-backend/accounts/admin.py
+++ apps/betterangels-backend/accounts/admin.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.contrib import admin
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.sessions.models import Session
 
 from .forms import UserChangeForm, UserCreationForm
 from .models import User
