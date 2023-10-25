8667bdfb526dfcaa883c1c159f851aaeb23685e4
Anthony Kim <antkim003@users.noreply.github.com>
2023-10-25 15:02:32 -0700

    Fix user profile page on admin by getting rid of django default fields (#56)

--- apps/betterangels-backend/accounts/models.py
+++ apps/betterangels-backend/accounts/models.py
@@ -18,6 +18,10 @@ class User(AbstractBaseUser, PermissionsMixin):
         ),
         validators=[username_validator],
     )
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(null=True, blank=True)
     email = models.EmailField(unique=True)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(
