20b6edd4f32934b10355eef6473f3501479049ca
Paul Vecchio <paul@betterangels.la>
2023-10-25 11:23:01 -0700

    Add send queued mail and cleanup mail schedules

--- apps/betterangels-backend/betterangels_backend/celery.py
+++ apps/betterangels-backend/betterangels_backend/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "betterangels_backend.settings")
@@ -15,3 +16,17 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    # In case of a temporary delivery failure, we retry.
+    "send-queued-mail": {
+        "task": "post_office.tasks.send_queued_mail",
+        "schedule": 600.0,  # 5 Minutes
+    },
+    # Cleanup old mail
+    "cleanup-mail": {
+        "task": "post_office.tasks.cleanup_mail",
+        "schedule": crontab(minute=0, hour=7),  # 7 AM UTC, corresponds to midnight PT
+        "kwargs": {"days": 90, "delete_attachments": True},
+    },
+}
