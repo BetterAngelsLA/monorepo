2b14a555c2d39becec7c8dd03528722a52ea3e06
Paul Vecchio <paul@betterangels.la>
2023-11-10 10:32:52 -0800

    Oops this is in two places

--- apps/betterangels-backend/betterangels_backend/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class BetterAngelsConfig(AppConfig):
-    name: str = 'betterangels_backend'
-
-    def ready(self) -> None:
-        from . import signals  # noqa: F401
