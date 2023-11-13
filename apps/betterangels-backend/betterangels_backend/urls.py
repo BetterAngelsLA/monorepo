5bebf0365ea36ce33fdf47b70c327231e8f29555
Paul Vecchio <paul@betterangels.la>
2023-11-13 15:14:06 -0800

    Remove healthcheck

--- apps/betterangels-backend/betterangels_backend/urls.py
+++ apps/betterangels-backend/betterangels_backend/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from betterangels_backend.views import health_check
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path("", health_check, name="health_check"),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("", include("accounts.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls"), name="accounts"),
--- apps/betterangels-backend/betterangels_backend/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpRequest, JsonResponse
-
-
-def health_check(request: HttpRequest) -> JsonResponse:
-    return JsonResponse({"status": "healthy"}, status=200)
