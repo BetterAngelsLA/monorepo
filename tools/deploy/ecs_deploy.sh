f2ae33b9d3de3a313f4d0b7e33f34668c1a09a89
Paul Vecchio <paul@betterangels.la>
2023-11-13 11:22:30 -0800

    Working local build and push

--- tools/deploy/ecs_deploy.sh
+++ tools/deploy/ecs_deploy.sh
@@ -2,43 +2,46 @@
 
 # Script Description:
 # This script creates a JSON file with container and image URI details, zips the file,
-# and uploads it to an S3 bucket.
+# and conditionally uploads it to an S3 bucket based on the Docker tag.
 
 # Check for AWS CLI installation
-if ! command -v aws &> /dev/null
-then
-    echo "AWS CLI could not be found. Please install it to run this script."
+if ! command -v aws &> /dev/null; then
+    echo "Error: AWS CLI could not be found. Please install it to run this script."
     exit 1
 fi
 
 # Ensuring all required environment variables are set
-MISSING_VARS=false
-
-for var in CONTAINER_NAME IMAGE_JSON_NAME DIST_PATH ECR_REGISTRY ZIP_NAME BUILD_ARTIFACT_BUCKET; do
+for var in CONTAINER_NAME ECR_REGISTRY BUILD_ARTIFACT_BUCKET APP_NAME DOCKER_TAG; do
     if [ -z "${!var}" ]; then
         echo "Error: Environment variable $var is not set."
-        MISSING_VARS=true
+        exit 1
     fi
 done
 
-if [ "$MISSING_VARS" = true ]; then
-    exit 1
-fi
+# Constants
+IMAGE_JSON_NAME="imagedefinitions.json"
+ZIP_NAME="imagedefinitions.zip"
 
-DIST_PATH=./dist/${APP_NAME}
-IMAGE_JSON_NAME=imagedefinitions.json
-ZIP_NAME=imagedefinitions.zip
-REPO_URI="${ECR_REGISTRY}:${DOCKER_TAG:-dev}"
+# Paths
+DIST_PATH="./dist/${APP_NAME}"
+REPO_URI="${ECR_REGISTRY}/${APP_NAME}:${DOCKER_TAG}"
 IMAGE_JSON_PATH="${DIST_PATH}/${IMAGE_JSON_NAME}"
 IMAGE_ZIP_PATH="${DIST_PATH}/${ZIP_NAME}"
-S3_PATH=${BUILD_ARTIFACT_BUCKET}/${APP_NAME}/${ZIP_NAME}"
+S3_PATH="${BUILD_ARTIFACT_BUCKET}/${APP_NAME}/${ZIP_NAME}"
 
+# Create the JSON file
 echo "Creating ${IMAGE_JSON_NAME} file..."
-mkdir -p "${DIST_PATH}"
-echo "[{\"name\": \"${CONTAINER_NAME}\", \"imageUri\": \"${REPO_URI}\"}]" > "${IMAGE_JSON_PATH}"
+mkdir -p "${DIST_PATH}" || { echo "Error creating directory ${DIST_PATH}"; exit 1; }
+echo "[{\"name\": \"${CONTAINER_NAME}\", \"imageUri\": \"${REPO_URI}\"}]" > "${IMAGE_JSON_PATH}" || { echo "Error writing to ${IMAGE_JSON_PATH}"; exit 1; }
 
+# Zip the file
 echo "Zipping ${IMAGE_JSON_NAME}..."
-zip -j "${IMAGE_ZIP_PATH}" "${IMAGE_JSON_PATH}"
+zip -j "${IMAGE_ZIP_PATH}" "${IMAGE_JSON_PATH}" || { echo "Error zipping file ${IMAGE_JSON_PATH}"; exit 1; }
 
-echo "Pushing ${ZIP_NAME} to S3..."
-aws s3 cp "${IMAGE_ZIP_PATH}" "${S3_PATH}"
+# Push to S3 if DOCKER_TAG is not 'dev'
+if [ "${DOCKER_TAG}" != "dev" ]; then
+    echo "Pushing ${ZIP_NAME} to S3..."
+    aws s3 cp "${IMAGE_ZIP_PATH}" "${S3_PATH}" || { echo "Error uploading ${ZIP_NAME} to S3"; exit 1; }
+else
+    echo "DOCKER_TAG is 'dev', skipping push to S3."
+fi
