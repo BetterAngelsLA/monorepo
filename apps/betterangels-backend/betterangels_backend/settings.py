5b239d79fcf3a252756fa8211804f78795e8ebff
Paul Vecchio <paul@betterangels.la>
2023-11-15 15:40:05 -0800

    Run CI within Devcontainer and Add Lint / Test Steps to Backend (#86)
    This PR consolidates our approach to a single Dockerfile for both development and deployment. CI processes now run using the development container image, ensuring consistency between CI and development environments. This approach also facilitates using the same Docker image from development through to production. While this change increases the image size (~500MB more), the effective use of Docker's layer caching minimizes the impact
    
    A subsequent PR will be necessary to integrate MyPy as a mandatory check, as addressing all errors within this PR would be overly burdensome.

--- apps/betterangels-backend/betterangels_backend/settings.py
+++ apps/betterangels-backend/betterangels_backend/settings.py
@@ -62,14 +62,14 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
+IS_LOCAL_DEV = env("IS_LOCAL_DEV")
+if IS_LOCAL_DEV:
+    environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
-IS_LOCAL_DEV = env("IS_LOCAL_DEV")
 
 # Application definition
 INSTALLED_APPS = [
@@ -110,6 +110,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "simple_history.middleware.HistoryRequestMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 
