6c0483a0a45bb1c0d7a55e2de5a34fa36b1b736c
Davit-BetterAngels <141072810+Davit-BetterAngels@users.noreply.github.com>
2023-11-14 19:01:58 -0800

    android scroll fix (#74)

--- apps/betterangels/src/app/(tabs)/appointment.tsx
+++ apps/betterangels/src/app/(tabs)/appointment.tsx
@@ -1,24 +1,5 @@
-import { StyleSheet } from 'react-native';
-
-import { Text, View } from '../components/Themed';
+import { SafeAreaView } from 'react-native';
 
 export default function AppointmentScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Appointment screen</Text>
-    </View>
-  );
+  return <SafeAreaView style={{ flex: 1 }}></SafeAreaView>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-});
