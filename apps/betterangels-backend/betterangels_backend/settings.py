c69d0348389cf04ab537ddc4972d6a07248a1439
Paul Vecchio <paul@betterangels.la>
2023-11-15 12:27:07 -0800

    still need to set the evn

--- apps/betterangels-backend/betterangels_backend/settings.py
+++ apps/betterangels-backend/betterangels_backend/settings.py
@@ -62,7 +62,8 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-if env("IS_LOCAL_DEV"):
+IS_LOCAL_DEV = env("IS_LOCAL_DEV")
+if IS_LOCAL_DEV:
     environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
