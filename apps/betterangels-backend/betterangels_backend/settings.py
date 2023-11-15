a807a63d90319670b2edc35f9d64d749fe716cd2
Paul Vecchio <paul@betterangels.la>
2023-11-15 09:28:54 -0800

    Comment out mypy

--- apps/betterangels-backend/betterangels_backend/settings.py
+++ apps/betterangels-backend/betterangels_backend/settings.py
@@ -110,6 +110,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "simple_history.middleware.HistoryRequestMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 
