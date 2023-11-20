92f4ba54bd4dc297ca3afa355c02480093bdb809
Anthony Kim <antkim003@users.noreply.github.com>
2023-11-20 09:36:05 -0800

    Extended Organization Invitation on admin page (#94)

--- apps/betterangels-backend/accounts/admin.py
+++ apps/betterangels-backend/accounts/admin.py
@@ -4,17 +4,23 @@ from django.contrib.admin import ModelAdmin
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from organizations.models import OrganizationUser
+from organizations.models import OrganizationInvitation, OrganizationUser
 from simple_history.admin import SimpleHistoryAdmin
 from simple_history.models import HistoricalRecords
 
 from .admin_request_mixin import AdminRequestMixin
 from .forms import OrganizationUserForm, UserChangeForm, UserCreationForm
-from .models import User
+from .models import ExtendedOrganizationInvitation, User
 
 
 class CustomOrganizationUserAdmin(AdminRequestMixin, ModelAdmin[User]):
     form = OrganizationUserForm
+
+
+class ExtendedOrganizationInvitationAdmin(ModelAdmin[ExtendedOrganizationInvitation]):
+    list_display = ("invited_by", "invitee", "organization", "accepted")
+    search_fields = ("invited_by__username", "invitee__username", "organization__name")
+    list_filter = ("organization",)
 
 
 class UserAdmin(SimpleHistoryAdmin, BaseUserAdmin):
@@ -47,7 +53,9 @@ class UserAdmin(SimpleHistoryAdmin, BaseUserAdmin):
 
 admin.site.register(User, UserAdmin)
 admin.site.unregister(OrganizationUser)
+admin.site.unregister(OrganizationInvitation)
 admin.site.register(OrganizationUser, CustomOrganizationUserAdmin)
+admin.site.register(ExtendedOrganizationInvitation, ExtendedOrganizationInvitationAdmin)
 
 admin.site.login = staff_member_required(  # type: ignore
     admin.site.login, login_url=settings.LOGIN_URL
--- apps/betterangels-backend/accounts/backends.py
+++ apps/betterangels-backend/accounts/backends.py
@@ -1,11 +1,22 @@
+from typing import ClassVar  # noqa
+from typing import Optional  # noqa
+from typing import Text  # noqa
 from typing import Any, Union
 
-from organizations.backends.defaults import InvitationBackend  # type:ignore
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.http import Http404
+from django.shortcuts import redirect, render
+from django.utils.translation import gettext as _
+from organizations.backends.defaults import (  # type:ignore
+    InvitationBackend,
+    RegistrationBackend,
+)
 from organizations.models import Organization, OrganizationInvitation
 from rest_framework.request import Request
 
 from .forms import UserCreationForm
-from .models import User
+from .models import ExtendedOrganizationInvitation, User
 
 
 class CustomInvitations(InvitationBackend):  # type:ignore
@@ -26,6 +37,7 @@ class CustomInvitations(InvitationBackend):  # type:ignore
             user.set_unusable_password()
             user.is_active = False
             user.save()
+
         self.send_invitation(user, sender, **kwargs)
 
         return user
@@ -36,11 +48,53 @@ class CustomInvitations(InvitationBackend):  # type:ignore
         """
         Creates an organization invite for given invitation user
         """
-        invitation: OrganizationInvitation = OrganizationInvitation.objects.create(
-            invited_by=invited_by_user,
-            invitee=invitee_user,
-            organization=organization,
-            invitee_identifier=invitee_user.email,
+        invitation: OrganizationInvitation = (
+            ExtendedOrganizationInvitation.objects.create(
+                invited_by=invited_by_user,
+                invitee=invitee_user,
+                organization=organization,
+                invitee_identifier=invitee_user.email,
+            )
         )
 
         return invitation
+
+    def activate_view(self, request: Request, user_id: str, token: str) -> Any:
+        """
+        View function that activates the given User by setting `is_active` to
+        true if the provided information is verified.
+        """
+        try:
+            user = self.user_model.objects.get(id=user_id, is_active=False)
+        except self.user_model.DoesNotExist:
+            raise Http404(_("Your URL may have expired."))
+
+        if not PasswordResetTokenGenerator().check_token(user, token):
+            raise Http404(_("Your URL may have expired."))
+        form = self.get_form(
+            data=request.POST or None, files=request.FILES or None, instance=user
+        )
+        if form.is_valid():
+            form.instance.is_active = True
+            user = form.save()
+            user.set_password(form.cleaned_data["password1"])
+            user.save()
+            self.activate_organizations(user)
+            self.update_invitation(user)
+            user = authenticate(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password1"],
+            )
+            if user is None:
+                raise Http404(_("Can't authenticate user"))
+            login(request, user)
+            return redirect(self.get_success_url())
+        return render(request, self.registration_form_template, {"form": form})
+
+    def update_invitation(self, user: User) -> None:
+        """
+        Updates the invitation to be accepted by the user.
+        """
+        invitation = ExtendedOrganizationInvitation.objects.get(invitee=user)
+        invitation.accepted = True
+        invitation.save()
