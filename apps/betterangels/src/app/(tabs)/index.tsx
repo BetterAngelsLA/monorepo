637d8233bfe8b2c17cdf310fc09eecade32ccf21
Davit-BetterAngels <davit@betterangels.la>
2023-11-29 08:51:21 +0000

    tabs

--- apps/betterangels/src/app/(tabs)/index.tsx
+++ apps/betterangels/src/app/(tabs)/index.tsx
@@ -12,12 +12,20 @@ import {
   SearchIcon,
 } from '@monorepo/expo/shared/icons';
 import { Colors, Spacings } from '@monorepo/expo/shared/static';
-import { Alert, Avatar, Button, H1 } from '@monorepo/expo/shared/ui-components';
+import {
+  Alert,
+  Avatar,
+  BodyText,
+  Button,
+  H1,
+  H4,
+} from '@monorepo/expo/shared/ui-components';
 import { useNavigation } from 'expo-router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 export default function TabOneScreen() {
+  const [tab, toggle] = useState(1);
   const { user } = useUser();
   const { signOut } = useSignOut();
   const { control } = useForm();
@@ -53,15 +61,40 @@ export default function TabOneScreen() {
         </View>
         <Avatar firstName="Davit" lastName="Manukyan" size="md" />
       </View>
-      <MainScrollContainer pt="sm" bg={Colors.NEUTRAL_EXTRA_LIGHT}>
-        <View>
+      <MainScrollContainer px={0} pt="sm" bg={Colors.NEUTRAL_EXTRA_LIGHT}>
+        <View style={{ paddingHorizontal: Spacings.sm }}>
           <Alert
+            mb="sm"
             text="4 shelter referrals are pending for over 14 days."
             variant="warning"
             onActionPress={() => console.log('press')}
             actionText="More"
           />
         </View>
+        <View style={styles.tabsContainer}>
+          <View
+            style={{
+              flexDirection: 'row',
+            }}
+          >
+            <View style={{ position: 'relative', paddingBottom: Spacings.sm }}>
+              <Pressable onPress={() => toggle(1)}>
+                <H4 mx="sm">Today</H4>
+              </Pressable>
+              {tab === 1 && <View style={styles.line} />}
+            </View>
+            <View style={{ position: 'relative', paddingBottom: Spacings.sm }}>
+              <Pressable onPress={() => toggle(2)}>
+                <H4 mx="sm">Daily Tasks</H4>
+              </Pressable>
+              {tab === 2 && <View style={styles.line} />}
+            </View>
+          </View>
+          <BodyText color={Colors.PRIMARY_DARK} size="sm">
+            Calendar
+          </BodyText>
+        </View>
+        <View style={styles.tab}></View>
         <Button
           borderColor={Colors.PRIMARY}
           icon={
@@ -102,5 +135,28 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+  },
+  tabsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: Spacings.lg,
+    paddingHorizontal: Spacings.sm,
+    backgroundColor: Colors.WHITE,
+  },
+  line: {
+    height: 5,
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    backgroundColor: Colors.PRIMARY,
+  },
+  tab: {
+    paddingVertical: Spacings.lg,
+    borderTopWidth: 1,
+    borderTopColor: Colors.NEUTRAL_EXTRA_LIGHT,
+    backgroundColor: Colors.WHITE,
+    marginBottom: Spacings.xl,
+    paddingHorizontal: Spacings.sm,
   },
 });
