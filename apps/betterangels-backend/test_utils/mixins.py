615bb6a5ff4493eb844e0797db10d04c4ad06b2a
Paul Vecchio <paul@betterangels.la>
2023-11-29 17:02:21 -0800

    Refactor mixin

---
+++ apps/betterangels-backend/test_utils/mixins.py
@@ -0,0 +1,29 @@
+from typing import Any, Dict, Optional
+
+from django.test import Client, TestCase
+
+
+class GraphQLTestCaseMixin:
+    graphql_url = "/graphql"  # Change this if your GraphQL endpoint is different
+
+    def setUp(self) -> None:
+        # Explicitly check if self is an instance of Django's TestCase
+        if isinstance(self, TestCase):
+            super(TestCase, self).setUp()
+        self.graphql_client = Client()
+
+    def execute_graphql_query(
+        self, query: str, variables: Optional[Dict[str, Any]] = None
+    ) -> Dict[str, Any]:
+        response = self.graphql_client.post(
+            self.graphql_url,
+            {
+                "query": query,
+                "variables": variables or {},
+            },
+            content_type="application/json",
+        )
+        json_data = response.json()
+        if not isinstance(json_data, dict):
+            raise ValueError("Response JSON is not a dictionary")
+        return json_data
