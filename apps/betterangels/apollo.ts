b24d247efe67dd87de286ae54ecc996098d41b69
Paul Vecchio <paul@betterangels.la>
2023-12-01 08:37:40 -0800

    refactor storage

--- apps/betterangels/apollo.ts
+++ apps/betterangels/apollo.ts
@@ -1,8 +1,25 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache, from } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { CSRF_TOKEN, getItem } from '@monorepo/expo/betterangels';
+import { RestLink } from 'apollo-link-rest';
 import { apiUrl } from './config';
 
+// Context link for adding CSRF token to request headers
+const authLink = setContext(async () => {
+  const token = await getItem(CSRF_TOKEN);
+  return {
+    headers: {
+      'X-CSRF-TOKEN': token || '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  uri: `${apiUrl}/graphql`,
+  link: from([
+    authLink,
+    new RestLink({ uri: apiUrl }),
+    new HttpLink({ uri: `${apiUrl}/graphql` }),
+  ]),
   cache: new InMemoryCache(),
 });
 
--- libs/expo/betterangels/src/lib/index.ts
+++ libs/expo/betterangels/src/lib/index.ts
@@ -1,4 +1,6 @@
+export * from './constants';
 export * from './helpers';
 export * from './hooks';
 export * from './providers';
+export * from './storage';
 export * from './ui-components';
