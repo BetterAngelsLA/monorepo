282742e6a4abcbbfb370496c4eba719020007e17
Paul Vecchio <paul@betterangels.la>
2023-12-04 19:54:49 -0800

    Add logout tests

---
+++ apps/betterangels-backend/accounts/tests/test_mutations.py
@@ -0,0 +1,30 @@
+from accounts.models import User
+from django.test import TestCase, ignore_warnings
+from model_bakery import baker
+from test_utils.mixins import GraphQLTestCaseMixin
+
+
+@ignore_warnings(category=UserWarning)
+class CurrentUserGraphQLTests(GraphQLTestCaseMixin, TestCase):
+    def test_anonymous_user_logout(self) -> None:
+        query = """
+        mutation {
+            logout
+        }
+        """
+        response = self.execute_graphql_query(query)
+        self.assertIsNone(response.get("errors"))
+        self.assertEqual(response["data"]["logout"], False)
+
+    def test_logged_in_user_logout(self) -> None:
+        user = baker.make(User, email="test@example.com", username="testuser")
+        self.graphql_client.force_login(user)
+
+        query = """
+        mutation {
+            logout
+        }
+        """
+        response = self.execute_graphql_query(query)
+        self.assertIsNone(response.get("errors"))
+        self.assertEqual(response["data"]["logout"], True)
