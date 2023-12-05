daa88e504db4b11726d6df91dc814383df1b6dae
Paul Vecchio <paul@betterangels.la>
2023-12-04 19:54:49 -0800

    Add logout tests

--- apps/betterangels-backend/accounts/schema.py
+++ apps/betterangels-backend/accounts/schema.py
@@ -9,4 +9,8 @@ from .types import UserType
 @strawberry.type
 class Query:
     current_user: Optional[UserType] = auth.current_user()
+
+
+@strawberry.type
+class Mutation:
     logout = auth.logout()
--- apps/betterangels-backend/betterangels_backend/schema.py
+++ apps/betterangels-backend/betterangels_backend/schema.py
@@ -1,3 +1,4 @@
+from accounts.schema import Mutation as AccountMutation
 from accounts.schema import Query as AccountQuery
 from strawberry import Schema
 from strawberry.tools import merge_types
@@ -6,6 +7,8 @@ from strawberry.tools import merge_types
 # https://github.com/strawberry-graphql/strawberry/issues/566#issuecomment-1346660629
 queries = (AccountQuery,)
 Query = merge_types("Query", queries)
-# mutations = (,)
-# Mutation = merge_types("Mutation", mutations)
-schema = Schema(query=Query, mutation=None)
+
+mutations = (AccountMutation,)
+Mutation = merge_types("Mutation", mutations)
+
+schema = Schema(query=Query, mutation=Mutation)
