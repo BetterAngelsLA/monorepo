cb1be6ee7626cc6bb61057810fa7efdda7e997c8
Paul Vecchio <paul@betterangels.la>
2023-12-06 11:06:43 -0800

    I think this is cleaner?

--- libs/expo/betterangels/src/lib/providers/user/UserProvider.tsx
+++ libs/expo/betterangels/src/lib/providers/user/UserProvider.tsx
@@ -1,49 +1,15 @@
-import { ReactNode, useEffect, useMemo, useState } from 'react';
+import { ReactNode, useState } from 'react';
 
-import { useQuery } from '@apollo/client';
-import { GET_CURRENT_USER } from '../../helpers/fetchUser';
 import UserContext, { TUser } from './UserContext';
 
 interface UserProviderProps {
   children: ReactNode;
 }
 
-function useProtectedRoute() {
-  const [user, setUser] = useState<TUser | undefined>();
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const { data, loading, error } = useQuery(GET_CURRENT_USER);
-
-  useEffect(() => {
-    if (data && data.user) {
-      setUser({
-        id: data.currentUser.id,
-        email: data.currentUser.email,
-        username: data.currentUser.name,
-        hasOrganization: false,
-      });
-    }
-
-    setIsLoading(loading);
-
-    if (error) {
-      console.error(error);
-    }
-  }, [data, loading, error]);
-
-  return { user, setUser, isLoading };
-}
-
 export default function UserProvider({ children }: UserProviderProps) {
-  const { isLoading, user, setUser } = useProtectedRoute();
+  const [user, setUser] = useState<TUser | undefined>();
 
-  const value = useMemo(
-    () => ({
-      user,
-      setUser,
-      isLoading,
-    }),
-    [user, isLoading, setUser]
-  );
+  const value = { user, setUser };
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 }
