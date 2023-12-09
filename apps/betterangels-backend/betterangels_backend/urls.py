634fa36fa759beb346b80c68d8dc5174d75dba4e
Paul Vecchio <paul@betterangels.la>
2023-12-08 22:23:03 -0800

    Fix Apollo Client CSRF Failures (#128)
    Fixes the below error we were getting in the Apollo client when adding CSRF protection to GraphQL
    
    Forbidden (403)
    
    CSRF verification failed. Request aborted.
    
    You are seeing this message because this HTTPS site requires a “Referer header” to be sent by your web browser, but none was sent. This header is required for security reasons, to ensure that your browser is not being hijacked by third parties.
    
    If you have configured your browser to disable “Referer” headers, please re-enable them, at least for this site, or for HTTPS connections, or for “same-origin” requests.
    
    If you are using the <meta name="referrer" content="no-referrer"> tag or including the “Referrer-Policy: no-referrer” header, please remove them. The CSRF protection requires the “Referer” header to do strict referer checking. If you’re concerned about privacy, use alternatives like <a rel="noreferrer" …> for links to third-party sites.

--- apps/betterangels-backend/betterangels_backend/urls.py
+++ apps/betterangels-backend/betterangels_backend/urls.py
@@ -15,42 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-
+from common.graphql.views import ProtectedGraphQLView
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import TemplateView
-from strawberry.django.views import GraphQLView
 
 from .schema import schema
-
-# class ProtectedGraphQLView(GraphQLView):
-#     """
-#     This subclass of GraphQLView is used to remove the CSRF exemption that is
-#     applied by default in Strawberry Django's GraphQLView. Strawberry Django
-#     typically exempts GraphQL views from CSRF checks to facilitate API interactions
-#     from various origins or clients. However, this exemption might not be desirable
-#     in all cases, especially when there's a need for increased security.
-
-#     By subclassing GraphQLView and not applying the @csrf_exempt decorator,
-#     this class effectively reinstates the default CSRF protection provided by Django.
-#     """
-
-#     def dispatch(
-#         self, request: HttpRequest, *args: Any, **kwargs: Any
-#     ) -> Union[HttpResponseNotAllowed, TemplateResponse, HttpResponse]:
-#         try:
-#             return self.run(request=request)
-#         except HTTPException as e:
-#             return HttpResponse(
-#                 content=e.reason,
-#                 status=e.status_code,
-#             )
-
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("", include("accounts.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls"), name="accounts"),
-    path("graphql", GraphQLView.as_view(schema=schema)),
+    path("graphql", ProtectedGraphQLView.as_view(schema=schema)),
 ]
--- libs/expo/betterangels/src/lib/apollo/links/csrf.ts
+++ libs/expo/betterangels/src/lib/apollo/links/csrf.ts
@@ -1,55 +1,82 @@
-import { ApolloLink, FetchResult, Observable } from '@apollo/client';
+import { ApolloLink, Observable } from '@apollo/client';
 import { CSRF_COOKIE_NAME, CSRF_HEADER_NAME } from '../../constants';
 import { getItem, setItem } from '../../storage';
 
-export const csrfLink = new ApolloLink((operation, forward) => {
-  return new Observable((observer) => {
-    const addCsrfTokenToRequest = async () => {
-      const token = await getItem(CSRF_COOKIE_NAME);
-      if (token) {
-        operation.setContext(({ headers = {} }) => ({
-          headers: {
-            ...headers,
-            [CSRF_HEADER_NAME]: token,
-          },
-        }));
-      }
-    };
+const csrfTokenRegex = new RegExp(`${CSRF_COOKIE_NAME}=([^;]+)`);
 
-    const processResponse = (response: FetchResult) => {
-      const context = operation.getContext();
-      // Helper function to extract 'set-cookie' headers
-      const extractSetCookieHeader = (response: Response | undefined) =>
-        response?.headers.get('set-cookie');
+export const csrfLink = (apiUrl: string, customFetch = fetch) => {
+  return new ApolloLink((operation, forward) => {
+    return new Observable((observer) => {
+      const processOperation = async () => {
+        try {
+          // Attempt to get the CSRF token from storage
+          const csrfToken = await getItem(CSRF_COOKIE_NAME);
 
-      // Get 'set-cookie' headers from GraphQL response
-      const graphqlCookie = extractSetCookieHeader(context['response']);
-      const graphqlCookies = graphqlCookie ? [graphqlCookie] : [];
+          // If the token is not in storage, try to fetch it
+          if (!csrfToken) {
+            const response = await customFetch(apiUrl, {
+              credentials: 'include',
+            });
+            const cookies = response.headers?.get('Set-Cookie');
+            const csrfTokenMatch = cookies?.match(csrfTokenRegex);
 
-      // Get 'set-cookie' headers from REST responses
-      const restCookies = ((context['restResponses'] as Response[]) || [])
-        .map(extractSetCookieHeader)
-        .filter(Boolean);
+            // If a token is found in the response, update csrfToken and store it
+            if (csrfTokenMatch?.[1]) {
+              await setItem(CSRF_COOKIE_NAME, csrfTokenMatch[1]);
+            }
+          }
 
-      // Extract CSRF token
-      const combinedCookies = [...graphqlCookies, ...restCookies].join('; ');
-      const csrfTokenRegex = new RegExp(`${CSRF_COOKIE_NAME}=([^;]+)`);
-      const csrfTokenMatch = combinedCookies?.match(csrfTokenRegex);
-      const csrfToken = csrfTokenMatch?.[1];
+          // If a CSRF token is available, set it in the request headers
+          operation.setContext(({ headers = {} }) => ({
+            headers: {
+              ...headers,
+              ...(csrfToken ? { [CSRF_HEADER_NAME]: csrfToken } : {}),
+            },
+          }));
 
-      if (csrfToken) {
-        setItem(CSRF_COOKIE_NAME, csrfToken);
-      }
+          // Forward the operation and process the response
+          forward(operation).subscribe({
+            next: (response) => {
+              const context = operation.getContext();
+              // Helper function to extract 'set-cookie' headers
+              const extractSetCookieHeader = (response: Response | undefined) =>
+                response?.headers.get('set-cookie');
 
-      return response;
-    };
+              // Get 'set-cookie' headers from GraphQL response
+              const graphqlCookie = extractSetCookieHeader(context['response']);
+              const graphqlCookies = graphqlCookie ? [graphqlCookie] : [];
 
-    addCsrfTokenToRequest().then(() => {
-      forward(operation).subscribe({
-        next: (response) => observer.next(processResponse(response)),
-        error: (e) => observer.error(e),
-        complete: () => observer.complete(),
-      });
+              // Get 'set-cookie' headers from REST responses
+              const restCookies = (
+                (context['restResponses'] as Response[]) || []
+              )
+                .map(extractSetCookieHeader)
+                .filter(Boolean);
+
+              // Extract CSRF token
+              const combinedCookies = [...graphqlCookies, ...restCookies].join(
+                '; '
+              );
+              const csrfTokenRegex = new RegExp(`${CSRF_COOKIE_NAME}=([^;]+)`);
+              const csrfTokenMatch = combinedCookies?.match(csrfTokenRegex);
+              const csrfToken = csrfTokenMatch?.[1];
+
+              if (csrfToken) {
+                setItem(CSRF_COOKIE_NAME, csrfToken);
+              }
+
+              observer.next(response);
+            },
+            error: observer.error.bind(observer),
+            complete: observer.complete.bind(observer),
+          });
+        } catch (error) {
+          console.error('Error in CSRF Apollo Link:', error);
+          observer.error(error);
+        }
+      };
+
+      processOperation();
     });
   });
-});
+};
