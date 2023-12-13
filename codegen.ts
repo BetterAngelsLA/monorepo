4ee7cb1e9cb4a1f5050f5c08ed100991f30a9826
Anthony <antkim003@gmail.com>
2023-12-13 10:44:32 -0800

    WIP

--- codegen.ts
+++ codegen.ts
@@ -2,8 +2,8 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'http://localhost:8000/graphql',
-  documents: 'libs/expo/betterangels/src/lib/apollo/graphql/**/*.ts',
+  schema: 'apps/betterangels-backend/out/schema.graphql',
+  // documents: 'libs/expo/betterangels/src/lib/apollo/graphql/**/*.ts',
   generates: {
     'libs/expo/betterangels/src/lib/apollo/gql/': {
       preset: 'client',
