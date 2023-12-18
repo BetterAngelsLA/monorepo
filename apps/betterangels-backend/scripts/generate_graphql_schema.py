4920d67f14541983570e0dc6359852995d254391
Davit-BetterAngels <141072810+Davit-BetterAngels@users.noreply.github.com>
2023-12-18 11:26:25 -0800

    Feat/types generator (#136)

---
+++ apps/betterangels-backend/scripts/generate_graphql_schema.py
@@ -0,0 +1,8 @@
+from betterangels_backend.schema import schema
+
+
+def run() -> None:
+    print("Generating graphql schema...")
+    schema_str = str(schema)
+    with open("out/schema.graphql", "w") as file:
+        file.write(schema_str)
