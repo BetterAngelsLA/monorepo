d8d8cc3ce81ddadfee14ae4acd8aae6630f92039
Anthony Kim <antkim003@users.noreply.github.com>
2023-12-19 10:38:26 -0800

    Adding notes graphql endpoints and model (#135)

---
+++ apps/betterangels-backend/notes/models.py
@@ -0,0 +1,16 @@
+from accounts.models import User
+from django.db import models
+
+
+class Note(models.Model):
+    id: int
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+
+    objects = models.Manager()
+
+    def __str__(self) -> str:
+        return self.body[:50]
---
+++ apps/betterangels-backend/notes/tests/test_models.py
@@ -0,0 +1,27 @@
+from accounts.models import User
+from django.test import TestCase
+from notes.models import Note
+
+
+class NotesTest(TestCase):
+    user: User
+    note: Note
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.user = User.objects.create_user(
+            email="normal@user.com",
+            password="foo",
+        )
+        cls.note = Note.objects.create(
+            created_by=cls.user, title="Just a test note", body="this is a body"
+        )
+
+    def test_notes_fields_exist(self) -> None:
+        note = Note.objects.get(id=self.note.id)
+        self.assertEqual(note.body, self.note.body)
+        self.assertEqual(note.title, self.note.title)
+
+    def test_user_assigned_to_note(self) -> None:
+        note = Note.objects.get(id=self.note.id)
+        self.assertEqual(note.created_by.id, self.user.id)
