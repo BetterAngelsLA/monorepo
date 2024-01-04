bcb5408c9bc82e94d98ff2da8cf54f2e4a044585
Anthony <antkim003@gmail.com>
2024-01-04 15:00:34 -0800

    Get rid of out folder

--- apps/betterangels-backend/scripts/generate_graphql_schema.py
+++ apps/betterangels-backend/scripts/generate_graphql_schema.py
@@ -4,5 +4,5 @@ from betterangels_backend.schema import schema
 def run() -> None:
     print("Generating graphql schema...")
     schema_str = str(schema)
-    with open("out/schema.graphql", "w") as file:
+    with open("schema.graphql", "w") as file:
         file.write(schema_str)
