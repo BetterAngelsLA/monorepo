5cfc937a2ecafc7cd15e6fd16c6fa82557a531ca
Anthony Kim <antkim003@users.noreply.github.com>
2024-01-04 15:15:11 -0800

    moving schema.graphql (#143)

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'apps/betterangels-backend/out/schema.graphql',
+  schema: 'apps/betterangels-backend/schema.graphql',
   documents: 'libs/expo/betterangels/src/lib/apollo/graphql/**/*.ts',
   generates: {
     'libs/expo/betterangels/src/lib/apollo/gql-types/': {
