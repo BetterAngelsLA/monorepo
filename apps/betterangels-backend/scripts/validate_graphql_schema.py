318d9f5e00e1f41216e3529fe4572696b670da62
Anthony <antkim003@gmail.com>
2024-01-08 13:29:21 -0800

    Adding changes using tempfile

--- apps/betterangels-backend/scripts/validate_graphql_schema.py
+++ apps/betterangels-backend/scripts/validate_graphql_schema.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 
 from betterangels_backend.schema import schema
 
@@ -6,24 +7,34 @@ from betterangels_backend.schema import schema
 def run() -> int:
     print("Generating a copy of the graphql schema...")
     schema_str = str(schema)
-    with open("copy_schema.graphql", "w") as file:
-        file.write(schema_str)
+    temp_file_path = write_to_temp_file(schema_str)
 
-    result = compare_files()
+    result = compare_files(temp_file_path, "schema.graphql")
 
-    cleanup()
+    os.remove(temp_file_path)
+
     return result
 
 
-def compare_files() -> int:
-    file1 = "copy_schema.graphql"
-    file2 = "schema.graphql"
-    print("Comparing the generated schema with the existing schema...")
-    with open(file1) as f1:
-        content1 = normalize_text(f1.read())
+def write_to_temp_file(content: str) -> str:
+    with tempfile.NamedTemporaryFile(mode="w", delete=False) as temp:
+        temp.write(content)
+        return temp.name
 
-    with open(file2) as f2:
-        content2 = normalize_text(f2.read())
+
+def read_file(filename: str) -> str:
+    try:
+        with open(filename, "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        print(f"File {filename} not found")
+        exit(1)
+
+
+def compare_files(file1: str, file2: str) -> int:
+    print("Comparing the generated schema with the existing schema...")
+    content1 = normalize_text(read_file(file1))
+    content2 = normalize_text(read_file(file2))
 
     if content1 == content2:
         print("Schema is up to date!")
@@ -45,3 +56,7 @@ def normalize_text(text: str) -> str:
 
 def cleanup() -> None:
     os.remove("copy_schema.graphql")
+
+
+if __name__ == "__main__":
+    exit(run())
