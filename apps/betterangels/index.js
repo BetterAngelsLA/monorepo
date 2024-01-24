0515ec2cb18620c3b32fb2b5bcb300920147c5bb
Paul Vecchio <paul@betterangels.la>
2024-01-24 12:21:38 -0800

    We don't need the polyfill anymore

--- apps/betterangels/index.js
+++ apps/betterangels/index.js
@@ -2,8 +2,6 @@ import { registerRootComponent } from 'expo';
 import { ExpoRoot } from 'expo-router';
 
 /* polyfills */
-//** URL polyfill */
-import 'react-native-url-polyfill/auto';
 
 // Must be exported or Fast Refresh won't update the context
 export function App() {
