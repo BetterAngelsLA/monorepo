917e5c1a1662ce6803ed7a90d3a365d46d7428a4
Paul Vecchio <paul@betterangels.la>
2024-01-24 10:51:51 -0800

    Fix android and ios

--- apps/betterangels/metro.config.js
+++ apps/betterangels/metro.config.js
@@ -1,5 +1,5 @@
 const { withNxMetro } = require('@nx/expo');
-const { getDefaultConfig } = require('@expo/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 const { mergeConfig } = require('metro-config');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 
