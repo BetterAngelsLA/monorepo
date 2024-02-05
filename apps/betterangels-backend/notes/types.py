30080a7cd9761126d696a8c62d6e014a91d6c26b
Paul Vecchio <paul@betterangels.la>
2024-02-04 23:45:42 -0800

    No need to cast

--- apps/betterangels-backend/notes/types.py
+++ apps/betterangels-backend/notes/types.py
@@ -29,10 +29,7 @@ class NoteType:
         # As of 1-24-2024 we are unable to apply HasRetvalPerm to a paginated list.
         # Instead we use get_objects_for_user to enforce permissions.
         user = get_current_user(info)
-        return cast(
-            QuerySet[models.Note],
-            get_objects_for_user(user, [NotePermissions.VIEW], klass=queryset),
-        )
+        return get_objects_for_user(user, [NotePermissions.VIEW], klass=queryset)
 
 
 @dataclasses.dataclass
