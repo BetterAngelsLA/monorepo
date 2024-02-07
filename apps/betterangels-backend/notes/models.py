91933b0ae4ee846be4505a35a7c2500e60546cbe
Paul Vecchio <paul@betterangels.la>
2024-02-06 18:01:47 -0800

    Move `get_object_for_user` tests from `Note` model to `SimpleModel` (#174)

--- apps/betterangels-backend/notes/models.py
+++ apps/betterangels-backend/notes/models.py
@@ -3,14 +3,15 @@ from django.db import models
 from guardian.models import GroupObjectPermissionBase, UserObjectPermissionBase
 
 
-# TODO: Figure out why User/Group Perms are failing type checks
-# https://github.com/typeddjango/django-stubs/issues/1354
-class Note(models.Model):  # type: ignore[django-manager-missing]
+class Note(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+
+    noteuserobjectpermission_set: models.QuerySet["Note"]
+    notegroupobjectpermission_set: models.QuerySet["Note"]
 
     def __str__(self) -> str:
         return self.body[:50]
