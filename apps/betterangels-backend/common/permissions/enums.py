1d2ff6093d258b1576d281484c4ad71a3d3a3fdd
Paul Vecchio <paul@betterangels.la>
2024-02-12 13:37:50 -0800

    Add enum lazy loading

--- apps/betterangels-backend/common/permissions/enums.py
+++ apps/betterangels-backend/common/permissions/enums.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class SimpleModelPermissions(models.TextChoices):
-    VIEW = "simplemodels.view_simplemodel", "Can view simplemodel"
-    CHANGE = "simplemodels.change_simplemodel", "Can change simplemodel"
-    DELETE = "simplemodels.delete_simplemodel", "Can delete simplemodel"
-    ADD = "simplemodels.add_simplemodel", "Can add simplemodel"
+    VIEW = "simplemodels.view_simplemodel", _("Can view simplemodel")
+    CHANGE = "simplemodels.change_simplemodel", _("Can change simplemodel")
+    DELETE = "simplemodels.delete_simplemodel", _("Can delete simplemodel")
+    ADD = "simplemodels.add_simplemodel", _("Can add simplemodel")
--- apps/betterangels-backend/notes/permissions.py
+++ apps/betterangels-backend/notes/permissions.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class NotePermissions(models.TextChoices):
-    VIEW = "notes.view_note", "Can add note"
-    CHANGE = "notes.change_note", "Can change note"
-    DELETE = "notes.delete_note", "Can delete note"
-    ADD = "notes.add_note", "Can view note"
+    VIEW = "notes.view_note", _("Can add note")
+    CHANGE = "notes.change_note", _("Can change note")
+    DELETE = "notes.delete_note", _("Can delete note")
+    ADD = "notes.add_note", _("Can view note")
