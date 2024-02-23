7f0c774e3a114b758150f990ac330eb7dd33825c
Davit-BetterAngels <141072810+Davit-BetterAngels@users.noreply.github.com>
2024-02-22 18:30:30 -0800

    Feat/location picker (#187)
    * map setup
    
    * expo updates
    
    * location screen
    
    * location required
    
    * lint fix
    
    * type fix

---
+++ libs/expo/shared/icons/src/lib/components/LocationArrowIcon.tsx
@@ -0,0 +1,6 @@
+import LocationArrowIconSVG from '../../assets/location-arrow.svg';
+import createSvgIcon from '../createSvgIcon';
+
+const LocationArrowIcon = createSvgIcon(LocationArrowIconSVG);
+
+export default LocationArrowIcon;
