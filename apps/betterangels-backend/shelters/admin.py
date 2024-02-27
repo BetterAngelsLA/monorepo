f6624834fc23c96ca9f63038b1d8bfd9a80ad1fc
Yitzchak Blank <yitzchakblank@gmail.com>
2024-02-27 22:44:34 +0000

    Updated admin console for shelter object

--- apps/betterangels-backend/shelters/admin.py
+++ apps/betterangels-backend/shelters/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
 
-from .models import Shelter, Location, Service
+from .models import Shelter, Location, Service, Population, Requirement
 
 
+class ShelterAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('title', 'email', 'phone', 'website', 'location'),
+        }),
+        ('Details', {
+            'fields': ('services', 'requirements', 'population', 'max_stay',
+                       'how_to_enter', 'description', 'bed_layout_description', 'typical_stay_description')
+        }),
+        ('Other Information', {
+            'fields': ('image_url',)
+        }),
+    )
+
+    list_display = ('title',)
+    search_fields = ('title',)
+
+
+admin.site.register(Requirement)
+admin.site.register(Population)
 admin.site.register(Service)
-admin.site.register(Shelter)
+admin.site.register(Shelter, ShelterAdmin)
 admin.site.register(Location)
