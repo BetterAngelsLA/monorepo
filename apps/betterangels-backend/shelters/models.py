6cb7768a8ecc161d3966346c4fa7668517a5cf1d
Yitzchak Blank <yitzchakblank@gmail.com>
2024-03-14 23:52:55 +0000

    Added defaults to make api return behavior consistent between textfields and charfields

--- apps/betterangels-backend/shelters/models.py
+++ apps/betterangels-backend/shelters/models.py
@@ -1,14 +1,8 @@
 from django.db import models
-from django_choices_field import TextChoicesField
 
 # from django.contrib.gis.db.models import PointField
 from .enums import (
-    ServiceEnum,
-    PopulationEnum,
-    RequirementEnum,
     HowToEnterEnum,
-    BedStateEnum,
-    ShelterTypeEnum
 )
 
 
@@ -20,8 +14,7 @@ class TimeStampedModel(models.Model):
     class Meta:  # Prevent a naming conflict
         abstract = True
 
-
-# Models with enumerated types
+# Service model should include some reference to the icon
 class Service(models.Model):
     title = models.CharField(max_length=64)
 
@@ -49,7 +42,7 @@ class ShelterType(models.Model):
     def __str__(self) -> str:
         return str(self.title)
 
-
+# Set default to make gql returns consistent between charfield and textfield
 class Shelter(TimeStampedModel):
     title = models.CharField(max_length=255)
     image_url = models.URLField()
@@ -59,40 +52,36 @@ class Shelter(TimeStampedModel):
     services = models.ManyToManyField(Service, blank=True)
     population = models.ManyToManyField(Population, blank=True)
     requirements = models.ManyToManyField(Requirement, blank=True)
-    how_to_enter = models.CharField(choices=[(x, x.value) for x in HowToEnterEnum])
-
-    max_stay = models.PositiveSmallIntegerField(null=True, blank=True)
-    average_bed_rate = models.DecimalField(max_digits=10, decimal_places=2,
-                                           null=True, blank=True)
+    how_to_enter = models.CharField(choices=[(x, x.value) for x in HowToEnterEnum],
+                                    blank=True, default='')
 
     # Location Fields - flat for easier creation via admin console
     latitude = models.FloatField(blank=True, null=True)
     longitude = models.FloatField(blank=True, null=True)
 
     spa = models.PositiveSmallIntegerField(blank=True, null=True)
-    address = models.CharField(max_length=255, blank=True, null=True)
-    city = models.CharField(max_length=255, blank=True, null=True)
-    state = models.CharField(max_length=255, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, default='')
+    city = models.CharField(max_length=255, blank=True, default='')
+    state = models.CharField(max_length=255, blank=True, default='')
     zip_code = models.PositiveIntegerField(blank=True, null=True)
     confidential = models.BooleanField(blank=True, null=True)
 
     # Contact Information
     email = models.EmailField(max_length=254, null=True, blank=True)
-    phone = models.CharField(max_length=20, null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, default='')
     website = models.URLField(null=True, blank=True)
 
     # Description
-    description = models.TextField(null=True, blank=True)
-    bed_layout_description = models.TextField(null=True, blank=True)
-    typical_stay_description = models.TextField(null=True, blank=True)
+    description = models.TextField(blank=True, null=True)
+    typical_stay_description = models.TextField(blank=True, null=True)
 
     # Bed Information
     total_beds = models.PositiveIntegerField(blank=True, null=True)
     private_beds = models.PositiveIntegerField(blank=True, null=True)
-    max_day = models.PositiveIntegerField(blank=True, null=True)
+    max_stay = models.PositiveIntegerField(blank=True, null=True)
     average_bed_rate = models.DecimalField(blank=True, null=True,
                                            max_digits=10, decimal_places=2)
-
+    bed_layout_description = models.TextField(blank=True, null=True)
 
     # TODO -- handle notes
 
