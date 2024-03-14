12bde6e4cdbd46cd04c22be55abadfe0d53a0386
Mike Feldberg <mike@betterangels.la>
2024-03-14 10:24:42 -0700

    fix tests

--- apps/betterangels-backend/notes/tests/test_queries.py
+++ apps/betterangels-backend/notes/tests/test_queries.py
@@ -107,6 +107,79 @@ class NoteQueryTestCase(NoteGraphQLBaseTestCase):
         # TODO: Add more validations once sort is implemented
         self.assertEqual(self.note, notes[0])
 
+    @parametrize(
+        (
+            "case_manager_label, client_label, is_submitted, "
+            "expected_results_count, returned_note_label_1, "
+            "returned_note_label_2"
+        ),
+        [
+            # Filter by:
+            # case manager, client, and/or is_submitted
+            ("case_manager_1", None, None, 1, "note", None),
+            ("case_manager_2", None, None, 2, "note_2", "note_3"),
+            ("case_manager_1", "client_1", None, 1, "note", None),
+            ("case_manager_1", "client_2", None, 0, None, None),
+            ("case_manager_2", "client_1", None, 1, "note_2", None),
+            ("case_manager_2", "client_2", None, 1, "note_3", None),
+            ("case_manager_2", "client_1", True, 0, None, None),
+            ("case_manager_2", "client_1", False, 1, "note_2", None),
+            ("case_manager_2", None, False, 2, "note_2", "note_3"),
+            (None, None, True, 0, None, None),
+            (None, None, None, 3, None, None),
+        ],
+    )
+    def test_notes_query_filter(
+        self,
+        case_manager_label: Optional[str],
+        client_label: Optional[str],
+        is_submitted: Optional[bool],
+        expected_results_count: int,
+        returned_note_label_1: Optional[str],
+        returned_note_label_2: Optional[str],
+    ) -> None:
+        self.graphql_client.force_login(self.case_manager_2)
+
+        self.note_2 = self._create_note_fixture(
+            {"title": "Client 1's Note", "client": self.client_1.id}
+        )["data"]["createNote"]
+
+        self.note_3 = self._create_note_fixture(
+            {"title": "Client 2's Note", "client": self.client_2.id}
+        )["data"]["createNote"]
+
+        query = """
+            query Notes($filters: NoteFilter) {
+                notes(filters: $filters) {
+                    id
+                }
+            }
+        """
+
+        filters: dict[str, Any] = {}
+
+        if case_manager_label:
+            filters["createdBy"] = {"pk": getattr(self, case_manager_label).id}
+
+        if client_label:
+            filters["client"] = {"pk": getattr(self, client_label).id}
+
+        if is_submitted is not None:
+            filters["isSubmitted"] = is_submitted
+
+        expected_query_count = 2
+        with self.assertNumQueries(expected_query_count):
+            response = self.execute_graphql(query, variables={"filters": filters})
+
+        notes = response["data"]["notes"]
+        self.assertEqual(len(notes), expected_results_count)
+
+        if returned_note_label_1:
+            self.assertEqual(notes[0]["id"], getattr(self, returned_note_label_1)["id"])
+
+        if returned_note_label_2:
+            self.assertEqual(notes[1]["id"], getattr(self, returned_note_label_2)["id"])
+
 
 @freeze_time("2024-03-11 10:11:12")
 @ignore_warnings(category=UserWarning)
@@ -192,85 +265,6 @@ class ServiceRequestQueryTestCase(ServiceRequestGraphQLBaseTestCase):
         service_requests = response["data"]["serviceRequests"]
         self.assertEqual(len(service_requests), 1)
         self.assertEqual(self.service_request, service_requests[0])
-
-    @parametrize(
-        (
-            "case_manager_label, client_label, is_submitted, "
-            "expected_results_count, returned_note_label_1, "
-            "returned_note_label_2"
-        ),
-        [
-            # Filter by:
-            # case manager, client, and/or is_submitted
-            ("case_manager_1", None, None, 1, "note", None),
-            ("case_manager_2", None, None, 2, "note_2", "note_3"),
-            ("case_manager_1", "client_1", None, 1, "note", None),
-            ("case_manager_1", "client_2", None, 0, None, None),
-            ("case_manager_2", "client_1", None, 1, "note_2", None),
-            ("case_manager_2", "client_2", None, 1, "note_3", None),
-            ("case_manager_2", "client_1", True, 0, None, None),
-            ("case_manager_2", "client_1", False, 1, "note_2", None),
-            ("case_manager_2", None, False, 2, "note_2", "note_3"),
-            (None, None, True, 0, None, None),
-            (None, None, None, 3, None, None),
-        ],
-    )
-    def test_notes_query_filter(
-        self,
-        case_manager_label: Optional[str],
-        client_label: Optional[str],
-        is_submitted: Optional[bool],
-        expected_results_count: int,
-        returned_note_label_1: Optional[str],
-        returned_note_label_2: Optional[str],
-    ) -> None:
-        self.graphql_client.force_login(self.case_manager_2)
-
-        self.note_2 = self._create_note_fixture(
-            {
-                "title": "Client 1's Note",
-                "client": {"id": self.client_1.id},
-            }
-        )["data"]["createNote"]
-
-        self.note_3 = self._create_note_fixture(
-            {
-                "title": "Client 2's Note",
-                "client": {"id": self.client_2.id},
-            }
-        )["data"]["createNote"]
-
-        query = """
-            query Notes($filters: NoteFilter) {
-                notes(filters: $filters) {
-                    id
-                }
-            }
-        """
-
-        filters: dict[str, Any] = {}
-
-        if case_manager_label:
-            filters["createdBy"] = {"pk": getattr(self, case_manager_label).id}
-
-        if client_label:
-            filters["client"] = {"pk": getattr(self, client_label).id}
-
-        if is_submitted is not None:
-            filters["isSubmitted"] = is_submitted
-
-        expected_query_count = 2
-        with self.assertNumQueries(expected_query_count):
-            response = self.execute_graphql(query, variables={"filters": filters})
-
-        notes = response["data"]["notes"]
-        self.assertEqual(len(notes), expected_results_count)
-
-        if returned_note_label_1:
-            self.assertEqual(notes[0]["id"], getattr(self, returned_note_label_1)["id"])
-
-        if returned_note_label_2:
-            self.assertEqual(notes[1]["id"], getattr(self, returned_note_label_2)["id"])
 
 
 @freeze_time("2024-03-11 10:11:12")
