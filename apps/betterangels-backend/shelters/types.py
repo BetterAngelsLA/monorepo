1e55b26c735878d9bf691abc85f5ed6a20962895
Yitzchak Blank <yitzchakblank@gmail.com>
2024-03-14 23:56:21 +0000

    Updated resolvers to be the fields directly. Updated typing to support nullable fields

--- apps/betterangels-backend/shelters/types.py
+++ apps/betterangels-backend/shelters/types.py
@@ -1,60 +1,49 @@
 import dataclasses
-import decimal
+from typing import Optional, List
 
 import strawberry_django
 import strawberry
 
 from strawberry import auto
-from strawberry.types import Info
-from typing import List
 
 from . import models
 
 
-@dataclasses.dataclass
-@strawberry_django.type(models.Service)
-class ServiceType:
-    title: auto
-
-
 @strawberry.type
 class DescriptionType:
-    description: str
-    bed_layout_description: str
-    typical_stay_description: str
+    description: Optional[str]
+    typical_stay_description: Optional[str]
 
 
 @strawberry.type
 class BedsType:
-    total_beds: int
-    private_beds: int
-    max_stay: int
-    average_bed_rate: float
+    total_beds: Optional[int]
+    private_beds: Optional[int]
+    max_stay: Optional[int]
+    average_bed_rate: Optional[float]
+    bed_layout_description: str
 
 
 @strawberry.type
 class LocationType:
-    latitude: float
-    longitude: float
-    spa: int
+    latitude: Optional[float]
+    longitude: Optional[float]
+    spa: Optional[int]
     address: str
     city: str
     state: str
-    zip_code: int
-    confidential: bool
+    zip_code: Optional[int]
+    confidential: Optional[bool]
 
 
 @dataclasses.dataclass
 @strawberry_django.type(models.Shelter, pagination=True)
 class ShelterType:
     id: auto
-
     title: auto
     image_url: auto
 
-    location: LocationType
-    how_to_enter: str
-    max_stay: auto
+    how_to_enter: auto
 
     email: auto
     phone: auto
@@ -64,26 +53,11 @@ class ShelterType:
     population: List[str]
     requirements: List[str]
 
-    description: DescriptionType
-
-    beds: BedsType
-
-    def resolve_description(self, info: Info) -> DescriptionType:
-        return DescriptionType(
-            description=self.description,
-            bed_layout_description=self.bed_layout_description,
-            typical_stay_description=self.typical_stay_description,
-        )
-
-    def resolve_beds(self, info: Info) -> BedsType:
-        return BedsType(
-            total_beds=self.total_beds,
-            private_beds=self.private_beds,
-            max_stay=self.max_stay,
-            average_bed_rate=self.average_bed_rate
-        )
-
-    def resolve_location(self, info: Info) -> LocationType:
+    # I've been unable to figure out how to resolve the mypy errors here as it
+    # does not detect the strawberry-django behavior of resolving fields in the
+    # parent Shelter class
+    @strawberry_django.field
+    def location(self) -> LocationType:
         return LocationType(
             latitude=self.latitude,
             longitude=self.longitude,
@@ -93,4 +67,22 @@ class ShelterType:
             state=self.state,
             zip_code=self.zip_code,
             confidential=self.confidential
+        )
+
+    @strawberry_django.field
+    def description(self) -> DescriptionType:
+        return DescriptionType(
+            description=self.description,
+            typical_stay_description=self.typical_stay_description,
+        )
+
+    @strawberry_django.field
+    def beds(self) -> BedsType:
+        return BedsType(
+            total_beds=self.total_beds,
+            private_beds=self.private_beds,
+            max_stay=self.max_stay,
+            average_bed_rate=self.average_bed_rate,
+            bed_layout_description=self.bed_layout_description,
+
         )
