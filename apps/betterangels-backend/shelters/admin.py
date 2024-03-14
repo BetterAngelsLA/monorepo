91af3c67f84d53f40a491de66d766a3e202e34b7
Yitzchak Blank <yitzchakblank@gmail.com>
2024-03-14 23:59:32 +0000

    Organized shelter creation in admin console

--- apps/betterangels-backend/shelters/admin.py
+++ apps/betterangels-backend/shelters/admin.py
@@ -8,17 +8,23 @@ class ShelterAdmin(admin.ModelAdmin):
     fieldsets = (
         ('Basic Information', {
             'fields': ('title', 'shelter_type', 'email', 'phone',
-                       'website'),
+                       'website', 'image_url'),
         }),
-        ('Details', {
-            'fields': ('services', 'requirements', 'population', 'max_stay',
-                       'how_to_enter', 'description', 'bed_layout_description',
-                       'typical_stay_description', 'total_beds', 'private_beds',
-                       'average_bed_rate',)
+        ('Location', {
+            'fields': ('address', 'city', 'state', 'zip_code', 'confidential', 'spa',
+                       'latitude', 'longitude')
         }),
-        ('Other Information', {
-            'fields': ('image_url',)
+        ('Criteria', {
+            'fields': ('services', 'requirements', 'population',)
         }),
+        ('Beds', {
+            'fields': ('total_beds', 'private_beds', 'average_bed_rate',
+                       'bed_layout_description')
+        }),
+        ('Other Details', {
+            'fields': ('max_stay', 'how_to_enter', 'description',
+                       'typical_stay_description',)
+        })
     )
 
     list_display = ('title',)
