c0c67c30a21925fa8a659b77e771e1addaaed012
Paul Vecchio <paul@betterangels.la>
2024-03-18 15:05:11 -0700

    Fix attachment descrption swapping the value

--- apps/betterangels-backend/common/permissions/enums.py
+++ apps/betterangels-backend/common/permissions/enums.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class AttachmentPermissions(models.TextChoices):
-    VIEW = "common.view_attachment", _("Can add attachment")
+    VIEW = "common.view_attachment", _("Can view attachment")
     CHANGE = "common.change_attachment", _("Can change attachment")
     DELETE = "common.delete_attachment", _("Can delete attachment")
-    ADD = "common.add_attachment", _("Can view attachment")
+    ADD = "common.add_attachment", _("Can add attachment")
