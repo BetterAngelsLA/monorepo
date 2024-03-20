f5be62c040fac124557f60f0d11dc66179ad0f32
Davit-BetterAngels <141072810+Davit-BetterAngels@users.noreply.github.com>
2024-03-20 13:04:33 -0700

    open maps (#208)
    * open maps
    
    * validate graphql schema
    
    * updated google map end address
    
    * shadow for directions, copy above selection
    
    * copy stories
    
    * expo updates
    
    * updated copy component and back to user location
    
    * fixed create note error
    
    * changed geo to address in native maps
    
    * changed address to geo
    
    * expo update
    
    * hid default user location button

---
+++ apps/betterangels/src/app/(private-screens)/add-note/Location/LocationMapModal/Header.tsx
@@ -0,0 +1,44 @@
+import { Colors, Spacings } from '@monorepo/expo/shared/static';
+import { BodyText, H4 } from '@monorepo/expo/shared/ui-components';
+import { Pressable, View } from 'react-native';
+
+interface IHeaderProps {
+  closeModal: () => void;
+}
+
+export default function Header(props: IHeaderProps) {
+  const { closeModal } = props;
+  return (
+    <View
+      style={{
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: Colors.WHITE,
+        paddingHorizontal: Spacings.sm,
+        paddingTop: Spacings.sm,
+        paddingBottom: Spacings.xs,
+        borderBottomWidth: 0.5,
+        borderBottomColor: Colors.NEUTRAL_LIGHT,
+      }}
+    >
+      <Pressable
+        style={{ marginRight: Spacings.xs, flex: 1 }}
+        accessibilityRole="button"
+        onPress={closeModal}
+        accessibilityHint="close map modal"
+      >
+        <BodyText size="sm">Back</BodyText>
+      </Pressable>
+      <H4
+        align="center"
+        style={{
+          flex: 2,
+        }}
+      >
+        Type or Pin Location
+      </H4>
+      <View style={{ flex: 1 }} />
+    </View>
+  );
+}
