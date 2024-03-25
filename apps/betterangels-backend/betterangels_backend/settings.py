4c8bebb6cc0750a9eb42d76175386e0ec4df0052
Paul Vecchio <paul@betterangels.la>
2024-03-24 19:50:05 -0700

    Types

--- apps/betterangels-backend/betterangels_backend/settings.py
+++ apps/betterangels-backend/betterangels_backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-from typing import Any, Dict, List
+from typing import List
 
 import django_stubs_ext
 import environ
