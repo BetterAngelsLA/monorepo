53622f3e918e09638c082ae52ec855902d177e2a
Lena Feldberg <lena@sureapp.com>
2024-04-03 20:08:42 +0000

    add better tests

--- apps/betterangels-backend/notes/tests/test_mutations.py
+++ apps/betterangels-backend/notes/tests/test_mutations.py
@@ -601,6 +601,13 @@ class NoteRevertMutationTestCase(NoteGraphQLBaseTestCase):
 
         self.assertEqual(len(reverted_note["purposes"]), 1)
         self.assertEqual(len(reverted_note["nextSteps"]), 1)
+        # Assert that unassociated tasks were not deleted
+        self.assertEqual(
+            Task.objects.filter(
+                id__in=[self.next_step_2["id"], self.purpose_2["id"]]
+            ).count(),
+            2,
+        )
 
     def test_revert_note_mutation_removes_added_service_requests(self) -> None:
         """
@@ -638,32 +645,39 @@ class NoteRevertMutationTestCase(NoteGraphQLBaseTestCase):
         saved_at = timezone.now()
 
         # Add associations that will be discarded
-        self._create_note_service_request_fixture(
+        discarded_requested_service_id = self._create_note_service_request_fixture(
             {
                 "service": "CLOTHES",
                 "customService": None,
                 "noteId": note_id,
                 "serviceRequestType": "REQUESTED",
             }
-        )
-        self._create_note_service_request_fixture(
+        )["data"]["createNoteServiceRequest"]["id"]
+
+        discarded_provided_service_id = self._create_note_service_request_fixture(
             {
                 "service": "FOOD",
                 "customService": None,
                 "noteId": note_id,
                 "serviceRequestType": "PROVIDED",
             }
-        )
+        )["data"]["createNoteServiceRequest"]["id"]
 
         # Revert to saved_at state
         variables = {"id": note_id, "savedAt": saved_at}
 
-        expected_query_count = 25
+        expected_query_count = 39
         with self.assertNumQueriesWithoutCache(expected_query_count):
             reverted_note = self._revert_note_fixture(variables)["data"]["revertNote"]
 
         self.assertEqual(len(reverted_note["providedServices"]), 1)
         self.assertEqual(len(reverted_note["requestedServices"]), 1)
+        self.assertEqual(
+            ServiceRequest.objects.filter(
+                id__in=[discarded_requested_service_id, discarded_provided_service_id]
+            ).count(),
+            0,
+        )
 
     def test_revert_note_mutation_returns_removed_tasks(self) -> None:
         """
