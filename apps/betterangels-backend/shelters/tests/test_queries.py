60de3c1340ee7e67e6c8011f5fab17f3b2bb5238
Paul Vecchio <paul@betterangels.la>
2024-04-08 16:23:47 -0700

    Model bakery for testS

--- apps/betterangels-backend/shelters/tests/test_queries.py
+++ apps/betterangels-backend/shelters/tests/test_queries.py
@@ -1,5 +1,6 @@
 from django.contrib.gis.geos import Point
 from django.test import TestCase
+from model_bakery import baker
 from shelters.models import (
     Location,
     Population,
@@ -15,32 +16,29 @@ class ShelterQueryTestCase(GraphQLTestCaseMixin, TestCase):
     def setUp(self) -> None:
         super().setUp()
 
-        shelter1_data = {
-            "title": "Shelter-1",
-            "email": "shelter1@test.com",
-            "description": "Some description",
-            "typical_stay_description": "Some typical stay description",
-            "total_beds": 100,
-            "available_beds": 50,
-            "private_beds": 25,
-            "max_stay": 12,
-            "average_bed_rate": 500.23,
-            "bed_layout_description": "Some bed layout description",
-        }
-        shelter1 = Shelter.objects.create(**shelter1_data)
-        ShelterType.objects.create(title="Emergency Shelter", shelter=shelter1).save()
-        Service.objects.create(title="Mail", shelter=shelter1).save()
-        Service.objects.create(title="Showers", shelter=shelter1).save()
-        Population.objects.create(title="Men", shelter=shelter1).save()
-        Population.objects.create(title="Women", shelter=shelter1).save()
-        Requirement.objects.create(title="Veteran", shelter=shelter1).save()
-        Requirement.objects.create(title="Medicaid or Medicare", shelter=shelter1).save()
+        # Use model_bakery to create Shelter with associated Location in one go
+        self.shelter1 = baker.make(
+            Shelter,
+            title="Shelter-1",
+            email="shelter1@test.com",
+            description="Some description",
+            typical_stay_description="Some typical stay description",
+            total_beds=100,
+            available_beds=50,
+            private_beds=25,
+            max_stay=12,
+            average_bed_rate=500.23,
+            bed_layout_description="Some bed layout description",
+            location=baker.make(Location, point=Point(5.152149, 46.199615), address="1234 Main St."),
+        )
 
-        location = Location(point=Point(5.152149, 46.199615), address="1234 Main St.")
-        location.save()
-
-        shelter1.location = location
-        shelter1.save()
+        # Create related instances for Shelter
+        baker.make(ShelterType, shelter=self.shelter1, title="Emergency Shelter")
+        baker.make(Service, shelter=self.shelter1, title="Mail")
+        baker.make(Service, shelter=self.shelter1, title="Showers")
+        baker.make(Population, shelter=self.shelter1, title="Men")
+        baker.make(Population, shelter=self.shelter1, title="Women")
+        baker.make(Requirement, shelter=self.shelter1, title="Veteran")
 
     def test_shelters_query(self) -> None:
         query = """
