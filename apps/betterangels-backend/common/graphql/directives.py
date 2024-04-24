abcedbd61d424f69249b3ce7fbef517b496dee70
Paul Vecchio <paul@betterangels.la>
2024-04-24 11:48:55 -0700

    Remove comment

--- apps/betterangels-backend/common/graphql/directives.py
+++ apps/betterangels-backend/common/graphql/directives.py
@@ -4,7 +4,6 @@ import strawberry
 from strawberry.directive import DirectiveLocation
 
 
-# Define the @rest directive
 @strawberry.directive(locations=[DirectiveLocation.FIELD_DEFINITION])
 def rest(type: str, path: str, method: str = "GET", bodyKey: Optional[str] = None) -> None:
     # This is only to define the directive for usage in SDL
