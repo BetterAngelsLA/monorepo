2ed3610d56bdd5364f056c9705797d2d1757cfd5
Paul Vecchio <paul@betterangels.la>
2024-04-25 11:39:50 -0700

    Introduce `@rest` Directive and `near-operation-file` GraphQL Code Generation (#274)

---
+++ apps/betterangels-backend/common/graphql/directives.py
@@ -0,0 +1,10 @@
+from typing import Optional
+
+import strawberry
+from strawberry.directive import DirectiveLocation
+
+
+@strawberry.directive(locations=[DirectiveLocation.FIELD_DEFINITION])
+def rest(type: str, path: str, method: str = "GET", bodyKey: Optional[str] = None) -> None:
+    # This is only to define the directive for usage in SDL
+    return None
