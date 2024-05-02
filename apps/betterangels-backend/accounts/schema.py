a04af0f4d91c06825d690eaa70df3ebd5e453a29
Paul Vecchio <paul@betterangels.la>
2024-05-02 12:12:48 -0700

    This instead

--- apps/betterangels-backend/accounts/schema.py
+++ apps/betterangels-backend/accounts/schema.py
@@ -63,8 +63,7 @@ class Mutation:
     @strawberry_django.mutation(extensions=[HasPerm(perms=[ClientPermissions.ADD])])
     def create_client(self, info: Info, data: CreateClientInput) -> ClientType:
         with transaction.atomic():
-            client_profile_data = strawberry.asdict(data)
-            _ = client_profile_data.pop("client_profile")
+            client_data = strawberry.asdict(data)
 
             user = get_current_user(info)
             permission_group = get_user_permission_group(user)
@@ -78,7 +77,7 @@ class Mutation:
                 info,
                 ClientProfile,
                 {
-                    **client_profile_data,
+                    **client_data,
                     "user": client,
                 },
             )
