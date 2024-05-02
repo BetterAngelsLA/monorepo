8eb96f8455a62fe38066f8edeb68f3f26581138c
Paul Vecchio <paul@betterangels.la>
2024-05-02 09:00:15 -0700

    Use Generated Hooks for Mutation and Queries (#276)

--- libs/expo/betterangels/src/lib/apollo/graphql/__generated__/mutations.generated.ts
+++ libs/expo/betterangels/src/lib/apollo/graphql/__generated__/mutations.generated.ts
@@ -8,13 +8,6 @@ export type GenerateMagicLinkMutationVariables = Types.Exact<{ [key: string]: ne
 
 export type GenerateMagicLinkMutation = { __typename?: 'Mutation', generateMagicLink: { __typename?: 'MagicLinkResponse', message: string } };
 
-export type CreateNoteMutationVariables = Types.Exact<{
-  data: Types.CreateNoteInput;
-}>;
-
-
-export type CreateNoteMutation = { __typename?: 'Mutation', createNote: { __typename?: 'NoteType', id: string, title: string, publicDetails: string, createdAt: any, client?: { __typename?: 'UserType', id: string, username: string, firstName?: string | null, lastName?: string | null, email: string } | null, createdBy: { __typename?: 'UserType', id: string, username: string, email: string } } | { __typename?: 'OperationInfo' } };
-
 export type UpdateNoteMutationVariables = Types.Exact<{
   data: Types.UpdateNoteInput;
 }>;
@@ -134,56 +127,6 @@ export function useGenerateMagicLinkMutation(baseOptions?: Apollo.MutationHookOp
 export type GenerateMagicLinkMutationHookResult = ReturnType<typeof useGenerateMagicLinkMutation>;
 export type GenerateMagicLinkMutationResult = Apollo.MutationResult<GenerateMagicLinkMutation>;
 export type GenerateMagicLinkMutationOptions = Apollo.BaseMutationOptions<GenerateMagicLinkMutation, GenerateMagicLinkMutationVariables>;
-export const CreateNoteDocument = gql`
-    mutation CreateNote($data: CreateNoteInput!) {
-  createNote(data: $data) {
-    ... on NoteType {
-      id
-      title
-      publicDetails
-      client {
-        id
-        username
-        firstName
-        lastName
-        email
-      }
-      createdAt
-      createdBy {
-        id
-        username
-        email
-      }
-    }
-  }
-}
-    `;
-export type CreateNoteMutationFn = Apollo.MutationFunction<CreateNoteMutation, CreateNoteMutationVariables>;
-
-/**
- * __useCreateNoteMutation__
- *
- * To run a mutation, you first call `useCreateNoteMutation` within a React component and pass it any options that fit your needs.
- * When your component renders, `useCreateNoteMutation` returns a tuple that includes:
- * - A mutate function that you can call at any time to execute the mutation
- * - An object with fields that represent the current status of the mutation's execution
- *
- * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
- *
- * @example
- * const [createNoteMutation, { data, loading, error }] = useCreateNoteMutation({
- *   variables: {
- *      data: // value for 'data'
- *   },
- * });
- */
-export function useCreateNoteMutation(baseOptions?: Apollo.MutationHookOptions<CreateNoteMutation, CreateNoteMutationVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useMutation<CreateNoteMutation, CreateNoteMutationVariables>(CreateNoteDocument, options);
-      }
-export type CreateNoteMutationHookResult = ReturnType<typeof useCreateNoteMutation>;
-export type CreateNoteMutationResult = Apollo.MutationResult<CreateNoteMutation>;
-export type CreateNoteMutationOptions = Apollo.BaseMutationOptions<CreateNoteMutation, CreateNoteMutationVariables>;
 export const UpdateNoteDocument = gql`
     mutation UpdateNote($data: UpdateNoteInput!) {
   updateNote(data: $data) {
