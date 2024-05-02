d4a84a4677e564b161f7e02c9aaa9ceb39a101c1
Davit-BetterAngels <davit@betterangels.la>
2024-05-02 17:02:06 +0000

    clients graphql

---
+++ libs/expo/betterangels/src/lib/screens/Home/__generated__/ActiveClients.generated.ts
@@ -0,0 +1,59 @@
+import * as Types from '../../../apollo/graphql/__generated__/types';
+
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+const defaultOptions = {} as const;
+export type ClientsQueryVariables = Types.Exact<{
+  filters?: Types.InputMaybe<Types.ClientFilter>;
+  pagination?: Types.InputMaybe<Types.OffsetPaginationInput>;
+}>;
+
+
+export type ClientsQuery = { __typename?: 'Query', clients: Array<{ __typename?: 'ClientType', email: string, firstName?: string | null, id: string, lastName?: string | null, username: string }> };
+
+
+export const ClientsDocument = gql`
+    query Clients($filters: ClientFilter, $pagination: OffsetPaginationInput) {
+  clients(filters: $filters, pagination: $pagination) {
+    email
+    firstName
+    id
+    lastName
+    username
+  }
+}
+    `;
+
+/**
+ * __useClientsQuery__
+ *
+ * To run a query within a React component, call `useClientsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useClientsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useClientsQuery({
+ *   variables: {
+ *      filters: // value for 'filters'
+ *      pagination: // value for 'pagination'
+ *   },
+ * });
+ */
+export function useClientsQuery(baseOptions?: Apollo.QueryHookOptions<ClientsQuery, ClientsQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<ClientsQuery, ClientsQueryVariables>(ClientsDocument, options);
+      }
+export function useClientsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<ClientsQuery, ClientsQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<ClientsQuery, ClientsQueryVariables>(ClientsDocument, options);
+        }
+export function useClientsSuspenseQuery(baseOptions?: Apollo.SuspenseQueryHookOptions<ClientsQuery, ClientsQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<ClientsQuery, ClientsQueryVariables>(ClientsDocument, options);
+        }
+export type ClientsQueryHookResult = ReturnType<typeof useClientsQuery>;
+export type ClientsLazyQueryHookResult = ReturnType<typeof useClientsLazyQuery>;
+export type ClientsSuspenseQueryHookResult = ReturnType<typeof useClientsSuspenseQuery>;
+export type ClientsQueryResult = Apollo.QueryResult<ClientsQuery, ClientsQueryVariables>;
