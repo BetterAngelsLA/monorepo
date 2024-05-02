056fd598db20696b6c7986f0d91c09956713a73d
Davit-BetterAngels <davit@betterangels.la>
2024-05-02 17:56:34 +0000

    updated client types

--- libs/expo/betterangels/src/lib/screens/Home/__generated__/ActiveClients.generated.ts
+++ libs/expo/betterangels/src/lib/screens/Home/__generated__/ActiveClients.generated.ts
@@ -1,152 +1,49 @@
 import * as Types from '../../../apollo/graphql/__generated__/types';
 
-import * as Apollo from '@apollo/client';
 import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
 const defaultOptions = {} as const;
+export type CreateNoteMutationVariables = Types.Exact<{
+  data: Types.CreateNoteInput;
+}>;
+
+
+export type CreateNoteMutation = { __typename?: 'Mutation', createNote: { __typename?: 'NoteType', id: string, title: string, publicDetails: string, createdAt: any, client?: { __typename?: 'UserType', id: string, username: string, firstName?: string | null, lastName?: string | null, email: string } | null, createdBy: { __typename?: 'UserType', id: string, username: string, email: string } } | { __typename?: 'OperationInfo' } };
+
 export type ClientsQueryVariables = Types.Exact<{
   filters?: Types.InputMaybe<Types.ClientFilter>;
   pagination?: Types.InputMaybe<Types.OffsetPaginationInput>;
 }>;
 
-export type ClientsQuery = {
-  __typename?: 'Query';
-  clients: Array<{
-    __typename?: 'ClientType';
-    email: string;
-    firstName?: string | null;
-    id: string;
-    lastName?: string | null;
-    username: string;
-  }>;
-};
 
-export const ClientsDocument = gql`
-  query Clients($filters: ClientFilter, $pagination: OffsetPaginationInput) {
-    clients(filters: $filters, pagination: $pagination) {
-      email
-      firstName
-      id
-      lastName
-      username
-    }
-  }
-`;
+export type ClientsQuery = { __typename?: 'Query', clients: Array<{ __typename?: 'ClientType', email: string, firstName?: string | null, id: string, lastName?: string | null, username: string }> };
 
-/**
- * __useClientsQuery__
- *
- * To run a query within a React component, call `useClientsQuery` and pass it any options that fit your needs.
- * When your component renders, `useClientsQuery` returns an object from Apollo Client that contains loading, error, and data properties
- * you can use to render your UI.
- *
- * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
- *
- * @example
- * const { data, loading, error } = useClientsQuery({
- *   variables: {
- *      filters: // value for 'filters'
- *      pagination: // value for 'pagination'
- *   },
- * });
- */
-export function useClientsQuery(
-  baseOptions?: Apollo.QueryHookOptions<ClientsQuery, ClientsQueryVariables>
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useQuery<ClientsQuery, ClientsQueryVariables>(
-    ClientsDocument,
-    options
-  );
-}
-export function useClientsLazyQuery(
-  baseOptions?: Apollo.LazyQueryHookOptions<ClientsQuery, ClientsQueryVariables>
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useLazyQuery<ClientsQuery, ClientsQueryVariables>(
-    ClientsDocument,
-    options
-  );
-}
-export function useClientsSuspenseQuery(
-  baseOptions?: Apollo.SuspenseQueryHookOptions<
-    ClientsQuery,
-    ClientsQueryVariables
-  >
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useSuspenseQuery<ClientsQuery, ClientsQueryVariables>(
-    ClientsDocument,
-    options
-  );
-}
-export type ClientsQueryHookResult = ReturnType<typeof useClientsQuery>;
-export type ClientsLazyQueryHookResult = ReturnType<typeof useClientsLazyQuery>;
-export type ClientsSuspenseQueryHookResult = ReturnType<
-  typeof useClientsSuspenseQuery
->;
-export type ClientsQueryResult = Apollo.QueryResult<
-  ClientsQuery,
-  ClientsQueryVariables
->;
-export type CreateNoteMutationVariables = Types.Exact<{
-  data: Types.CreateNoteInput;
-}>;
-
-export type CreateNoteMutation = {
-  __typename?: 'Mutation';
-  createNote:
-    | {
-        __typename?: 'NoteType';
-        id: string;
-        title: string;
-        publicDetails: string;
-        createdAt: any;
-        client?: {
-          __typename?: 'UserType';
-          id: string;
-          username: string;
-          firstName?: string | null;
-          lastName?: string | null;
-          email: string;
-        } | null;
-        createdBy: {
-          __typename?: 'UserType';
-          id: string;
-          username: string;
-          email: string;
-        };
-      }
-    | { __typename?: 'OperationInfo' };
-};
 
 export const CreateNoteDocument = gql`
-  mutation CreateNote($data: CreateNoteInput!) {
-    createNote(data: $data) {
-      ... on NoteType {
+    mutation CreateNote($data: CreateNoteInput!) {
+  createNote(data: $data) {
+    ... on NoteType {
+      id
+      title
+      publicDetails
+      client {
         id
-        title
-        publicDetails
-        client {
-          id
-          username
-          firstName
-          lastName
-          email
-        }
-        createdAt
-        createdBy {
-          id
-          username
-          email
-        }
+        username
+        firstName
+        lastName
+        email
+      }
+      createdAt
+      createdBy {
+        id
+        username
+        email
       }
     }
   }
-`;
-export type CreateNoteMutationFn = Apollo.MutationFunction<
-  CreateNoteMutation,
-  CreateNoteMutationVariables
->;
+}
+    `;
+export type CreateNoteMutationFn = Apollo.MutationFunction<CreateNoteMutation, CreateNoteMutationVariables>;
 
 /**
  * __useCreateNoteMutation__
@@ -165,24 +62,57 @@ export type CreateNoteMutationFn = Apollo.MutationFunction<
  *   },
  * });
  */
-export function useCreateNoteMutation(
-  baseOptions?: Apollo.MutationHookOptions<
-    CreateNoteMutation,
-    CreateNoteMutationVariables
-  >
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useMutation<CreateNoteMutation, CreateNoteMutationVariables>(
-    CreateNoteDocument,
-    options
-  );
+export function useCreateNoteMutation(baseOptions?: Apollo.MutationHookOptions<CreateNoteMutation, CreateNoteMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateNoteMutation, CreateNoteMutationVariables>(CreateNoteDocument, options);
+      }
+export type CreateNoteMutationHookResult = ReturnType<typeof useCreateNoteMutation>;
+export type CreateNoteMutationResult = Apollo.MutationResult<CreateNoteMutation>;
+export type CreateNoteMutationOptions = Apollo.BaseMutationOptions<CreateNoteMutation, CreateNoteMutationVariables>;
+export const ClientsDocument = gql`
+    query Clients($filters: ClientFilter, $pagination: OffsetPaginationInput) {
+  clients(filters: $filters, pagination: $pagination) {
+    ... on ClientType {
+      email
+      firstName
+      id
+      lastName
+      username
+    }
+  }
 }
-export type CreateNoteMutationHookResult = ReturnType<
-  typeof useCreateNoteMutation
->;
-export type CreateNoteMutationResult =
-  Apollo.MutationResult<CreateNoteMutation>;
-export type CreateNoteMutationOptions = Apollo.BaseMutationOptions<
-  CreateNoteMutation,
-  CreateNoteMutationVariables
->;
+    `;
+
+/**
+ * __useClientsQuery__
+ *
+ * To run a query within a React component, call `useClientsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useClientsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useClientsQuery({
+ *   variables: {
+ *      filters: // value for 'filters'
+ *      pagination: // value for 'pagination'
+ *   },
+ * });
+ */
+export function useClientsQuery(baseOptions?: Apollo.QueryHookOptions<ClientsQuery, ClientsQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<ClientsQuery, ClientsQueryVariables>(ClientsDocument, options);
+      }
+export function useClientsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<ClientsQuery, ClientsQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<ClientsQuery, ClientsQueryVariables>(ClientsDocument, options);
+        }
+export function useClientsSuspenseQuery(baseOptions?: Apollo.SuspenseQueryHookOptions<ClientsQuery, ClientsQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<ClientsQuery, ClientsQueryVariables>(ClientsDocument, options);
+        }
+export type ClientsQueryHookResult = ReturnType<typeof useClientsQuery>;
+export type ClientsLazyQueryHookResult = ReturnType<typeof useClientsLazyQuery>;
+export type ClientsSuspenseQueryHookResult = ReturnType<typeof useClientsSuspenseQuery>;
+export type ClientsQueryResult = Apollo.QueryResult<ClientsQuery, ClientsQueryVariables>;
