4b387067380286cd4f595f26ddce3ad545dcd930
Davit-BetterAngels <davit@betterangels.la>
2024-05-03 06:15:48 +0000

    general header for main screens

---
+++ libs/expo/betterangels/src/lib/ui-components/Header.tsx
@@ -0,0 +1,52 @@
+import { Colors, Spacings } from '@monorepo/expo/shared/static';
+import { TextBold } from '@monorepo/expo/shared/ui-components';
+import { ElementType } from 'react';
+import { StyleSheet, View } from 'react-native';
+import NavModal from './NavModal';
+
+export default function Header({
+  Logo,
+  title,
+}: {
+  Logo: ElementType;
+  title: string;
+}) {
+  return (
+    <View style={styles.heading}>
+      <View>
+        <Logo
+          style={{ marginBottom: Spacings.xs }}
+          color={Colors.WHITE}
+          width={73}
+          height={11}
+        />
+        <TextBold size="xl" color={Colors.WHITE}>
+          {title}
+        </TextBold>
+      </View>
+      <View style={{ alignItems: 'center' }}>
+        <NavModal />
+        <View
+          style={{
+            height: 6,
+            width: 6,
+            borderRadius: 100,
+            backgroundColor: Colors.ERROR,
+          }}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  heading: {
+    paddingHorizontal: Spacings.sm,
+    paddingBottom: Spacings.xs,
+    backgroundColor: Colors.BRAND_DARK_BLUE,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});
