0f7cdc7d02ee39f20ada56a4e646447a50020da7
Paul Vecchio <paul@betterangels.la>
2024-05-07 16:59:41 -0700

    Migrate to ClientProfile and Update Subsequent Permissions (#295)

--- apps/betterangels-backend/accounts/permissions.py
+++ apps/betterangels-backend/accounts/permissions.py
@@ -7,3 +7,10 @@ class ClientPermissions(models.TextChoices):
     CHANGE = "accounts.change_client", _("Can change client")
     DELETE = "accounts.delete_client", _("Can delete client")
     ADD = "accounts.add_client", _("Can add client")
+
+
+class ClientProfilePermissions(models.TextChoices):
+    VIEW = "accounts.view_clientprofile", _("Can view clientprofile")
+    CHANGE = "accounts.change_clientprofile", _("Can change clientprofile")
+    DELETE = "accounts.delete_clientprofile", _("Can delete clientprofile")
+    ADD = "accounts.add_clientprofile", _("Can add clientprofile")
