e71ec8c1f4b033fd146a9242fb8cb2a8287ebad4
Paul Vecchio <paul@betterangels.la>
2024-05-13 13:18:59 -0700

    Add break test script

---
+++ apps/betterangels-backend/scripts/break_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Infinite loop to run Django tests until an error occurs
+while true; do
+    echo "Running Django tests..."
+    python manage.py test
+    if [ $? -ne 0 ]; then  # Check if the last command returned an error
+        echo "Tests failed"
+        break  # Exit the loop if tests fail
+    fi
+    sleep 1  # Optional: Wait for 1 second before running the tests again
+done
+
+echo "Exiting script."
