2e05fae1f72a1064f23c86ec920f4a42672b84f4
Paul Vecchio <paul@betterangels.la>
2024-05-13 15:28:01 -0700

    Add flaky test script comments (#309)

--- apps/betterangels-backend/scripts/break_tests.sh
+++ apps/betterangels-backend/scripts/break_tests.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-# Infinite loop to run Django tests until an error occurs
+# This script continuously runs Django tests using pytest with a custom PytestTestRunner.
+# The loop will execute until a test fails or until it is manually stopped.
+# Usage:
+# 1. Navigate to the project directory: cd apps/betterangels-backend
+# 2. Run the script: ./test_script.sh
+# To stop the tests manually, press Ctrl+C in the terminal.
+
 while true; do
-    echo "Running Django tests..."
+    echo "Running Django tests with pytest..."
+
+    # Specify the Django app and test directory. Adjust or remove 'notes' as needed.
+    # Include any pytest-specific arguments after '--'.
+    # Example for verbose output: python manage.py test notes -- notes/tests -- -vv
     python manage.py test notes -- notes/tests -k 'test_queries'
-    if [ $? -ne 0 ]; then  # Check if the last command returned an error
+
+    # Check if the last command resulted in an error
+    if [ $? -ne 0 ]; then
         echo "Tests failed"
-        break  # Exit the loop if tests fail
+        break
     fi
-    sleep 1  # Optional: Wait for 1 second before running the tests again
 done
 
 echo "Exiting script."
