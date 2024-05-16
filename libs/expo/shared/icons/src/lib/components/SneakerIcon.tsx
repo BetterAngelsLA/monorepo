8cbb8888548395ed732cf7b70d2dca4a3573b03e
Mike Feldberg <mike@betterangels.la>
2024-05-15 20:36:01 -0700

    rename shoe-prints to sneaker

---
+++ libs/expo/shared/icons/src/lib/components/SneakerIcon.tsx
@@ -0,0 +1,6 @@
+import SneakerIconSVG from '../../assets/sneaker.svg';
+import createSvgIcon from '../createSvgIcon';
+
+const SneakerIcon = createSvgIcon(SneakerIconSVG);
+
+export default SneakerIcon;
