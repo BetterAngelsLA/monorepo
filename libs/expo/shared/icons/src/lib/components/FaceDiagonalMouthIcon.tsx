fbe975133082465c24115e9e9d4cd773e8f9f968
Mike Feldberg <mike@betterangels.la>
2024-05-15 17:55:40 -0700

    delete a bunch of icons

--- libs/expo/shared/icons/src/lib/components/FaceDiagonalMouthIcon.tsx
+++
@@ -1,6 +0,0 @@
-import FaceDiagonalMouthIconSVG from '../../assets/face-diagonal-mouth.svg';
-import createSvgIcon from '../createSvgIcon';
-
-const FaceDiagonalMouthIcon = createSvgIcon(FaceDiagonalMouthIconSVG);
-
-export default FaceDiagonalMouthIcon;
---
+++ libs/expo/shared/icons/src/lib/components/FaceDizzyIcon.tsx
@@ -0,0 +1,6 @@
+import FaceDizzyIconSVG from '../../assets/face-dizzy.svg';
+import createSvgIcon from '../createSvgIcon';
+
+const FaceDizzyIcon = createSvgIcon(FaceDizzyIconSVG);
+
+export default FaceDizzyIcon;
