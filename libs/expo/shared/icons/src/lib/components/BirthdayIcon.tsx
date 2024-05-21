88b96721600a5b127bded290afb7cd460b0ea9a3
Mike Feldberg <mike@betterangels.la>
2024-05-21 16:15:27 -0700

    update birthday, gender, globe

--- libs/expo/shared/icons/src/lib/components/BirthdayIcon.tsx
+++ libs/expo/shared/icons/src/lib/components/BirthdayIcon.tsx
@@ -1,4 +1,4 @@
-import BirthdayIconSVG from '../../assets/birthday.svg';
+import BirthdayIconSVG from '../../assets/third_party_icons/mingcute_design/svg/mingcute:birthday-2-line.svg';
 import createSvgIcon from '../createSvgIcon';
 
 const BirthdayIcon = createSvgIcon(BirthdayIconSVG);
--- libs/expo/shared/icons/src/lib/components/GenderIcon.tsx
+++ libs/expo/shared/icons/src/lib/components/GenderIcon.tsx
@@ -1,4 +1,4 @@
-import GenderIconSVG from '../../assets/gender.svg';
+import GenderIconSVG from '../../assets/third_party_icons/phosphor/svg/ph:gender-neuter-bold.svg';
 import createSvgIcon from '../createSvgIcon';
 
 const GenderIcon = createSvgIcon(GenderIconSVG);
