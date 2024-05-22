92504205094114259c2aabebc7ead2e95c1c2c1b
Mike Feldberg <mike@betterangels.la>
2024-05-22 12:48:50 -0700

    delete unused SolidUsersIcon

--- libs/expo/shared/icons/src/lib/components/SolidUsersIcon.tsx
+++
@@ -1,6 +0,0 @@
-import UsersSolidIconSVG from '../../assets/third_party_icons/pictogrammers/svg/mdi:users.svg';
-import createSvgIcon from '../createSvgIcon';
-
-const UsersSolidIcon = createSvgIcon(UsersSolidIconSVG);
-
-export default UsersSolidIcon;
