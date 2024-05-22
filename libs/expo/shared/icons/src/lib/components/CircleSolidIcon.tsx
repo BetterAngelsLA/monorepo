a6a199d5cdba5316eb01636922fe0b90a6cdd4b8
Mike Feldberg <mike@betterangels.la>
2024-05-22 12:01:14 -0700

    update circle solid

---
+++ libs/expo/shared/icons/src/lib/components/CircleSolidIcon.tsx
@@ -0,0 +1,6 @@
+import SolidCircle from '../../assets/third_party_icons/pictogrammers/svg/mdi:circle.svg';
+import createSvgIcon from '../createSvgIcon';
+
+const CircleSolidIcon = createSvgIcon(SolidCircle);
+
+export default CircleSolidIcon;
--- libs/expo/shared/icons/src/lib/components/SolidCircleIcon.tsx
+++
@@ -1,6 +0,0 @@
-import SolidCircle from '../../assets/third_party_icons/_source_unknown/svg/circle-solid.svg';
-import createSvgIcon from '../createSvgIcon';
-
-const SolidCircleIcon = createSvgIcon(SolidCircle);
-
-export default SolidCircleIcon;
