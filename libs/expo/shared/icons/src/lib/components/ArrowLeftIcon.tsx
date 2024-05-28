09410a328e933f8634b5f1573e5791843ecbd2a5
Mike Feldberg <mike@betterangels.la>
2024-05-28 11:15:00 -0700

    update arrow left icon

--- libs/expo/shared/icons/src/lib/components/ArrowLeftIcon.tsx
+++ libs/expo/shared/icons/src/lib/components/ArrowLeftIcon.tsx
@@ -1,4 +1,4 @@
-import ArrowLeftIconSVG from '../../assets/third_party_icons/gerrit_halfmann/svg/majesticons:arrow-left.svg';
+import ArrowLeftIconSVG from '../../assets/third_party_icons/mingcute_design/svg/mingcute:arrow-left-line.svg';
 import createSvgIcon from '../createSvgIcon';
 
 const ArrowLeftIcon = createSvgIcon(ArrowLeftIconSVG);
