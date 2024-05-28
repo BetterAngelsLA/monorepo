8bd7d6a058a0dcbc01590733d0893eccb33e0d35
Paul Vecchio <paul@betterangels.la>
2024-05-28 07:28:53 -0700

    Config updates

--- apps/betterangels-backend/accounts/views/auth_views.py
+++ apps/betterangels-backend/accounts/views/auth_views.py
@@ -21,12 +21,6 @@ class GoogleLogin(SocialLoginView):
     serializer_class = SocialLoginSerializer
     authentication_classes: List[Any] = []
 
-    def post(self, request: Request, *args: T, **kwargs: Any) -> Response:
-        # Get callback_url from the POST data or URL parameters,
-        # if not provided use a default
-        self.callback_url = request.query_params.get("redirect_uri")
-        return cast(Response, super().post(request, *args, **kwargs))
-
 
 class AuthRedirectView(APIView):
     def get(self, request: Request) -> Response:
