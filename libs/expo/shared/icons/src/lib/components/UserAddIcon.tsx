fd0666513e99a07177e84f244317d0190a0471e4
Mike Feldberg <mike@betterangels.la>
2024-06-05 14:57:47 -0700

    update icon; hardcode svg color

--- libs/expo/shared/icons/src/lib/components/UserAddIcon.tsx
+++ libs/expo/shared/icons/src/lib/components/UserAddIcon.tsx
@@ -1,6 +1,6 @@
 import UserAddIconSVG from '../../assets/third_party/leonid_tsvetkov/svg/lets-icons:user-add.svg';
 import createSvgIcon from '../createSvgIcon';
 
-const UserAddIcon = createSvgIcon(UserAddIconSVG, true);
+const UserAddIcon = createSvgIcon(UserAddIconSVG);
 
 export default UserAddIcon;
