fe10e89b1601d74a0ffd35cb53da9b3a22cb324b
Davit-BetterAngels <141072810+Davit-BetterAngels@users.noreply.github.com>
2024-06-10 15:18:17 -0700

    Feat/dev 331 main nav direction (#353)
    * implemented react-native-modal lib
    
    * change from RN to RN Modal and refactored under it
    
    * removed comment

--- libs/expo/betterangels/src/lib/ui-components/MainModal.tsx
+++ libs/expo/betterangels/src/lib/ui-components/MainModal.tsx
@@ -3,7 +3,8 @@ import { Colors, Spacings } from '@monorepo/expo/shared/static';
 import { TextRegular } from '@monorepo/expo/shared/ui-components';
 import { useRouter } from 'expo-router';
 import * as React from 'react';
-import { Modal, Pressable, StyleSheet, View } from 'react-native';
+import { DimensionValue, Pressable, StyleSheet, View } from 'react-native';
+import Modal from 'react-native-modal';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 interface IMainModalProps {
@@ -21,7 +22,10 @@ interface IMainModalProps {
   bottomSection?: React.ReactNode;
   topSection?: React.ReactNode;
   closeButton?: boolean;
-  transparent?: boolean;
+  opacity?: number;
+  vertical?: boolean;
+  ml?: number;
+  height?: DimensionValue;
 }
 
 export default function MainModal(props: IMainModalProps) {
@@ -32,98 +36,103 @@ export default function MainModal(props: IMainModalProps) {
     bottomSection,
     topSection,
     closeButton,
-    transparent = false,
+    opacity = 0,
+    vertical = false,
+    ml = 0,
+    height = 'auto',
   } = props;
 
   const router = useRouter();
 
   const insets = useSafeAreaInsets();
   const bottomOffset = insets.bottom;
+  const topOffset = insets.top;
   return (
     <Modal
-      animationType="slide"
-      transparent={transparent}
-      visible={isModalVisible}
-      onRequestClose={closeModal}
+      style={{
+        margin: 0,
+        marginLeft: ml,
+        flex: 1,
+        justifyContent: 'flex-end',
+      }}
+      animationIn={vertical ? 'slideInUp' : 'slideInRight'}
+      animationOut={vertical ? 'slideOutDown' : 'slideOutRight'}
+      backdropOpacity={opacity}
+      isVisible={isModalVisible}
+      onBackdropPress={closeModal}
     >
       <View
         style={{
-          flex: 1,
-          justifyContent: 'flex-end',
+          borderTopLeftRadius: 8,
+          borderTopRightRadius: 8,
+          paddingTop: topOffset + Spacings.xs,
+          paddingHorizontal: Spacings.md,
+          paddingBottom: 35 + bottomOffset,
+          backgroundColor: Colors.WHITE,
+          height,
         }}
       >
-        <View
-          style={{
-            borderTopLeftRadius: 8,
-            borderTopRightRadius: 8,
-            paddingTop: Spacings.md,
-            paddingHorizontal: Spacings.md,
-            paddingBottom: 35 + bottomOffset,
-            backgroundColor: Colors.WHITE,
-          }}
-        >
-          {closeButton && (
+        {closeButton && (
+          <Pressable
+            style={{ marginLeft: 'auto' }}
+            accessible
+            accessibilityHint="closes the modal"
+            accessibilityRole="button"
+            accessibilityLabel="close"
+            onPress={closeModal}
+          >
+            <XmarkIcon size="md" color={Colors.BLACK} />
+          </Pressable>
+        )}
+        <View style={styles.modalOverlay}>
+          {topSection}
+          {actions.map((action, idx: number) => (
             <Pressable
-              style={{ marginLeft: 'auto' }}
-              accessible
-              accessibilityHint="closes the modal"
+              onPress={() => {
+                closeModal();
+                router.navigate({
+                  pathname: action.route,
+                  params: action.params,
+                });
+              }}
               accessibilityRole="button"
-              accessibilityLabel="close"
-              onPress={closeModal}
+              key={idx}
+              style={styles.container}
             >
-              <XmarkIcon size="md" color={Colors.BLACK} />
-            </Pressable>
-          )}
-          <View style={styles.modalOverlay}>
-            {topSection}
-            {actions.map((action, idx: number) => (
-              <Pressable
-                onPress={() => {
-                  closeModal();
-                  router.navigate({
-                    pathname: action.route,
-                    params: action.params,
-                  });
-                }}
-                accessibilityRole="button"
-                key={idx}
-                style={styles.container}
-              >
-                {({ pressed }) => (
+              {({ pressed }) => (
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    flex: 1,
+                    backgroundColor: pressed
+                      ? Colors.NEUTRAL_EXTRA_LIGHT
+                      : Colors.WHITE,
+                    borderRadius: 8,
+                    paddingHorizontal: Spacings.sm,
+                    paddingVertical: Spacings.sm,
+                  }}
+                >
                   <View
                     style={{
-                      flexDirection: 'row',
+                      marginRight: Spacings.sm,
+                      height: 40,
+                      width: 40,
                       alignItems: 'center',
-                      flex: 1,
-                      backgroundColor: pressed
-                        ? Colors.NEUTRAL_EXTRA_LIGHT
-                        : Colors.WHITE,
-                      borderRadius: 8,
-                      paddingHorizontal: Spacings.sm,
-                      paddingVertical: Spacings.sm,
+                      justifyContent: 'center',
                     }}
                   >
-                    <View
-                      style={{
-                        marginRight: Spacings.sm,
-                        height: 40,
-                        width: 40,
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                      }}
-                    >
-                      <action.Icon color={Colors.PRIMARY_EXTRA_DARK} />
-                    </View>
-
-                    <TextRegular color={Colors.PRIMARY_EXTRA_DARK}>
-                      {action.title}
-                    </TextRegular>
+                    <action.Icon color={Colors.PRIMARY_EXTRA_DARK} />
                   </View>
-                )}
-              </Pressable>
-            ))}
-            {bottomSection}
-          </View>
+
+                  <TextRegular color={Colors.PRIMARY_EXTRA_DARK}>
+                    {action.title}
+                  </TextRegular>
+                </View>
+              )}
+            </Pressable>
+          ))}
+          {bottomSection}
         </View>
       </View>
     </Modal>
