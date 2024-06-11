a5e71c801516e8c4ad8c094770b59ef445f0314f
Davit-BetterAngels <141072810+Davit-BetterAngels@users.noreply.github.com>
2024-06-11 00:08:41 -0700

    Feat/DEV-347 Retrieve Client Client Profile Data (#352)
    * gender, dob and language real data
    
    * removed console
    
    * updated age from query

--- libs/expo/betterangels/src/lib/screens/Client/ClientHeader.tsx
+++ libs/expo/betterangels/src/lib/screens/Client/ClientHeader.tsx
@@ -14,10 +14,15 @@ import { View } from 'react-native';
 interface IClientHeaderProps {
   firstName: string | null | undefined;
   lastName: string | null | undefined;
+  gender: string | null | undefined;
+  preferredLanguage: string | null | undefined;
+  dateOfBirth: string | null | undefined;
+  age: number | null | undefined;
 }
 
 export default function ClientHeader(props: IClientHeaderProps) {
-  const { firstName, lastName } = props;
+  const { firstName, lastName, gender, preferredLanguage, dateOfBirth, age } =
+    props;
   return (
     <View
       style={{
@@ -43,19 +48,31 @@ export default function ClientHeader(props: IClientHeaderProps) {
           {firstName} {lastName}
         </TextMedium>
       </View>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <GenderIcon mr="xs" color={Colors.PRIMARY_EXTRA_DARK} />
-          <TextRegular mr="sm">Male</TextRegular>
-        </View>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <GlobeIcon mr="xs" color={Colors.PRIMARY_EXTRA_DARK} />
-          <TextRegular mr="sm">Spanish</TextRegular>
-        </View>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <BirthdayIcon mr="xs" color={Colors.PRIMARY_EXTRA_DARK} />
-          <TextRegular>1/1/1970 (53)</TextRegular>
-        </View>
+      <View
+        style={{ flexDirection: 'row', alignItems: 'center', gap: Spacings.sm }}
+      >
+        {gender && (
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <GenderIcon mr="xs" color={Colors.PRIMARY_EXTRA_DARK} />
+            <TextRegular textTransform="capitalize">{gender}</TextRegular>
+          </View>
+        )}
+        {preferredLanguage && (
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <GlobeIcon mr="xs" color={Colors.PRIMARY_EXTRA_DARK} />
+            <TextRegular textTransform="capitalize">
+              {preferredLanguage}
+            </TextRegular>
+          </View>
+        )}
+        {dateOfBirth && (
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <BirthdayIcon mr="xs" color={Colors.PRIMARY_EXTRA_DARK} />
+            <TextRegular>
+              {dateOfBirth} ({age})
+            </TextRegular>
+          </View>
+        )}
       </View>
     </View>
   );
