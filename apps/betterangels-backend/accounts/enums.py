fe367e00e96f21983b5eeea8a260e019a6dafe24
Mike Feldberg <mike@betterangels.la>
2024-06-10 22:22:25 -0700

    model and types; temp solution to languageenum

--- apps/betterangels-backend/accounts/enums.py
+++ apps/betterangels-backend/accounts/enums.py
@@ -1,3 +1,4 @@
+import strawberry
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -10,4 +11,34 @@ class GenderEnum(models.TextChoices):
 
 class LanguageEnum(models.TextChoices):
     ENGLISH = "english", _("English")
+    RUSSIAN = "russian", _("Russian")
+    JAPANESE = "japanese", _("Japanese")
+    ARABIC = "arabic", _("Arabic")
+    PERSIAN = "persian", _("Persian")
+    ARMENIAN = "armenian", _("Armenian")
+    KOREAN = "korean", _("Korean")
+    VIETNAMESE = "vietnamese", _("Vietnamese")
+    TAGALOG = "tagalog", _("Tagalog")
+    CHINESE = "chinese", _("Chinese")
     SPANISH = "spanish", _("Spanish")
+
+
+@strawberry.enum
+class StrawberryLanguageEnum(models.TextChoices):
+    ENGLISH = "english", "English"
+    RUSSIAN = "russian", "Russian"
+    JAPANESE = "japanese", "Japanese"
+    ARABIC = "arabic", "Arabic"
+    PERSIAN = "persian", "Persian"
+    ARMENIAN = "armenian", "Armenian"
+    KOREAN = "korean", "Korean"
+    VIETNAMESE = "vietnamese", "Vietnamese"
+    TAGALOG = "tagalog", "Tagalog"
+    CHINESE = "chinese", "Chinese"
+    SPANISH = "spanish", "Spanish"
+
+
+class YesNoPreferNotToSayEnum(models.TextChoices):
+    YES = "yes", _("Yes")
+    NO = "no", _("No")
+    PREFER_NOT_TO_SAY = "prefer_not_to_say", _("Prefer not to say")
--- apps/betterangels-backend/accounts/models.py
+++ apps/betterangels-backend/accounts/models.py
@@ -1,7 +1,7 @@
 from typing import TYPE_CHECKING, Any, Dict, Iterable, Tuple
 
 import pghistory
-from accounts.enums import GenderEnum, LanguageEnum
+from accounts.enums import GenderEnum, LanguageEnum, YesNoPreferNotToSayEnum
 from accounts.managers import UserManager
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -10,6 +10,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.forms import ValidationError
 from django_choices_field import TextChoicesField
@@ -79,10 +80,16 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class ClientProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="client_profile")
-    hmis_id = models.CharField(max_length=50, blank=True, null=True, db_index=True, unique=True)
+    address = models.TextField(blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
     gender = TextChoicesField(choices_enum=GenderEnum, blank=True, null=True)
+    hmis_id = models.CharField(max_length=50, blank=True, null=True, db_index=True, unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    spoken_languages = ArrayField(base_field=models.CharField(max_length=10), blank=True, null=True, default=list)
     preferred_language = TextChoicesField(choices_enum=LanguageEnum, blank=True, null=True)
+    pronouns = models.CharField(max_length=50, blank=True, null=True)
+    social_security_number = models.CharField(max_length=9, blank=True, null=True)
+    veteran_status = TextChoicesField(choices_enum=YesNoPreferNotToSayEnum, blank=True, null=True)
 
 
 class ExtendedOrganizationInvitation(OrganizationInvitation):
