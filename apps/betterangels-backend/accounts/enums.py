cf2820a9976177612d388f994b718d02b919be84
Mike Feldberg <mike@betterangels.la>
2024-06-12 20:21:13 +0000

    Update ClientProfile fields (DEV-229) (#359)

--- apps/betterangels-backend/accounts/enums.py
+++ apps/betterangels-backend/accounts/enums.py
@@ -1,3 +1,4 @@
+import strawberry
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -8,6 +9,25 @@ class GenderEnum(models.TextChoices):
     NONBINARY = "nonbinary", _("Non-binary")
 
 
+@strawberry.enum
 class LanguageEnum(models.TextChoices):
+    ARABIC = "arabic", _("Arabic")
+    ARMENIAN = "armenian", _("Armenian")
     ENGLISH = "english", _("English")
+    FARSI = "farsi", _("Farsi")
+    INDONESIAN = "indonesian", _("Indonesian")
+    JAPANESE = "japanese", _("Japanese")
+    KHMER = "khmer", _("Khmer")
+    KOREAN = "korean", _("Korean")
+    RUSSIAN = "russian", _("Russian")
+    SIMPLIFIED_CHINESE = "simplified_chinese", _("Simplified Chinese")
     SPANISH = "spanish", _("Spanish")
+    TAGALOG = "tagalog", _("Tagalog")
+    TRADITIONAL_CHINESE = "traditional_chinese", _("Traditional Chinese")
+    VIETNAMESE = "vietnamese", _("Vietnamese")
+
+
+class YesNoPreferNotToSayEnum(models.TextChoices):
+    YES = "yes", _("Yes")
+    NO = "no", _("No")
+    PREFER_NOT_TO_SAY = "prefer_not_to_say", _("Prefer not to say")
