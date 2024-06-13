f9761e7ca5780fe48bee4a7ca10284191b26af0a
Paul Vecchio <paul@betterangels.la>
2024-06-13 11:49:10 -0700

    Fix some formatting

--- apps/betterangels-backend/legal/urls.py
+++ apps/betterangels-backend/legal/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("privacy-policy/", views.privacy_policy, name="privacy_policy"),
+    path("privacy-policy", views.privacy_policy, name="privacy_policy"),
 ]
