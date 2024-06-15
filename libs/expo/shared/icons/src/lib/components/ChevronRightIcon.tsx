a9198720db6a0da2aacc17b1a0a116bb47b8cfe8
Mike Feldberg <mike@betterangels.la>
2024-06-15 12:40:21 -0700

    add icons & attribution

---
+++ libs/expo/shared/icons/src/lib/components/ChevronRightIcon.tsx
@@ -0,0 +1,6 @@
+import ChevronRightSVG from '../../assets/third_party/dave_gandy/svg/fa6-solid:chevron-right.svg';
+import createSvgIcon from '../createSvgIcon';
+
+const ChevronRightIcon = createSvgIcon(ChevronRightSVG);
+
+export default ChevronRightIcon;
---
+++ libs/expo/shared/icons/src/lib/components/FlagIcon.tsx
@@ -0,0 +1,6 @@
+import FlagIconSVG from '../../assets/third_party/dave_gandy/svg/fa6-solid:flag.svg';
+import createSvgIcon from '../createSvgIcon';
+
+const FlagIconIcon = createSvgIcon(FlagIconSVG);
+
+export default FlagIconIcon;
