b2d70096d0cfcea5e03594245e216e4b67af0252
Mike Feldberg <mike@betterangels.la>
2024-06-16 14:44:14 -0700

    add task list icon components; update createSvgIcon

---
+++ libs/expo/shared/icons/src/lib/components/FlagIcon.tsx
@@ -0,0 +1,6 @@
+import FlagIconSVG from '../../assets/third_party/dave_gandy/svg/fa6-solid:flag.svg';
+import createSvgIcon from '../createSvgIcon';
+
+const FlagIconIcon = createSvgIcon(FlagIconSVG, true, 'sm');
+
+export default FlagIconIcon;
