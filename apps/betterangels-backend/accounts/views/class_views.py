165d428d2c60ae729e5830b008b85fb77df93149
Paul Vecchio <paul@betterangels.la>
2024-06-17 13:03:16 -0700

    Add `google-oauth2-homepage` (DEV-430) (#379)
    * Add oauth2 homepage
    
    * Some copy updates and reverse links

--- apps/betterangels-backend/accounts/views/class_views.py
+++ apps/betterangels-backend/accounts/views/class_views.py
@@ -3,7 +3,7 @@ from typing import TypeVar
 from accounts.forms import UserCreationForm
 from django.db import models
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic import CreateView, TemplateView
 
 T = TypeVar("T")
 
@@ -12,3 +12,7 @@ class SignUpView(CreateView[models.Model, UserCreationForm]):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+class GoogleOauthHomePage(TemplateView):
+    template_name = "account/google-oauth-homepage.html"
