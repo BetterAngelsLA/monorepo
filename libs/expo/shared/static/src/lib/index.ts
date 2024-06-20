87a3b03b5014559ce4e55ea676e5f219e214af3b
Davit-BetterAngels <davit@betterangels.la>
2024-06-20 06:24:39 +0000

    static languages

--- libs/expo/shared/static/src/lib/index.ts
+++ libs/expo/shared/static/src/lib/index.ts
@@ -1,4 +1,5 @@
 export { Colors } from './colors';
 export { FontSizes } from './fontSizes';
+export { Languages } from './languages';
 export { Regex } from './regex';
 export { Spacings } from './spacings';
