4f878750c61e52d370105ea92acc999e408cbbbe
Davit-BetterAngels <davit@betterangels.la>
2024-06-20 08:03:35 +0000

    Veteran status

---
+++ libs/expo/betterangels/src/lib/screens/AddClient/VeteranStatus.tsx
@@ -0,0 +1,82 @@
+import { Spacings } from '@monorepo/expo/shared/static';
+import {
+  BasicRadio,
+  FieldCard,
+  TextMedium,
+  TextRegular,
+} from '@monorepo/expo/shared/ui-components';
+import { RefObject } from 'react';
+import { ScrollView, View } from 'react-native';
+import {
+  CreateClientProfileInput,
+  YesNoPreferNotToSayEnum,
+} from '../../apollo';
+
+interface IVeteranStatusProps {
+  client: CreateClientProfileInput;
+  setClient: (client: CreateClientProfileInput) => void;
+  expanded: undefined | string | null;
+  setExpanded: (expanded: undefined | string | null) => void;
+  scrollRef: RefObject<ScrollView>;
+}
+
+const VETERAN_STATUS: Array<'Yes' | 'No'> = ['Yes', 'No'];
+
+export default function VeteranStatus(props: IVeteranStatusProps) {
+  const { expanded, setExpanded, client, scrollRef, setClient } = props;
+
+  const isVeteranStatus = expanded === 'Veteran Status';
+  return (
+    <FieldCard
+      scrollRef={scrollRef}
+      expanded={expanded}
+      setExpanded={() => {
+        setExpanded(isVeteranStatus ? null : 'Veteran Status');
+      }}
+      required
+      mb="xs"
+      actionName={
+        !client.veteranStatus && !isVeteranStatus ? (
+          <TextMedium size="sm">Add Veteran Status</TextMedium>
+        ) : (
+          <TextMedium size="sm">{client.veteranStatus}</TextMedium>
+        )
+      }
+      title="Veteran Status"
+    >
+      <View
+        style={{
+          gap: Spacings.sm,
+          height: isVeteranStatus ? 'auto' : 0,
+          overflow: 'hidden',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}
+      >
+        <TextRegular>Are you a veteran?</TextRegular>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            gap: Spacings.sm,
+          }}
+        >
+          {VETERAN_STATUS.map((q) => (
+            <BasicRadio
+              label={q}
+              accessibilityHint={`Select ${q}`}
+              key={q}
+              value={client.veteranStatus}
+              onPress={() =>
+                setClient({
+                  ...client,
+                  veteranStatus: YesNoPreferNotToSayEnum[q],
+                })
+              }
+            />
+          ))}
+        </View>
+      </View>
+    </FieldCard>
+  );
+}
