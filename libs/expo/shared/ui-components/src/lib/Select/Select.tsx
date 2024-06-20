532a09efae19dacecb4fcc3576d0f38725d5a2ed
Davit-BetterAngels <davit@betterangels.la>
2024-06-20 06:38:48 +0000

    select component

--- libs/expo/shared/ui-components/src/lib/Select/Select.tsx
+++ libs/expo/shared/ui-components/src/lib/Select/Select.tsx
@@ -1,26 +1,12 @@
-import { ChevronLeftIcon } from '@monorepo/expo/shared/icons';
 import { Colors, Spacings } from '@monorepo/expo/shared/static';
-import { useRef, useState } from 'react';
-import {
-  Dimensions,
-  Platform,
-  Pressable,
-  ScrollView,
-  StyleSheet,
-  TouchableOpacity,
-  View,
-  ViewStyle,
-} from 'react-native';
+import { StyleSheet, View, ViewStyle } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 import TextRegular from '../TextRegular';
-
-const MIN_FITABLE_HEIGHT = 300;
-const DROPDOWN_MAX_HEIGHT = 150;
 
 type TSpacing = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
 interface ISelectProps {
   data: string[];
-  setExternalValue: (e: string) => void;
   mb?: TSpacing;
   mt?: TSpacing;
   my?: TSpacing;
@@ -29,42 +15,16 @@ interface ISelectProps {
   mr?: TSpacing;
   label?: string;
   placeholder?: string;
+  onChange: (e: any) => void;
+  value: string;
 }
 
 export function Select(props: ISelectProps) {
-  const { setExternalValue, data, mb, mt, mr, ml, my, mx, label, placeholder } =
-    props;
-  const [value, setValue] = useState('');
-  const [showDropdown, setShowDropdown] = useState(false);
-  const [dropdownPosition, setDropdownPosition] = useState('bottom');
-  const buttonRef = useRef<View>(null);
-
-  const handleDropdown = () => {
-    setShowDropdown(true);
-    if (buttonRef.current) {
-      buttonRef.current?.measure((fx, fy, width, height, px, py) => {
-        const screenHeight = Dimensions.get('window').height;
-        const bottomSpace = screenHeight - (py + height);
-        if (bottomSpace < MIN_FITABLE_HEIGHT) {
-          setDropdownPosition('top');
-        } else {
-          setDropdownPosition('bottom');
-        }
-      });
-    }
-  };
-
-  const handlePress = (newValue: string) => {
-    setValue(newValue);
-    setExternalValue(newValue);
-    setShowDropdown(false);
-  };
+  const { value, data, mb, mt, mr, ml, my, mx, label, onChange } = props;
 
   const containerStyle: ViewStyle = {
-    position: 'relative',
     width: '100%',
     maxWidth: 600,
-    ...(Platform.OS === 'ios' && showDropdown ? { zIndex: 10 } : null),
   };
 
   return (
@@ -86,48 +46,16 @@ export function Select(props: ISelectProps) {
           {label}
         </TextRegular>
       )}
-      <Pressable
-        accessible
-        accessibilityRole="button"
-        accessibilityHint="opens dropdown"
-        onPress={handleDropdown}
-      >
-        <View style={styles.select} ref={buttonRef}>
-          <TextRegular>{value ? value : placeholder}</TextRegular>
-          <View style={styles.icon}>
-            <ChevronLeftIcon
-              rotate={showDropdown ? '90deg' : '-90deg'}
-              color={Colors.PRIMARY_EXTRA_DARK}
-            />
-          </View>
-        </View>
-      </Pressable>
-      {showDropdown && (
-        <ScrollView
-          contentContainerStyle={{ padding: Spacings.xs }}
-          keyboardShouldPersistTaps="handled"
-          style={[
-            styles.dropdown,
-            dropdownPosition === 'top'
-              ? styles.dropdownTop
-              : styles.dropdownBottom,
-          ]}
-          testID="select-dropdown"
-        >
-          {data.map((item, idx) => (
-            <TouchableOpacity
-              accessible
-              accessibilityRole="button"
-              accessibilityHint={'selects ${item} from opened dropdown'}
-              style={{ padding: Spacings.xs }}
-              key={idx}
-              onPress={() => handlePress(item)}
-            >
-              <TextRegular>{item}</TextRegular>
-            </TouchableOpacity>
-          ))}
-        </ScrollView>
-      )}
+      <RNPickerSelect
+        useNativeAndroidPickerStyle={false}
+        style={{
+          inputIOSContainer: styles.select,
+          inputAndroidContainer: styles.select,
+        }}
+        onValueChange={(value) => onChange(value)}
+        value={value}
+        items={data.map((item) => ({ label: item, value: item }))}
+      />
     </View>
   );
 }
@@ -137,30 +65,10 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.WHITE,
     borderWidth: 1,
     borderRadius: 8,
-    borderColor: Colors.PRIMARY_EXTRA_DARK,
+    borderColor: Colors.NEUTRAL_LIGHT,
     justifyContent: 'center',
     paddingLeft: Spacings.sm,
     paddingRight: 38,
     height: 56,
-  },
-  dropdown: {
-    width: '100%',
-    maxHeight: DROPDOWN_MAX_HEIGHT,
-    position: 'absolute',
-    zIndex: 100,
-    borderWidth: 1,
-    borderRadius: 8,
-    borderColor: Colors.PRIMARY_EXTRA_DARK,
-    backgroundColor: Colors.WHITE,
-  },
-  dropdownTop: {
-    bottom: '102%',
-  },
-  dropdownBottom: {
-    top: '102%',
-  },
-  icon: {
-    position: 'absolute',
-    right: 20,
   },
 });
