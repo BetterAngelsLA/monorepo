0f00073f0d2adec3c8c5455836d0590da611d904
Paul Vecchio <paul@betterangels.la>
2024-06-20 14:51:38 -0700

    Bump BetterAngels version to 1.0.16

--- apps/betterangels/app.config.js
+++ apps/betterangels/app.config.js
@@ -10,7 +10,7 @@ export default {
     name: IS_PRODUCTION ? 'BetterAngels' : 'BetterAngels (Dev)',
     slug: 'betterangels',
     scheme: IS_PRODUCTION ? 'betterangels' : 'betterangels-dev',
-    version: '1.0.15',
+    version: '1.0.16',
     orientation: 'portrait',
     icon: './src/app/assets/images/icon.png',
     splash: {
