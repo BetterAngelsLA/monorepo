122e11e5b46f13e5ad846d5903308e8502b1955f
Paul Vecchio <paul@betterangels.la>
2024-06-21 12:25:48 -0700

    Bump app store submission versions

--- apps/betterangels/app.config.js
+++ apps/betterangels/app.config.js
@@ -26,6 +26,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: BUNDLE_IDENTIFIER,
+      buildNumber: '1.0.1',
       associatedDomains: [`applinks:${HOSTNAME}`],
       usesAppleSignIn: true,
       config: {
@@ -58,6 +59,7 @@ export default {
           apiKey: process.env.EXPO_PUBLIC_ANDROID_GOOGLEMAPS_APIKEY,
         },
       },
+      versionCode: 2,
     },
     web: {
       favicon: './src/app/assets/images/favicon.png',
