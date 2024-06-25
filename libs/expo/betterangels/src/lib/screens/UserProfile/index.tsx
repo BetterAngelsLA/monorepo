efb6041948e8d174c3fb73c9f82f714f3e42a78f
Mike Feldberg <mike@betterangels.la>
2024-06-25 16:37:32 -0700

    add explicit signout to clear user

--- libs/expo/betterangels/src/lib/screens/UserProfile/index.tsx
+++ libs/expo/betterangels/src/lib/screens/UserProfile/index.tsx
@@ -9,7 +9,7 @@ import {
 import { router } from 'expo-router';
 import { StyleSheet, View } from 'react-native';
 import { useDeleteCurrentUserMutation } from '../../apollo';
-import { useUser } from '../../hooks';
+import { useSignOut, useUser } from '../../hooks';
 import InfoCard from './InfoCard';
 export default function UserProfile() {
   const { user } = useUser();
@@ -27,11 +27,13 @@ export default function UserProfile() {
           : 'None',
     },
   ];
+  const { signOut } = useSignOut();
 
   async function deleteCurrentUserFunction() {
     try {
       await deleteCurrentUser();
       router.navigate('/auth');
+      signOut();
     } catch (err) {
       console.log(err);
     }
