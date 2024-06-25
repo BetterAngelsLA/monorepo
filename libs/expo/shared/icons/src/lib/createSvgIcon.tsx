8886a89b297c9eafb4c9d82d690eb96ac61303f6
Paul Vecchio <paul@betterangels.la>
2024-06-25 13:04:03 -0700

    Keep default white

--- libs/expo/shared/icons/src/lib/createSvgIcon.tsx
+++ libs/expo/shared/icons/src/lib/createSvgIcon.tsx
@@ -11,7 +11,7 @@ const createSvgIcon = (
 ) => {
   const IconComponent: React.FC<IIconProps> = ({
     size = 'md',
-    color = '#052B73',
+    color = '#FFFFFF',
     rotate = '0deg',
     mb,
     mt,
