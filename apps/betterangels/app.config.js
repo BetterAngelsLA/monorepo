607aa120824329945b6040633acea2009da99983
Paul Vecchio <paul@betterangels.la>
2024-06-26 12:46:50 -0700

    Adjust comment

--- apps/betterangels/app.config.js
+++ apps/betterangels/app.config.js
@@ -2,7 +2,7 @@ const IS_PRODUCTION = process.env.APP_VARIANT === 'production';
 
 const HOSTNAME = IS_PRODUCTION
   ? 'api.prod.betterangels.la' // TODO: We should adjust this to be app.betterangels.la
-  : 'api.dev.betterangels.la'; // TODO: We should adjust this to be api.dev.betterangels.la
+  : 'api.dev.betterangels.la'; // TODO: We should adjust this to be app.dev.betterangels.la
 
 const BUNDLE_IDENTIFIER = IS_PRODUCTION
   ? 'la.betterangels.app'
