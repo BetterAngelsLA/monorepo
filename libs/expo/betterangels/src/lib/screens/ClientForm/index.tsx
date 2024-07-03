760c91e1a96fa258f635c789b65461a789bc4f0f
Mike Feldberg <mike@betterangels.la>
2024-07-02 23:07:58 -0700

    abstract AddClient

---
+++ libs/expo/betterangels/src/lib/screens/ClientForm/index.tsx
@@ -0,0 +1,116 @@
+import { Colors } from '@monorepo/expo/shared/static';
+import { BottomActions, TextButton } from '@monorepo/expo/shared/ui-components';
+import { useNavigation } from 'expo-router';
+import { useRef, useState } from 'react';
+import { ScrollView, View } from 'react-native';
+import {
+  CreateClientProfileInput,
+  GenderEnum,
+  LanguageEnum,
+  YesNoPreferNotToSayEnum,
+} from '../../apollo';
+import { MainScrollContainer } from '../../ui-components';
+import ContactInfo from './ContactInfo';
+import Dob from './Dob';
+import Gender from './Gender';
+import HMIS from './HMIS';
+import Language from './Language';
+import Name from './Name';
+import VeteranStatus from './VeteranStatus';
+import { useCreateClientProfileMutation } from './__generated__/AddClient.generated';
+
+const INITIAL_STATE = {
+  address: '',
+  dateOfBirth: undefined as Date | undefined,
+  gender: undefined as GenderEnum | undefined,
+  hmisId: null,
+  nickname: '',
+  phoneNumber: '',
+  preferredLanguage: undefined as LanguageEnum | undefined,
+  pronouns: '',
+  spokenLanguages: [] as LanguageEnum[],
+  user: {
+    firstName: '',
+    lastName: '',
+    email: '',
+  },
+  veteranStatus: undefined as YesNoPreferNotToSayEnum | undefined,
+};
+
+export default function ClientForm() {
+  const [expanded, setExpanded] = useState<undefined | string | null>();
+  const [client, setClient] = useState<CreateClientProfileInput>(INITIAL_STATE);
+  const [createClient] = useCreateClientProfileMutation();
+  const [errorState, setErrorState] = useState<string | null>(null);
+  const navigation = useNavigation();
+  const scrollRef = useRef<ScrollView>(null);
+
+  const props = {
+    expanded,
+    setExpanded,
+    client,
+    setClient,
+    scrollRef,
+    errorState,
+  };
+
+  const createClientProfile = async () => {
+    if (!client.user.firstName) {
+      setErrorState('First Name is required');
+      return;
+    }
+    setErrorState(null);
+    const input = {
+      ...client,
+    };
+
+    if (client.dateOfBirth) {
+      input.dateOfBirth = new Date(client.dateOfBirth);
+    }
+
+    try {
+      const { data } = await createClient({
+        variables: {
+          data: input,
+        },
+      });
+      if (
+        data?.createClientProfile?.__typename === 'OperationInfo' &&
+        data.createClientProfile.messages
+      ) {
+        throw new Error(
+          `Failed to create a client profile: ${data?.createClientProfile.messages[0].message}`
+        );
+      }
+      navigation.goBack();
+    } catch (err) {
+      throw new Error(`Failed to create a client profile: ${err}`);
+    }
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <MainScrollContainer ref={scrollRef} bg={Colors.NEUTRAL_EXTRA_LIGHT}>
+        <Name {...props} />
+        <Dob {...props} />
+        <Gender {...props} />
+        <Language {...props} />
+        <HMIS {...props} />
+        <ContactInfo {...props} />
+        <VeteranStatus {...props} />
+      </MainScrollContainer>
+      <BottomActions
+        submitTitle="Create"
+        cancel={
+          <TextButton
+            onPress={navigation.goBack}
+            fontSize="sm"
+            accessibilityHint="cancels the creation of a new client profile"
+            title="Cancel"
+          />
+        }
+        onSubmit={createClientProfile}
+      />
+    </View>
+  );
+}
--- libs/expo/betterangels/src/lib/screens/index.ts
+++ libs/expo/betterangels/src/lib/screens/index.ts
@@ -2,6 +2,8 @@ export { default as AddClient } from './AddClient';
 export { default as Client } from './Client';
 export { default as Clients } from './Clients';
 export { default as EditClient } from './EditClient';
+// export { default as EditClient2 } from './EditClient';
+export { default as ClientForm } from './ClientForm';
 export { default as Home } from './Home';
 export { default as Interactions } from './Interactions';
 export { default as Note } from './Note';
