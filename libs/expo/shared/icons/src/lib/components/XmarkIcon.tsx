a6bb3503c6ae3d0066a63a1e4dc6d2d7b844d499
Dave Lindqvist <lindqvist.dave@gmail.com>
2024-07-10 16:59:30 +0000

    Delete XmarkIcon

--- libs/expo/shared/icons/src/lib/components/XmarkIcon.tsx
+++
@@ -1,6 +0,0 @@
-import XmarkIconSVG from '../../assets/third_party/mingcute_design/svg/mingcute:close-fill.svg';
-import createSvgIcon from '../createSvgIcon';
-
-const XmarkIcon = createSvgIcon(XmarkIconSVG);
-
-export default XmarkIcon;
