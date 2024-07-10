c5dae43988ac57c8994c4bd89b41f7bcb9355ff7
Paul Vecchio <paul@betterangels.la>
2024-07-10 11:36:10 -0700

    Remove unused

--- apps/betterangels-backend/common/widgets.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class CheckboxSelectMultiple(forms.CheckboxSelectMultiple):
-    template_name = "admin/widgets/checkbox_select.html"
-
-
-class CustomManyToManyField(forms.ModelMultipleChoiceField):
-    widget = CheckboxSelectMultiple
