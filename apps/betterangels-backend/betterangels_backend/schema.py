d8820f4799cb354660c5e055b90824ec55be0dbf
Paul Vecchio <paul@betterangels.la>
2024-07-15 13:14:31 -0400

    Shetler db schema update without file upload (DEV-514) (#464)

--- apps/betterangels-backend/betterangels_backend/schema.py
+++ apps/betterangels-backend/betterangels_backend/schema.py
@@ -3,14 +3,13 @@ from accounts.schema import Query as AccountQuery
 from common.graphql.schema import Query as CommonQuery
 from notes.schema import Mutation as NoteMutation
 from notes.schema import Query as NoteQuery
-from shelters.schema import Query as ShelterQuery
 from strawberry import Schema
 from strawberry.tools import merge_types
 from strawberry_django.optimizer import DjangoOptimizerExtension
 
 # Schema Stiching
 # https://github.com/strawberry-graphql/strawberry/issues/566#issuecomment-1346660629
-queries = (AccountQuery, CommonQuery, NoteQuery, ShelterQuery)
+queries = (AccountQuery, CommonQuery, NoteQuery)
 Query = merge_types("Query", queries)
 
 mutations = (AccountMutation, NoteMutation)
