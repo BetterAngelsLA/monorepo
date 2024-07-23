dbbc27204cb97d4de92a01df1de15f24ac0d405c
Mike Feldberg <mike@betterangels.la>
2024-07-22 21:46:10 -0700

    update model

--- apps/betterangels-backend/accounts/models.py
+++ apps/betterangels-backend/accounts/models.py
@@ -1,7 +1,16 @@
 from typing import TYPE_CHECKING, Any, Dict, Iterable, Tuple
 
 import pghistory
-from accounts.enums import GenderEnum, LanguageEnum, YesNoPreferNotToSayEnum
+from accounts.enums import (
+    EyeColorEnum,
+    GenderEnum,
+    HairColorEnum,
+    LanguageEnum,
+    MaritalStatusEnum,
+    PronounEnum,
+    RaceEnum,
+    YesNoPreferNotToSayEnum,
+)
 from accounts.groups import GroupTemplateNames
 from accounts.managers import UserManager
 from django.contrib.auth.models import (
@@ -104,13 +113,20 @@ class User(AbstractBaseUser, PermissionsMixin):
 class ClientProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="client_profile")
     address = models.TextField(blank=True, null=True)
+    city_of_birth = models.CharField(max_length=100, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
+    eye_color = TextChoicesField(choices_enum=EyeColorEnum, blank=True, null=True)
     gender = TextChoicesField(choices_enum=GenderEnum, blank=True, null=True)
+    hair_color = TextChoicesField(choices_enum=HairColorEnum, blank=True, null=True)
+    height_in_inches = models.FloatField(blank=True, null=True)
     hmis_id = models.CharField(max_length=50, blank=True, null=True, db_index=True, unique=True)
+    marital_status = TextChoicesField(choices_enum=MaritalStatusEnum, blank=True, null=True)
     nickname = models.CharField(max_length=50, blank=True, null=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
+    physical_description = models.TextField(blank=True, null=True)
     preferred_language = TextChoicesField(choices_enum=LanguageEnum, blank=True, null=True)
-    pronouns = models.CharField(max_length=50, blank=True, null=True)
+    pronouns = TextChoicesField(choices_enum=PronounEnum, blank=True, null=True)
+    race = TextChoicesField(choices_enum=RaceEnum, blank=True, null=True)
     spoken_languages = ArrayField(base_field=TextChoicesField(choices_enum=LanguageEnum), blank=True, null=True)
     veteran_status = TextChoicesField(choices_enum=YesNoPreferNotToSayEnum, blank=True, null=True)
 
