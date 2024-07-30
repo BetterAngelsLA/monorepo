f09f28592e425c5ae3a35c28deb6d030119d6a11
Mike Feldberg <mike@betterangels.la>
2024-07-30 22:48:29 +0000

    Add support for multiple HMIS IDs (DEV-383) (#482)

--- apps/betterangels-backend/accounts/tests/test_mutations.py
+++ apps/betterangels-backend/accounts/tests/test_mutations.py
@@ -1,7 +1,12 @@
 from unittest.mock import ANY
 
-from accounts.enums import GenderEnum, LanguageEnum, YesNoPreferNotToSayEnum
-from accounts.models import ClientProfile, User
+from accounts.enums import (
+    GenderEnum,
+    HmisAgencyEnum,
+    LanguageEnum,
+    YesNoPreferNotToSayEnum,
+)
+from accounts.models import ClientProfile, HmisProfile, User
 from accounts.tests.utils import ClientProfileGraphQLBaseTestCase
 from django.test import TestCase, ignore_warnings
 from model_bakery import baker
@@ -74,12 +79,18 @@ class ClientProfileMutationTestCase(ClientProfileGraphQLBaseTestCase):
             "middleName": "Middly",
             "email": "firsty_lasty@example.com",
         }
+        client_profile_hmis_profile = {
+            "hmisId": "12345678",
+            "agency": HmisAgencyEnum.LAHSA.name,
+        }
+        expected_hmis_profile = {**client_profile_hmis_profile, "id": ANY}
 
         variables = {
             "address": "1234 Main St",
             "dateOfBirth": self.date_of_birth,
             "gender": GenderEnum.FEMALE.name,
             "hmisId": "12345678",
+            "hmisProfiles": [client_profile_hmis_profile],
             "nickname": "Fasty",
             "phoneNumber": "2125551212",
             "preferredLanguage": LanguageEnum.ENGLISH.name,
@@ -98,6 +109,7 @@ class ClientProfileMutationTestCase(ClientProfileGraphQLBaseTestCase):
             "dateOfBirth": self.date_of_birth.strftime("%Y-%m-%d"),
             "gender": GenderEnum.FEMALE.name,
             "hmisId": "12345678",
+            "hmisProfiles": [expected_hmis_profile],
             "nickname": "Fasty",
             "phoneNumber": "2125551212",
             "preferredLanguage": LanguageEnum.ENGLISH.name,
@@ -116,57 +128,62 @@ class ClientProfileMutationTestCase(ClientProfileGraphQLBaseTestCase):
         self.assertEqual(client, expected_client_profile)
 
     def test_update_client_profile_mutation(self) -> None:
+        client_profile_user = {
+            "id": self.client_profile_1["user"]["id"],
+            "firstName": "Firstey",
+            "lastName": "Lastey",
+            "middleName": "Middley",
+            "email": "firstey_lastey@example.com",
+        }
+        hmis_profile_to_update = {
+            "id": str(self.client_profile_1_hmis_profile_1.id),
+            "hmisId": "UPDATEDHMISID",
+            "agency": HmisAgencyEnum.SANTA_MONICA.name,
+        }
+        hmis_profile_to_create = {
+            "hmisId": "NEWHMISPROFILEID",
+            "agency": HmisAgencyEnum.VASH.name,
+        }
+        hmis_profiles = [
+            hmis_profile_to_update,
+            hmis_profile_to_create,
+        ]
+
         variables = {
             "id": self.client_profile_1["id"],
             "address": "1234 Main St",
             "dateOfBirth": self.date_of_birth,
             "gender": GenderEnum.FEMALE.name,
             "hmisId": "12345678",
+            "hmisProfiles": hmis_profiles,
             "nickname": "Fasty",
             "phoneNumber": "2125551212",
             "preferredLanguage": LanguageEnum.ENGLISH.name,
             "pronouns": "she/her",
             "spokenLanguages": [LanguageEnum.ENGLISH.name, LanguageEnum.SPANISH.name],
             "veteranStatus": YesNoPreferNotToSayEnum.YES.name,
-            "user": {
-                "id": self.client_profile_1["user"]["id"],
-                "firstName": "Firstey",
-                "lastName": "Lastey",
-                "middleName": "Middley",
-                "email": "firstey_lastey@example.com",
-            },
+            "user": client_profile_user,
         }
 
         response = self._update_client_profile_fixture(variables)
         client = response["data"]["updateClientProfile"]
-        expected_client_profile = {
-            "id": self.client_profile_1["id"],
-            "age": self.EXPECTED_CLIENT_AGE,
-            "address": "1234 Main St",
-            "dateOfBirth": self.date_of_birth.strftime("%Y-%m-%d"),
-            "gender": GenderEnum.FEMALE.name,
-            "hmisId": "12345678",
-            "nickname": "Fasty",
-            "phoneNumber": "2125551212",
-            "preferredLanguage": LanguageEnum.ENGLISH.name,
-            "pronouns": "she/her",
-            "spokenLanguages": [LanguageEnum.ENGLISH.name, LanguageEnum.SPANISH.name],
-            "veteranStatus": YesNoPreferNotToSayEnum.YES.name,
-            "user": {
-                "id": ANY,
-                "firstName": "Firstey",
-                "lastName": "Lastey",
-                "middleName": "Middley",
-                "email": "firstey_lastey@example.com",
-            },
-        }
 
-        self.assertEqual(client, expected_client_profile)
+        hmis_profile_to_create = {
+            **hmis_profile_to_create,
+            "id": ANY,
+        }
+        expected_client_profile = {
+            **variables,
+            "age": self.EXPECTED_CLIENT_AGE,
+            "dateOfBirth": self.date_of_birth.strftime("%Y-%m-%d"),
+        }
+        self.assertCountEqual(client, expected_client_profile)
 
     def test_delete_client_profile_mutation(self) -> None:
         client_profile_id = self.client_profile_1["id"]
         client_profile = ClientProfile.objects.get(id=client_profile_id)
         user = client_profile.user
+        hmis_profile_ids = client_profile.hmis_profiles.values_list("id", flat=True)
 
         mutation = """
             mutation DeleteClientProfile($id: ID!) {
@@ -186,7 +203,7 @@ class ClientProfileMutationTestCase(ClientProfileGraphQLBaseTestCase):
         """
         variables = {"id": client_profile_id}
 
-        expected_query_count = 31
+        expected_query_count = 33
         with self.assertNumQueriesWithoutCache(expected_query_count):
             response = self.execute_graphql(mutation, variables)
 
@@ -197,3 +214,5 @@ class ClientProfileMutationTestCase(ClientProfileGraphQLBaseTestCase):
 
         with self.assertRaises(User.DoesNotExist):
             User.objects.get(id=user.pk)
+
+        self.assertEqual(HmisProfile.objects.filter(id__in=hmis_profile_ids).count(), 0)
