090ca4e5c5691a9f30e43fc0afd2066780de3781
Paul Vecchio <paul@betterangels.la>
2024-08-06 19:33:24 -0400

    Remove common attachments from Shelter and update Shelter Group Permissions (#506)

--- apps/betterangels-backend/common/permissions/enums.py
+++ apps/betterangels-backend/common/permissions/enums.py
@@ -7,3 +7,10 @@ class AttachmentPermissions(models.TextChoices):
     CHANGE = "common.change_attachment", _("Can change attachment")
     DELETE = "common.delete_attachment", _("Can delete attachment")
     ADD = "common.add_attachment", _("Can add attachment")
+
+
+class AddressPermissions(models.TextChoices):
+    ADD = "common.add_address", _("Can add address")
+    CHANGE = "common.change_address", _("Can change address")
+    DELETE = "common.delete_address", _("Can delete address")
+    VIEW = "common.view_address", _("Can view address")
