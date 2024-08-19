b1c09eefe443655099e36c7a0cfc0a447e5b5643
Dave Lindqvist <lindqvist.dave@gmail.com>
2024-08-19 23:54:05 +0000

    Add ConsentModal

--- apps/betterangels/src/app/(tabs)/_layout.tsx
+++ apps/betterangels/src/app/(tabs)/_layout.tsx
@@ -1,7 +1,12 @@
 import { Redirect, Tabs, useRouter } from 'expo-router';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 
-import { MainPlusModal, hexToRGBA, useUser } from '@monorepo/expo/betterangels';
+import {
+  ConsentModal,
+  hexToRGBA,
+  MainPlusModal,
+  useUser,
+} from '@monorepo/expo/betterangels';
 import {
   CalendarLineIcon,
   CalendarSolidIcon,
@@ -17,16 +22,29 @@ import {
 } from '@monorepo/expo/shared/icons';
 import { Colors, FontSizes } from '@monorepo/expo/shared/static';
 import { Loading, TextRegular } from '@monorepo/expo/shared/ui-components';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { privacyPolicyUrl, termsOfServiceUrl } from '../../../config';
 
 export default function TabLayout() {
   const insets = useSafeAreaInsets();
   const [isModalVisible, setModalVisible] = useState(false);
+  const [tosModalIsOpen, setTosModalIsOpen] = useState<boolean>(false);
   const [validTos, setValidTos] = useState(false);
   const router = useRouter();
 
   const { user, isLoading } = useUser();
+
+  useEffect(() => {
+    // TODO: When PR #515 / DEV-673 is merged then delete `validTos` hook; https://github.com/BetterAngelsLA/monorepo/pull/515
+    // if (user && (!user.hasAcceptedTos || !user.hasAcceptedPrivacyPolicy)) {
+    //   setModalIsOpen(true)
+    // }
+
+    if (!validTos) {
+      setTosModalIsOpen(true);
+    }
+  }, [validTos]);
 
   const openModal = () => {
     setModalVisible(true);
@@ -50,15 +68,6 @@ export default function TabLayout() {
 
   if (user && (!user.organizations || user.organizations.length < 1)) {
     return <Redirect href="/welcome" />;
-  }
-
-  // TODO: When PR #515 / DEV-673 is merged then delete `validTos` hook; https://github.com/BetterAngelsLA/monorepo/pull/515
-  // if (user && (!user.hasAcceptedTos || !user.hasAcceptedPrivacyPolicy)) {
-  //   return <Redirect href="/terms-of-service" />;
-  // }
-
-  if (!validTos) {
-    return <Redirect href="/terms-of-service" />;
   }
 
   return (
@@ -263,6 +272,12 @@ export default function TabLayout() {
         />
       </Tabs>
       <MainPlusModal closeModal={closeModal} isModalVisible={isModalVisible} />
+      <ConsentModal
+        isModalVisible={tosModalIsOpen}
+        closeModal={() => setTosModalIsOpen(false)}
+        privacyPolicyUrl={privacyPolicyUrl}
+        termsOfServiceUrl={termsOfServiceUrl}
+      />
     </>
   );
 }
---
+++ libs/expo/betterangels/src/lib/ui-components/ConsentModal.tsx
@@ -0,0 +1,174 @@
+import { Colors, Radiuses, Spacings } from '@monorepo/expo/shared/static';
+import {
+  Button,
+  Checkbox,
+  TextRegular,
+} from '@monorepo/expo/shared/ui-components';
+import { Link } from 'expo-router';
+import { useState } from 'react';
+import { DimensionValue, Image, StyleSheet, View } from 'react-native';
+import Modal from 'react-native-modal';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useSignOut } from '../hooks';
+
+interface IMainModalProps {
+  isModalVisible: boolean;
+  closeModal: () => void;
+  opacity?: number;
+  vertical?: boolean;
+  ml?: number;
+  height?: DimensionValue;
+  privacyPolicyUrl: string;
+  termsOfServiceUrl: string;
+}
+
+export default function ConsentModal(props: IMainModalProps) {
+  const {
+    isModalVisible,
+    opacity = 0,
+    vertical = false,
+    ml = 0,
+    height = 'auto',
+    privacyPolicyUrl,
+    termsOfServiceUrl,
+    closeModal,
+  } = props;
+
+  const [checkedItems, setCheckedItems] = useState({
+    isTosChecked: false,
+    isPrivacyPolicyChecked: false,
+  });
+
+  const { signOut } = useSignOut();
+
+  const handleCheck = (key: 'isTosChecked' | 'isPrivacyPolicyChecked') => {
+    setCheckedItems((prevState) => ({
+      ...prevState,
+      [key]: !prevState[key],
+    }));
+  };
+  const insets = useSafeAreaInsets();
+  const bottomOffset = insets.bottom;
+  const topOffset = insets.top;
+
+  return (
+    <Modal
+      style={{
+        margin: 0,
+        marginLeft: ml,
+        flex: 1,
+        justifyContent: 'flex-end',
+      }}
+      animationIn={vertical ? 'slideInUp' : 'slideInRight'}
+      animationOut={vertical ? 'slideOutDown' : 'slideOutRight'}
+      backdropOpacity={opacity}
+      isVisible={isModalVisible}
+      useNativeDriverForBackdrop={true}
+    >
+      <View
+        style={{
+          borderTopLeftRadius: Radiuses.md,
+          borderTopRightRadius: Radiuses.md,
+          paddingTop: topOffset + Spacings.xs,
+          paddingHorizontal: Spacings.md,
+          paddingBottom: 175 + bottomOffset,
+          backgroundColor: Colors.WHITE,
+          height,
+        }}
+      >
+        <View style={styles.modalOverlay}>
+          <Image
+            source={require('../../../../shared/images/consent.png')}
+            accessibilityIgnoresInvertColors={true}
+          />
+          <TextRegular color={Colors.PRIMARY_EXTRA_DARK}>
+            Welcome to BetterAngels Outreach app!
+          </TextRegular>
+          <TextRegular color={Colors.PRIMARY_EXTRA_DARK}>
+            Please confirm the following:
+          </TextRegular>
+          <Checkbox
+            isChecked={checkedItems.isTosChecked}
+            hasBorder
+            onCheck={() => handleCheck('isTosChecked')}
+            accessibilityHint={'Accept the terms of service'}
+            label={
+              <View style={styles.labelContainer}>
+                <TextRegular ml="xs">I accept the </TextRegular>
+                <Link
+                  style={{ textDecorationLine: 'underline' }}
+                  href={termsOfServiceUrl}
+                  onPress={() => handleCheck('isTosChecked')}
+                >
+                  Terms of Service
+                </Link>
+              </View>
+            }
+          />
+          <Checkbox
+            isChecked={checkedItems.isPrivacyPolicyChecked}
+            hasBorder
+            onCheck={() => handleCheck('isPrivacyPolicyChecked')}
+            accessibilityHint={'Accept the privacy policy'}
+            label={
+              <View style={styles.labelContainer}>
+                <TextRegular ml="xs">I accept the </TextRegular>
+                <Link
+                  style={{ textDecorationLine: 'underline' }}
+                  href={privacyPolicyUrl}
+                  onPress={() => handleCheck('isPrivacyPolicyChecked')}
+                >
+                  Privacy Policy
+                </Link>
+              </View>
+            }
+          />
+          <View>
+            <Button
+              accessibilityHint="Submits agreement and goes to welcome screen"
+              onPress={closeModal}
+              disabled={
+                !checkedItems.isPrivacyPolicyChecked ||
+                !checkedItems.isTosChecked
+                  ? true
+                  : false
+              }
+              title="Get Started"
+              size="full"
+              variant="primaryDark"
+              borderWidth={0}
+            />
+            <Button
+              accessibilityHint="Cancels agreement"
+              onPress={signOut}
+              title="Cancel"
+              size="full"
+              variant="primary"
+              borderWidth={0}
+            />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  modalOverlay: {
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: Colors.WHITE,
+    position: 'relative',
+    width: '100%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  labelContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
