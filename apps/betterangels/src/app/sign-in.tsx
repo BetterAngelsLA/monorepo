3b3375f955feeaac1e4261af868a7dd14770ae24
Davit-BetterAngels <141072810+Davit-BetterAngels@users.noreply.github.com>
2024-08-19 15:33:17 -0700

    Loading for login form and error message to top level to show after render (#526)

--- apps/betterangels/src/app/sign-in.tsx
+++ apps/betterangels/src/app/sign-in.tsx
@@ -29,12 +29,21 @@ import {
 export default function SignIn() {
   const [isLoading, setIsLoading] = useState(false);
   const { user } = useUser();
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     if (user) {
       setIsLoading(false);
     }
   }, [user]);
+
+  if (isLoading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <Loading size="large" />
+      </View>
+    );
+  }
 
   return (
     <KeyboardAvoidingView
@@ -49,74 +58,65 @@ export default function SignIn() {
           paddingBottom: Spacings.md,
         }}
       >
-        {isLoading ? (
-          <View style={styles.loadingContainer}>
-            <Loading size="large" />
+        <TextBold mb="xs" size="xl">
+          Welcome!
+        </TextBold>
+        <TextRegular size="sm" mb="xl">
+          Log in for Better Angels and start making a difference in the LA
+          community.
+        </TextRegular>
+        {Platform.OS === 'ios' && <AppleSignIn />}
+        <GoogleSignIn
+          clientId={googleClientId}
+          redirectUri={redirectUri}
+          setIsLoading={setIsLoading}
+        />
+        <View style={styles.orContainer}>
+          <View
+            style={{
+              width: 50,
+              backgroundColor: Colors.WHITE,
+              position: 'relative',
+              zIndex: 10,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <TextRegular size="sm" style={styles.orText}>
+              OR
+            </TextRegular>
           </View>
-        ) : (
-          <>
-            <TextBold mb="xs" size="xl">
-              Welcome!
-            </TextBold>
-            <TextRegular size="sm" mb="xl">
-              Log in for Better Angels and start making a difference in the LA
-              community.
-            </TextRegular>
-            {Platform.OS === 'ios' && <AppleSignIn />}
-            <GoogleSignIn
-              clientId={googleClientId}
-              redirectUri={redirectUri}
-              setIsLoading={setIsLoading}
-            />
-            <View style={styles.orContainer}>
-              <View
-                style={{
-                  width: 50,
-                  backgroundColor: Colors.WHITE,
-                  position: 'relative',
-                  zIndex: 10,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}
-              >
-                <TextRegular size="sm" style={styles.orText}>
-                  OR
-                </TextRegular>
-              </View>
-              <View
-                style={{
-                  width: '100%',
-                  zIndex: 2,
-                  height: 1,
-                  backgroundColor: Colors.NEUTRAL_LIGHT,
-                  position: 'absolute',
-                }}
-              />
-            </View>
-            <LoginForm />
-            <TextRegular
-              textAlign="center"
-              size="sm"
-              color={Colors.BLACK}
-              mt="xl"
-            >
-              By continuing, you agree to our{' '}
-              <Link
-                style={{ textDecorationLine: 'underline' }}
-                href={termsOfServiceUrl}
-              >
-                Terms of Service
-              </Link>{' '}
-              and{' '}
-              <Link
-                style={{ textDecorationLine: 'underline' }}
-                href={privacyPolicyUrl}
-              >
-                Privacy Policy.
-              </Link>
-            </TextRegular>
-          </>
-        )}
+          <View
+            style={{
+              width: '100%',
+              zIndex: 2,
+              height: 1,
+              backgroundColor: Colors.NEUTRAL_LIGHT,
+              position: 'absolute',
+            }}
+          />
+        </View>
+        <LoginForm
+          errorMessage={errorMessage}
+          setErrorMessage={setErrorMessage}
+          setIsLoading={setIsLoading}
+        />
+        <TextRegular textAlign="center" size="sm" color={Colors.BLACK} mt="xl">
+          By continuing, you agree to our{' '}
+          <Link
+            style={{ textDecorationLine: 'underline' }}
+            href={termsOfServiceUrl}
+          >
+            Terms of Service
+          </Link>{' '}
+          and{' '}
+          <Link
+            style={{ textDecorationLine: 'underline' }}
+            href={privacyPolicyUrl}
+          >
+            Privacy Policy.
+          </Link>
+        </TextRegular>
       </ScrollView>
     </KeyboardAvoidingView>
   );
