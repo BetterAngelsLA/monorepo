b5f0e932d15d5150c1206660d5b7ee9ffc94f2ae
Davit-BetterAngels <davit@betterangels.la>
2024-08-19 21:38:16 +0000

    updated scroll view with keyboardawarescrollview

--- apps/betterangels/src/app/sign-in.tsx
+++ apps/betterangels/src/app/sign-in.tsx
@@ -11,13 +11,8 @@ import {
   TextRegular,
 } from '@monorepo/expo/shared/ui-components';
 import React, { useEffect, useState } from 'react';
-import {
-  KeyboardAvoidingView,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  View,
-} from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { googleClientId, redirectUri } from '../../config';
 
 export default function SignIn() {
@@ -31,75 +26,69 @@ export default function SignIn() {
   }, [user]);
 
   return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      behavior={Platform.OS === 'ios' ? 'position' : 'height'}
-      keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 0}
+    <KeyboardAwareScrollView
+      style={{ flex: 1, backgroundColor: Colors.WHITE }}
+      contentContainerStyle={{
+        paddingHorizontal: Spacings.sm,
+        paddingBottom: Spacings.md,
+      }}
+      enableOnAndroid={true}
+      keyboardOpeningTime={0}
     >
-      <ScrollView
-        contentContainerStyle={{
-          flexGrow: 1,
-          paddingHorizontal: Spacings.sm,
-          paddingBottom: Spacings.md,
-        }}
-      >
-        {isLoading ? (
-          <View style={styles.loadingContainer}>
-            <Loading size="large" />
-          </View>
-        ) : (
-          <>
-            <TextBold mb="xs" size="xl">
-              Welcome!
-            </TextBold>
-            <TextRegular size="sm" mb="xl">
-              Log in for Better Angels and start making a difference in the LA
-              community.
-            </TextRegular>
-            {Platform.OS === 'ios' && <AppleSignIn />}
-            <GoogleSignIn
-              clientId={googleClientId}
-              redirectUri={redirectUri}
-              setIsLoading={setIsLoading}
-            />
-            <View style={styles.orContainer}>
-              <View
-                style={{
-                  width: 50,
-                  backgroundColor: Colors.WHITE,
-                  position: 'relative',
-                  zIndex: 10,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}
-              >
-                <TextRegular size="sm" style={styles.orText}>
-                  OR
-                </TextRegular>
-              </View>
-              <View
-                style={{
-                  width: '100%',
-                  zIndex: 2,
-                  height: 1,
-                  backgroundColor: Colors.NEUTRAL_LIGHT,
-                  position: 'absolute',
-                }}
-              />
+      {isLoading ? (
+        <View style={styles.loadingContainer}>
+          <Loading size="large" />
+        </View>
+      ) : (
+        <>
+          <TextBold mb="xs" size="xl">
+            Welcome!
+          </TextBold>
+          <TextRegular size="sm" mb="xl">
+            Log in for Better Angels and start making a difference in the LA
+            community.
+          </TextRegular>
+          {Platform.OS === 'ios' && <AppleSignIn />}
+          <GoogleSignIn
+            clientId={googleClientId}
+            redirectUri={redirectUri}
+            setIsLoading={setIsLoading}
+          />
+          <View style={styles.orContainer}>
+            <View
+              style={{
+                width: 50,
+                backgroundColor: Colors.WHITE,
+                position: 'relative',
+                zIndex: 10,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <TextRegular size="sm" style={styles.orText}>
+                OR
+              </TextRegular>
             </View>
-            <LoginForm />
-          </>
-        )}
-      </ScrollView>
-    </KeyboardAvoidingView>
+            <View
+              style={{
+                width: '100%',
+                zIndex: 2,
+                height: 1,
+                backgroundColor: Colors.NEUTRAL_LIGHT,
+                position: 'absolute',
+              }}
+            />
+          </View>
+          <LoginForm />
+        </>
+      )}
+    </KeyboardAwareScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    backgroundColor: Colors.WHITE,
-    flexGrow: 1,
+    flex: 1,
   },
   loadingContainer: {
     flex: 1,
