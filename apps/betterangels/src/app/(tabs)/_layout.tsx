15e25d3f4fff0424db5ded3d9339995cad4fc39c
Mike Feldberg <mike@betterangels.la>
2024-08-20 22:41:44 +0000

    Delete profile tab (OUT-90), bump versionCode and buildNumber (#528)

--- apps/betterangels/src/app/(tabs)/_layout.tsx
+++ apps/betterangels/src/app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Redirect, Tabs, useRouter } from 'expo-router';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, View } from 'react-native';
 
 import {
   ConsentModal,
@@ -246,25 +246,6 @@ export default function TabLayout() {
                 )}
                 <TextRegular color={color} size="xs">
                   Calendar
-                </TextRegular>
-              </View>
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="profile"
-          options={{
-            title: 'Profile',
-            href: null,
-            tabBarIcon: ({ color }) => (
-              <View style={{ alignItems: 'center' }}>
-                <View style={styles.profileContainer}>
-                  <Text style={styles.profileText}>
-                    {user.username?.slice(0, 2)}
-                  </Text>
-                </View>
-                <TextRegular color={color} size="xs">
-                  Profile
                 </TextRegular>
               </View>
             ),
