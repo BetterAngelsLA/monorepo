682e1808168ee35fd38ebf04625d5a9f526af36b
Davit-BetterAngels <141072810+Davit-BetterAngels@users.noreply.github.com>
2024-09-16 15:58:38 -0700

    Client Profile Docs upload (DEV-89) (#519)
    * added file modal with each section
    
    * driver license section start
    
    * driver license FE ready
    
    * switch changed to default
    
    * updated camera icon
    
    * consent form
    
    * upload docs UI
    
    * circles before buttons
    
    * migration
    
    * file uploads with refetch logic and ui
    
    * removed unused variable
    
    * migrate
    
    * update set state for forms
    
    * add incomeform type
    
    * delete for multiple images
    
    * updated uploads under reusable components
    
    * updated to general camera component
    
    * updates from comments
    
    * library modal to MediaPickerModal
    
    * removed idUploadDoc
    
    * type fix
    
    * small fixes
    
    * opacity for modal
    
    * use one upload component
    
    * Revert "use one upload component"
    
    This reverts commit edb56dcea41618f4176f2437d0af4f43843a1763.
    
    * fix filename typo
    
    ---------
    
    Co-authored-by: Mike Feldberg <mike@betterangels.la>

---
+++ libs/expo/betterangels/src/lib/screens/Client/Docs/UploadModal/DriverLicense/index.tsx
@@ -0,0 +1,385 @@
+import { ReactNativeFile } from '@monorepo/expo/shared/apollo';
+import { PlusIcon, UploadIcon } from '@monorepo/expo/shared/icons';
+import { Colors, Radiuses, Spacings } from '@monorepo/expo/shared/static';
+import {
+  BasicInput,
+  IconButton,
+  MediaPickerModal,
+  TextBold,
+  TextRegular,
+} from '@monorepo/expo/shared/ui-components';
+import { Dispatch, SetStateAction, useState } from 'react';
+import { Image, Pressable, Switch, View } from 'react-native';
+import { ClientDocumentNamespaceEnum } from '../../../../../apollo';
+import {
+  ClientProfileDocument,
+  ClientProfileQuery,
+  useCreateClientDocumentMutation,
+} from '../../../__generated__/Client.generated';
+import Section from '../UploadSection';
+import { Docs, ITab } from '../types';
+
+export default function DriverLicense({
+  setTab,
+  client,
+  setDocs,
+  docs,
+}: {
+  setTab: (tab: ITab) => void;
+  client: ClientProfileQuery | undefined;
+  docs: Docs;
+  setDocs: Dispatch<SetStateAction<Docs>>;
+}) {
+  const [isCaLicense, setIsCaLicense] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [uploadingType, setUploadingType] = useState<'front' | 'back'>('front');
+  const [createDocument, { loading }] = useCreateClientDocumentMutation({
+    refetchQueries: [
+      {
+        query: ClientProfileDocument,
+        variables: {
+          id: client?.clientProfile.id,
+        },
+      },
+    ],
+  });
+
+  const uploadDocument = async () => {
+    if (!docs?.DriversLicenseFront || !docs.DriversLicenseBack || !client) {
+      return;
+    }
+    const fileToUploadFront = new ReactNativeFile({
+      uri: docs.DriversLicenseFront.uri,
+      type: docs.DriversLicenseFront.type,
+      name: docs.DriversLicenseFront.name,
+    });
+
+    const fileToUploadBack = new ReactNativeFile({
+      uri: docs.DriversLicenseBack.uri,
+      type: docs.DriversLicenseBack.type,
+      name: docs.DriversLicenseBack.name,
+    });
+
+    try {
+      await Promise.all([
+        createDocument({
+          variables: {
+            data: {
+              file: fileToUploadFront,
+              clientProfile: client.clientProfile.id,
+              namespace: ClientDocumentNamespaceEnum.DriversLicenseFront,
+            },
+          },
+        }),
+        createDocument({
+          variables: {
+            data: {
+              file: fileToUploadBack,
+              clientProfile: client.clientProfile.id,
+              namespace: ClientDocumentNamespaceEnum.DriversLicenseBack,
+            },
+          },
+        }),
+      ]);
+
+      setTab(undefined);
+    } catch (err) {
+      console.error('error uploading driver license', err);
+    }
+  };
+
+  const onDelete = (type: 'front' | 'back') => {
+    if (type === 'front') {
+      setDocs({
+        ...docs,
+        DriversLicenseFront: undefined,
+      });
+    } else {
+      setDocs({
+        ...docs,
+        DriversLicenseBack: undefined,
+      });
+    }
+  };
+
+  return (
+    <>
+      <Section
+        loading={loading}
+        title="Upload Driver's License"
+        subtitle="You need to upload front and back of the license."
+        onSubmit={uploadDocument}
+        onCancel={() => {
+          setDocs({
+            ...docs,
+            DriversLicenseFront: undefined,
+            DriversLicenseBack: undefined,
+          });
+          setTab(undefined);
+        }}
+      >
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            paddingTop: Spacings.md,
+            paddingBottom: Spacings.sm,
+            marginBottom: Spacings.sm,
+          }}
+        >
+          <TextRegular>Is this a CA Driver's License?</TextRegular>
+
+          <Switch
+            value={isCaLicense}
+            onChange={() => setIsCaLicense(!isCaLicense)}
+          />
+        </View>
+        <View
+          style={{
+            padding: Spacings.sm,
+            paddingBottom: Spacings.lg,
+            marginBottom: Spacings.sm,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: Spacings.sm,
+            borderBottomColor: Colors.NEUTRAL_LIGHT,
+            borderBottomWidth: 1,
+          }}
+        >
+          <Pressable
+            onPress={() => {
+              setUploadingType('front');
+              setIsModalVisible(true);
+            }}
+            accessibilityRole="button"
+            style={{ alignItems: 'center' }}
+          >
+            <TextRegular mb="xs" size="sm">
+              Front
+            </TextRegular>
+            <View
+              style={{
+                flexDirection: 'row',
+                gap: Spacings.xs,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderWidth: 1,
+                borderColor: Colors.NEUTRAL_LIGHT,
+                borderRadius: Radiuses.xs,
+                height: 66,
+                width: 139,
+              }}
+            >
+              <UploadIcon size="lg" />
+              <TextBold size="sm">Upload</TextBold>
+            </View>
+          </Pressable>
+          <Pressable
+            onPress={() => {
+              setUploadingType('back');
+              setIsModalVisible(true);
+            }}
+            accessibilityRole="button"
+            style={{ alignItems: 'center' }}
+          >
+            <TextRegular mb="xs" size="sm">
+              Back
+            </TextRegular>
+            <View
+              style={{
+                flexDirection: 'row',
+                gap: Spacings.xs,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderWidth: 1,
+                borderColor: Colors.NEUTRAL_LIGHT,
+                borderRadius: Radiuses.xs,
+                height: 66,
+                width: 139,
+              }}
+            >
+              <UploadIcon size="lg" />
+              <TextBold size="sm">Upload</TextBold>
+            </View>
+          </Pressable>
+        </View>
+        {(docs?.DriversLicenseFront || docs?.DriversLicenseBack) && (
+          <View style={{ paddingTop: Spacings.sm }}>
+            <TextBold mb="sm" size="md">
+              Uploaded Image
+            </TextBold>
+            {docs.DriversLicenseFront && (
+              <View style={{ marginBottom: Spacings.md, position: 'relative' }}>
+                <TextBold size="sm">Front</TextBold>
+                <View
+                  style={{
+                    position: 'relative',
+                    height: 86.5,
+                    width: 129,
+                    marginBottom: Spacings.sm,
+                  }}
+                >
+                  <IconButton
+                    borderColor="transparent"
+                    borderRadius={Radiuses.xxxl}
+                    onPress={() => onDelete('front')}
+                    style={{
+                      position: 'absolute',
+                      top: 5,
+                      right: 5,
+                      zIndex: 1000,
+                    }}
+                    variant="secondary"
+                    height="xs"
+                    width="xs"
+                    accessibilityLabel="delete"
+                    accessibilityHint="deletes the image"
+                  >
+                    <PlusIcon size="sm" rotate="45deg" />
+                  </IconButton>
+                  <Image
+                    style={{
+                      height: 86.5,
+                      width: 129,
+                      borderRadius: Radiuses.xs,
+                    }}
+                    source={{ uri: docs.DriversLicenseFront.uri }}
+                    resizeMode="cover"
+                    accessibilityIgnoresInvertColors
+                  />
+                </View>
+                <BasicInput
+                  label="File Name"
+                  value={docs.DriversLicenseFront.name}
+                  onDelete={() =>
+                    setDocs({
+                      ...docs,
+                      DriversLicenseFront: {
+                        ...docs.DriversLicenseFront,
+                        name: '',
+                        uri: docs.DriversLicenseFront?.uri || '',
+                        type: docs.DriversLicenseFront?.type || '',
+                      },
+                    })
+                  }
+                  onChangeText={(e) =>
+                    setDocs({
+                      ...docs,
+                      DriversLicenseFront: {
+                        ...docs.DriversLicenseFront,
+                        name: e,
+                        uri: docs.DriversLicenseFront?.uri || '',
+                        type: docs.DriversLicenseFront?.type || '',
+                      },
+                    })
+                  }
+                />
+              </View>
+            )}
+            {docs.DriversLicenseBack && (
+              <View>
+                <TextBold size="sm">Back</TextBold>
+                <View
+                  style={{
+                    position: 'relative',
+                    height: 86.5,
+                    width: 129,
+                    marginBottom: Spacings.sm,
+                  }}
+                >
+                  <IconButton
+                    borderColor="transparent"
+                    borderRadius={Radiuses.xxxl}
+                    onPress={() => onDelete('back')}
+                    style={{
+                      position: 'absolute',
+                      top: 5,
+                      right: 5,
+                      zIndex: 1000,
+                    }}
+                    variant="secondary"
+                    height="xs"
+                    width="xs"
+                    accessibilityLabel="delete"
+                    accessibilityHint="deletes the image"
+                  >
+                    <PlusIcon size="sm" rotate="45deg" />
+                  </IconButton>
+                  <Image
+                    style={{
+                      height: 86.5,
+                      width: 129,
+                      borderRadius: Radiuses.xs,
+                    }}
+                    source={{ uri: docs.DriversLicenseBack.uri }}
+                    resizeMode="cover"
+                    accessibilityIgnoresInvertColors
+                  />
+                </View>
+                <BasicInput
+                  label="File Name"
+                  value={docs.DriversLicenseBack.name}
+                  onDelete={() =>
+                    setDocs({
+                      ...docs,
+                      DriversLicenseBack: {
+                        ...docs.DriversLicenseBack,
+                        name: '',
+                        uri: docs.DriversLicenseBack?.uri || '',
+                        type: docs.DriversLicenseBack?.type || '',
+                      },
+                    })
+                  }
+                  onChangeText={(e) =>
+                    setDocs({
+                      ...docs,
+                      DriversLicenseBack: {
+                        ...docs.DriversLicenseBack,
+                        name: e,
+                        uri: docs.DriversLicenseBack?.uri || '',
+                        type: docs.DriversLicenseBack?.type || '',
+                      },
+                    })
+                  }
+                />
+              </View>
+            )}
+          </View>
+        )}
+      </Section>
+      <MediaPickerModal
+        allowMultiple={false}
+        onCapture={(file) => {
+          if (uploadingType === 'front') {
+            setDocs({
+              ...docs,
+              DriversLicenseFront: file,
+            });
+          } else {
+            setDocs({
+              ...docs,
+              DriversLicenseBack: file,
+            });
+          }
+        }}
+        setModalVisible={setIsModalVisible}
+        isModalVisible={isModalVisible}
+        setFiles={(files) => {
+          if (uploadingType === 'front') {
+            setDocs({
+              ...docs,
+              DriversLicenseFront: files[0],
+            });
+          } else {
+            setDocs({
+              ...docs,
+              DriversLicenseBack: files[0],
+            });
+          }
+        }}
+      />
+    </>
+  );
+}
