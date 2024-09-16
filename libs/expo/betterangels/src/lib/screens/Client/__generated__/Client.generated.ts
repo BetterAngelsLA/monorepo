d58fc2039bd6b5db9600a6c50fb708f7f5ee6bd4
Davit-BetterAngels <davit@betterangels.la>
2024-09-16 23:35:35 +0000

    generate types

--- libs/expo/betterangels/src/lib/screens/Client/__generated__/Client.generated.ts
+++ libs/expo/betterangels/src/lib/screens/Client/__generated__/Client.generated.ts
@@ -1,77 +1,88 @@
 import * as Types from '../../../apollo/graphql/__generated__/types';
 
-import * as Apollo from '@apollo/client';
 import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
 const defaultOptions = {} as const;
 export type ClientProfileQueryVariables = Types.Exact<{
   id: Types.Scalars['ID']['input'];
 }>;
 
+
+export type ClientProfileQuery = { __typename?: 'Query', clientProfile: { __typename?: 'ClientProfileType', id: string, address?: string | null, dateOfBirth?: any | null, gender?: Types.GenderEnum | null, age?: number | null, hmisId?: string | null, nickname?: string | null, phoneNumber?: any | null, preferredLanguage?: Types.LanguageEnum | null, pronouns?: Types.PronounEnum | null, veteranStatus?: Types.YesNoPreferNotToSayEnum | null, livingSituation?: Types.LivingSituationEnum | null, hmisProfiles?: Array<{ __typename?: 'HmisProfileType', agency: Types.HmisAgencyEnum, hmisId: string, id: string }> | null, user: { __typename?: 'UserType', id: string, email?: string | null, firstName?: string | null, middleName?: string | null, lastName?: string | null }, contacts?: Array<{ __typename?: 'ClientContactType', id: string, email?: string | null, mailingAddress?: string | null, name?: string | null, phoneNumber?: any | null, relationshipToClient?: Types.RelationshipTypeEnum | null, relationshipToClientOther?: string | null }> | null, docReadyDocuments?: Array<{ __typename?: 'ClientDocumentType', id: string, namespace: Types.ClientDocumentNamespaceEnum, file: { __typename?: 'DjangoFileType', url: string, name: string } }> | null, consentFormDocuments?: Array<{ __typename?: 'ClientDocumentType', id: string, namespace: Types.ClientDocumentNamespaceEnum, file: { __typename?: 'DjangoFileType', url: string, name: string } }> | null, otherDocuments?: Array<{ __typename?: 'ClientDocumentType', id: string, namespace: Types.ClientDocumentNamespaceEnum, file: { __typename?: 'DjangoFileType', url: string, name: string } }> | null } };
+
+export type CreateClientDocumentMutationVariables = Types.Exact<{
+  data: Types.CreateClientDocumentInput;
+}>;
+
+
+export type CreateClientDocumentMutation = { __typename?: 'Mutation', createClientDocument: { __typename?: 'ClientDocumentType', id: string } | { __typename?: 'OperationInfo', messages: Array<{ __typename?: 'OperationMessage', kind: Types.OperationMessageKind, field?: string | null, message: string }> } };
+
+
 export const ClientProfileDocument = gql`
-  query ClientProfile($id: ID!) {
-    clientProfile(pk: $id) {
-      ... on ClientProfileType {
-        id
-        address
-        dateOfBirth
-        gender
-        age
+    query ClientProfile($id: ID!) {
+  clientProfile(pk: $id) {
+    ... on ClientProfileType {
+      id
+      address
+      dateOfBirth
+      gender
+      age
+      hmisId
+      hmisProfiles {
+        agency
         hmisId
-        hmisProfiles {
-          agency
-          hmisId
-          id
-        }
-        nickname
+        id
+      }
+      nickname
+      phoneNumber
+      preferredLanguage
+      pronouns
+      veteranStatus
+      livingSituation
+      user {
+        id
+        email
+        firstName
+        middleName
+        lastName
+      }
+      contacts {
+        id
+        email
+        mailingAddress
+        name
         phoneNumber
-        preferredLanguage
-        pronouns
-        veteranStatus
-        livingSituation
-        user {
-          id
-          email
-          firstName
-          middleName
-          lastName
-        }
-        contacts {
-          id
-          email
-          mailingAddress
+        relationshipToClient
+        relationshipToClientOther
+      }
+      docReadyDocuments {
+        id
+        namespace
+        file {
+          url
           name
-          phoneNumber
-          relationshipToClient
-          relationshipToClientOther
         }
-        docReadyDocuments {
-          id
-          namespace
-          file {
-            url
-            name
-          }
+      }
+      consentFormDocuments {
+        id
+        namespace
+        file {
+          url
+          name
         }
-        consentFormDocuments {
-          id
-          namespace
-          file {
-            url
-            name
-          }
-        }
-        otherDocuments {
-          id
-          namespace
-          file {
-            url
-            name
-          }
+      }
+      otherDocuments {
+        id
+        namespace
+        file {
+          url
+          name
         }
       }
     }
   }
-`;
+}
+    `;
 
 /**
  * __useClientProfileQuery__
@@ -89,79 +100,39 @@ export const ClientProfileDocument = gql`
  *   },
  * });
  */
-export function useClientProfileQuery(
-  baseOptions: Apollo.QueryHookOptions<
-    ClientProfileQuery,
-    ClientProfileQueryVariables
-  > &
-    (
-      | { variables: ClientProfileQueryVariables; skip?: boolean }
-      | { skip: boolean }
-    )
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useQuery<ClientProfileQuery, ClientProfileQueryVariables>(
-    ClientProfileDocument,
-    options
-  );
-}
-export function useClientProfileLazyQuery(
-  baseOptions?: Apollo.LazyQueryHookOptions<
-    ClientProfileQuery,
-    ClientProfileQueryVariables
-  >
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useLazyQuery<ClientProfileQuery, ClientProfileQueryVariables>(
-    ClientProfileDocument,
-    options
-  );
-}
-export function useClientProfileSuspenseQuery(
-  baseOptions?: Apollo.SuspenseQueryHookOptions<
-    ClientProfileQuery,
-    ClientProfileQueryVariables
-  >
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useSuspenseQuery<
-    ClientProfileQuery,
-    ClientProfileQueryVariables
-  >(ClientProfileDocument, options);
-}
-export type ClientProfileQueryHookResult = ReturnType<
-  typeof useClientProfileQuery
->;
-export type ClientProfileLazyQueryHookResult = ReturnType<
-  typeof useClientProfileLazyQuery
->;
-export type ClientProfileSuspenseQueryHookResult = ReturnType<
-  typeof useClientProfileSuspenseQuery
->;
-export type ClientProfileQueryResult = Apollo.QueryResult<
-  ClientProfileQuery,
-  ClientProfileQueryVariables
->;
-export const CreateClientDocumentDocument = gql`
-  mutation CreateClientDocument($data: CreateClientDocumentInput!) {
-    createClientDocument(data: $data) {
-      ... on OperationInfo {
-        messages {
-          kind
-          field
-          message
-        }
+export function useClientProfileQuery(baseOptions: Apollo.QueryHookOptions<ClientProfileQuery, ClientProfileQueryVariables> & ({ variables: ClientProfileQueryVariables; skip?: boolean; } | { skip: boolean; }) ) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<ClientProfileQuery, ClientProfileQueryVariables>(ClientProfileDocument, options);
       }
-      ... on ClientDocumentType {
-        id
+export function useClientProfileLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<ClientProfileQuery, ClientProfileQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<ClientProfileQuery, ClientProfileQueryVariables>(ClientProfileDocument, options);
+        }
+export function useClientProfileSuspenseQuery(baseOptions?: Apollo.SuspenseQueryHookOptions<ClientProfileQuery, ClientProfileQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<ClientProfileQuery, ClientProfileQueryVariables>(ClientProfileDocument, options);
+        }
+export type ClientProfileQueryHookResult = ReturnType<typeof useClientProfileQuery>;
+export type ClientProfileLazyQueryHookResult = ReturnType<typeof useClientProfileLazyQuery>;
+export type ClientProfileSuspenseQueryHookResult = ReturnType<typeof useClientProfileSuspenseQuery>;
+export type ClientProfileQueryResult = Apollo.QueryResult<ClientProfileQuery, ClientProfileQueryVariables>;
+export const CreateClientDocumentDocument = gql`
+    mutation CreateClientDocument($data: CreateClientDocumentInput!) {
+  createClientDocument(data: $data) {
+    ... on OperationInfo {
+      messages {
+        kind
+        field
+        message
       }
     }
+    ... on ClientDocumentType {
+      id
+    }
   }
-`;
-export type CreateClientDocumentMutationFn = Apollo.MutationFunction<
-  CreateClientDocumentMutation,
-  CreateClientDocumentMutationVariables
->;
+}
+    `;
+export type CreateClientDocumentMutationFn = Apollo.MutationFunction<CreateClientDocumentMutation, CreateClientDocumentMutationVariables>;
 
 /**
  * __useCreateClientDocumentMutation__
@@ -180,24 +151,10 @@ export type CreateClientDocumentMutationFn = Apollo.MutationFunction<
  *   },
  * });
  */
-export function useCreateClientDocumentMutation(
-  baseOptions?: Apollo.MutationHookOptions<
-    CreateClientDocumentMutation,
-    CreateClientDocumentMutationVariables
-  >
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useMutation<
-    CreateClientDocumentMutation,
-    CreateClientDocumentMutationVariables
-  >(CreateClientDocumentDocument, options);
-}
-export type CreateClientDocumentMutationHookResult = ReturnType<
-  typeof useCreateClientDocumentMutation
->;
-export type CreateClientDocumentMutationResult =
-  Apollo.MutationResult<CreateClientDocumentMutation>;
-export type CreateClientDocumentMutationOptions = Apollo.BaseMutationOptions<
-  CreateClientDocumentMutation,
-  CreateClientDocumentMutationVariables
->;
+export function useCreateClientDocumentMutation(baseOptions?: Apollo.MutationHookOptions<CreateClientDocumentMutation, CreateClientDocumentMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateClientDocumentMutation, CreateClientDocumentMutationVariables>(CreateClientDocumentDocument, options);
+      }
+export type CreateClientDocumentMutationHookResult = ReturnType<typeof useCreateClientDocumentMutation>;
+export type CreateClientDocumentMutationResult = Apollo.MutationResult<CreateClientDocumentMutation>;
+export type CreateClientDocumentMutationOptions = Apollo.BaseMutationOptions<CreateClientDocumentMutation, CreateClientDocumentMutationVariables>;
