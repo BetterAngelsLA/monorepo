718f8b95b368864b29aa46a913679ccb4c679f47
Davit-BetterAngels <141072810+Davit-BetterAngels@users.noreply.github.com>
2024-09-16 18:45:41 -0700

    create and edit client profile relevant contacts (DEV-169) (#513)
    * enum display for relative
    
    * relative section in create and edit
    
    * removed console
    
    * updated input types
    
    * updated ui for releveant contacts
    
    * removed console
    
    * updated spacings
    
    * migrate and generate types
    
    * updated relevant contancts
    
    * updated types
    
    * fixed spelling
    
    * sorting so current case manager will always be at top
    
    * reset fields
    
    * color and px fixes
    
    * updated via comments
    
    * checking to show relationshipToClientOther

--- libs/expo/shared/ui-components/src/lib/index.ts
+++ libs/expo/shared/ui-components/src/lib/index.ts
@@ -1,3 +1,4 @@
+export { default as Accordion } from './Accordion';
 export { default as Alert } from './Alert';
 export { default as Avatar } from './Avatar';
 export { default as BasicInput } from './BasicInput';
