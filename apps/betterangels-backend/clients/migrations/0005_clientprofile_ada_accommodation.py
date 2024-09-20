e7471da70d5a76cc755740b860d6c1e654f99c01
Mike Feldberg <mike@betterangels.la>
2024-09-19 18:20:26 -0700

    resolve migration conflict... again

--- apps/betterangels-backend/clients/migrations/0005_clientprofile_ada_accommodation.py
+++ apps/betterangels-backend/clients/migrations/0005_clientprofile_ada_accommodation.py
@@ -9,7 +9,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("clients", "0003_clientprofile_gender_other"),
+        ("clients", "0004_clienthouseholdmember_gender_other"),
     ]
 
     operations = [
