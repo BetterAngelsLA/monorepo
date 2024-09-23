e50eda3c007eb6b7191258e03c626f0dc63fc595
Mike Feldberg <mike@betterangels.la>
2024-09-23 15:50:04 +0000

    Add client ada accommodation backend (DEV-714) (#556)

--- apps/betterangels-backend/clients/types.py
+++ apps/betterangels-backend/clients/types.py
@@ -6,7 +6,12 @@ from typing import List, Optional, Tuple, Union
 import strawberry
 import strawberry_django
 from accounts.types import CreateUserInput, UpdateUserInput, UserType
-from clients.enums import ClientDocumentNamespaceEnum, LanguageEnum, LivingSituationEnum
+from clients.enums import (
+    AdaAccommodationEnum,
+    ClientDocumentNamespaceEnum,
+    LanguageEnum,
+    LivingSituationEnum,
+)
 from common.graphql.types import AttachmentInterface
 from common.models import Attachment
 from django.db.models import Max, Q, QuerySet
@@ -140,6 +145,7 @@ PhoneNumberScalar: Union[PhoneNumber, str] = strawberry.scalar(
 
 @strawberry_django.type(ClientProfile)
 class ClientProfileBaseType:
+    ada_accommodation: Optional[List[AdaAccommodationEnum]]
     address: auto
     age: auto
     date_of_birth: auto
