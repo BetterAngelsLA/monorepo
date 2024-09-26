3ca4bc3e791b0dd9351fba8198919e8e16cf7c38
Mike Feldberg <mike@betterangels.la>
2024-09-26 13:58:17 -0700

    add UserIcon

---
+++ libs/expo/shared/icons/src/lib/components/UserIcon.tsx
@@ -0,0 +1,6 @@
+import UserIconSVG from '../../assets/third_party/pictogrammers/svg/mdi:user.svg';
+import createSvgIcon from '../createSvgIcon';
+
+const UserIcon = createSvgIcon(UserIconSVG);
+
+export default UserIcon;
