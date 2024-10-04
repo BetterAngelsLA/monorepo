283b4ffef05982af4e88984fe31ea55e060d8571
Mike Feldberg <mike@betterangels.la>
2024-10-04 14:08:13 -0400

    Add clientprofile important_notes (DEV-757) (#592)

--- apps/betterangels-backend/clients/models.py
+++ apps/betterangels-backend/clients/models.py
@@ -95,6 +95,7 @@ class ClientProfile(models.Model):
     hair_color = TextChoicesField(choices_enum=HairColorEnum, blank=True, null=True)
     height_in_inches = models.FloatField(blank=True, null=True)
     hmis_id = models.CharField(max_length=50, blank=True, null=True, db_index=True, unique=True)
+    important_notes = models.TextField(blank=True, null=True)
     living_situation = TextChoicesField(choices_enum=LivingSituationEnum, blank=True, null=True)
     mailing_address = models.TextField(blank=True, null=True)
     marital_status = TextChoicesField(choices_enum=MaritalStatusEnum, blank=True, null=True)
