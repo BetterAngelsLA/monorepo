9e7d9f64a0357e94549b9ef5ca8a7acb0343669e
Davit-BetterAngels <141072810+Davit-BetterAngels@users.noreply.github.com>
2024-10-16 09:30:58 -0700

    Client Profile Contact Info (DEV-168) (#601)
    * note working phase
    
    * solved social medias
    
    * phone numbers
    
    * summary contact info
    
    * fixed Toms comments
    
    * created Icons, fixed all comments,2 icons missing
    
    * updated social media
    
    * updated default social
    
    * removed console
    
    * removed whatsapp

---
+++ libs/expo/betterangels/src/lib/ui-components/PreferredCommunicationPicker.tsx
@@ -0,0 +1,108 @@
+import {
+  CallIcon,
+  ChatBubbleIcon,
+  EmailIcon,
+  FacebookIcon,
+  InstagramIcon,
+  LinkedinIcon,
+  WhatsappIcon,
+} from '@monorepo/expo/shared/icons';
+import { Colors, Spacings } from '@monorepo/expo/shared/static';
+import {
+  CardWrapper,
+  Checkbox,
+  TextButton,
+  TextRegular,
+} from '@monorepo/expo/shared/ui-components';
+import { ReactNode } from 'react';
+import { View } from 'react-native';
+import { PreferredCommunicationEnum } from '../apollo';
+import { enumDisplayPreferredCommunication } from '../static/enumDisplayMapping';
+
+interface IPreferredCommunicationPickerProps {
+  onPress: (e: PreferredCommunicationEnum) => void;
+  withCard?: boolean;
+  cardTitle?: boolean;
+  title?: boolean;
+  onReset?: () => void;
+  selected: PreferredCommunicationEnum[];
+}
+
+const icons = {
+  [PreferredCommunicationEnum.Call]: (
+    <CallIcon color={Colors.PRIMARY_EXTRA_DARK} size="md" />
+  ),
+  [PreferredCommunicationEnum.Email]: (
+    <EmailIcon color={Colors.PRIMARY_EXTRA_DARK} size="md" />
+  ),
+  [PreferredCommunicationEnum.Facebook]: (
+    <FacebookIcon color={Colors.PRIMARY_EXTRA_DARK} size="md" />
+  ),
+  [PreferredCommunicationEnum.Instagram]: (
+    <InstagramIcon color={Colors.PRIMARY_EXTRA_DARK} size="md" />
+  ),
+  [PreferredCommunicationEnum.Linkedin]: (
+    <LinkedinIcon color={Colors.PRIMARY_EXTRA_DARK} size="md" />
+  ),
+  [PreferredCommunicationEnum.Text]: (
+    <ChatBubbleIcon color={Colors.PRIMARY_EXTRA_DARK} size="md" />
+  ),
+  [PreferredCommunicationEnum.Whatsapp]: (
+    <WhatsappIcon color={Colors.PRIMARY_EXTRA_DARK} size="md" />
+  ),
+};
+
+export default function PreferredCommunicationPicker(
+  props: IPreferredCommunicationPickerProps
+) {
+  const { onPress, withCard, cardTitle, title, onReset, selected } = props;
+
+  const Wrapper = ({ children }: { children: ReactNode }) => {
+    if (withCard) {
+      return (
+        <CardWrapper title={cardTitle ? 'Preferred Communication' : ''}>
+          {children}
+        </CardWrapper>
+      );
+    }
+    return children;
+  };
+
+  return (
+    <Wrapper>
+      <View style={{ gap: Spacings.xs }}>
+        {title && <TextRegular size="sm">PreferredCommunications</TextRegular>}
+        {Object.entries(enumDisplayPreferredCommunication).map(
+          ([enumValue, displayValue]) => (
+            <Checkbox
+              key={enumValue}
+              isChecked={selected.includes(
+                enumValue as PreferredCommunicationEnum
+              )}
+              hasBorder
+              onCheck={() => onPress(enumValue as PreferredCommunicationEnum)}
+              accessibilityHint={`Select ${displayValue}`}
+              label={
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  {icons[enumValue as PreferredCommunicationEnum]}
+                  <TextRegular ml="xs">{displayValue}</TextRegular>
+                </View>
+              }
+            />
+          )
+        )}
+        {onReset && (
+          <View style={{ alignItems: 'flex-end' }}>
+            <TextButton
+              mt="sm"
+              color={Colors.PRIMARY}
+              title="Reset"
+              accessibilityHint="resets Preferred Communication"
+              onPress={onReset}
+            />
+          </View>
+        )}
+      </View>
+    </Wrapper>
+  );
+}
---
+++ libs/expo/shared/icons/src/lib/components/FacebookIcon.tsx
@@ -0,0 +1,6 @@
+import FacebookIconSVG from '../../assets/third_party/material_design_authors/svg/ic:sharp-facebook.svg';
+import createSvgIcon from '../createSvgIcon';
+
+const FacebookIcon = createSvgIcon(FacebookIconSVG);
+
+export default FacebookIcon;
