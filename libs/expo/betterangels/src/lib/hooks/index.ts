46eb6adfc456ead8ab0e6ecfb238550986d3aa07
Tom Glaz <tgmessages@gmail.com>
2024-10-17 10:45:48 -0700

    DEV-205 add snackbar component (#602)
    * add snackbar component
    
    * update snackbar interface

--- libs/expo/betterangels/src/lib/hooks/index.ts
+++ libs/expo/betterangels/src/lib/hooks/index.ts
@@ -1,4 +1,4 @@
-// user
+export { default as useSnackbar } from './snackbar/useSnackbar';
 export { default as useSignIn } from './user/useSignIn';
 export { default as useSignOut } from './user/useSignOut';
 export { default as useUser } from './user/useUser';
