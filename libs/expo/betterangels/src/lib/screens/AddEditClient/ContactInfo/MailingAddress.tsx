44bcb44883851ba803e3606eb8fad2ab051abefe
Tom Glaz <tgmessages@gmail.com>
2024-10-26 11:40:28 -0700

    use AddressField with MailingAddress

--- libs/expo/betterangels/src/lib/screens/AddEditClient/ContactInfo/MailingAddress.tsx
+++ libs/expo/betterangels/src/lib/screens/AddEditClient/ContactInfo/MailingAddress.tsx
@@ -1,100 +1,36 @@
-import { useApiConfig } from '@monorepo/expo/shared/clients';
-import { Colors, Spacings } from '@monorepo/expo/shared/static';
-import {
-  BasicInput,
-  CardWrapper,
-  TextRegular,
-} from '@monorepo/expo/shared/ui-components';
-import { debounce } from '@monorepo/expo/shared/utils';
-import { useCallback, useState } from 'react';
-import { useFormContext } from 'react-hook-form';
-import { TouchableOpacity, View } from 'react-native';
+import { CardWrapper } from '@monorepo/expo/shared/ui-components';
+import { useFormContext, useWatch } from 'react-hook-form';
 import {
   CreateClientProfileInput,
   UpdateClientProfileInput,
 } from '../../../apollo';
-import {
-  searchPlacesInCalifornia,
-  selectAutocompletePlace,
-} from '../../../helpers';
+import { AddressField } from '../../../ui-components';
+
+const FIELD_KEY = 'mailingAddress';
 
 export default function MailingAddress() {
-  const { baseUrl } = useApiConfig();
-  const [suggestions, setSuggestions] = useState<any>([]);
-  const { setValue, watch } = useFormContext<
+  const { setValue } = useFormContext<
     UpdateClientProfileInput | CreateClientProfileInput
   >();
 
-  const mailingAddress = watch('mailingAddress') || '';
+  const value = useWatch({ name: FIELD_KEY }) || '';
 
   const onReset = () => {
-    setValue('mailingAddress', '');
+    setValue(FIELD_KEY, '');
   };
 
-  const debouncedSearch = useCallback(
-    debounce(async (query: string) => {
-      await searchPlacesInCalifornia(baseUrl, query, setSuggestions);
-    }, 300),
-    []
-  );
-
-  const onChangeText = (e: string) => {
-    setValue('mailingAddress', e);
-    if (e.trim() === '' || e.length < 3) {
-      setSuggestions([]);
-      debouncedSearch.cancel();
-      return;
-    }
-    debouncedSearch(e);
+  const onChangeText = (input: string) => {
+    setValue(FIELD_KEY, input);
   };
 
   return (
     <CardWrapper onReset={onReset} title="Personal Mailing Address">
-      <BasicInput
-        onDelete={() => {
-          setValue('mailingAddress', null);
-          setSuggestions([]);
-        }}
-        value={mailingAddress}
-        onChangeText={onChangeText}
-        placeholder="Enter mailing address"
-        autoCorrect={false}
+      <AddressField
+        value={value}
+        placeholder="Enter residence address"
+        onChange={onChangeText}
+        onReset={onReset}
       />
-      {suggestions.length > 0 && (
-        <View
-          style={{
-            backgroundColor: Colors.WHITE,
-            flex: 1,
-          }}
-        >
-          {suggestions.map((item: any) => (
-            <TouchableOpacity
-              key={item.place_id}
-              style={{
-                borderBottomWidth: 1,
-                borderBottomColor: Colors.NEUTRAL_LIGHT,
-                paddingVertical: Spacings.sm,
-              }}
-              accessibilityRole="button"
-              onPress={() =>
-                selectAutocompletePlace(
-                  baseUrl,
-                  item,
-                  'mailingAddress',
-                  setValue,
-                  setSuggestions
-                )
-              }
-            >
-              <TextRegular>{item.description.split(', ')[0]}</TextRegular>
-              <TextRegular color={Colors.NEUTRAL_DARK} size="xxs">
-                {item.description.split(', ')[1]},{' '}
-                {item.description.split(', ')[2]}
-              </TextRegular>
-            </TouchableOpacity>
-          ))}
-        </View>
-      )}
     </CardWrapper>
   );
 }
