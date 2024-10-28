1c03c09c9ec089719274f84a16968f0dd45e1a34
Mike Feldberg <mike@betterangels.la>
2024-10-28 13:51:44 -0700

    rename component from Title to Purpose

--- apps/betterangels/src/app/(private-screens)/add-note/Purpose/index.tsx
+++ apps/betterangels/src/app/(private-screens)/add-note/Purpose/index.tsx
@@ -34,7 +34,7 @@ interface IPurposeProps {
   }) => void;
 }
 
-export default function Title(props: IPurposeProps) {
+export default function Purpose(props: IPurposeProps) {
   const {
     expanded,
     setExpanded,
@@ -114,7 +114,7 @@ export default function Title(props: IPurposeProps) {
           error={!!errors.purpose}
           errorMessage={errors.purpose ? 'Purpose is required' : ''}
           // TODO: remove default value after backend cleanup (DEV-804)
-          value={value || undefined}
+          value={value || 'Session with client'}
           onChangeText={(e) => onChange(e)}
         />
       </View>
