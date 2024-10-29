b1bfdfb4ac1fba669902ea0a40ea803013e5877c
Tom Glaz <tgmessages@gmail.com>
2024-10-28 17:12:03 -0700

    use picker in preferredLanguage

--- libs/expo/betterangels/src/lib/screens/AddEditClient/PersonalInfo/PreferredLanguage.tsx
+++ libs/expo/betterangels/src/lib/screens/AddEditClient/PersonalInfo/PreferredLanguage.tsx
@@ -1,9 +1,6 @@
 import { Spacings } from '@monorepo/expo/shared/static';
-import {
-  CardWrapper,
-  Radio,
-  TextBold,
-} from '@monorepo/expo/shared/ui-components';
+import { CardWrapper, TextBold } from '@monorepo/expo/shared/ui-components';
+import { Picker } from '@react-native-picker/picker';
 import { useFormContext } from 'react-hook-form';
 import { StyleSheet, View } from 'react-native';
 import {
@@ -23,23 +20,24 @@ export default function PreferredLanguage() {
   const onReset = () => {
     setValue('preferredLanguage', null);
   };
+
+  const hello = enumDisplayLanguage[LanguageEnum.Arabic];
+
   return (
     <CardWrapper onReset={onReset} title="Preferred Language">
       <View style={styles.container}>
         <TextBold size="sm">Select Language</TextBold>
-        {Object.entries(enumDisplayLanguage).map(
-          ([enumValue, displayValue]) => (
-            <Radio
-              key={enumValue}
-              value={enumDisplayLanguage[preferredLanguage as LanguageEnum]}
-              label={displayValue}
-              onPress={() =>
-                setValue('preferredLanguage', enumValue as LanguageEnum)
-              }
-              accessibilityHint="selects preferred language"
-            />
-          )
-        )}
+
+        <Picker
+          selectedValue={preferredLanguage as LanguageEnum}
+          onValueChange={(itemValue: LanguageEnum, itemIndex: number) =>
+            setValue('preferredLanguage', itemValue as LanguageEnum)
+          }
+        >
+          {Object.entries(enumDisplayLanguage).map(([key, label]) => {
+            return <Picker.Item label={label} value={key} key={key} />;
+          })}
+        </Picker>
       </View>
     </CardWrapper>
   );
