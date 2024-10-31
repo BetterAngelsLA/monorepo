fc62103484ce2835c54d1017a0b353dc462b59c6
Mike Feldberg <mike@betterangels.la>
2024-10-31 13:26:42 -0700

    fix interaction purpose

--- apps/betterangels/src/app/(private-screens)/add-note/Purpose/index.tsx
+++ apps/betterangels/src/app/(private-screens)/add-note/Purpose/index.tsx
@@ -113,8 +113,7 @@ export default function Purpose(props: IPurposeProps) {
           }}
           error={!!errors.purpose}
           errorMessage={errors.purpose ? 'Purpose is required' : ''}
-          // TODO: remove default value after backend cleanup (DEV-804)
-          value={value || 'Session with client'}
+          value={value || undefined}
           onChangeText={(e) => onChange(e)}
         />
       </View>
