507e35c38e37e29fa27ea0f17ae4aadf54a832bb
Tom Glaz <tgmessages@gmail.com>
2024-11-07 15:32:31 -0800

    use AddressAutocomplete on all address fields (#634)
    * use AddressAutocomplete on all address fields
    
    * move types/google.maps to dev dependencies
    
    * move @types/google.maps to devDependencies in betterangels/package.json
    
    * DEV-803 scroll address field to top (#673)
    
    * scroll address field to top
    
    * change maps autocomplete debounce to 100ms
    
    * store the useScrollToScreenTop keyboardDidShow listener in a ref for cleanup

--- libs/expo/betterangels/src/lib/hooks/index.ts
+++ libs/expo/betterangels/src/lib/hooks/index.ts
@@ -1,6 +1,7 @@
 export { default as useKeyboardToolbar } from './keyboardToolbar/useKeyboardToolbar';
 export { default as useSnackbar } from './snackbar/useSnackbar';
 // user
+export { default as useScrollToScreenTop } from './scroll/useScrollToScreenTop';
 export { default as useSignIn } from './user/useSignIn';
 export { default as useSignOut } from './user/useSignOut';
 export { default as useUser } from './user/useUser';
