26fbb20c674645c2ba13488f910bc91e9b90964c
Tom Glaz <tgmessages@gmail.com>
2024-11-07 15:32:31 -0800

    use AddressAutocomplete on all address fields (#634)
    * use AddressAutocomplete on all address fields
    
    * move types/google.maps to dev dependencies
    
    * move @types/google.maps to devDependencies in betterangels/package.json
    
    * DEV-803 scroll address field to top (#673)
    
    * scroll address field to top
    
    * change maps autocomplete debounce to 100ms
    
    * store the useScrollToScreenTop keyboardDidShow listener in a ref for cleanup

---
+++ libs/expo/betterangels/src/lib/services/PlaceAutocomplete/getPlaceAutocomplete.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+import { LA_COUNTY_CENTER } from './constants.google.maps';
+import { TPlaceLatLng, TPlacesPrediction } from './types';
+
+type TgetPlaceAutocomplete = {
+  baseUrl: string;
+  query: string;
+  boundsCenter?: TPlaceLatLng;
+  boundsRadiusMiles?: number;
+};
+
+export async function getPlaceAutocomplete(props: TgetPlaceAutocomplete) {
+  const {
+    baseUrl,
+    query,
+    boundsCenter = LA_COUNTY_CENTER,
+    boundsRadiusMiles = 10,
+  } = props;
+
+  if (query.length < 3) {
+    return [];
+  }
+
+  // 1 degree of lat/long is approximately 69 miles
+  const boundsRadiusDegrees = boundsRadiusMiles / 69;
+
+  const defaultBounds = {
+    north: boundsCenter.lat + boundsRadiusDegrees,
+    south: boundsCenter.lat - boundsRadiusDegrees,
+    east: boundsCenter.lng + boundsRadiusDegrees,
+    west: boundsCenter.lng - boundsRadiusDegrees,
+  };
+
+  const url = `${baseUrl}/proxy/maps/api/place/autocomplete/json`;
+
+  const response = await axios.get(url, {
+    params: {
+      bounds: defaultBounds,
+      input: query,
+      components: 'country:us',
+      strictBounds: true,
+      withCredentials: true,
+    },
+  });
+
+  return response.data.predictions as TPlacesPrediction[];
+}
