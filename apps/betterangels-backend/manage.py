7d0be2d4f79bf5f0b70cdaca98f647681b9ede13
Paul Vecchio <paul@betterangels.la>
2024-11-13 15:44:45 -0800

    Add gevent types

--- apps/betterangels-backend/manage.py
+++ apps/betterangels-backend/manage.py
@@ -5,11 +5,9 @@
 import os
 import sys
 
-# TODO: Delete our generated type stubs once the below PR has been merged.
-# https://github.com/python/typeshed/pull/10527
 from gevent import monkey
 
-# monkey.patch_all()
+monkey.patch_all()
 
 
 def main() -> None:
