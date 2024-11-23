c9c32175add6df684314ffc55bc8357307e5cc31
Tom Glaz <tgmessages@gmail.com>
2024-11-22 17:24:57 -0800

    test camera

--- libs/expo/shared/ui-components/src/lib/MediaPickerModal/index.tsx
+++ libs/expo/shared/ui-components/src/lib/MediaPickerModal/index.tsx
@@ -1,12 +1,15 @@
 import { ReactNativeFile } from '@monorepo/expo/shared/clients';
-import { Colors, Shadow, Spacings } from '@monorepo/expo/shared/static';
-import { resizeImage } from '@monorepo/expo/shared/utils';
-import { useCameraPermissions } from 'expo-camera';
-import * as ImagePicker from 'expo-image-picker';
+import { Colors, Spacings } from '@monorepo/expo/shared/static';
 import { useState } from 'react';
 import { Alert, Pressable, View } from 'react-native';
 import Modal from 'react-native-modal';
-import Camera from '../Camera';
+import {
+  Camera,
+  useCameraDevice,
+  useCameraDevices,
+  useCameraPermission,
+  useCodeScanner,
+} from 'react-native-vision-camera';
 import TextBold from '../TextBold';
 import TextRegular from '../TextRegular';
 
@@ -26,17 +29,70 @@ export default function MediaPickerModal(props: IMediaPickerModalProps) {
     setFiles,
     allowMultiple = true,
   } = props;
+  console.log('################################### MediaPickerModal');
   const [isCameraOpen, setIsCameraOpen] = useState(false);
 
-  const [permission, requestPermission] = useCameraPermissions();
+  console.log('*****************  isCameraOpen:', isCameraOpen);
+
+  // const [permission, requestPermission] = useCameraPermissions();
+  const devices = useCameraDevices();
+
+  console.log();
+  console.log('| -------------  devices  ------------- |');
+  console.log(devices);
+  console.log();
+
+  const camera = useCameraDevice('back');
+
+  console.log();
+  console.log('| -------------  camera  ------------- |');
+  console.log(camera);
+  console.log();
+
+  const { hasPermission, requestPermission } = useCameraPermission();
+
+  console.log('*****************  hasPermission:', hasPermission);
+
+  const codeScanner = useCodeScanner({
+    codeTypes: ['pdf-417'],
+    onCodeScanned: (codes) => {
+      console.log(`Scanned ${codes.length} codes!`);
+    },
+  });
 
   const closeModal = () => {
     setModalVisible(false);
   };
 
   const getPermissionsAndOpenCamera = async () => {
-    if (permission) {
-      const { granted } = await requestPermission();
+    console.log('######################### getPermissionsAndOpenCamera');
+
+    console.log('*****************  !!camera:', !!camera);
+
+    if (!!camera) {
+      Alert.alert('CAMERA -- YES');
+    }
+
+    if (!camera) {
+      Alert.alert('NO CAMERA');
+
+      return;
+    }
+
+    if (hasPermission) {
+      console.log('hasPermission');
+      setIsCameraOpen(true);
+
+      return;
+    }
+
+    if (!hasPermission) {
+      console.log('hasPermission NO - so ask');
+
+      const granted = await requestPermission();
+
+      console.log('*****************  granted:', granted);
+
       if (granted) {
         setIsCameraOpen(true);
       } else {
@@ -45,36 +101,6 @@ export default function MediaPickerModal(props: IMediaPickerModalProps) {
           'You need to grant camera permission to use this app'
         );
       }
-    }
-  };
-
-  const pickImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        allowsEditing: false,
-        allowsMultipleSelection: allowMultiple,
-      });
-
-      if (!result.canceled && result.assets) {
-        const uploadPromises = result.assets.map(async (asset) => {
-          const resizedPhoto = await resizeImage({ uri: asset.uri });
-          const file = new ReactNativeFile({
-            uri: resizedPhoto.uri,
-            name: asset?.fileName || Date.now().toString(),
-            type: asset.mimeType || 'image/jpeg',
-          });
-
-          return file;
-        });
-
-        const uploadedImages = await Promise.all(uploadPromises);
-
-        setFiles(uploadedImages);
-        setModalVisible(false);
-      }
-    } catch (err) {
-      console.error(err);
     }
   };
 
@@ -88,7 +114,55 @@ export default function MediaPickerModal(props: IMediaPickerModalProps) {
       style={{ justifyContent: 'flex-end', margin: isCameraOpen ? 0 : 20 }}
       useNativeDriverForBackdrop={true}
     >
-      {isCameraOpen ? (
+      {!!camera && !!codeScanner && (
+        <Camera
+          // style={StyleSheet.absoluteFill}
+          device={camera}
+          isActive={true}
+          // codeScanner={codeScanner}
+        />
+      )}
+
+      {!camera && (
+        <>
+          <View
+            style={{
+              backgroundColor: Colors.WHITE,
+              marginBottom: Spacings.xs,
+              borderRadius: 8,
+            }}
+          >
+            <Pressable
+              onPress={getPermissionsAndOpenCamera}
+              style={{
+                padding: Spacings.sm,
+                alignItems: 'center',
+                borderTopWidth: 0.5,
+                borderTopColor: Colors.NEUTRAL_LIGHT,
+              }}
+              accessibilityRole="button"
+              accessibilityHint="opens camera"
+            >
+              <TextRegular color={Colors.PRIMARY}>Take Photo</TextRegular>
+            </Pressable>
+          </View>
+          <Pressable
+            style={{
+              backgroundColor: Colors.WHITE,
+              borderRadius: 8,
+              padding: Spacings.sm,
+              alignItems: 'center',
+            }}
+            accessibilityRole="button"
+            accessibilityHint="close camera selection"
+            onPress={closeModal}
+          >
+            <TextBold color={Colors.PRIMARY}>Cancel</TextBold>
+          </Pressable>
+        </>
+      )}
+
+      {/* {isCameraOpen ? (
         <Camera
           setModalVisible={setModalVisible}
           setIsCameraOpen={setIsCameraOpen}
@@ -146,7 +220,7 @@ export default function MediaPickerModal(props: IMediaPickerModalProps) {
             <TextBold color={Colors.PRIMARY}>Cancel</TextBold>
           </Pressable>
         </>
-      )}
+      )} */}
     </Modal>
   );
 }
