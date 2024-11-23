35d7f78ec6ec5ebd9f3f53c5af469ffafbb8033b
Tom Glaz <tgmessages@gmail.com>
2024-11-22 17:34:49 -0800

    scan drivers license code and show parsed

--- libs/expo/shared/ui-components/src/lib/MediaPickerModal/index.tsx
+++ libs/expo/shared/ui-components/src/lib/MediaPickerModal/index.tsx
@@ -1,17 +1,57 @@
 import { ReactNativeFile } from '@monorepo/expo/shared/clients';
 import { Colors, Spacings } from '@monorepo/expo/shared/static';
+import { Parse } from 'aamva-parser';
 import { useState } from 'react';
-import { Alert, Pressable, View } from 'react-native';
+import { Alert, Pressable, StyleSheet, View } from 'react-native';
 import Modal from 'react-native-modal';
 import {
   Camera,
   useCameraDevice,
-  useCameraDevices,
   useCameraPermission,
   useCodeScanner,
 } from 'react-native-vision-camera';
 import TextBold from '../TextBold';
 import TextRegular from '../TextRegular';
+
+type IDLData = {
+  driversLicenseId: string;
+  firstName: string;
+  lastName: string;
+  firstNameAlias: string;
+  dateOfBirth?: Date | null;
+  gender: string;
+  heightInches?: number;
+  weightLbs: string;
+  eyeColor: string;
+  streetAddress: string;
+  streetAddressSupplement: string;
+  city: string;
+  country: string;
+  postalCode: string;
+};
+
+function toCodeData(licenseData: IParsedLicense): IDLData | null {
+  if (!licenseData) {
+    return null;
+  }
+
+  return {
+    driversLicenseId: licenseData.driversLicenseId || '',
+    firstName: licenseData.firstName || '',
+    lastName: licenseData.lastName || '',
+    firstNameAlias: licenseData.firstNameAlias || '',
+    dateOfBirth: licenseData.dateOfBirth,
+    gender: licenseData.gender || '',
+    heightInches: licenseData.height || 0,
+    weightLbs: licenseData.weight || '',
+    eyeColor: licenseData.eyeColor || '',
+    streetAddress: licenseData.streetAddress || '',
+    streetAddressSupplement: licenseData.streetAddressSupplement || '',
+    city: licenseData.city || '',
+    country: licenseData.country || '',
+    postalCode: licenseData.postalCode || '',
+  };
+}
 
 interface IMediaPickerModalProps {
   onCapture: (file: ReactNativeFile) => void;
@@ -22,41 +62,38 @@ interface IMediaPickerModalProps {
 }
 
 export default function MediaPickerModal(props: IMediaPickerModalProps) {
-  const {
-    onCapture,
-    setModalVisible,
-    isModalVisible,
-    setFiles,
-    allowMultiple = true,
-  } = props;
-  console.log('################################### MediaPickerModal');
-  const [isCameraOpen, setIsCameraOpen] = useState(false);
-
-  console.log('*****************  isCameraOpen:', isCameraOpen);
-
-  // const [permission, requestPermission] = useCameraPermissions();
-  const devices = useCameraDevices();
-
-  console.log();
-  console.log('| -------------  devices  ------------- |');
-  console.log(devices);
-  console.log();
+  const { setModalVisible, isModalVisible } = props;
 
   const camera = useCameraDevice('back');
 
-  console.log();
-  console.log('| -------------  camera  ------------- |');
-  console.log(camera);
-  console.log();
+  const [dlData, setDLData] = useState<IDLData | null>(null);
 
   const { hasPermission, requestPermission } = useCameraPermission();
+  const [isCameraOpen, setIsCameraOpen] = useState(false);
 
-  console.log('*****************  hasPermission:', hasPermission);
+  function exitCamera() {
+    setIsCameraOpen(false);
+  }
 
   const codeScanner = useCodeScanner({
     codeTypes: ['pdf-417'],
     onCodeScanned: (codes) => {
-      console.log(`Scanned ${codes.length} codes!`);
+      if (codes.length && codes[0].value) {
+        const codeData = codes[0].value;
+
+        const parsedLicense = Parse(codeData);
+
+        console.log();
+        console.log('| -------------  PARSED LICENSE DATA  ------------- |');
+        console.log(parsedLicense);
+        console.log();
+
+        setDLData(toCodeData(parsedLicense));
+
+        exitCamera();
+
+        return;
+      }
     },
   });
 
@@ -65,33 +102,12 @@ export default function MediaPickerModal(props: IMediaPickerModalProps) {
   };
 
   const getPermissionsAndOpenCamera = async () => {
-    console.log('######################### getPermissionsAndOpenCamera');
-
-    console.log('*****************  !!camera:', !!camera);
-
-    if (!!camera) {
-      Alert.alert('CAMERA -- YES');
-    }
-
-    if (!camera) {
-      Alert.alert('NO CAMERA');
-
-      return;
-    }
-
     if (hasPermission) {
-      console.log('hasPermission');
       setIsCameraOpen(true);
-
-      return;
     }
 
     if (!hasPermission) {
-      console.log('hasPermission NO - so ask');
-
       const granted = await requestPermission();
-
-      console.log('*****************  granted:', granted);
 
       if (granted) {
         setIsCameraOpen(true);
@@ -114,16 +130,32 @@ export default function MediaPickerModal(props: IMediaPickerModalProps) {
       style={{ justifyContent: 'flex-end', margin: isCameraOpen ? 0 : 20 }}
       useNativeDriverForBackdrop={true}
     >
-      {!!camera && !!codeScanner && (
+      {!isCameraOpen && !!dlData && (
+        <View style={{ backgroundColor: '#fff', padding: 10 }}>
+          {Object.entries(dlData).map(([k, v]) => (
+            <TextRegular key={k}>
+              {String(k)} --- {String(v)}
+            </TextRegular>
+          ))}
+        </View>
+      )}
+
+      {isCameraOpen && camera && (
         <Camera
-          // style={StyleSheet.absoluteFill}
+          style={StyleSheet.absoluteFill}
           device={camera}
           isActive={true}
-          // codeScanner={codeScanner}
+          codeScanner={codeScanner}
         />
       )}
 
-      {!camera && (
+      {isCameraOpen && !camera && (
+        <View>
+          <TextRegular>CameraOpen but NO CAMERA</TextRegular>
+        </View>
+      )}
+
+      {!isCameraOpen && (
         <>
           <View
             style={{
@@ -143,7 +175,7 @@ export default function MediaPickerModal(props: IMediaPickerModalProps) {
               accessibilityRole="button"
               accessibilityHint="opens camera"
             >
-              <TextRegular color={Colors.PRIMARY}>Take Photo</TextRegular>
+              <TextRegular color={Colors.PRIMARY}>Scan DL Barcode</TextRegular>
             </Pressable>
           </View>
           <Pressable
@@ -153,74 +185,111 @@ export default function MediaPickerModal(props: IMediaPickerModalProps) {
               padding: Spacings.sm,
               alignItems: 'center',
             }}
-            accessibilityRole="button"
-            accessibilityHint="close camera selection"
             onPress={closeModal}
+            accessibilityRole="button"
+            accessibilityHint="cancel"
           >
             <TextBold color={Colors.PRIMARY}>Cancel</TextBold>
           </Pressable>
         </>
       )}
-
-      {/* {isCameraOpen ? (
-        <Camera
-          setModalVisible={setModalVisible}
-          setIsCameraOpen={setIsCameraOpen}
-          onCapture={onCapture}
-        />
-      ) : (
-        <>
-          <View
-            style={{
-              backgroundColor: Colors.WHITE,
-              marginBottom: Spacings.xs,
-              borderRadius: 8,
-              ...Shadow,
-            }}
-          >
-            <Pressable
-              onPress={pickImage}
-              style={{
-                padding: Spacings.sm,
-                alignItems: 'center',
-                borderBottomWidth: 0.5,
-                borderBottomColor: Colors.NEUTRAL_LIGHT,
-              }}
-              accessibilityRole="button"
-              accessibilityHint="opens photo library"
-            >
-              <TextRegular color={Colors.PRIMARY}>From Photo Album</TextRegular>
-            </Pressable>
-            <Pressable
-              onPress={getPermissionsAndOpenCamera}
-              style={{
-                padding: Spacings.sm,
-                alignItems: 'center',
-                borderTopWidth: 0.5,
-                borderTopColor: Colors.NEUTRAL_LIGHT,
-              }}
-              accessibilityRole="button"
-              accessibilityHint="opens camera"
-            >
-              <TextRegular color={Colors.PRIMARY}>Take Photo</TextRegular>
-            </Pressable>
-          </View>
-          <Pressable
-            style={{
-              backgroundColor: Colors.WHITE,
-              borderRadius: 8,
-              padding: Spacings.sm,
-              alignItems: 'center',
-              ...Shadow,
-            }}
-            accessibilityRole="button"
-            accessibilityHint="close map selection"
-            onPress={closeModal}
-          >
-            <TextBold color={Colors.PRIMARY}>Cancel</TextBold>
-          </Pressable>
-        </>
-      )} */}
     </Modal>
   );
+}
+
+interface IParsedLicense {
+  firstName?: string | null;
+  lastName?: string | null;
+  middleName?: string | null;
+  expirationDate?: Date | null;
+  issueDate?: Date | null;
+  dateOfBirth?: Date | null;
+  gender?: Gender;
+  eyeColor?: EyeColor;
+  height?: number | null;
+  streetAddress?: string | null;
+  city?: string | null;
+  state?: string | null;
+  postalCode?: string | null;
+  driversLicenseId?: string | null;
+  documentId?: string | null;
+  country?: IssuingCountry;
+  middleNameTruncation?: Truncation;
+  firstNameTruncation?: Truncation;
+  lastNameTruncation?: Truncation;
+  streetAddressSupplement?: string | null;
+  hairColor?: HairColor;
+  placeOfBirth?: string | null;
+  auditInformation?: string | null;
+  inventoryControlNumber?: string | null;
+  lastNameAlias?: string | null;
+  firstNameAlias?: string | null;
+  suffixAlias?: string | null;
+  suffix?: NameSuffix;
+  version?: string | null;
+  pdf417?: string | null;
+  isExpired(): boolean;
+  expired?: boolean;
+  hasBeenIssued(): boolean;
+  isAcceptable(): boolean;
+  weight?: string;
+}
+
+enum HairColor {
+  Bald = 'Bald',
+  Black = 'Black',
+  Blond = 'Blond',
+  Brown = 'Brown',
+  Grey = 'Grey',
+  Red = 'Red',
+  Sandy = 'Sandy',
+  White = 'White',
+  Unknown = 'Unknown',
+}
+
+enum Gender {
+  Male = 'Male',
+  Female = 'Female',
+  Other = 'Other',
+  Unknown = 'Unknown',
+}
+
+enum NameSuffix {
+  Junior = 'Junior',
+  Senior = 'Senior',
+  First = 'First',
+  Second = 'Second',
+  Third = 'Third',
+  Fourth = 'Fourth',
+  Fifth = 'Fifth',
+  Sixth = 'Sixth',
+  Seventh = 'Seventh',
+  Eighth = 'Eighth',
+  Ninth = 'Ninth',
+  Unknown = 'Unknown',
+}
+
+enum EyeColor {
+  Black = 'Black',
+  Blue = 'Blue',
+  Brown = 'Brown',
+  Gray = 'Gray',
+  Green = 'Green',
+  Hazel = 'Hazel',
+  Maroon = 'Maroon',
+  Pink = 'Pink',
+  Dichromatic = 'Dichromatic',
+  Unknown = 'Unknown',
+}
+
+enum IssuingCountry {
+  UnitedStates = 'United States',
+  Canada = 'Canada',
+  Unknown = 'Unknown',
+}
+
+enum Truncation {
+  Truncated = 'Truncated',
+  None = 'None',
+  Unknown = 'Unknown',
 }
