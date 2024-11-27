0f1ca3f8b2eb4c53bec3763185f3e53e16d89d68
Tom Glaz <tgmessages@gmail.com>
2024-11-27 12:49:49 -0800

    move routes to file

--- apps/shelter-web/src/app/app.tsx
+++ apps/shelter-web/src/app/app.tsx
@@ -1,48 +1,28 @@
-import NxWelcome from './nx-welcome';
-
 import { Link, Route, Routes } from 'react-router-dom';
+
+import { routeChildren } from './routes/appRoutes';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="shelter-web" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
+    <div className="px-24">
       <div role="navigation">
         <ul>
-          <li>
-            <Link to="/">Home</Link>
+          <li className="my-4">
+            <Link to="/">Home link</Link>
           </li>
           <li>
-            <Link to="/page-2">Page 2</Link>
+            <Link to="/page-2">Page 2 link</Link>
           </li>
         </ul>
       </div>
+
+      <hr className="my-12" />
+
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
+        {routeChildren.map((route) => (
+          <Route path={route.path} element={route.element} />
+        ))}
       </Routes>
-      {/* END: routes */}
     </div>
   );
 }
