7d20b9814f162f9b0116cde31768d358f7ddf126
Paul Vecchio <paul@betterangels.la>
2024-11-27 13:13:48 -0800

    Try dev null again

--- apps/betterangels/app.config.js
+++ apps/betterangels/app.config.js
@@ -39,7 +39,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: BUNDLE_IDENTIFIER,
-      buildNumber: '1.0.30',
+      buildNumber: '1.0.31',
       associatedDomains: [`applinks:${HOSTNAME}`],
       usesAppleSignIn: true,
       config: {
@@ -78,7 +78,7 @@ export default {
           apiKey: process.env.EXPO_PUBLIC_ANDROID_GOOGLEMAPS_APIKEY,
         },
       },
-      versionCode: 30,
+      versionCode: 31,
     },
     web: {
       favicon: './src/app/assets/images/favicon.png',
