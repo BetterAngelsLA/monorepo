461f216b771baa26570a42626588d133be5e3df2
Paul Vecchio <paul@betterangels.la>
2024-11-27 13:15:36 -0800

    Try this

--- apps/betterangels/app.config.js
+++ apps/betterangels/app.config.js
@@ -39,7 +39,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: BUNDLE_IDENTIFIER,
-      buildNumber: '1.0.31',
+      buildNumber: '1.0.32',
       associatedDomains: [`applinks:${HOSTNAME}`],
       usesAppleSignIn: true,
       config: {
@@ -78,7 +78,7 @@ export default {
           apiKey: process.env.EXPO_PUBLIC_ANDROID_GOOGLEMAPS_APIKEY,
         },
       },
-      versionCode: 31,
+      versionCode: 32,
     },
     web: {
       favicon: './src/app/assets/images/favicon.png',
