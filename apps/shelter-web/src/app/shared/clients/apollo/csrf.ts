7b2c8f208989067d6c24957a15d9f532778ea1eb
Tom Glaz <tgmessages@gmail.com>
2024-12-04 17:09:04 -0800

    feedback implement

--- apps/shelter-web/src/app/shared/clients/apollo/csrf.ts
+++ apps/shelter-web/src/app/shared/clients/apollo/csrf.ts
@@ -1,27 +1,23 @@
-import { ApolloLink, FetchResult, Observable } from '@apollo/client';
-import { getItem, setItem } from '../../utils/storage/cookies';
+import { ApolloLink, Observable } from '@apollo/client';
+import { getCookie } from '../../utils/storage/cookies';
 
-const CSRF_COOKIE_NAME = import.meta.env.VITE_CSRF_COOKIE_NAME;
-const CSRF_HEADER_NAME = import.meta.env.VITE_CSRF_HEADER_NAME;
-
-const csrfTokenRegex = new RegExp(`${CSRF_COOKIE_NAME}=([^;]+)`);
+const csrfCookieName = import.meta.env.VITE_CSRF_COOKIE_NAME;
+const csrfHeaderName = import.meta.env.VITE_CSRF_HEADER_NAME;
 
 const extractCsrfToken = async (apiUrl: string, customFetch = fetch) => {
-  let csrfToken = await getItem(CSRF_COOKIE_NAME);
-  if (!csrfToken) {
-    const response = await customFetch(apiUrl, { credentials: 'include' });
-    const cookies = response.headers?.get('Set-Cookie');
-    const match = cookies?.match(csrfTokenRegex);
-    if (match) {
-      csrfToken = match[1];
-      await setItem(CSRF_COOKIE_NAME, csrfToken);
-    }
+  const csrfToken = getCookie(csrfCookieName);
+
+  if (csrfToken) {
+    return csrfToken;
   }
-  return csrfToken;
+
+  await customFetch(apiUrl, { credentials: 'include' });
+
+  return getCookie(csrfCookieName);
 };
 
 const updateHeadersWithCsrf = (csrfToken: string | null) =>
-  csrfToken ? { [CSRF_HEADER_NAME]: csrfToken } : {};
+  csrfToken ? { [csrfHeaderName]: csrfToken } : {};
 
 export const csrfLink = (apiUrl: string, customFetch = fetch) =>
   new ApolloLink(
@@ -38,31 +34,13 @@ export const csrfLink = (apiUrl: string, customFetch = fetch) =>
               },
             }));
 
-            forward(operation).subscribe({
-              next: (response: FetchResult) => {
-                const context = operation.getContext();
-                const combinedCookies = [
-                  // Extract 'set-cookie' header from the GraphQL response if it exists
-                  context['response']?.headers?.get('set-cookie'),
-                  // Extract 'set-cookie' headers from any REST responses and filter out any null or undefined values
-                  ...((context['restResponses'] as Response[]) || [])
-                    .map((res) => res?.headers.get('set-cookie'))
-                    .filter(Boolean),
-                ]
-                  .filter(Boolean)
-                  .join('; ');
-
-                const match = combinedCookies.match(csrfTokenRegex);
-
-                if (match) {
-                  setItem(CSRF_COOKIE_NAME, match[1]);
-                }
-
-                observer.next(response);
-              },
+            const subscription = forward(operation).subscribe({
+              next: observer.next.bind(observer),
               error: observer.error.bind(observer),
               complete: observer.complete.bind(observer),
             });
+
+            return () => subscription.unsubscribe();
           } catch (err) {
             console.error('Error in CSRF Apollo Link:', err);
             observer.error(err);
--- apps/shelter-web/src/app/shared/utils/storage/cookies.ts
+++ apps/shelter-web/src/app/shared/utils/storage/cookies.ts
@@ -1,7 +1,7 @@
 const DEFAULT_COOKIE_EXPIRY_DAYS = 30;
 const ONE_DAY_MS = 24 * 60 * 60 * 1000;
 
-const setCookie = (
+export const setCookie = (
   name: string,
   value: string,
   days = DEFAULT_COOKIE_EXPIRY_DAYS
@@ -17,7 +17,7 @@ const setCookie = (
   document.cookie = name + '=' + (value || '') + expires + '; path=/';
 };
 
-const getCookie = (name: string) => {
+export const getCookie = (name: string) => {
   const nameEQ = name + '=';
   const ca = document.cookie.split(';');
 
@@ -36,18 +36,6 @@ const getCookie = (name: string) => {
   return null;
 };
 
-const eraseCookie = (name: string) => {
+export const eraseCookie = (name: string) => {
   document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 };
-
-export async function getItem(key: string): Promise<string | null> {
-  return getCookie(key);
-}
-
-export async function setItem(key: string, value: string): Promise<void> {
-  setCookie(key, value);
-}
-
-export async function deleteItem(key: string): Promise<void> {
-  eraseCookie(key);
-}
