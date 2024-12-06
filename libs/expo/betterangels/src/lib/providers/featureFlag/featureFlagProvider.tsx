490409d0ab0759e82b0b1cb635f67534acac217f
Aaron <aaronipollock@gmail.com>
2024-12-06 15:53:00 -0800

    silent error

--- libs/expo/betterangels/src/lib/providers/featureFlag/featureFlagProvider.tsx
+++ libs/expo/betterangels/src/lib/providers/featureFlag/featureFlagProvider.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
 import { FeatureFlagContext, FeatureFlags } from "./featureFlagContext";
-import { View, Text } from "react-native";
 import { useGetFeatureFlagsQuery } from "./__generated__/featureFlagProvider.generated"
 
 
@@ -10,38 +9,23 @@ interface FeatureFlagProviderProps {
 
 export const FeatureFlagProvider: React.FC<FeatureFlagProviderProps> = ({ children }) => {
     const [featureFlags, setFeatureFlags] = useState<FeatureFlags>({});
-    const { data, loading, error } = useGetFeatureFlagsQuery();
+    const { data, error } = useGetFeatureFlagsQuery();
 
     useEffect(() => {
         if (data?.featureControls?.flags) {
-
             const flags = data.featureControls.flags.reduce((acc: FeatureFlags, flag) => {
                 acc[flag.name] = flag.isActive ?? false;
                     return acc;
                 }, {}
             );
             setFeatureFlags(flags);
-            console.log(data)
         }
     }, [data]);
 
     const memoizedFlags = useMemo(() => featureFlags, [featureFlags]);
 
-    if (loading) {
-        return (
-            <View>
-                <Text>Loading...</Text>
-            </View>
-        );
-    }
-
     if (error) {
         console.error("FeatureFlagProvider encountered an error:", error.message);
-        return (
-            <View>
-                <Text>Something went wrong. Please try again later.</Text>
-            </View>
-        );
     }
 
     return (
