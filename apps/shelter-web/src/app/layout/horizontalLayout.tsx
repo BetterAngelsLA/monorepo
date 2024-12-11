50ab5a1995e81fba03e317413f0ea1224e98ea14
Tom Glaz <tgmessages@gmail.com>
2024-12-10 23:04:39 -0800

    layout fixes

--- apps/shelter-web/src/app/layout/horizontalLayout.tsx
+++ apps/shelter-web/src/app/layout/horizontalLayout.tsx
@@ -1,27 +1,26 @@
 import { PropsWithChildren, ReactElement } from 'react';
+import { twCss } from '../shared/utils/styles/twCss';
+import { MaxWLayout } from './maxWLayout';
 
 interface IParams extends PropsWithChildren {
   className?: string;
-  fullWidth?: boolean;
 }
 
 export function HorizontalLayout(props: IParams): ReactElement {
-  const { className = '', fullWidth, children } = props;
+  const { className = '', children } = props;
 
   const parentCss = [
-    className,
     'w-full',
     'flex',
     'flex-col',
     'items-center',
-    fullWidth ? 'w-screen -mx-4' : 'px-4',
-  ].join(' ');
-
-  const innerCss = ['max-w-7xl', 'w-full', 'flex', 'flex-col'].join(' ');
+    'px-4',
+    className,
+  ];
 
   return (
-    <div className={parentCss}>
-      <div className={fullWidth ? 'w-full' : innerCss}>{children}</div>
+    <div className={twCss(parentCss)}>
+      <MaxWLayout>{children}</MaxWLayout>
     </div>
   );
 }
--- apps/shelter-web/src/app/pages/home/home.tsx
+++ apps/shelter-web/src/app/pages/home/home.tsx
@@ -1,5 +1,5 @@
 import { APIProvider as MapsApiProvider } from '@vis.gl/react-google-maps';
-import { HorizontalLayout } from '../../layout/horizontalLayout';
+import { MaxWLayout } from '../../layout/maxWLayout';
 import { Map } from '../../shared/components/maps/map';
 import { ShelterSearch } from '../../shared/components/shelters/shelterSearch';
 
@@ -10,11 +10,17 @@ export function Home() {
     console.error(`MapsApiProvider error ${error}`);
   }
 
+  const mapContainerCss = [
+    'border-4 border-red-500 md:border-yellow-500 lg:md:border-purple-500',
+    '-mx-4',
+    'md:mx-0',
+  ];
+
   return (
     <MapsApiProvider apiKey={googleMapsApiKey} onError={onProviderError}>
-      <HorizontalLayout fullWidth={true}>
+      <MaxWLayout className="border-red-500 border-4xxx" negativeMx={1}>
         <Map className="h-[480px] md:h-80" />
-      </HorizontalLayout>
+      </MaxWLayout>
       <ShelterSearch />
     </MapsApiProvider>
   );
