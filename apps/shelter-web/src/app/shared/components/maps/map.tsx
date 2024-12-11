182bbbd7b0c28b015eada58bd8c6932f718f40af
Tom Glaz <tgmessages@gmail.com>
2024-12-11 02:33:39 -0800

    add mapHandler

--- apps/shelter-web/src/app/shared/components/maps/map.tsx
+++ apps/shelter-web/src/app/shared/components/maps/map.tsx
@@ -5,46 +5,64 @@ import {
   useAdvancedMarkerRef,
   useMap,
 } from '@vis.gl/react-google-maps';
+import { useEffect, useState } from 'react';
 import { mergeCss } from '../../utils/styles/mergeCss';
-import { LA_COUNTY_CENTER_LAT, LA_COUNTY_CENTER_LNG } from './constants.maps';
+import {
+  DEFAULT_MAP_ZOOM,
+  LA_COUNTY_CENTER_LAT,
+  LA_COUNTY_CENTER_LNG,
+} from './constants.maps';
+import { MapHandler, getPlacesBounds } from './mapHandler';
 import { MapZoomControls } from './mapZoomControls';
+import { TLatLng } from './types.maps';
+
+const defaultCenter = {
+  lat: LA_COUNTY_CENTER_LAT,
+  lng: LA_COUNTY_CENTER_LNG,
+};
 
 type TMap = {
   className?: string;
   mapBounds?: google.maps.LatLngBoundsLiteral;
+  center?: TLatLng | null;
 };
 
+const defaultBounds = getPlacesBounds({
+  boundsCenter: defaultCenter,
+});
+
 export function Map(props: TMap) {
-  const { className = '', mapBounds } = props;
+  const { className = '', mapBounds = defaultBounds, center } = props;
 
   const map = useMap();
-  const [markerRef, marker] = useAdvancedMarkerRef();
+  const [markerRef] = useAdvancedMarkerRef();
+  const [mapCenter, setMapCenter] = useState<TLatLng | null | undefined>(null);
+
+  useEffect(() => {
+    if (center === undefined) {
+      return;
+    }
+
+    setMapCenter(center || defaultCenter);
+  }, [center]);
 
   const mapCss = ['h-12', 'w-full', className];
 
   return (
     <GoogleMap
-      mapId={'bf51a910020fa25a'}
+      mapId={'shelter-map'}
       className={mergeCss(mapCss)}
-      defaultCenter={{
-        lat: LA_COUNTY_CENTER_LAT,
-        lng: LA_COUNTY_CENTER_LNG,
-      }}
+      defaultCenter={defaultCenter}
       defaultBounds={mapBounds}
       gestureHandling={'greedy'}
-      defaultZoom={11}
+      defaultZoom={DEFAULT_MAP_ZOOM}
       disableDefaultUI={true}
     >
-      <AdvancedMarker
-        ref={markerRef}
-        position={{
-          lat: LA_COUNTY_CENTER_LAT,
-          lng: LA_COUNTY_CENTER_LNG,
-        }}
-      >
+      <AdvancedMarker ref={markerRef} position={mapCenter}>
         <MapPin />
       </AdvancedMarker>
       <MapZoomControls map={map} className="mr-4" />
+      <MapHandler center={center} />
     </GoogleMap>
   );
 }
