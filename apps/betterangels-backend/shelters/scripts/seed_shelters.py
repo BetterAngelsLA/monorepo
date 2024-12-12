bbacf9fbec2cc2b6c6515fe2064662557f70db1b
Mike Feldberg <mike@betterangels.la>
2024-12-11 23:14:15 -0800

    add docstring

--- apps/betterangels-backend/shelters/scripts/seed_shelters.py
+++ apps/betterangels-backend/shelters/scripts/seed_shelters.py
@@ -10,6 +10,33 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "betterangels_backend.settings")
 django.setup()
 
 
+"""
+Script to seed the database with a specified number of shelters and associated data.
+
+Usage:
+    python seed_shelters.py <number_of_shelters>
+
+Arguments:
+    <number_of_shelters> : An integer specifying the number of shelters to generate.
+
+Details:
+- Generates shelters using the `shelter_recipe` defined in `baker_recipes`.
+- Adds a between 0 and 3 contacts to each shelter using `shelter_contact_recipe`.
+- Adds a placeholder image to each shelter using colocated stock image.
+
+Dependencies:
+- The `shelter_seed_image.png` file must exist in the script's directory.
+
+Example:
+    python seed_shelters.py 10
+    This generates 10 shelters with associated placeholder images and random contacts.
+
+Note:
+- cd to script's directory before running.
+- Make sure you've run migrations.
+"""
+
+
 def main() -> None:
     from shelters.models import ExteriorPhoto
     from shelters.tests.baker_recipes import shelter_contact_recipe, shelter_recipe
