7a3b7b2a3bcfd276fe0e5b898e4f354ee3e564df
Paul Vecchio <paul@betterangels.la>
2024-12-11 17:10:02 -0800

    Shelter CSRF workaround (#778)

--- apps/shelter-web/src/app/shared/utils/storage/cookies.ts
+++ apps/shelter-web/src/app/shared/utils/storage/cookies.ts
@@ -1,41 +1,68 @@
+interface CookieInfo {
+  values: string[];
+  hasDuplicates: boolean;
+}
+
+interface CookieOptions {
+  days?: number;
+  path?: string;
+  secure?: boolean;
+}
+
 const DEFAULT_COOKIE_EXPIRY_DAYS = 30;
 const ONE_DAY_MS = 24 * 60 * 60 * 1000;
 
 export const setCookie = (
   name: string,
   value: string,
-  days = DEFAULT_COOKIE_EXPIRY_DAYS
+  days = DEFAULT_COOKIE_EXPIRY_DAYS,
+  options: Partial<CookieOptions> = {}
 ) => {
-  let expires = '';
+  const { path = '/', secure = true } = options;
 
+  let expires = '';
   if (days) {
     const date = new Date();
     date.setTime(date.getTime() + days * ONE_DAY_MS);
     expires = '; expires=' + date.toUTCString();
   }
 
-  document.cookie = name + '=' + (value || '') + expires + '; path=/';
+  const securePart = secure ? '; Secure' : '';
+  document.cookie = `${name}=${
+    value || ''
+  }${expires}; path=${path}${securePart}`;
 };
 
-export const getCookie = (name: string) => {
+export const getCookie = (name: string): string[] => {
   const nameEQ = name + '=';
   const ca = document.cookie.split(';');
+  const matches: string[] = [];
 
   for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-
-    while (c.charAt(0) === ' ') {
-      c = c.substring(1, c.length);
-    }
-
+    const c = ca[i].trim();
     if (c.indexOf(nameEQ) === 0) {
-      return c.substring(nameEQ.length, c.length);
+      matches.push(c.substring(nameEQ.length, c.length));
     }
   }
 
-  return null;
+  return matches;
 };
 
-export const eraseCookie = (name: string) => {
-  document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+export const getCookieInfo = (name: string): CookieInfo => {
+  const values = getCookie(name);
+  return {
+    values,
+    hasDuplicates: values.length > 1,
+  };
+};
+
+export const eraseCookie = (name: string, hostname?: string) => {
+  if (hostname) {
+    document.cookie = `${name}=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; domain=${hostname}`;
+    document.cookie = `${name}=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; domain=${hostname}; Secure`;
+  } else {
+    // If no hostname provided, try without domain
+    document.cookie = `${name}=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+    document.cookie = `${name}=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Secure`;
+  }
 };
