ba363077ca394b32ef0162dd526c1d0ece4ba412
Mike Feldberg <mike@betterangels.la>
2024-12-18 12:18:17 -0800

    typescript happy

--- apps/shelter-web/src/app/shared/components/map/types.maps.ts
+++ apps/shelter-web/src/app/shared/components/map/types.maps.ts
@@ -11,7 +11,7 @@ export type TMarker = {
   position: TLatLng;
   id?: string;
   label?: string;
-  onClick?: (id?: string) => void;
+  onClick?: ((e: google.maps.MapMouseEvent) => void) | undefined;
 };
 
 export type TMapGestureHandling = 'cooperative' | 'greedy' | 'none' | 'auto';
