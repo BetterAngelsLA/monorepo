35e396ea663497d2778a5f4c7158e2813c7535cb
Tom Glaz <tgmessages@gmail.com>
2024-12-18 15:11:33 -0800

    libs: add react check icon

--- libs/react/icons/src/lib/index.ts
+++ libs/react/icons/src/lib/index.ts
@@ -1,3 +1,4 @@
+export { default as CheckIcon } from './components/checkIcon';
 export { default as ChevronDownIcon } from './components/chevronDownIcon';
 export { default as CloseIcon } from './components/closeIcon';
 export { default as FilterIcon } from './components/filterIcon';
