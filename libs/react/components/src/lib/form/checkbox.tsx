3302e6877b2ecd35ec1e7856c87df3b2a9d1fcf5
Tom Glaz <tgmessages@gmail.com>
2024-12-18 15:13:13 -0800

    libs/react: add checkbox component

---
+++ libs/react/components/src/lib/form/checkbox.tsx
@@ -0,0 +1,76 @@
+import { CheckIcon } from '@monorepo/react/icons';
+import { ReactElement, useEffect, useState } from 'react';
+
+export type ICheckbox = {
+  label: string;
+  checked?: boolean;
+  onChange?: (checked: boolean) => void;
+  className?: string;
+  required?: boolean;
+  disabled?: boolean;
+};
+
+export default function Checkbox(props: ICheckbox): ReactElement {
+  const { checked, onChange, label, disabled, className } = props;
+
+  const [isChecked, setIsChecked] = useState(!!checked);
+
+  function handleChange(): void {
+    if (disabled) {
+      return;
+    }
+
+    const newChecked = !isChecked;
+
+    setIsChecked(newChecked);
+  }
+
+  useEffect(() => {
+    if (onChange) {
+      onChange(isChecked);
+    }
+  }, [isChecked]);
+
+  useEffect(() => {
+    setIsChecked(!!checked);
+  }, [checked]);
+
+  let parentCss: string = [
+    'text-sm',
+    'px-4',
+    'py-2',
+    'cursor-pointer',
+    'border',
+    'rounded-lg',
+    !!isChecked
+      ? 'border-primary-95 bg-primary-95'
+      : 'border-neutral-90 bg-white',
+    className,
+  ].join(' ');
+
+  let checkboxContainerCss: string = [
+    'mt-0.5',
+    'w-4',
+    'h-4',
+    'overflow-hidden',
+    'flex',
+    'items-center',
+    'border',
+    !!isChecked
+      ? 'border-primary-20 bg-primary-20'
+      : 'border-neutral-90 bg-white',
+    'rounded-sm',
+    className,
+  ].join(' ');
+
+  return (
+    <div className={parentCss} onClick={handleChange}>
+      <label className="flex justify-between items-start">
+        <span className="flex flex-wrap">{label}</span>
+        <div className={checkboxContainerCss}>
+          {isChecked && <CheckIcon className="text-white" />}
+        </div>
+      </label>
+    </div>
+  );
+}
--- libs/react/components/src/lib/index.ts
+++ libs/react/components/src/lib/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Pill } from './Pill';
 export { default as PillContainer } from './PillContainer';
+export { default as Checkbox } from './form/checkbox';
