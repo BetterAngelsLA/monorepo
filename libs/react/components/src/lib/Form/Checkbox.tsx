e8b634f93212602e895a7adc8bca843831e4c4b2
Tom Glaz <tgmessages@gmail.com>
2024-12-23 10:13:29 -0800

    libs/react: add checkbox and expandable components (DEV-1080)  (#798)
    * react: add checkbox and checkbox group components
    
    * react: add icons and global color

---
+++ libs/react/components/src/lib/Form/Checkbox.tsx
@@ -0,0 +1,63 @@
+import { CheckIcon } from '@monorepo/react/icons';
+import { ReactElement } from 'react';
+import { mergeCss } from '../../utils/mergeCss';
+
+export type ICheckbox = {
+  label: string;
+  checked?: boolean;
+  onChange: (checked: boolean) => void;
+  className?: string;
+  required?: boolean;
+  disabled?: boolean;
+};
+
+export function Checkbox(props: ICheckbox): ReactElement {
+  const { checked, onChange, label, disabled, className } = props;
+
+  function handleChange(): void {
+    if (disabled) {
+      return;
+    }
+
+    onChange(!checked);
+  }
+
+  const parentCss = [
+    'flex',
+    'justify-between',
+    'items-start',
+    'w-full',
+    'text-sm',
+    'px-4',
+    'py-2',
+    'border',
+    'rounded-lg',
+    checked ? 'border-primary-95 bg-primary-95' : 'border-neutral-90 bg-white',
+    className,
+  ];
+
+  const checkboxContainerCss = [
+    'mt-0.5',
+    'w-4',
+    'h-4',
+    'overflow-hidden',
+    'flex',
+    'items-center',
+    'border',
+    checked ? 'border-primary-20 bg-primary-20' : 'border-neutral-90 bg-white',
+    'rounded-sm',
+  ];
+
+  return (
+    <button
+      className={mergeCss(parentCss)}
+      onClick={handleChange}
+      disabled={disabled}
+    >
+      <div className="text-left text-wrap">{label}</div>
+      <div className={mergeCss(checkboxContainerCss)}>
+        {checked && <CheckIcon className="text-white" />}
+      </div>
+    </button>
+  );
+}
