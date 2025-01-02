ecf5945504345735f0ebd000c0baead0c7138a64
Davit-BetterAngels <141072810+Davit-BetterAngels@users.noreply.github.com>
2025-01-02 12:09:09 -0800

    View Shelter 3 (DEV-1079) (#790)
    * ready UI no icons
    
    * updated some icons
    
    * dublicate fix
    
    * share icon

---
+++ libs/react/icons/src/lib/components/globeIcon.tsx
@@ -0,0 +1,4 @@
+import svg from '../../../../../../libs/expo/shared/icons/src/assets/third_party/arturo_wibawa/svg/akar-icons:globe.svg';
+import { createSvgComponent } from '../../toComponent';
+
+export default createSvgComponent(svg);
