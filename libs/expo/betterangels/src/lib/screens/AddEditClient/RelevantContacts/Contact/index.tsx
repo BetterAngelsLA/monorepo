b29489950ea319a696a07c58a12ea286694c832e
Davit-BetterAngels <davit@betterangels.la>
2025-01-07 07:29:48 +0000

    checking for US area code

--- libs/expo/betterangels/src/lib/screens/AddEditClient/RelevantContacts/Contact/index.tsx
+++ libs/expo/betterangels/src/lib/screens/AddEditClient/RelevantContacts/Contact/index.tsx
@@ -136,8 +136,15 @@ export default function Contact(props: IContactProps) {
         }
         rules={{
           validate: (value: string) => {
-            if (value && !Regex.phoneNumber.test(value)) {
-              return 'Enter a 10-digit phone number without space or special characters';
+            if (value) {
+              const areaCode = value.substring(0, 3);
+
+              if (
+                !Regex.phoneNumber.test(value) ||
+                !Regex.usAreaCode.test(areaCode)
+              ) {
+                return 'Enter a 10-digit phone number without space or special characters';
+              }
             }
             return true;
           },
