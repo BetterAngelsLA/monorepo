5b444a3e0d2ddc1e66ea6300836463325cc624fc
Paul Vecchio <paul@betterangels.la>
2025-01-22 13:10:40 -0800

    Add google analytics

--- apps/wildfires/src/app/app.tsx
+++ apps/wildfires/src/app/app.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { MainLayout } from './layout/mainLayout';
 import { routeChildren } from './routes/appRoutes';
+import RouteTracker from './shared/components/RouterTracker';
+import { initGA } from './shared/utils/analytics';
 
 export function App() {
+  useEffect(() => {
+    initGA();
+  }, []);
+
   return (
+    <>
+      <RouteTracker />
       <Routes>
         <Route path="/" element={<MainLayout />}>
           {routeChildren.map((route) => (
@@ -11,6 +20,7 @@ export function App() {
           ))}
         </Route>
       </Routes>
+    </>
   );
 }
 
---
+++ apps/wildfires/src/app/shared/utils/analytics.ts
@@ -0,0 +1,37 @@
+import ReactGA from "react-ga4";
+
+export const initGA = (): void => {
+  const isAnalyticsEnabled = import.meta.env.VITE_ANALYTICS_ENABLED === "true";
+  const analyticsID = import.meta.env.VITE_ANALYTICS_ID;
+
+  if (isAnalyticsEnabled && analyticsID) {
+    ReactGA.initialize(analyticsID);
+    console.log(`Google Analytics initialized with ID: ${analyticsID}`);
+  } else {
+    console.warn("Google Analytics is disabled or missing configuration.");
+  }
+};
+
+export const trackPageview = (path: string): void => {
+  const isAnalyticsEnabled = import.meta.env.VITE_ANALYTICS_ENABLED === "true";
+
+  if (isAnalyticsEnabled) {
+    console.log(`Page view: ${path}`)
+    ReactGA.send({ hitType: "pageview", page: path });
+  }
+};
+
+interface AnalyticsEvent {
+  category: string;
+  action: string;
+  label?: string;
+  value?: number;
+}
+
+export const trackEvent = ({ category, action, label, value }: AnalyticsEvent): void => {
+  const isAnalyticsEnabled = import.meta.env.VITE_ANALYTICS_ENABLED === "true";
+
+  if (isAnalyticsEnabled) {
+    ReactGA.event({ category, action, label, value });
+  }
+};
