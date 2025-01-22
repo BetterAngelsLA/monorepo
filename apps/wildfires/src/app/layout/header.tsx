5174d67b326e139c58be5bba2e73e551b7d3f0ed
jluna2000 <jesseluna2000@gmail.com>
2025-01-21 20:35:05 -0800

    Added Google Translate Element

--- apps/wildfires/src/app/layout/header.tsx
+++ apps/wildfires/src/app/layout/header.tsx
@@ -5,7 +5,6 @@ import {
 } from '@monorepo/react/icons';
 import { ReactElement } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from '../shared/components/button/Button';
 
 type IParams = {
   className?: string;
@@ -32,7 +31,7 @@ export function Header(props: IParams): ReactElement {
       <Link to="/" className="flex items-center">
         <BetterAngelsLogoIcon className="h-7 sm:h-10 text-brand-sky-blue fill-current" />
         <div className="text-white flex ml-2 text-xl md:text-4xl">
-          <div className="font-normal">
+          <div className="font-normal notranslate">
             Wildfire <span className="font-semibold">LA</span>
           </div>
         </div>
