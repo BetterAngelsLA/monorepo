49f0737b26066bd2a0a2be0bbad25291f77a2e7e
Paul Vecchio <paul@betterangels.la>
2025-01-21 18:23:23 -0800

    Add privacy policy and page

--- apps/wildfires/src/app/routes/appRoutes.tsx
+++ apps/wildfires/src/app/routes/appRoutes.tsx
@@ -1,9 +1,14 @@
 import { RouteObject } from 'react-router-dom';
 import { HomePage } from '../pages/home/homePage';
+import Policy from '../pages/Policy';
 
 export const routeChildren: RouteObject[] = [
   {
     path: '/',
     element: <HomePage />,
   },
+  {
+    path: '/privacy-policy',
+    element: <Policy />
+  }
 ];
