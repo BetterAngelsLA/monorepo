257fd377e89137ba965a05e2b57f5f5560a5dbc0
Mike Feldberg <mike@betterangels.la>
2025-01-21 16:17:36 -0800

    Add Wildfire LA static assets (DEV-1234) (#832)

---
+++ libs/expo/shared/icons/src/lib/components/WFCallIcon.tsx
@@ -0,0 +1,6 @@
+import WFCallSVG from '../../assets/third_party/microsoft_corporation/svg/fluent:call-16-regular.svg';
+import createSvgIcon from '../createSvgIcon';
+
+const WFCall = createSvgIcon(WFCallSVG);
+
+export default WFCall;
