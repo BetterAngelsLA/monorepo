8ee0d306d339eb0ff88356d9ad2c9091610f8525
Paul Vecchio <paul@betterangels.la>
2025-01-21 21:23:03 -0800

    Remove file name generation

--- apps/wildfires/src/app/pages/home/homePage.tsx
+++ apps/wildfires/src/app/pages/home/homePage.tsx
@@ -4,26 +4,11 @@ import { FiresSurvey } from './firesSurvey/FiresSurvey';
 
 export function HomePage() {
   const pageRef = useRef<HTMLDivElement | null>(null);
-
-  // TODO: move this to the actual result page when we get these
-  const generateFileName = () => {
-    const now = new Date();
-    const timestamp = now.toLocaleString("en-US", {
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-      hour12: true,
-    });
-    return `Wildfire Action Plan - ${timestamp}.pdf`;
-  };
-
   return (
     <div ref={pageRef}>
       <FiresSurvey />
 
-      <GeneratePDF pageRef={pageRef} fileName={generateFileName} />
+      <GeneratePDF pageRef={pageRef} fileName="Wildfire LA" />
     </div>
   );
 }
