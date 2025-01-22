34090d9e996f9b8fa9ccab30194f779420d5d835
Paul Vecchio <paul@betterangels.la>
2025-01-21 22:29:11 -0800

    Wildfire simple generate PDF (#837)

---
+++ apps/wildfires/src/app/shared/components/GeneratePDF.tsx
@@ -0,0 +1,36 @@
+import { MutableRefObject } from "react";
+import { useReactToPrint } from "react-to-print";
+
+interface GeneratePDFProps {
+  pageRef: MutableRefObject<HTMLDivElement | null>;
+  fileName: string | (() => string);
+}
+
+const GeneratePDF: React.FC<GeneratePDFProps> = ({ pageRef, fileName }) => {
+  const handlePrint = useReactToPrint({
+    contentRef: pageRef,
+    documentTitle: typeof fileName === "function" ? fileName() : fileName,
+    pageStyle: `
+      @page {
+        size: auto;
+        margin: 0;
+      }
+      body {
+        margin: 0;
+        padding: 0;
+        -webkit-print-color-adjust: exact;
+      }
+      .no-page-break {
+        page-break-inside: avoid;
+      }
+    `,
+  });
+
+  return (
+    <button onClick={() => handlePrint()}>
+      Download Page as PDF
+    </button>
+  );
+};
+
+export default GeneratePDF;
