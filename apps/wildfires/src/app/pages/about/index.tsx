8b30a04f0ab8543c545f9be2482228e1f0bc7aeb
Davit-BetterAngels <davit@betterangels.la>
2025-01-23 18:51:21 +0000

    align left

--- apps/wildfires/src/app/pages/about/index.tsx
+++ apps/wildfires/src/app/pages/about/index.tsx
@@ -13,7 +13,7 @@ export default function About() {
           <div className="w-full h-full absolute top-0 z-10 left-[50%] bg-brand-sky-blue " />
           <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-stretch relative z-20">
             <div className="bg-brand-dark-blue w-full md:w-[60%] py-40 flex items-center  md:py-0">
-              <h1 className="border-l-[10px] border-brand-yellow font-light font-primary md:font-bold pl-4 md:pl-8 text-5xl md:text-[74px] text-white md:leading-[96.2px] text-left md:text-center">
+              <h1 className="border-l-[10px] border-brand-yellow font-light font-primary md:font-bold pl-4 md:pl-8 text-5xl md:text-[74px] text-white md:leading-[96.2px] text-left">
                 About <span className="whitespace-nowrap">LA Disaster</span>{' '}
                 Relief Navigator
               </h1>
