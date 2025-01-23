7a07726e4d7a87f05f64217437c1352947cdf760
Davit-BetterAngels <davit@betterangels.la>
2025-01-23 07:34:59 +0000

    mobile donate button

--- apps/wildfires/src/app/layout/header.tsx
+++ apps/wildfires/src/app/layout/header.tsx
@@ -3,7 +3,7 @@ import {
   GlobeIcon,
   MenuIcon,
 } from '@monorepo/react/icons';
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { mergeCss } from '../shared/utils/styles/mergeCss';
 
@@ -13,6 +13,7 @@ type IParams = {
 
 export function Header(props: IParams): ReactElement {
   const { className = '' } = props;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const parentCss = [
     'w-full',
@@ -23,44 +24,71 @@ export function Header(props: IParams): ReactElement {
     'h-[104px]',
     'md:h-[78px]',
     'text-white',
+    'relative',
+    'z-30',
     className,
   ];
 
   return (
-    <header className={mergeCss(parentCss)}>
-      <Link to="/" className="flex items-center">
-        <BetterAngelsLogoIcon className="h-7 sm:h-10 text-brand-sky-blue fill-current" />
-        <div className="text-white flex ml-2 text-xl md:text-4xl">
-          <div className="font-normal">
-            Wildfire <span className="font-semibold">LA</span>
+    <>
+      <header className={mergeCss(parentCss)}>
+        <Link to="/" className="flex items-center">
+          <BetterAngelsLogoIcon className="h-7 sm:h-10 text-brand-sky-blue fill-current" />
+          <div className="text-white flex ml-2 text-xl md:text-4xl">
+            <div className="font-normal">
+              Wildfire <span className="font-semibold">LA</span>
+            </div>
+          </div>
+        </Link>
+        {/* Desktop */}
+        <div className="hidden md:flex items-center font-bold">
+          <p className="mr-8">Select Language</p>
+          <Link className="mr-12" to="/about">
+            About Wildfire LA
+          </Link>
+          <a
+            target="_blank"
+            href="https://www.pledge.to/widgets/donate/JS11PeUKJh7pCqZC"
+            className="border-2 border-brand-yellow rounded-full py-1 px-7 hover:bg-brand-yellow hover:text-black"
+            rel="noreferrer"
+          >
+            Donate
+          </a>
+        </div>
+        {/* Mobile */}
+        <div className="flex md:hidden items-center font-bold gap-7">
+          <div className="h-10 w-10 flex items-center justify-center">
+            <GlobeIcon className="h-6 w-6" stroke="white" />
+          </div>
+          <div
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            className="h-10 w-10 flex items-center justify-center"
+          >
+            <MenuIcon className="h-6 w-6" fill="white" />
           </div>
         </div>
-      </Link>
-      {/* Desktop */}
-      <div className="hidden md:flex items-center font-bold">
-        <p className="mr-8">Select Language</p>
-        <Link className="mr-12" to="/about">
-          About Wildfire LA
-        </Link>
-        <a
-          target="_blank"
-          href="https://www.pledge.to/widgets/donate/JS11PeUKJh7pCqZC"
-          className="border-2 border-brand-yellow rounded-full py-1 px-7 hover:bg-brand-yellow hover:text-black"
-          onClick={() => console.log('donate')}
-          rel="noreferrer"
-        >
-          Donate
-        </a>
-      </div>
-      {/* Mobile */}
-      <div className="flex md:hidden items-center font-bold gap-7">
-        <div className="h-10 w-10 flex items-center justify-center">
-          <GlobeIcon className="h-6 w-6" stroke="white" />
-        </div>
-        <div className="h-10 w-10 flex items-center justify-center">
-          <MenuIcon className="h-6 w-6" fill="white" />
-        </div>
-      </div>
-    </header>
+        {isMenuOpen && (
+          <div className="md:hidden absolute -left-4 w-[110%] top-full bg-brand-dark-blue p-8 z-20">
+            <Link to="/about" className="block mb-6">
+              About Wildfire LA
+            </Link>
+            <a
+              target="_blank"
+              href="https://www.pledge.to/widgets/donate/JS11PeUKJh7pCqZC"
+              className="border-2 border-brand-yellow rounded-full py-1 w-full inline-flex items-center justify-center hover:bg-brand-yellow hover:text-black"
+              rel="noreferrer"
+            >
+              Donate
+            </a>
+          </div>
+        )}
+      </header>
+      {isMenuOpen && (
+        <div
+          onClick={() => setIsMenuOpen(false)}
+          className="fixed inset-0 bg-[rgba(30,51,66,0.5)] bg-cover z-10"
+        />
+      )}
+    </>
   );
 }
