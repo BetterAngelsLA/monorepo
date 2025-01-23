f1e61258ef4aafc3f090b09f51642bd0319f8e87
Tom Glaz <tgmessages@gmail.com>
2025-01-22 21:06:30 -0800

    survey layout styles (DEV-1246) (#862)
    * move survey to introduction page
    
    * getting started section layout
    
    * style survey nav button layout
    
    * update surveyButton disbled text color
    
    * update margins between questions
    
    * checkbox styles
    
    * checkbox border radius
    
    * radio styles
    
    * render income data table

--- apps/wildfires/src/app/pages/introduction/firesSurvey/config/forms/questions/incomeAssessment.tsx
+++ apps/wildfires/src/app/pages/introduction/firesSurvey/config/forms/questions/incomeAssessment.tsx
@@ -1,4 +1,31 @@
 import { TQuestion } from '../../../../../../shared/components/survey/types';
+import { Table } from '../../../../../../shared/components/table/Table';
+
+function IncomeByHouseholdTable() {
+  return (
+    <div className="flex justify-center">
+      <Table
+        className="flex mt-12 md:mt-24 max-w-3xl"
+        headers={[
+          'Household Size',
+          'Monthly Gross Income Limit (Simplified)',
+          'Annualized',
+        ]}
+        values={[
+          ['1', '$2,500', '$30,000'],
+          ['2', '$3,400', '$40,800'],
+          ['3', '$4,300', '$51,600'],
+          ['4', '$5,200', '$62,400'],
+          ['5', '$6,100', '$73,200'],
+          ['6', '$7,000', '$84,000'],
+          ['7', '$7,900', '$94,000'],
+          ['8', '$8,800', '$105,600'],
+          ['Each add’l person', '+$900', '+$10,800'],
+        ]}
+      />
+    </div>
+  );
+}
 
 export const qIncomeAssessment: TQuestion = {
   id: 'qIncomeAssessment',
@@ -18,9 +45,5 @@ export const qIncomeAssessment: TQuestion = {
   rules: {
     required: true,
   },
-  renderAfter: (
-    <div className="my-6 border-4 p-6 border-gray-500">
-      table (component) with household data rendered here
-    </div>
-  ),
+  renderAfter: <IncomeByHouseholdTable />,
 };
