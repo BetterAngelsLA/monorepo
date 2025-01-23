8e07e51bc4d2413e946ad28d0a16ef6542d4b565
Davit-BetterAngels <davit@betterangels.la>
2025-01-23 07:57:55 +0000

    important tip card

--- apps/wildfires/src/app/pages/result/index.tsx
+++ apps/wildfires/src/app/pages/result/index.tsx
@@ -7,15 +7,17 @@ import Register from '../../shared/components/register/Register';
 export default function Result() {
   return (
     <>
-      <div className="bg-brand-dark-blue w-full">
-        <Hero className="max-w-7xl min-h-[60vh] px-4 md:px-10 mx-auto py-14 md:py-28">
+      <HorizontalLayout className="bg-brand-dark-blue">
+        <Hero className="min-h-[60vh] py-14 md:py-28">
           <h1 className="font-light border-l-[10px] pl-4 md:pl-8 border-brand-yellow text-5xl text-white md:text-[74px] md:leading-[96.2px]">
             Thank you for helping us understand your situation. Here is a
             personalized action plan for you.
           </h1>
         </Hero>
-      </div>
-      <BestPractices />
+      </HorizontalLayout>
+      <HorizontalLayout>
+        <BestPractices />
+      </HorizontalLayout>
       <HorizontalLayout>
         <ImportantTips />
       </HorizontalLayout>
--- apps/wildfires/src/app/shared/components/importantTips/ImportantTips.tsx
+++ apps/wildfires/src/app/shared/components/importantTips/ImportantTips.tsx
@@ -1,4 +1,28 @@
 import { CameraIcon, InfoIcon } from '@monorepo/react/icons';
+import ImportantTipCard from './ImportantTipCard';
+
+const DATA = {
+  tax: [
+    {
+      title: 'Crowdfunding and Tax Considerations',
+      description:
+        'GoFundMe and other crowd sourcing funds will be considered as taxable income unless they are specifically designated as gifts.',
+      Icon: InfoIcon,
+    },
+    {
+      title: 'Eligibility Restrictions for FEMA Assistance',
+      description:
+        'If you have rental or homeowners insurance, do not apply for FEMA, unless you are self-employed or a gig worker.',
+      Icon: InfoIcon,
+    },
+  ],
+  photo: [
+    {
+      title: 'Take pictures and keep receipts for everything you do!',
+      Icon: CameraIcon,
+    },
+  ],
+};
 
 export default function ImportantTips() {
   return (
@@ -6,44 +30,8 @@ export default function ImportantTips() {
       <h3 className="uppercase font-bold md:text-2xl mb-8 md:mb-10">
         Important Tips
       </h3>
-      <h2 className="font-bold text-2xl md:text-[40px] md:leading-[52px] pl-6 border-l-[10px] border-brand-orange mb-4 md:mb-[3.875rem]">
-        Tax & Insurance
-      </h2>
-      <div className="bg-white rounded-[20px] w-full p-6 mb-4 md:mb-10">
-        <div className="flex items-center gap-4 mb-6">
-          <InfoIcon className="h-6 min-w-6 md:h-8 md:min-w-8" />
-          <h3 className="font-bold text-xl md:text-2xl">
-            Crowdfunding and Tax Considerations
-          </h3>
-        </div>
-        <p className="md:text-xl">
-          GoFundMe and other crowd sourcing funds will be considered as taxable
-          income unless they are specifically designated as gifts.
-        </p>
-      </div>
-      <div className="bg-white rounded-[20px] w-full p-6 mb-10 md:mb-20">
-        <div className="flex items-center gap-4 mb-6">
-          <InfoIcon className="h-6 min-w-6 md:h-8 md:min-w-8" />
-          <h3 className="font-bold text-xl md:text-2xl">
-            Eligibility Restrictions for FEMA Assistance
-          </h3>
-        </div>
-        <p className="md:text-xl">
-          If you have rental or homeowners insurance, do not apply for FEMA,
-          unless you are self-employed or a gig worker.
-        </p>
-      </div>
-      <h2 className="text-2xl font-bold md:text-[40px] md:leading-[52px] pl-6 border-l-[10px] border-brand-orange mb-4 md:mb-[3.875rem]">
-        Document Everything!
-      </h2>
-      <div className="bg-white rounded-[20px] w-full p-6">
-        <div className="flex items-center gap-4">
-          <CameraIcon className="h-6 min-w-6 md:h-8 md:min-w-8" />
-          <h3 className="font-bold text-xl  md:text-2xl">
-            Take pictures and keep receipts for everything you do!
-          </h3>
-        </div>
-      </div>
+      <ImportantTipCard title="Tax & Insurance" data={DATA.tax} />
+      <ImportantTipCard title="Document Everything!" data={DATA.photo} />
     </div>
   );
 }
