479151806b6ab482df0b96af010c33658d2702f6
Mike Feldberg <mike@betterangels.la>
2025-01-23 11:00:14 -0800

    use disaster relief logo

---
+++ libs/react/icons/src/lib/components/laDisasterReliefIcon.tsx
@@ -0,0 +1,4 @@
+import svg from '../../../../../../libs/expo/shared/icons/src/assets/better_angels/la_disaster_relief_navigator_logo.svg';
+import { createSvgComponent } from '../../toComponent';
+
+export default createSvgComponent(svg);
