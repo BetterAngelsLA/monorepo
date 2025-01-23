212102aedf8a2413b006cf34ce6df36b7f298901
Tom Glaz <tgmessages@gmail.com>
2025-01-23 09:52:55 -0800

    scrollTop on route change and survey question next

--- apps/wildfires/src/app/app.tsx
+++ apps/wildfires/src/app/app.tsx
@@ -1,16 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import { MainLayout } from './layout/mainLayout';
 import { routeChildren } from './routes/appRoutes';
+import { useScrollTopOnLocationChange } from './shared/hooks/useScrollTopOnLocationChange';
 
 export function App() {
+  useScrollTopOnLocationChange();
+
   return (
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          {routeChildren.map((route) => (
-            <Route key={route.path} path={route.path} element={route.element} />
-          ))}
-        </Route>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        {routeChildren.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Route>
+    </Routes>
   );
 }
 
