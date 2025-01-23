f1015648e0042bc035b088ea50637199271468a1
Davit-BetterAngels <141072810+Davit-BetterAngels@users.noreply.github.com>
2025-01-23 09:37:19 -0800

    important tips (DEV-1274) (#864)
    * best practices
    
    * updates
    
    * important tips
    
    * important tip card

--- apps/wildfires/src/app/pages/result/index.tsx
+++ apps/wildfires/src/app/pages/result/index.tsx
@@ -1,7 +1,10 @@
-import { useAtom } from 'jotai';
-import { surveyResultsAtom } from '../../shared/atoms/surveyResultsAtom';
+import { HorizontalLayout } from '../../layout/horizontalLayout';
 import BestPractices from '../../shared/components/bestPractices/BestPractices';
 import Hero from '../../shared/components/hero/Hero';
+import ImportantTips from '../../shared/components/importantTips/ImportantTips';
+import Register from '../../shared/components/register/Register';
+import { useAtom } from 'jotai';
+import { surveyResultsAtom } from '../../shared/atoms/surveyResultsAtom';
 import { SurveyResults } from '../../shared/components/surveyResults/SurveyResults';
 
 export default function Result() {
@@ -9,16 +12,21 @@ export default function Result() {
 
   return (
     <>
-      <div className="bg-brand-dark-blue w-full">
-        <Hero className="max-w-7xl min-h-[60vh] px-4 md:px-10 mx-auto py-14 md:py-28">
+      <HorizontalLayout className="bg-brand-dark-blue">
+        <Hero className="min-h-[60vh] py-14 md:py-28">
           <h1 className="font-light border-l-[10px] pl-4 md:pl-8 border-brand-yellow text-5xl text-white md:text-[74px] md:leading-[96.2px]">
             Thank you for helping us understand your situation. Here is a
             personalized action plan for you.
           </h1>
         </Hero>
-      </div>
-      <BestPractices />
-
+      </HorizontalLayout>
+      <HorizontalLayout>
+        <BestPractices />
+      </HorizontalLayout>
+      <HorizontalLayout>
+        <ImportantTips />
+      </HorizontalLayout>
+      <Register />
       {!!surveyResults && (
         <SurveyResults className="mt-8 mb-24" results={surveyResults} />
       )}
