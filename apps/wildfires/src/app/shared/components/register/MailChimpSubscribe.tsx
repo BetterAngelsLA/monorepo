f00f85a6bdfd4e61474accfad48537673bb7cf9d
Mike Feldberg <mike@betterangels.la>
2025-01-23 10:05:04 -0800

    wip

--- apps/wildfires/src/app/shared/components/register/MailChimpSubscribe.tsx
+++ apps/wildfires/src/app/shared/components/register/MailChimpSubscribe.tsx
@@ -52,17 +52,8 @@ const SignupForm = ({ status, message, subscribe }: ISubscribeFormProps) => {
     }
   }, [status]);
 
-  const parentCss = [
-    'p-4',
-    'flex',
-    'items-center',
-    'border ',
-    'border-neutral-90',
-    'rounded-lg',
-  ].join(' ');
-
   return (
-    <div className={parentCss}>
+    <div className="bg-white flex-1 p-6 flex flex-col items-center justify-between aspect-video rounded-lg">
       {status === 'sending' && <div>Sending...</div>}
       {status === 'error' && (
         <div
@@ -75,52 +66,54 @@ const SignupForm = ({ status, message, subscribe }: ISubscribeFormProps) => {
       )}
 
       <form onSubmit={handleSubmit}>
-        <div style={styles.formGroup}>
-          <label style={styles.label}>First Name</label>
-          <input
-            style={styles.input}
-            onChange={handleChange}
-            type="text"
-            name="firstName"
-            value={formData.firstName}
-            placeholder="First Name"
-          />
-        </div>
+        <div style={styles.formGroupRow}>
+          <div style={styles.formGroup}>
+            <label style={styles.label}>First Name</label>
+            <input
+              style={styles.input}
+              onChange={handleChange}
+              type="text"
+              name="firstName"
+              value={formData.firstName}
+              placeholder="First Name"
+            />
+          </div>
 
-        <div style={styles.formGroup}>
-          <label style={styles.label}>Last Name</label>
-          <input
-            style={styles.input}
-            onChange={handleChange}
-            type="text"
-            name="lastName"
-            value={formData.lastName}
-            placeholder="Last Name"
-          />
-        </div>
+          <div style={styles.formGroup}>
+            <label style={styles.label}>Last Name</label>
+            <input
+              style={styles.input}
+              onChange={handleChange}
+              type="text"
+              name="lastName"
+              value={formData.lastName}
+              placeholder="Last Name"
+            />
+          </div>
 
-        <div style={styles.formGroup}>
-          <label style={styles.label}>Email</label>
-          <input
-            style={styles.input}
-            onChange={handleChange}
-            type="email"
-            name="email"
-            value={formData.email}
-            placeholder="Email Address"
-          />
-        </div>
+          <div style={styles.formGroup}>
+            <label style={styles.label}>Email</label>
+            <input
+              style={styles.input}
+              onChange={handleChange}
+              type="email"
+              name="email"
+              value={formData.email}
+              placeholder="Email Address"
+            />
+          </div>
 
-        <div style={styles.formGroup}>
-          <label style={styles.label}>Cell Number</label>
-          <input
-            style={styles.input}
-            onChange={handleChange}
-            type="text"
-            name="phone"
-            value={formData.phone}
-            placeholder="Phone Number"
-          />
+          <div style={styles.formGroup}>
+            <label style={styles.label}>Cell Number</label>
+            <input
+              style={styles.input}
+              onChange={handleChange}
+              type="text"
+              name="phone"
+              value={formData.phone}
+              placeholder="Phone Number"
+            />
+          </div>
         </div>
 
         <div style={styles.formGroup}>
@@ -134,8 +127,9 @@ const SignupForm = ({ status, message, subscribe }: ISubscribeFormProps) => {
             placeholder="Zipcode"
           />
         </div>
-
-        <button type="submit">Subscribe</button>
+        <div className="flex flex-col items-center">
+          <button type="submit">Subscribe</button>
+        </div>
       </form>
     </div>
   );
@@ -162,6 +156,12 @@ const styles = {
     flexDirection: columnDirection,
     marginBottom: '1rem',
   },
+  formGroupRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: '1rem',
+  },
   label: {
     fontSize: '16px',
     fontWeight: '600',
@@ -172,5 +172,6 @@ const styles = {
     borderRadius: '4px',
     lineHeight: '32px',
     padding: '0 10px',
+    marginRight: '1rem',
   },
 };
