96126e7626701b747fc254a7d8e13ceb81ab8ad4
Mike Feldberg <mike@betterangels.la>
2025-01-23 09:56:30 -0800

    Wildfire questionnaire copy updates (DEV-1312) (#868)

--- apps/wildfires/src/app/pages/introduction/firesSurvey/config/forms/questions/haveRentersInsurance.tsx
+++ apps/wildfires/src/app/pages/introduction/firesSurvey/config/forms/questions/haveRentersInsurance.tsx
@@ -3,7 +3,7 @@ import { TQuestion } from '../../../../../../shared/components/survey/types';
 export const qHaveRentersInsurance: TQuestion = {
   id: 'qHaveRentersInsurance',
   type: 'radio',
-  title: 'Do you have Renter’s Insurance? ',
+  title: 'Do you have Renter’s Insurance?',
   options: [
     {
       optionId: 'rentersInsuranceYes',
