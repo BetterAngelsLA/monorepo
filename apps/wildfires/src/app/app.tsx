6b10b8e79184b4c1012e03a15db2323ee01dccae
Paul Vecchio <paul@betterangels.la>
2025-01-23 16:30:47 -0800

    Add google analytics to Wildfire (DEV-1259) (#850)
    * Add google analytics
    
    * Don't print path in console
    
    * Try this
    
    * Add reference
    
    * simplify
    
    * Add google analytics config
    
    * resolve merge conflicts
    
    ---------
    
    Co-authored-by: Mike Feldberg <mike@betterangels.la>

--- apps/wildfires/src/app/app.tsx
+++ apps/wildfires/src/app/app.tsx
@@ -1,19 +1,28 @@
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { MainLayout } from './layout/mainLayout';
 import { routeChildren } from './routes/appRoutes';
+import RouteTracker from './shared/components/RouterTracker';
 import { useScrollTopOnLocationChange } from './shared/hooks/useScrollTopOnLocationChange';
+import { initGA } from './shared/utils/analytics';
 
 export function App() {
   useScrollTopOnLocationChange();
+  useEffect(() => {
+    initGA();
+  }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        {routeChildren.map((route) => (
-          <Route key={route.path} path={route.path} element={route.element} />
-        ))}
-      </Route>
-    </Routes>
+    <>
+      <RouteTracker />
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          {routeChildren.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+        </Route>
+      </Routes>
+    </>
   );
 }
 
