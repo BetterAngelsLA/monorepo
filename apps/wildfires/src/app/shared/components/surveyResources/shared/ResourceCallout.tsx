eb9da4b6861bcd3331f40287540038d0673bd603
Paul Vecchio <paul@betterangels.la>
2025-01-27 14:09:29 -0800

    Add Tips Description (#946) (DEV-1424)
    * tips description
    
    * Add missing file
    
    * updated wisiwig UI
    
    * updated filename
    
    ---------
    
    Co-authored-by: Davit-BetterAngels <davit@betterangels.la>

--- apps/wildfires/src/app/shared/components/surveyResources/shared/ResourceCallout.tsx
+++ apps/wildfires/src/app/shared/components/surveyResources/shared/ResourceCallout.tsx
@@ -1,4 +1,5 @@
-import { PropsWithChildren, ReactElement } from 'react';
+import { ChevronLeftIcon } from '@monorepo/react/icons';
+import { PropsWithChildren, ReactElement, useState } from 'react';
 import { mergeCss } from '../../../utils/styles/mergeCss';
 
 interface IProps extends PropsWithChildren {
@@ -9,9 +10,10 @@ interface IProps extends PropsWithChildren {
 
 export function ResourceCallout(props: IProps) {
   const { icon, type = 'alert', className, children } = props;
+  const [show, setShow] = useState(true);
 
   const parentCss = [
-    'flex',
+    'items-start',
     'p-4',
     'md:px-6',
     type === 'alert' ? 'bg-neutral-99' : 'white',
@@ -22,8 +24,20 @@ export function ResourceCallout(props: IProps) {
 
   return (
     <div className={mergeCss(parentCss)}>
-      {!!icon && <div className={mergeCss(iconCss)}>{icon}</div>}
-      {children}
+      <div className="w-full flex items-center">
+        {!!icon && <div className={mergeCss(iconCss)}>{icon}</div>}
+        <div className="flex items-center justify-between w-full flex-1">
+          <div className="font-bold h-8 flex items-center text-xl">
+            Useful Tips
+          </div>
+          <div onClick={() => setShow(!show)}>
+            <ChevronLeftIcon
+              className={`h-4 w-4 ${show ? 'rotate-90' : '-rotate-90'}`}
+            />
+          </div>
+        </div>
+      </div>
+      {show && <div className="mt-4">{children}</div>}
     </div>
   );
 }
