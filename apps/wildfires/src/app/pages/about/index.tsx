9130f712c23f0a6ad016cb7bc00b98f065a59990
Mike Feldberg <mike@betterangels.la>
2025-01-27 19:04:53 -0800

    Wildfire about oxford commas (#962)

--- apps/wildfires/src/app/pages/about/index.tsx
+++ apps/wildfires/src/app/pages/about/index.tsx
@@ -23,14 +23,14 @@ export default function About() {
             About
           </h2>
           <p className="mb-6 md:mb-16 sm:mb-12 text-xl leading-normal">
-            Better Angels, in partnership with ImagineLA and the Mayor's Fund for Los Angeles,
-            has developed the LA Disaster Relief Navigator, an online tool to
-            help Angelenos impacted by the wildfires effectively navigate the
-            many resources available to them. The solution is
+            Better Angels, in partnership with ImagineLA and the Mayor's Fund
+            for Los Angeles, has developed the LA Disaster Relief Navigator, an
+            online tool to help Angelenos impacted by the wildfires effectively
+            navigate the many resources available to them. The solution is
             designed to help people navigate every aspect of fire relief,
-            including applying for federal, state and local government
+            including applying for federal, state, and local government
             assistance, making insurance claims, home damage remediation,
-            temporary and permanent housing solutions and other critical
+            temporary and permanent housing solutions, and other critical
             services. The Navigator provides victims with a personalized action
             plan based on their specific situation, making the recovery process
             immediately actionable and helping our community to rebuild even
@@ -40,10 +40,10 @@ export default function About() {
             Who We Are
           </h2>
           <p className="mb-4 md:mb-4 sm:mb-4 text-xl leading-normal">
-            Better Angels, Imagine LA, and the Mayor's Fund for Los Angeles have come
-            together to support Angelenos affected by the devastating January
-            2025 wildfires. Each partner brings unique expertise and a shared
-            commitment to fostering community strength and resilience.
+            Better Angels, Imagine LA, and the Mayor's Fund for Los Angeles have
+            come together to support Angelenos affected by the devastating
+            January 2025 wildfires. Each partner brings unique expertise and a
+            shared commitment to fostering community strength and resilience.
           </p>
           <ul className="ml-6 mb-6 md:mb-16 sm:mb-12 text-xl leading-normal">
             <li className="list-disc">
@@ -53,14 +53,14 @@ export default function About() {
               Angels is taking a holistic approach, harnessing new models of
               intervention, community engagement, advocacy, world-class
               technology, and a strong dose of pragmatism across five critical
-              areas of need: Prevention, Services, Shelter, Housing and
+              areas of need: Prevention, Services, Shelter, Housing, and
               Technology.
             </li>
             <li className="list-disc">
-              <span className="font-bold">Imagine LA</span>: A social impact organization specializing
-              in helping individuals and families maximize public benefits and chart permanent pathways
-              out of homelessness and poverty. Imagine LA has created the
-              {' '}
+              <span className="font-bold">Imagine LA</span>: A social impact
+              organization specializing in helping individuals and families
+              maximize public benefits and chart permanent pathways out of
+              homelessness and poverty. Imagine LA has created the{' '}
               <a
                 aria-label="open imagine LA benefits navigator website in new tab"
                 className="underline"
@@ -70,24 +70,29 @@ export default function About() {
               >
                 Benefits Navigator
               </a>
-              , a suite of online tools that helps case managers and their clients to quickly
-              navigate and access the complex arena of federal, state, and local public benefits
-              and refundable tax credits. The Benefit Navigator has helped people access over
-              $125 million in additional benefits and feel confident to increase their earned
-              income by an average of 25%.
+              , a suite of online tools that helps case managers and their
+              clients to quickly navigate and access the complex arena of
+              federal, state, and local public benefits and refundable tax
+              credits. The Benefit Navigator has helped people access over $125
+              million in additional benefits and feel confident to increase
+              their earned income by an average of 25%.
             </li>
             <li className="list-disc">
-              <span className="font-bold">The Mayor’s Fund for Los Angeles</span>:
-              In support of Mayor’s Karen Bass' work to reduce and end homelessness
-              in Los Angeles, the Mayor’s Fund for Los Angeles is focused on preventing
-              Angelenos from becoming homeless. It has brought together business, philanthropy,
-              nonprofits, and local government to help keep Angelenos housed. Through outreach,
-              case management and expanded legal services, the Mayor’s Fund's We Are LA program
-              has connected over 53,000 Angelenos to needed services and helped stabilize housing
-              for more than 23,000 Angelenos at risk of eviction and homelessness.  The Mayor’s Fund,
-              through their hotline, is spearheading case management services to the Angelenos
-              severely economically impacted by the Wildfires - those at risk of homelessness.
-              The Hotline is a featured resource in the Navigator.
+              <span className="font-bold">
+                The Mayor’s Fund for Los Angeles
+              </span>
+              : In support of Mayor’s Karen Bass' work to reduce and end
+              homelessness in Los Angeles, the Mayor’s Fund for Los Angeles is
+              focused on preventing Angelenos from becoming homeless. It has
+              brought together business, philanthropy, nonprofits, and local
+              government to help keep Angelenos housed. Through outreach, case
+              management and expanded legal services, the Mayor’s Fund's We Are
+              LA program has connected over 53,000 Angelenos to needed services
+              and helped stabilize housing for more than 23,000 Angelenos at
+              risk of eviction and homelessness. The Mayor’s Fund, through their
+              hotline, is spearheading case management services to the Angelenos
+              severely economically impacted by the Wildfires - those at risk of
+              homelessness. The Hotline is a featured resource in the Navigator.
             </li>
           </ul>
           <h2 className="text-2xl md:text-[40px] md:leading-[1.2] mb-6 md:mb-16 border-l-[10px] border-brand-sky-blue pl-4 md:pl-8 font-bold">
