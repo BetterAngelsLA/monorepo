e4ac1a1615f5d329f159da648e4e043b960b8584
Paul Vecchio <paul@betterangels.la>
2025-01-28 11:58:54 -0800

    Remove donate button (DEV-1439) (#966)
    * Remove donate button
    
    * Keep donate component

--- apps/wildfires/src/app/shared/components/navBar/MenuMobile.tsx
+++ apps/wildfires/src/app/shared/components/navBar/MenuMobile.tsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 import { mergeCss } from '../../utils/styles/mergeCss';
 import { AboutLink } from './shared/AboutLink';
-import { DonateButton } from './shared/DonateButton';
 
 type IProps = {
   className?: string;
@@ -36,7 +35,6 @@ export function MenuMobile(props: IProps) {
       >
         Contact Us
       </a>
-      <DonateButton className="max-w-40 mb-4" />
     </div>
   );
 }
