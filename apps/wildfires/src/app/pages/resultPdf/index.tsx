d95b3dad17fb79a391896ea4a2e6da9e225a9943
Tom Glaz <tgmessages@gmail.com>
2025-01-29 09:43:48 -0800

    accept tags as query params in SurveyResults

--- apps/wildfires/src/app/pages/resultPdf/index.tsx
+++ apps/wildfires/src/app/pages/resultPdf/index.tsx
@@ -1,11 +1,13 @@
 import { HorizontalLayout } from '../../layout/horizontalLayout';
 import BestPractices from '../../shared/components/bestPractices/BestPractices';
+import { SurveyResults } from '../../shared/components/surveyResults/SurveyResults';
 
 export default function ResultPdfpage() {
   return (
-    <HorizontalLayout className="pt-4">
-      HELLO PDF
+    <HorizontalLayout className="">
       <BestPractices expanded={true} />
+
+      <SurveyResults className="mt-8 mb-24" />
     </HorizontalLayout>
   );
 }
