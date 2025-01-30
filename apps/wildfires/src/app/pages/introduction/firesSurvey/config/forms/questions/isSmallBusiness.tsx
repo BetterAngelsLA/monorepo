5c6a9d3edf760b496e1b643b8cceb6a4a15f5a38
Mike Feldberg <mike@betterangels.la>
2025-01-29 16:18:56 -0800

    Update small bus question (DEV-1454) (#993)
    * update small bus question
    
    * margins
    
    * margins again
    
    * Important Alerts -> Important Tips (DEV-1461) (#995)
    
    * add fema link
    
    ---------
    
    Co-authored-by: Paul Vecchio <paul@betterangels.la>

--- apps/wildfires/src/app/pages/introduction/firesSurvey/config/forms/questions/isSmallBusiness.tsx
+++ apps/wildfires/src/app/pages/introduction/firesSurvey/config/forms/questions/isSmallBusiness.tsx
@@ -3,7 +3,24 @@ import { TQuestion } from '../../../../../../shared/components/survey/types';
 export const qIsSmallBusiness: TQuestion = {
   id: 'qIsSmallBusiness',
   type: 'radio',
-  title: 'Does your business qualify as a SMALL BUSINESS?',
+  title:
+    'Do you own or run a small business that was impacted by the wildfires?',
+  body: 'A small business is an independent for profit* company with less than 500 employees and $5 million in annual revenue – the revenue limit amount can vary by industry.',
+  note: (
+    <span>
+      *Note: Qualified non-profit organizations whose facilities have been
+      impacted by the wildfires, can be eligible for FEMA disaster assistance (
+      <a
+        href="https://www.fema.gov"
+        target="_blank"
+        rel="noreferrer"
+        className="underline"
+      >
+        FEMA.gov
+      </a>
+      )
+    </span>
+  ),
   options: [
     {
       optionId: 'smallBusinessYes',
