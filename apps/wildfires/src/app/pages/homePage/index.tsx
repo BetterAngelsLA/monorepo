0763a068c18ee893876c24d05ab1bc524f314912
Paul Vecchio <paul@betterangels.la>
2025-01-30 16:30:32 -0800

    Remove contact us

--- apps/wildfires/src/app/pages/homePage/index.tsx
+++ apps/wildfires/src/app/pages/homePage/index.tsx
@@ -1,5 +1,4 @@
 import { HorizontalLayout } from '../../layout/horizontalLayout';
-import ContactUs from '../../shared/components/contactUs/ContactUs';
 import { GetStarted } from './getStarted/GetStarted';
 import { HomePageHero } from './homeHero/HomePageHero';
 
@@ -9,9 +8,6 @@ export function HomePage() {
       <HomePageHero className="min-h-[75vh]" />
       <HorizontalLayout>
         <GetStarted className="mt-8 md:mt-24 mb-12 md:mb-24" />
-      </HorizontalLayout>
-      <HorizontalLayout className="bg-steel-blue">
-        <ContactUs />
       </HorizontalLayout>
     </>
   );
