0bd3e4152f596474ba567b8dc3f40b73000cd05e
Davit-BetterAngels <davit@betterangels.la>
2025-01-31 00:40:24 +0000

    updated design

--- apps/wildfires/src/app/pages/contact/index.tsx
+++ apps/wildfires/src/app/pages/contact/index.tsx
@@ -1,3 +1,4 @@
+import { CallRegularIcon, EmailOutlinedIcon } from '@monorepo/react/icons';
 import { HorizontalLayout } from '../../layout/horizontalLayout';
 
 export default function Contact() {
@@ -15,33 +16,45 @@ export default function Contact() {
         </div>
       </HorizontalLayout>
       <HorizontalLayout>
-        <div className="max-w-[62.5rem] py-7 md:py-32">
+        <div className="py-6 md:py-16 w-full">
           <h2 className="text-2xl md:text-[40px] md:leading-[1.2] mb-6 md:mb-16 border-l-[10px] border-brand-sky-blue pl-4 md:pl-8 font-bold">
-            Thank you for reaching out
+            Please fill free to reach out with any question
           </h2>
-          <p className="mb-6 md:mb-16 sm:mb-12 text-xl leading-normal">
-            To connect with us by phone, please call the Mayor's Fund for LA
-            Hotline at{' '}
-            <a
-              aria-label="call the Mayor's Fund for LA Hotline"
-              className="underline"
-              href="tel:2135841808"
-            >
-              (213) 584-1808
-            </a>
-            .
-          </p>
-          <p className="text-xl leading-normal">
-            To get answers via email, please contact{' '}
-            <a
-              aria-label="email wildfires support at Imagine LA"
-              className="underline"
-              href="mailto:wildfires@imaginela.org"
-            >
-              wildfires@imaginela.org
-            </a>
-            .
-          </p>
+          <div className="flex flex-wrap items-center justify-center gap-4 w-full flex-1">
+            <div className="min-h-[286px] md:min-h-[362px] min-w-[343px] md:min-w-[457px] flex flex-1 flex-col gap-6 items-center justify-between border border-steel-blue rounded-3xl p-6 md:p-12">
+              <div className="flex flex-col items-center gap-6">
+                <CallRegularIcon className="w-[60px] h-[60px] md:w-[80px] md:h-[80px]" />
+                <span className="text-xl md:text-[28px] font-bold">Phone</span>
+                <p className="md:text-xl leading-normal text-center">
+                  To connect with us by phone, please call the Mayor's Fund for
+                  LA Hotline at:
+                </p>
+              </div>
+              <a
+                aria-label="call the Mayor's Fund for LA Hotline"
+                className="underline text-xl md:text-[28px] font-bold"
+                href="tel:2135841808"
+              >
+                (213) 584-1808
+              </a>
+            </div>
+            <div className="flex flex-1 min-h-[286px] md:min-h-[362px] min-w-[343px] md:min-w-[457px] flex-col gap-6 items-center justify-between border border-steel-blue rounded-3xl p-6 md:p-12">
+              <div className="flex flex-col items-center gap-6">
+                <EmailOutlinedIcon className="w-[60px] h-[60px] md:w-[80px] md:h-[80px]" />
+                <span className="text-xl md:text-[28px] font-bold">Email</span>
+                <p className="md:text-xl leading-normal text-center">
+                  To get answers via email, please contact:
+                </p>
+              </div>
+              <a
+                aria-label="email wildfires support at Imagine LA"
+                className="underline text-xl md:text-[28px] font-bold"
+                href="mailto:wildfires@imaginela.org"
+              >
+                wildfires@imaginela.org
+              </a>
+            </div>
+          </div>
         </div>
       </HorizontalLayout>
     </>
--- libs/expo/shared/icons/src/lib/index.ts
+++ libs/expo/shared/icons/src/lib/index.ts
@@ -14,6 +14,7 @@ export { default as CalendarLineIcon } from './components/CalendarLineIcon';
 export { default as CalendarPlusIcon } from './components/CalendarPlusIcon';
 export { default as CalendarSolidIcon } from './components/CalendarSolidIcon';
 export { default as CallIcon } from './components/CallIcon';
+export { default as CallOutlinedIcon } from './components/CallOutlinedIcon';
 export { default as CameraIcon } from './components/CameraIcon';
 export { default as CameraRotateIcon } from './components/CameraRotateIcon';
 export { default as CarIcon } from './components/CarIcon';
