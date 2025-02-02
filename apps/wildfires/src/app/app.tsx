8fe09f7b1cdab9a184b1164d6ce615cb2b20edab
Paul Vecchio <paul@betterangels.la>
2025-02-01 16:48:28 -0800

    Recovery plan

--- apps/wildfires/src/app/app.tsx
+++ apps/wildfires/src/app/app.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { MainLayout } from './layout/mainLayout';
+import PrintResult from './pages/printResult';
 import { routeChildren } from './routes/appRoutes';
 import RouteTracker from './shared/components/RouterTracker';
 import { useScrollTopOnLocationChange } from './shared/hooks/useScrollTopOnLocationChange';
@@ -12,6 +13,7 @@ const queryClient = new QueryClient();
 
 export function App() {
   useScrollTopOnLocationChange();
+
   useEffect(() => {
     initGA();
   }, []);
@@ -21,6 +23,11 @@ export function App() {
       <PrintProvider>
         <RouteTracker />
         <Routes>
+          {/**
+           * Define any route that should _not_ include the header and footer
+           * outside the MainLayout route.
+           */}
+          <Route path="/printResult" element={<PrintResult />} />
           <Route path="/" element={<MainLayout />}>
             {routeChildren.map((route) => (
               <Route
