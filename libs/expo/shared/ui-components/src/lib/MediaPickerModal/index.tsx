c1083239d6e1e5809f218d80852ca3170c429296
Tom Glaz <tgmessages@gmail.com>
2025-02-03 15:43:51 -0800

    add expo-document-picker

--- libs/expo/shared/ui-components/src/lib/MediaPickerModal/index.tsx
+++ libs/expo/shared/ui-components/src/lib/MediaPickerModal/index.tsx
@@ -2,6 +2,7 @@ import { ReactNativeFile } from '@monorepo/expo/shared/clients';
 import { Colors, Shadow, Spacings } from '@monorepo/expo/shared/static';
 import { resizeImage } from '@monorepo/expo/shared/utils';
 import { useCameraPermissions } from 'expo-camera';
+import * as DocumentPicker from 'expo-document-picker';
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
 import { Alert, Pressable, View } from 'react-native';
@@ -32,6 +33,38 @@ export default function MediaPickerModal(props: IMediaPickerModalProps) {
 
   const closeModal = () => {
     setModalVisible(false);
+  };
+
+  const getDocuments = async () => {
+    try {
+      // DocumentPicker.DocumentPickerResult
+      // canceled: false;
+      // /**
+      //  * An array of picked assets.
+      //  */
+      // assets: DocumentPickerAsset[];
+      // /**
+      //  * `FileList` object for the parity with web File API.
+      //  * @platform web
+      //  */
+      // output?: FileList;
+
+      // const result: DocumentPicker.DocumentPickerResult =
+      const result = await DocumentPicker.getDocumentAsync({
+        type: '*/*', // Adjust file type if necessary (e.g., 'application/pdf' for PDFs)
+      });
+
+      if (!result.canceled) {
+        // Handle the selected file here
+        const file = result.assets[0]; // Get the first selected file (if multiple are allowed)
+        console.log('File URI: ', file.uri);
+        console.log('File name: ', file.name);
+      } else {
+        console.log('User canceled the document picker');
+      }
+    } catch (error) {
+      console.error('DocumentPicker Error: ', error);
+    }
   };
 
   const getPermissionsAndOpenCamera = async () => {
@@ -129,6 +162,19 @@ export default function MediaPickerModal(props: IMediaPickerModalProps) {
               accessibilityHint="opens camera"
             >
               <TextRegular color={Colors.PRIMARY}>Take Photo</TextRegular>
+            </Pressable>
+            <Pressable
+              onPress={getDocuments}
+              style={{
+                padding: Spacings.sm,
+                alignItems: 'center',
+                borderTopWidth: 0.5,
+                borderTopColor: Colors.NEUTRAL_LIGHT,
+              }}
+              accessibilityRole="button"
+              accessibilityHint="opens file library"
+            >
+              <TextRegular color={Colors.PRIMARY}>Upload PDF file</TextRegular>
             </Pressable>
           </View>
           <Pressable
