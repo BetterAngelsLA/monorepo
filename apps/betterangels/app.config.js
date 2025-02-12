dce66c6c08cbe3d4d0c879d63f9938d889bd2192
Paul Vecchio <paul@betterangels.la>
2025-02-12 15:44:55 -0800

    Force build and return ci back to normal

--- apps/betterangels/app.config.js
+++ apps/betterangels/app.config.js
@@ -34,7 +34,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: BUNDLE_IDENTIFIER,
-      buildNumber: '1.0.42',
+      buildNumber: '1.0.44',
       associatedDomains: [`applinks:${HOSTNAME}`],
       usesAppleSignIn: true,
       config: {
@@ -73,7 +73,7 @@ export default {
           apiKey: process.env.EXPO_PUBLIC_ANDROID_GOOGLEMAPS_APIKEY,
         },
       },
-      versionCode: 43,
+      versionCode: 44,
     },
     web: {
       favicon: './src/app/assets/images/favicon.png',
