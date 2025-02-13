457c9e3ba3f66979a6611c6db6bc511a339ed9a1
Mike Feldberg <mike@betterangels.la>
2025-02-12 17:04:37 -0800

    update types

--- libs/expo/betterangels/src/lib/static/enumDisplayMapping.ts
+++ libs/expo/betterangels/src/lib/static/enumDisplayMapping.ts
@@ -22,10 +22,12 @@ import {
 export const enumDisplayLanguage: { [key in LanguageEnum]: string } = {
   [LanguageEnum.Arabic]: 'Arabic',
   [LanguageEnum.Armenian]: 'Armenian',
+  [LanguageEnum.Asl]: 'ASL',
   [LanguageEnum.SimplifiedChinese]: 'Chinese, Simplified',
   [LanguageEnum.TraditionalChinese]: 'Chinese, Traditional',
   [LanguageEnum.English]: 'English',
   [LanguageEnum.Farsi]: 'Farsi',
+  [LanguageEnum.French]: 'French',
   [LanguageEnum.Indonesian]: 'Indonesian',
   [LanguageEnum.Japanese]: 'Japanese',
   [LanguageEnum.Khmer]: 'Khmer',
@@ -60,6 +62,8 @@ export const enumDisplayHmisAgency: {
 export const enumDisplayGender: { [key in GenderEnum]: string } = {
   [GenderEnum.Female]: 'Female',
   [GenderEnum.Male]: 'Male',
+  [GenderEnum.TransFemale]: 'Transgender Female',
+  [GenderEnum.TransMale]: 'Transgender Male',
   [GenderEnum.NonBinary]: 'Non-Binary',
   [GenderEnum.PreferNotToSay]: 'Prefer not to say',
   [GenderEnum.Other]: 'Other',
