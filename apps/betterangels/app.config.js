51256344bcc4653d0d54c1347c76b7d6abb6a371
Tom Glaz <tgmessages@gmail.com>
2025-02-12 16:08:50 -0800

    bump ios buildNumber and android versionCode

--- apps/betterangels/app.config.js
+++ apps/betterangels/app.config.js
@@ -34,7 +34,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: BUNDLE_IDENTIFIER,
-      buildNumber: '1.0.34',
+      buildNumber: '1.0.35',
       associatedDomains: [`applinks:${HOSTNAME}`],
       usesAppleSignIn: true,
       config: {
@@ -73,7 +73,7 @@ export default {
           apiKey: process.env.EXPO_PUBLIC_ANDROID_GOOGLEMAPS_APIKEY,
         },
       },
-      versionCode: 34,
+      versionCode: 35,
     },
     web: {
       favicon: './src/app/assets/images/favicon.png',
