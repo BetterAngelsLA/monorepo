34c458cec30851cf8bd360abd97a87b3842cf533
Tom Glaz <tgmessages@gmail.com>
2025-02-18 14:44:37 -0800

    try webview

--- libs/expo/betterangels/src/lib/screens/FileScreenComponent/index.tsx
+++ libs/expo/betterangels/src/lib/screens/FileScreenComponent/index.tsx
@@ -62,6 +62,9 @@ export default function FileScreenComponent(props: TFileScreenComponent) {
   const isImage = attachmentType === AttachmentType.Image;
   const isPdf = mimeType === MimeTypes.PDF;
 
+  const pdfUrl =
+    'https://www.adobe.com/support/products/enterprise/knowledgecenter/media/c4611_sample_explain.pdf';
+
   return (
     <>
       <MainContainer bg={Colors.NEUTRAL_EXTRA_LIGHT}>
@@ -85,6 +88,21 @@ export default function FileScreenComponent(props: TFileScreenComponent) {
           )}
 
           {isPdf && (
+            // <WebBrowserLink href={pdfUrl} accessibilityHint="view pdf file">
+            //   <FileThumbnail
+            //     uri={file.url}
+            //     mimeType={mimeType}
+            //     thumbnailSize={fileDisplaySizeMap[namespace]}
+            //     accessibilityHint="view pdf file"
+            //     // onPress={() =>
+            //     //   setFileView({
+            //     //     // content: <PdfViewer url={file.url} />,
+            //     //     content: <PdfViewer url={pdfUrl} />,
+            //     //     title: originalFilename || '',
+            //     //   })
+            //     // }
+            //   />
+            // </WebBrowserLink>
             <FileThumbnail
               uri={file.url}
               mimeType={mimeType}
@@ -92,7 +110,8 @@ export default function FileScreenComponent(props: TFileScreenComponent) {
               accessibilityHint="view pdf file"
               onPress={() =>
                 setFileView({
-                  content: <PdfViewer url={file.url} />,
+                  // content: <PdfViewer url={file.url} />,
+                  content: <PdfViewer url={pdfUrl} />,
                   title: originalFilename || '',
                 })
               }
--- libs/expo/shared/ui-components/src/lib/PdfViewer/PdfViewer.tsx
+++ libs/expo/shared/ui-components/src/lib/PdfViewer/PdfViewer.tsx
@@ -1,4 +1,8 @@
-import PDF from 'react-native-pdf';
+import * as FileSystem from 'expo-file-system';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { WebView } from 'react-native-webview';
+// import PDF from 'react-native-pdf';
 
 type TProps = {
   url?: string;
@@ -8,17 +12,55 @@ type TProps = {
 export default function PdfViewer(props: TProps) {
   const { url, cache = true } = props;
 
+  const [fileUri, setFileUri] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!url) return;
+
+    const downloadPdf = async () => {
+      try {
+        const fileUri = FileSystem.cacheDirectory + 'temp.pdf';
+        const { uri } = await FileSystem.downloadAsync(url, fileUri);
+        setFileUri(uri);
+      } catch (error) {
+        console.error('Error downloading PDF:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    downloadPdf();
+  }, [url]);
+
   if (!url) {
     return null;
   }
 
+  if (loading) return <ActivityIndicator size="large" color="blue" />;
+
   return (
-    <PDF
-      source={{
-        uri: url,
-        cache,
-      }}
-      style={{ flex: 1 }}
-    />
+    <WebView source={{ uri: fileUri }} style={{ flex: 1 }} />
+
+    // <PDF
+    //   source={{
+    //     uri: url,
+    //     cache,
+    //   }}
+    //   style={{ flex: 1 }}
+    //   onLoadComplete={(numberOfPages, filePath) => {
+    //     console.log(`Number of pages: ${numberOfPages}`);
+    //   }}
+    //   onPageChanged={(page, numberOfPages) => {
+    //     console.log('numberOfPages: ', numberOfPages);
+    //     console.log(`Current page: ${page}`);
+    //   }}
+    //   onError={(error) => {
+    //     console.log(error);
+    //   }}
+    //   onPressLink={(uri) => {
+    //     console.log(`Link pressed: ${uri}`);
+    //   }}
+    // />
   );
 }
