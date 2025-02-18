2bfe0d296b3a7b94807a112b45503e2b4c21ca6f
Jesse Luna <90876085+jluna2000@users.noreply.github.com>
2025-02-18 07:44:35 -0800

    Add interaction authors endpoint and filter (DEV-1215, DEV-1216) (#1029)

--- libs/expo/betterangels/src/lib/apollo/graphql/__generated__/types.ts
+++ libs/expo/betterangels/src/lib/apollo/graphql/__generated__/types.ts
@@ -708,6 +708,31 @@ export type ImmediateNeedType = {
   name?: Maybe<ImmediateNeedChoices>;
 };
 
+export type InteractionAuthorFilter = {
+  AND?: InputMaybe<InteractionAuthorFilter>;
+  DISTINCT?: InputMaybe<Scalars['Boolean']['input']>;
+  NOT?: InputMaybe<InteractionAuthorFilter>;
+  OR?: InputMaybe<InteractionAuthorFilter>;
+  search?: InputMaybe<Scalars['String']['input']>;
+};
+
+export type InteractionAuthorType = {
+  __typename?: 'InteractionAuthorType';
+  firstName?: Maybe<Scalars['String']['output']>;
+  id: Scalars['ID']['output'];
+  lastName?: Maybe<Scalars['String']['output']>;
+  middleName?: Maybe<Scalars['String']['output']>;
+};
+
+export type InteractionAuthorTypeOffsetPaginated = {
+  __typename?: 'InteractionAuthorTypeOffsetPaginated';
+  pageInfo: OffsetPaginationInfo;
+  /** List of paginated results. */
+  results: Array<InteractionAuthorType>;
+  /** Total count of existing results. */
+  totalCount: Scalars['Int']['output'];
+};
+
 export enum LanguageEnum {
   Arabic = 'ARABIC',
   Armenian = 'ARMENIAN',
@@ -1233,6 +1258,7 @@ export type Query = {
   clientProfilesPaginated: ClientProfileTypeOffsetPaginated;
   currentUser: UserType;
   featureControls: FeatureControlData;
+  interactionAuthors: InteractionAuthorTypeOffsetPaginated;
   note: NoteType;
   noteAttachment: NoteAttachmentType;
   noteAttachments: Array<NoteAttachmentType>;
@@ -1277,6 +1303,12 @@ export type QueryClientProfilesArgs = {
 export type QueryClientProfilesPaginatedArgs = {
   filters?: InputMaybe<ClientProfileFilter>;
   order?: InputMaybe<ClientProfileOrder>;
+  pagination?: InputMaybe<OffsetPaginationInput>;
+};
+
+
+export type QueryInteractionAuthorsArgs = {
+  filters?: InputMaybe<InteractionAuthorFilter>;
   pagination?: InputMaybe<OffsetPaginationInput>;
 };
 
