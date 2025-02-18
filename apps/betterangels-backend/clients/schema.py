be3c33f0b9976cae77cf99905f7056e43c8ae5ad
Mike Feldberg <mike@betterangels.la>
2025-02-18 11:49:11 -0800

    add discrete active clients query (DEV-1486) (#1058)

--- apps/betterangels-backend/clients/schema.py
+++ apps/betterangels-backend/clients/schema.py
@@ -242,6 +242,10 @@ class Query:
         extensions=[HasRetvalPerm(perms=[ClientProfilePermissions.VIEW])],
     )
 
+    active_client_profiles: OffsetPaginated[ClientProfileType] = strawberry_django.offset_paginated(
+        extensions=[HasRetvalPerm(perms=[ClientProfilePermissions.VIEW])],
+    )
+
     client_profiles_paginated: OffsetPaginated[ClientProfileType] = strawberry_django.offset_paginated(
         extensions=[HasRetvalPerm(perms=[ClientProfilePermissions.VIEW])],
     )
