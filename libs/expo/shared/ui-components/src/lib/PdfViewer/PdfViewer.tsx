ea2cc090fb7aa2e031fecb53d221a8ec2c09e383
Tom Glaz <tgmessages@gmail.com>
2025-02-19 18:44:57 -0800

    retry react-native-pdf with expo-file-system

--- libs/expo/shared/ui-components/src/lib/PdfViewer/PdfViewer.tsx
+++ libs/expo/shared/ui-components/src/lib/PdfViewer/PdfViewer.tsx
@@ -1,72 +1,70 @@
-// import PDF from 'react-native-pdf';
 import * as FileSystem from 'expo-file-system';
 import { useEffect, useState } from 'react';
-import { WebView } from 'react-native-webview';
+import { ActivityIndicator, Dimensions, View } from 'react-native';
+import PDF from 'react-native-pdf';
 
 type TProps = {
   url?: string;
   cache?: boolean;
 };
 
-export default function PdfViewer(props: TProps) {
-  console.log('################################### PdfViewer');
-  // const { url, cache = true } = props;
-  const { url } = props;
-
-  const [fileUri, setFileUri] = useState<string | null>(null);
-  const [loading, setLoading] = useState(true);
-
-  console.log('################################### loading: ', loading);
+export default function PdfViewer({ url, cache = true }: TProps) {
+  const [localPdfUri, setLocalPdfUri] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    if (!url) return;
+    async function downloadAndSetPdf() {
+      if (!url) return;
 
-    const downloadPdf = async () => {
       try {
-        const fileUri = FileSystem.cacheDirectory + 'temp.pdf';
+        if (url.startsWith('file://')) {
+          setLocalPdfUri(url);
+          setLoading(false);
+          return;
+        }
+
+        const fileUri = `${FileSystem.documentDirectory}temp.pdf`;
+        console.log(`Downloading PDF from ${url}...`);
 
         const { uri } = await FileSystem.downloadAsync(url, fileUri);
+        console.log(`PDF saved to: ${uri}`);
 
-        setFileUri(uri);
+        setLocalPdfUri(uri);
       } catch (error) {
-        console.error('Error downloading PDF:', error);
+        console.error('Failed to download PDF:', error);
       } finally {
         setLoading(false);
       }
-    };
+    }
 
-    downloadPdf();
+    downloadAndSetPdf();
   }, [url]);
 
-  if (!fileUri) {
-    return null;
+  if (!url) return null;
+
+  if (loading) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
   }
 
-  // if (loading) return <ActivityIndicator size="large" color="blue" />;
-
   return (
-    // <></>
-    <WebView source={{ uri: fileUri }} style={{ flex: 1 }} />
-
-    // <PDF
-    //   source={{
-    //     uri: url,
-    //     cache,
-    //   }}
-    //   style={{ flex: 1 }}
-    //   onLoadComplete={(numberOfPages, filePath) => {
-    //     console.log(`Number of pages: ${numberOfPages}`);
-    //   }}
-    //   onPageChanged={(page, numberOfPages) => {
-    //     console.log('numberOfPages: ', numberOfPages);
-    //     console.log(`Current page: ${page}`);
-    //   }}
-    //   onError={(error) => {
-    //     console.log(error);
-    //   }}
-    //   onPressLink={(uri) => {
-    //     console.log(`Link pressed: ${uri}`);
-    //   }}
-    // />
+    <View style={{ flex: 1 }}>
+      <PDF
+        source={{ uri: localPdfUri || url, cache }}
+        style={{ flex: 1, width: Dimensions.get('window').width }}
+        onLoadComplete={(numberOfPages, filePath) => {
+          console.log(`Number of pages: ${numberOfPages}`);
+        }}
+        onPageChanged={(page, numberOfPages) => {
+          console.log('numberOfPages: ', numberOfPages);
+          console.log(`Current page: ${page}`);
+        }}
+        onError={(error) => {
+          console.error('PDF Load Error:', error);
+        }}
+        onPressLink={(uri) => {
+          console.log(`Link pressed: ${uri}`);
+        }}
+      />
+    </View>
   );
 }
