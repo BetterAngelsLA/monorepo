f149d4f87de7ccbabdcd2706c98926f3877f7d1d
Mike Feldberg <mike@betterangels.la>
2025-02-19 18:19:53 -0800

    wip CA id

--- libs/expo/betterangels/src/lib/helpers/parseClientProfileErrors.ts
+++ libs/expo/betterangels/src/lib/helpers/parseClientProfileErrors.ts
@@ -5,10 +5,11 @@ export type TValidationError = {
 };
 
 export const ERROR_MESSAGE_MAP: Record<string, string> = {
+  CA_ID_IN_USE: 'California ID must be 1 letter followed by 7 numbers',
   EMAIL_IN_USE: 'User with this Email already exists',
-  NO_NAME_PROVIDED: 'Filling out one of the fields is required',
-  INVALID_PHONE_NUMBER: 'Please enter a valid 10-digit phone number',
   HMIS_ID_IN_USE: 'HMIS ID in use by another client',
+  INVALID_PHONE_NUMBER: 'Please enter a valid 10-digit phone number',
+  NO_NAME_PROVIDED: 'Filling out one of the fields is required',
 };
 
 export function parseValidationErrors(
