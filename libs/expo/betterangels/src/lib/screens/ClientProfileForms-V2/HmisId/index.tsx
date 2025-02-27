ceed31a46dc08b3fd7d0fde219670328f08aaf77
Davit-BetterAngels <141072810+Davit-BetterAngels@users.noreply.github.com>
2025-02-27 09:30:01 -0800

    Edit Screen Redesign (DEV-1220) (#1080)
    * added edit screen
    
    * type issue
    
    * title null by default

---
+++ libs/expo/betterangels/src/lib/screens/ClientProfileForms-V2/HmisId/index.tsx
@@ -0,0 +1,10 @@
+import { Slot } from 'expo-router';
+import { View } from 'react-native';
+
+export default function HmisId() {
+  return (
+    <View>
+      <Slot />
+    </View>
+  );
+}
---
+++ libs/expo/betterangels/src/lib/screens/ClientProfileForms-V2/RelevantContact/index.tsx
@@ -0,0 +1,10 @@
+import { Slot } from 'expo-router';
+import { View } from 'react-native';
+
+export default function RelevantContact() {
+  return (
+    <View>
+      <Slot />
+    </View>
+  );
+}
