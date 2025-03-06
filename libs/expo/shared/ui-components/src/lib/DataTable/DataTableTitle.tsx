29d844eb094f8a04f0d9f1a8cd8dff81af10ae07
Tom Glaz <tgmessages@gmail.com>
2025-03-06 08:56:09 -0800

    remove usage of TextOrNode

--- libs/expo/shared/ui-components/src/lib/DataTable/DataTableTitle.tsx
+++ libs/expo/shared/ui-components/src/lib/DataTable/DataTableTitle.tsx
@@ -1,11 +1,7 @@
 import { Colors, FontSizes, Spacings } from '@monorepo/expo/shared/static';
 import { ReactNode } from 'react';
 import { StyleProp, StyleSheet, TextStyle, ViewStyle } from 'react-native';
-import {
-  Provider as PaperProvider,
-  DataTable as RnpDataTable,
-} from 'react-native-paper';
-import TextOrNode from '../TextOrNode';
+import { DataTable as RnpDataTable } from 'react-native-paper';
 
 export type TDataTableTitle = {
   viewStyle?: StyleProp<ViewStyle>;
@@ -18,17 +14,13 @@ export function DataTableTitle(props: TDataTableTitle) {
   const { children, maxLines = 1, textStyle, viewStyle } = props;
 
   return (
-    <PaperProvider theme={{}}>
-      <RnpDataTable.Title
-        theme={{ fonts: {}, colors: {} }}
-        style={[styles.defaultViewStyle, viewStyle]}
-        numberOfLines={maxLines}
-      >
-        <TextOrNode textStyle={[styles.defaultTextStyle, textStyle]}>
-          {children}
-        </TextOrNode>
-      </RnpDataTable.Title>
-    </PaperProvider>
+    <RnpDataTable.Title
+      style={[styles.defaultViewStyle, viewStyle]}
+      textStyle={[styles.defaultTextStyle, textStyle]}
+      numberOfLines={maxLines}
+    >
+      {children}
+    </RnpDataTable.Title>
   );
 }
 
@@ -42,6 +34,6 @@ const styles = StyleSheet.create({
     fontSize: FontSizes.sm.fontSize,
     lineHeight: FontSizes.sm.lineHeight,
     fontFamily: 'Poppins-SemiBold',
-    fontWeight: 700,
+    fontWeight: 700, // RnpDataTable theme seems to use fontWeight
   },
 });
