5e52d10af618e8f1527eeec5a83d0a806f4aeb5c
Tom Glaz <tgmessages@gmail.com>
2025-03-11 18:07:27 -0700

    add HmisProfiles and HouseholdMember cards (#1108)

---
+++ libs/expo/betterangels/src/lib/screens/Client/ClientProfile_V2/ClientProfileCards/HmisProfilesCard/HmisProfileCard.tsx
@@ -0,0 +1,38 @@
+import { View, ViewStyle } from 'react-native';
+import { enumDisplayHmisAgency } from '../../../../../static';
+import {
+  ClientProfileCard,
+  TClientProfileCardItem,
+} from '../../../../../ui-components';
+import { TClientProfileHmisProfile } from '../../types';
+
+type TProps = {
+  hmisProfile?: TClientProfileHmisProfile;
+  style?: ViewStyle;
+};
+
+export function HmisProfileCard(props: TProps) {
+  const { hmisProfile, style } = props;
+
+  if (!hmisProfile) {
+    return null;
+  }
+
+  const { hmisId, agency } = hmisProfile;
+
+  const content: TClientProfileCardItem[] = [
+    {
+      header: ['HMIS ID'],
+      rows: [[hmisId]],
+    },
+  ];
+
+  return (
+    <View style={style}>
+      <ClientProfileCard
+        title={enumDisplayHmisAgency[agency]}
+        items={content}
+      />
+    </View>
+  );
+}
