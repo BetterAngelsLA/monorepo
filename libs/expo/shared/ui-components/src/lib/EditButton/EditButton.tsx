04d0c9ec05b454720676c226b3172387465c541a
Tom Glaz <tgmessages@gmail.com>
2025-03-20 13:09:35 -0700

    move editButton to lib

---
+++ libs/expo/shared/ui-components/src/lib/EditButton/EditButton.tsx
@@ -0,0 +1,46 @@
+import { WFEdit } from '@monorepo/expo/shared/icons';
+import { Colors, Spacings } from '@monorepo/expo/shared/static';
+import { Pressable, StyleSheet, ViewStyle } from 'react-native';
+
+type TProps = {
+  onClick: () => void;
+  style?: ViewStyle;
+  accessibilityHint?: string;
+  pressedColor?: Colors;
+};
+
+export function EditButton(props: TProps) {
+  const {
+    onClick,
+    accessibilityHint,
+    pressedColor = Colors.GRAY_PRESSED,
+    style,
+  } = props;
+
+  return (
+    <Pressable
+      onPress={onClick}
+      accessible
+      style={({ pressed }) => [
+        {
+          backgroundColor: pressed ? pressedColor : undefined,
+          ...styles.button,
+        },
+        style,
+      ]}
+      accessibilityHint={accessibilityHint || 'edit'}
+      accessibilityRole="button"
+    >
+      <WFEdit size="sm" />
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: Spacings.xl,
+    height: Spacings.xl,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
