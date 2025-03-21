258ebc82190dea4eb7c092b231d874413d840b53
Tom Glaz <tgmessages@gmail.com>
2025-03-20 17:03:30 -0700

    add cpr ProfilePhotoField

--- libs/expo/betterangels/src/lib/screens/ClientProfileForms-V2/extractClientFormData.ts
+++ libs/expo/betterangels/src/lib/screens/ClientProfileForms-V2/extractClientFormData.ts
@@ -27,6 +27,7 @@ export const extractClientFormData = (
         preferredLanguage,
         veteranStatus,
         livingSituation,
+        profilePhoto,
       } = clientProfile;
 
       return {
@@ -36,6 +37,7 @@ export const extractClientFormData = (
         preferredLanguage,
         veteranStatus,
         livingSituation,
+        profilePhoto,
       } as PersonalInfoState;
     }
     case 'importantNotes': {
