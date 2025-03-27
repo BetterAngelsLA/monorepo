991a22524b93db46b3da8137d528389040a41839
Paul Vecchio <paul@betterangels.la>
2025-03-27 11:18:11 -0700

    Disable username and password login

--- apps/betterangels-backend/betterangels_backend/admin.py
+++ apps/betterangels-backend/betterangels_backend/admin.py
@@ -1,9 +1,11 @@
 from typing import Any, Dict, Optional
 
 from accounts.services import send_magic_link
+from betterangels_backend import settings
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 
 UserModel = get_user_model()
 
@@ -11,20 +13,18 @@ UserModel = get_user_model()
 class MagicLinkAdminSite(admin.AdminSite):
     def login(self, request: HttpRequest, extra_context: Optional[Dict[str, Any]] = None) -> HttpResponse:
         context = extra_context or {}
+        is_local_dev = getattr(settings, "IS_LOCAL_DEV", False)
+        context["is_local_dev"] = is_local_dev
 
         if request.method == "POST" and "email" in request.POST:
             email = request.POST["email"]
-            user = UserModel.objects.filter(email=email, is_staff=True).first()
-
-            # Only send if the user exists
-            if user:
+            if UserModel.objects.filter(email=email, is_staff=True).exists():
                 send_magic_link(email, request)
-
-            # Always show a success message (avoid user enumeration)
-            context["email_sent"] = True
-            context["sent_email_address"] = email
-
-            # Switch the request to GET so default admin login logic just shows the form
+            context.update({"email_sent": True, "sent_email_address": email})
+            if not is_local_dev:
+                # In production, render the login page directly (disable username/password auth)
+                return render(request, "admin/login.html", context)
+            # In local development, switch the method to GET to fall back on default login
             request.method = "GET"
 
         return super().login(request, context)
