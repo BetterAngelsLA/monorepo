e730da695a014baf5718c98c632d60e0844c10f6
Mike Feldberg <mike@betterangels.la>
2025-04-01 18:08:03 -0700

    update query tests

--- apps/betterangels-backend/notes/tests/test_queries.py
+++ apps/betterangels-backend/notes/tests/test_queries.py
@@ -51,10 +51,7 @@ class NoteQueryTestCase(NoteGraphQLBaseTestCase):
         self._create_note_mood_fixture(
             {"descriptor": "EUTHYMIC", "noteId": note_id},
         )
-        # Add purposes and next steps
         note = Note.objects.get(pk=note_id)
-        note.purposes.set([self.purpose_1["id"], self.purpose_2["id"]])
-        note.next_steps.set([self.next_step_1["id"], self.next_step_2["id"]])
         note.provided_services.set(self.provided_services)
         note.requested_services.set(self.requested_services)
 
@@ -95,14 +92,6 @@ class NoteQueryTestCase(NoteGraphQLBaseTestCase):
                 "pointOfInterest": self.point_of_interest,
             },
             "moods": [{"descriptor": "ANXIOUS"}, {"descriptor": "EUTHYMIC"}],
-            "purposes": [
-                {"id": self.purpose_1["id"], "title": self.purpose_1["title"]},
-                {"id": self.purpose_2["id"], "title": self.purpose_2["title"]},
-            ],
-            "nextSteps": [
-                {"id": self.next_step_1["id"], "title": self.next_step_1["title"], "location": None},
-                {"id": self.next_step_2["id"], "title": self.next_step_2["title"], "location": None},
-            ],
             "providedServices": [
                 {
                     "id": str(self.provided_services[0].id),
