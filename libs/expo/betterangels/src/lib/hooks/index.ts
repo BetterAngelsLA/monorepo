bd51774d870700049ffd4ad6a285f233a8224469
Tom Glaz <tgmessages@gmail.com>
2025-04-08 09:36:01 -0700

    minor

--- libs/expo/betterangels/src/lib/hooks/index.ts
+++ libs/expo/betterangels/src/lib/hooks/index.ts
@@ -1,11 +1,11 @@
 export { default as useCaliforniaIdUniqueCheck } from './californiaIdUniqueCheck/useCaliforniaIdUniqueCheck';
+export { default as useAppVersion } from './expoUpdates/useAppVersion';
 export { default as useFeatureFlagActive } from './featureFlag/useFeatureFlagActive';
 export { default as useKeyboardToolbar } from './keyboardToolbar/useKeyboardToolbar';
 export { default as useInfiniteScroll } from './scroll/useInfiniteScroll';
 export { default as useScrollToScreenTop } from './scroll/useScrollToScreenTop';
 export { default as useSnackbar } from './snackbar/useSnackbar';
 // user
-export { default as useAppVersion } from './expoUpdates/useAppVersion';
 export { default as useSignIn } from './user/useSignIn';
 export { default as useSignOut } from './user/useSignOut';
 export { default as useUser } from './user/useUser';
