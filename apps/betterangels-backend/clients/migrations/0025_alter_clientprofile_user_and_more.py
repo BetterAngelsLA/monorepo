b0773db702f7d4677b0289953c9fe83ce46f9077
Mike Feldberg <mike@betterangels.la>
2025-04-08 17:45:52 -0700

    migration conflict

--- apps/betterangels-backend/clients/migrations/0025_alter_clientprofile_user_and_more.py
+++ apps/betterangels-backend/clients/migrations/0025_alter_clientprofile_user_and_more.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("clients", "0023_clientprofile_user_required"),
+        ("clients", "0024_clientcontact_permissions"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
