b5b6d9d1b92f0abe7967d02cfdd1fdeeb39c5794
Tom Glaz <tgmessages@gmail.com>
2025-04-15 19:19:01 -0700

    update useAppState

--- libs/expo/betterangels/src/lib/hooks/appState/useAppState.ts
+++ libs/expo/betterangels/src/lib/hooks/appState/useAppState.ts
@@ -1,33 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import {
   AppState,
   AppStateStatus,
   NativeEventSubscription,
 } from 'react-native';
 
-let appStateSubscription: NativeEventSubscription | null = null;
-
 export default function useAppState() {
+  const appStateSubscription = useRef<NativeEventSubscription | null>(null);
+
+  const [becameActive, setBecameActive] = useState(false);
   const [currentState, setCurrentState] = useState<AppStateStatus>(
     AppState.currentState
   );
-  const [becameActive, setBecameActive] = useState(false);
 
-  useEffect(() => {
-    if (!appStateSubscription) {
-      appStateSubscription = AppState.addEventListener(
-        'change',
-        handleAppStateChange
-      );
-    }
-
-    return () => {
-      appStateSubscription?.remove();
-      appStateSubscription = null;
-    };
-  }, []);
-
-  const handleAppStateChange = (newState: AppStateStatus) => {
+  const handleAppStateChange = useCallback((newState: AppStateStatus) => {
     setCurrentState((prevState) => {
       const becameActive = prevState !== 'active' && newState === 'active';
 
@@ -35,7 +21,21 @@ export default function useAppState() {
 
       return newState;
     });
-  };
+  }, []);
+
+  useEffect(() => {
+    if (!appStateSubscription.current) {
+      appStateSubscription.current = AppState.addEventListener(
+        'change',
+        handleAppStateChange
+      );
+    }
+
+    return () => {
+      appStateSubscription.current?.remove();
+      appStateSubscription.current = null;
+    };
+  }, [handleAppStateChange]);
 
   return {
     currentAppState: currentState,
