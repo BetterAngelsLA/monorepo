0fdb24bbaf176d7f5923f1ceb8c83d16d0c84bf5
Tom Glaz <tgmessages@gmail.com>
2025-04-15 20:12:24 -0700

    update ExpandableContainer to manage open/closed state

--- libs/expo/shared/ui-components/src/lib/ExpandableContainer/ExpandableContainer.tsx
+++ libs/expo/shared/ui-components/src/lib/ExpandableContainer/ExpandableContainer.tsx
@@ -6,7 +6,7 @@ import {
   TMarginProps,
   getMarginStyles,
 } from '@monorepo/expo/shared/static';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import {
   Pressable,
   StyleSheet,
@@ -17,7 +17,7 @@ import {
 import TextOrNode from '../TextOrNode';
 
 interface IExpandableContainer extends TMarginProps {
-  onClick?: () => void;
+  onClick?: (newOpen: boolean) => void;
   isOpen?: boolean;
   title?: string | ReactNode;
   icon?: ReactNode;
@@ -45,44 +45,57 @@ export function ExpandableContainer(props: IExpandableContainer) {
     onClick,
   } = props;
 
+  const [expanded, setExpanded] = useState<boolean>(!!isOpen);
+
+  useEffect(() => {
+    setExpanded(!!isOpen);
+  }, [isOpen]);
+
   const accessibilityHintTitle = typeof title === 'string' ? title : 'section';
 
-  const accessibilityHintText = isOpen
+  const accessibilityHintText = expanded
     ? `close ${accessibilityHintTitle}`
     : `open ${accessibilityHintTitle}`;
 
+  function onExpandCollapse() {
+    const newExpanded = !expanded;
+
+    setExpanded(newExpanded);
+
+    onClick && onClick(newExpanded);
+  }
+
   return (
     <View style={[styles.container, getMarginStyles(props), style]}>
-      <Pressable
-        onPress={onClick}
-        disabled={disabled}
-        accessible
-        accessibilityRole="button"
-        accessibilityHint={accessibilityHintText}
-        style={({ pressed }) => [
-          {
-            backgroundColor: pressed ? bgPressed : undefined,
-            maxWidth: '100%',
-            width: '100%',
-            display: 'flex',
-          },
-        ]}
-      >
-        {header}
-        {!header && (
-          <View style={[styles.headerView, stylesHeader]}>
+      {header && header}
+      {!header && (
+        <Pressable
+          onPress={onExpandCollapse}
+          disabled={disabled}
+          accessible
+          accessibilityRole="button"
+          accessibilityHint={accessibilityHintText}
+          style={({ pressed }) => [
+            { backgroundColor: pressed ? bgPressed : undefined },
+            styles.defaultHeaderPressable,
+          ]}
+        >
+          <View style={[styles.defaultHeaderView, stylesHeader]}>
             <TextOrNode textStyle={[styles.stylesTitleText, stylesTitleText]}>
               {title}
             </TextOrNode>
 
             {icon}
             {!icon && (
-              <ChevronLeftIcon size="sm" rotate={isOpen ? '90deg' : '-90deg'} />
+              <ChevronLeftIcon
+                size="sm"
+                rotate={expanded ? '90deg' : '-90deg'}
+              />
             )}
           </View>
-        )}
-      </Pressable>
-      {isOpen && <View>{children}</View>}
+        </Pressable>
+      )}
+      {expanded && <View>{children}</View>}
     </View>
   );
 }
@@ -92,10 +105,14 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
     width: '100%',
   },
-  headerView: {
+  defaultHeaderPressable: {
+    maxWidth: '100%',
+    width: '100%',
+    display: 'flex',
+  },
+  defaultHeaderView: {
     justifyContent: 'space-between',
     flexDirection: 'row',
-    paddingHorizontal: Spacings.xs,
     paddingVertical: Spacings.sm,
     alignItems: 'center',
   },
