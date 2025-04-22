396f5dacf17113698c7e0d2f0514645d565fd9d2
Mike Feldberg <mike@betterangels.la>
2025-04-22 14:05:27 -0700

    style button

--- apps/shelter-web/src/app/shared/components/map/controls/searchMapAreaButton.tsx
+++ apps/shelter-web/src/app/shared/components/map/controls/searchMapAreaButton.tsx
@@ -7,13 +7,19 @@ type TProps = {
 export function SearchMapAreaButton(props: TProps) {
   const { onClick } = props;
 
-  const searchMapAreaCss = ['drop-shadow-lg', 'mt-3', 'text-primary-60'];
+  const searchMapAreaCss = [
+    '[box-shadow:0_3px_1px_0_#00000040]',
+    'mt-3',
+    'font-primary',
+    'rounded-lg',
+    'text-primary-60',
+  ];
 
   return (
     <Button
       onClick={onClick}
       variant="secondary"
-      size="md"
+      size="lg"
       className={mergeCss(searchMapAreaCss)}
     >
       Search this area
