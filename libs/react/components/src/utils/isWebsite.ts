0905a2017afe6f86abc42d65c72b53452643d0ab
Davit-BetterAngels <davit@betterangels.la>
2025-04-23 18:49:02 +0000

    safety check for isWebsite

--- libs/react/components/src/utils/isWebsite.ts
+++ libs/react/components/src/utils/isWebsite.ts
@@ -1,6 +1,10 @@
 import validator from 'validator';
 
 export default function isWebsite(value: string) {
+  if (typeof value !== 'string' || !value.trim()) {
+    return false;
+  }
+
   return validator.isURL(value, {
     protocols: ['http', 'https'],
     require_protocol: false,
