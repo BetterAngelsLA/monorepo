23e66474390a86a34a5d9a6b3813d2cc41114bd7
Tom Glaz <tgmessages@gmail.com>
2025-04-25 14:25:48 -0700

    CPR: Edit Household (DEV-1194) (#1254)
    * use gender and not displayGender in HouseholdMember card
    
    * add initial Household edit view and form
    
    * update houshold forms
    
    * some cleanup
    
    * update HmisProfileForm.tsx to refetch using lazyQuery
    
    * extract submitHouseholdMemberForm to file
    
    * update onSubmit helpter
    
    * typo fix
    
    * cleanup edits
    
    * add DatePicker_V2 for CPR household form (DEV-1194) (#1257)
    
    * add DatePicker_V2 for CPR household form
    
    * remove debug style and update onChange

--- libs/expo/betterangels/src/lib/screens/ClientProfileForms-V2/relatedClientProfileModel/forms/DeleteButton.tsx
+++ libs/expo/betterangels/src/lib/screens/ClientProfileForms-V2/relatedClientProfileModel/forms/DeleteButton.tsx
@@ -3,17 +3,21 @@ import { DeleteModal, TextButton } from '@monorepo/expo/shared/ui-components';
 
 type TProps = {
   deleteableItemName: string;
+  deleteableItemNameBody?: string;
   onDelete: () => void;
   disabled?: boolean;
 };
 
 export function DeleteButton(props: TProps) {
-  const { deleteableItemName, disabled, onDelete } = props;
+  const { deleteableItemName, deleteableItemNameBody, disabled, onDelete } =
+    props;
 
   return (
     <DeleteModal
       title={`Delete ${deleteableItemName}?`}
-      body={`All data associated with this ${deleteableItemName} will be deleted.`}
+      body={`All data associated with this ${
+        deleteableItemNameBody || deleteableItemName
+      } will be deleted.`}
       onDelete={onDelete}
       deleteableItemName={deleteableItemName}
       button={
