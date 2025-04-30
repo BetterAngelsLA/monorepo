d462fb6a1eed35fbd1d4e5b28d8392b1acc22802
Tom Glaz <tgmessages@gmail.com>
2025-04-30 12:52:13 -0700

    adjust to allow AddressAutocomplete scrollTop on focus

--- libs/expo/betterangels/src/lib/screens/ClientProfileForms-V2/relatedClientProfileModel/forms/ClientContactsForm/ClientContactForm.tsx
+++ libs/expo/betterangels/src/lib/screens/ClientProfileForms-V2/relatedClientProfileModel/forms/ClientContactsForm/ClientContactForm.tsx
@@ -6,8 +6,9 @@ import {
   TextRegular,
 } from '@monorepo/expo/shared/ui-components';
 import { useRouter } from 'expo-router';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Controller, useForm, useWatch } from 'react-hook-form';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import { useSnackbar } from '../../../../../hooks';
 import {
   ClientProfileSectionEnum,
@@ -129,119 +130,131 @@ export function ClientContactForm(props: TProps) {
     }
   };
 
+  const scrollRef = useRef<ScrollView | null>(null);
+
   return (
     <Form.Page
+      scrollViewRef={scrollRef}
       actionProps={{
         onSubmit: handleSubmit(onSubmit),
         onLeftBtnClick: router.back,
         disabled: isLoading,
       }}
     >
-      <TextRegular
-        mb="lg"
-        color={isSubmitted && isError ? Colors.ERROR : undefined}
-      >
-        Please select the type of relevant contact and enter at least one form
-        of contact information.
-      </TextRegular>
+      <View style={styles.formWrapper}>
+        <TextRegular
+          mb="lg"
+          color={isSubmitted && isError ? Colors.ERROR : undefined}
+        >
+          Please select the type of relevant contact and enter at least one form
+          of contact information.
+        </TextRegular>
 
-      <Form>
-        <Form.Fieldset>
-          <Controller
-            name="relationshipToClient"
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <SingleSelect
-                disabled={isLoading}
-                label="Relevant contact type"
-                placeholder="Select the relevant contact type"
-                items={Object.entries(clientRelevantContactEnumDisplay).map(
-                  ([value, displayValue]) => ({ value, displayValue })
-                )}
-                selectedValue={value}
-                onChange={(value) => onChange(value)}
-              />
-            )}
-          />
+        <Form>
+          <Form.Fieldset>
+            <Controller
+              name="relationshipToClient"
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <SingleSelect
+                  disabled={isLoading}
+                  label="Relevant contact type"
+                  placeholder="Select the relevant contact type"
+                  items={Object.entries(clientRelevantContactEnumDisplay).map(
+                    ([value, displayValue]) => ({ value, displayValue })
+                  )}
+                  selectedValue={value}
+                  onChange={(value) => onChange(value)}
+                />
+              )}
+            />
 
-          <ControlledInput
-            name={'name'}
-            control={control}
+            <ControlledInput
+              name={'name'}
+              control={control}
+              disabled={isLoading}
+              label={'Name'}
+              placeholder={'Enter name'}
+              onDelete={() => {
+                setValue('name', '');
+                clearErrors('name');
+              }}
+              error={!!errors.name}
+              errorMessage={errors.name?.message}
+            />
+
+            <ControlledInput
+              name="email"
+              control={control}
+              disabled={isLoading}
+              label="Email"
+              placeholder="Enter email address"
+              onDelete={() => {
+                setValue('email', '');
+                clearErrors('email');
+              }}
+              error={!!errors.email}
+              errorMessage={(errors.email?.message as string) || undefined}
+              rules={{
+                validate: (value: string) => {
+                  if (value && !Regex.email.test(value)) {
+                    return 'Enter a valid email address';
+                  }
+
+                  return true;
+                },
+              }}
+            />
+
+            <ControlledInput
+              name={'phoneNumber'}
+              control={control}
+              disabled={isLoading}
+              label={'Phone Number'}
+              placeholder="Enter phone number"
+              keyboardType="number-pad"
+              onDelete={() => {
+                setValue('phoneNumber', '');
+                clearErrors('phoneNumber');
+              }}
+              error={!!errors.phoneNumber}
+              errorMessage={errors.phoneNumber?.message}
+              rules={{
+                validate: (value: string) => {
+                  if (value && !Regex.phoneNumber.test(value)) {
+                    return 'Enter a 10-digit phone number without space or special characters';
+                  }
+
+                  return true;
+                },
+              }}
+            />
+
+            <AddressAutocomplete
+              control={control}
+              label="Mailing Address"
+              name="mailingAddress"
+              placeholder="Enter mailing address"
+              focusScroll={{ scrollViewRef: scrollRef }}
+            />
+          </Form.Fieldset>
+        </Form>
+
+        {isEditMode && (
+          <ClientContactDeleteBtn
+            relationId={relationId}
+            clientProfileId={clientProfileId}
+            setIsLoading={setIsLoading}
             disabled={isLoading}
-            label={'Name'}
-            placeholder={'Enter name'}
-            onDelete={() => {
-              setValue('name', '');
-              clearErrors('name');
-            }}
-            error={!!errors.name}
-            errorMessage={errors.name?.message}
           />
-
-          <ControlledInput
-            name="email"
-            control={control}
-            disabled={isLoading}
-            label="Email"
-            placeholder="Enter email address"
-            onDelete={() => {
-              setValue('email', '');
-              clearErrors('email');
-            }}
-            error={!!errors.email}
-            errorMessage={(errors.email?.message as string) || undefined}
-            rules={{
-              validate: (value: string) => {
-                if (value && !Regex.email.test(value)) {
-                  return 'Enter a valid email address';
-                }
-
-                return true;
-              },
-            }}
-          />
-
-          <ControlledInput
-            name={'phoneNumber'}
-            control={control}
-            disabled={isLoading}
-            label={'Phone Number'}
-            placeholder="Enter phone number"
-            keyboardType="number-pad"
-            onDelete={() => {
-              setValue('phoneNumber', '');
-              clearErrors('phoneNumber');
-            }}
-            error={!!errors.phoneNumber}
-            errorMessage={errors.phoneNumber?.message}
-            rules={{
-              validate: (value: string) => {
-                if (value && !Regex.phoneNumber.test(value)) {
-                  return 'Enter a 10-digit phone number without space or special characters';
-                }
-
-                return true;
-              },
-            }}
-          />
-
-          <AddressAutocomplete
-            control={control}
-            label="Mailing Address"
-            name="mailingAddress"
-            placeholder="Enter mailing address"
-          />
-        </Form.Fieldset>
-      </Form>
-
-      {isEditMode && (
-        <ClientContactDeleteBtn
-          relationId={relationId}
-          clientProfileId={clientProfileId}
-          setIsLoading={setIsLoading}
-          disabled={isLoading}
-        />
-      )}
+        )}
+      </View>
     </Form.Page>
   );
 }
+
+const styles = StyleSheet.create({
+  formWrapper: {
+    paddingBottom: 240, // give view space to scrollTop on AddressAutocomplete focus
+  },
+});
