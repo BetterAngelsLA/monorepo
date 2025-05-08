83180ca2814dda1b6e9894552ff1aeb7a2a7fb56
Tom Glaz <tgmessages@gmail.com>
2025-05-08 13:27:30 -0700

    android adjust

--- libs/expo/betterangels/src/lib/screens/Client/ClientNavMenu/ClientNavMenu.tsx
+++ libs/expo/betterangels/src/lib/screens/Client/ClientNavMenu/ClientNavMenu.tsx
@@ -1,7 +1,7 @@
 import { DeleteIcon, ThreeDotIcon } from '@monorepo/expo/shared/icons';
 import { Colors } from '@monorepo/expo/shared/static';
 import { useState } from 'react';
-import { Pressable, StyleSheet } from 'react-native';
+import { Platform, Pressable, StyleSheet } from 'react-native';
 import Tooltip from 'react-native-walkthrough-tooltip';
 import { useDeleteClientProfile } from '../ClientProfile/hooks/useDeleteClientProfile';
 import { ClientNavMenuBtn } from './ClientNavMenuBtn';
@@ -41,7 +41,12 @@ export function ClientNavMenu(props: TProps) {
         right: 12,
       }}
       disableShadow={true}
-      tooltipStyle={styles.contentWrapper}
+      tooltipStyle={[
+        styles.contentWrapper,
+        {
+          marginTop: Platform.OS === 'android' ? 14 : 8,
+        },
+      ]}
       contentStyle={styles.content}
       content={
         <>
@@ -80,13 +85,13 @@ export function ClientNavMenu(props: TProps) {
 
 const styles = StyleSheet.create({
   contentWrapper: {
-    marginTop: 8,
     backgroundColor: Colors.WHITE,
     shadowColor: Colors.BLACK,
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 0.2,
     shadowRadius: 32,
     borderRadius: 12,
+    elevation: 12, // android
   },
   content: {
     padding: 0,
