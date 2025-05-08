a2f7aa01791bf51f4de2b16b613d084d3c2520e0
Davit-BetterAngels <davit@betterangels.la>
2025-05-08 21:54:00 +0000

    comment changes

--- libs/expo/betterangels/src/lib/hooks/newRelic/useNewRelic.ts
+++ libs/expo/betterangels/src/lib/hooks/newRelic/useNewRelic.ts
@@ -1,3 +1,4 @@
+import * as Updates from 'expo-updates';
 import NewRelic from 'newrelic-react-native-agent';
 import { useEffect } from 'react';
 import { Platform } from 'react-native';
@@ -7,7 +8,10 @@ export default function useNewRelic(version?: string, runtimeVersion?: string) {
     if (!version || !runtimeVersion) {
       return;
     }
-    const appToken = process.env['EXPO_PUBLIC_NEW_RELIC_MOBILE_LICENSE_KEY'];
+    const appToken =
+      Platform.OS === 'ios'
+        ? process.env['EXPO_PUBLIC_NEW_RELIC_MOBILE_LICENSE_KEY_IOS']
+        : process.env['EXPO_PUBLIC_NEW_RELIC_MOBILE_LICENSE_KEY_ANDROID'];
 
     if (!appToken) {
       console.warn('New Relic not initialized: missing mobile license key');
@@ -20,12 +24,16 @@ export default function useNewRelic(version?: string, runtimeVersion?: string) {
       interactionTracingEnabled: true,
       analyticsEventEnabled: true,
       loggingEnabled: true,
-      logLevel: NewRelic.LogLevel.INFO,
+      logLevel:
+        process.env['EXPO_PUBLIC_APP_ENV'] === 'production'
+          ? NewRelic.LogLevel.WARN
+          : NewRelic.LogLevel.INFO,
       customAttributes: {
         env: process.env['EXPO_PUBLIC_APP_ENV'],
         version,
         runtimeVersion,
         platform: Platform.OS,
+        otaUpdateId: Updates.updateId ?? 'none',
       },
     });
 
