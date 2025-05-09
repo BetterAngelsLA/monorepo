de661fe6fe0bb483303b0f84f2bb022a66599c0f
Tom Glaz <tgmessages@gmail.com>
2025-05-08 18:24:05 -0700

    render markers

--- libs/expo/betterangels/src/lib/ui-components/Map/Map.tsx
+++ libs/expo/betterangels/src/lib/ui-components/Map/Map.tsx
@@ -1,6 +1,8 @@
+import { LocationPinIcon } from '@monorepo/expo/shared/icons';
 import { StyleSheet } from 'react-native';
-import { MapView, PROVIDER_GOOGLE } from '../../maps';
+import { MapView, Marker, PROVIDER_GOOGLE } from '../../maps';
 import { LA_COUNTY_CENTER } from '../../services';
+import { laLocations } from './locations';
 
 type TProps = {};
 
@@ -8,14 +10,32 @@ export function BaMap(props?: TProps) {
   return (
     <MapView
       provider={PROVIDER_GOOGLE}
+      zoomEnabled
+      scrollEnabled
+      zoomControlEnabled
+      mapType="standard"
       style={styles.map}
       initialRegion={{
-        longitudeDelta: 0.05,
-        latitudeDelta: 0.05,
+        longitudeDelta: 0.1,
+        latitudeDelta: 0.1,
         latitude: LA_COUNTY_CENTER.lat,
         longitude: LA_COUNTY_CENTER.lng,
       }}
-    />
+    >
+      {laLocations.map((marker, idx) => (
+        <Marker key={idx} coordinate={marker} zIndex={99}>
+          <LocationPinIcon size="lg" />
+        </Marker>
+      ))}
+      {/* <Marker
+        coordinate={{
+          latitude: LA_COUNTY_CENTER.lat,
+          longitude: LA_COUNTY_CENTER?.lng,
+        }}
+      >
+        <LocationPinIcon size="2xl" />
+      </Marker> */}
+    </MapView>
   );
 }
 
