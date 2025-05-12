fd9e206fafaa28b09fdebeb2c1a45621e4b9a32f
Paul Vecchio <paul@betterangels.la>
2025-05-12 10:21:42 -0700

    Change tagging and pass logging variable

--- libs/expo/betterangels/src/lib/hooks/newRelic/useNewRelic.ts
+++ libs/expo/betterangels/src/lib/hooks/newRelic/useNewRelic.ts
@@ -3,20 +3,33 @@ import NewRelic from 'newrelic-react-native-agent';
 import { useEffect } from 'react';
 import { Platform } from 'react-native';
 
+const logLevelMap = {
+  ERROR: NewRelic.LogLevel.ERROR,
+  WARN: NewRelic.LogLevel.WARN,
+  INFO: NewRelic.LogLevel.INFO,
+  DEBUG: NewRelic.LogLevel.DEBUG,
+};
+
 export default function useNewRelic(version?: string, runtimeVersion?: string) {
   useEffect(() => {
-    if (!version || !runtimeVersion) {
-      return;
-    }
-    const appToken =
-      Platform.OS === 'ios'
-        ? process.env['EXPO_PUBLIC_NEW_RELIC_MOBILE_LICENSE_KEY_IOS']
-        : process.env['EXPO_PUBLIC_NEW_RELIC_MOBILE_LICENSE_KEY_ANDROID'];
+    if (!version || !runtimeVersion) return;
+
+    const platformKey = `EXPO_PUBLIC_NEW_RELIC_MOBILE_LICENSE_KEY_${Platform.OS.toUpperCase()}`;
+    const appToken = process.env[platformKey];
 
     if (!appToken) {
       console.warn('New Relic not initialized: missing mobile license key');
       return;
     }
+
+    const envLogLevel = process.env[
+      'EXPO_PUBLIC_NEW_RELIC_LOG_LEVEL'
+    ] as keyof typeof logLevelMap;
+    const logLevel = logLevelMap[envLogLevel] ?? NewRelic.LogLevel.DEBUG;
+
+    const combinedVersion = Updates.updateId
+      ? `${version}-${Updates.updateId}`
+      : version;
 
     NewRelic.startAgent(appToken, {
       crashReportingEnabled: true,
@@ -24,19 +37,17 @@ export default function useNewRelic(version?: string, runtimeVersion?: string) {
       interactionTracingEnabled: true,
       analyticsEventEnabled: true,
       loggingEnabled: true,
-      logLevel:
-        process.env['EXPO_PUBLIC_APP_ENV'] === 'production'
-          ? NewRelic.LogLevel.WARN
-          : NewRelic.LogLevel.INFO,
+      logLevel,
       customAttributes: {
         env: process.env['EXPO_PUBLIC_APP_ENV'],
         version,
         runtimeVersion,
         platform: Platform.OS,
-        otaUpdateId: Updates.updateId ?? 'none',
+        ...(Updates.updateId && { otaUpdateId: Updates.updateId }),
+        ...(Updates.channel && { channel: Updates.channel }),
       },
     });
 
-    NewRelic.setJSAppVersion(version);
+    NewRelic.setJSAppVersion(combinedVersion);
   }, [version, runtimeVersion]);
 }
