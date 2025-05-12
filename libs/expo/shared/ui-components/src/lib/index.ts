84d70ec7021036606c05b31167f2998522af3f9b
Tom Glaz <tgmessages@gmail.com>
2025-05-12 12:46:37 -0700

    remove Picker_v2 from index

--- libs/expo/shared/ui-components/src/lib/index.ts
+++ libs/expo/shared/ui-components/src/lib/index.ts
@@ -41,11 +41,11 @@ export { default as ImageViewer } from './ImageViewer';
 export * from './Input';
 export { default as KeyboardAwareScrollView } from './KeyboardAwareScrollView';
 export {
+  Length,
+  TLengthUnit,
   feetInchesToInches,
   getFormattedLength,
   inchesToFeetInches,
-  Length,
-  TLengthUnit,
 } from './Length';
 export { default as Loading } from './Loading';
 export { default as LoadingView } from './LoadingView';
@@ -54,7 +54,6 @@ export { BaseModal } from './Modal';
 export { default as MultiSelect } from './MultiSelect';
 export { default as PdfViewer } from './PdfViewer';
 export { default as Picker } from './Picker';
-export { default as Picker_V2 } from './Picker_V2';
 export { default as Pill } from './Pill';
 export { default as PillContainer } from './PillContainer';
 export {
@@ -71,10 +70,10 @@ export { default as Snackbar } from './Snackbar';
 export { SocialMediaIcon, TSocialMediaType } from './SocialMediaIcon';
 export { default as StatusBadge } from './StatusBadge';
 export { default as Tag } from './Tag';
-export { default as Textarea } from './Textarea';
 export { default as TextBold } from './TextBold';
 export { default as TextButton } from './TextButton';
 export { default as TextMedium } from './TextMedium';
 export { default as TextOrNode } from './TextOrNode';
 export { default as TextRegular } from './TextRegular';
+export { default as Textarea } from './Textarea';
 export * from './types';
