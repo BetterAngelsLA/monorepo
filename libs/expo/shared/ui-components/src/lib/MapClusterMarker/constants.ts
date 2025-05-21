9bb567be4caa107e9c32a3697c26832fafd768b6
Davit-BetterAngels <davit@betterangels.la>
2025-05-21 16:27:12 +0000

    map cluster marker icon

--- libs/expo/shared/ui-components/src/lib/MapClusterMarker/constants.ts
+++ libs/expo/shared/ui-components/src/lib/MapClusterMarker/constants.ts
@@ -1,14 +1,47 @@
+import { Colors } from '@monorepo/expo/shared/static';
+import { TMapClusterMarkerVariant } from './MapClusterMarker';
+
 export const SIZES = {
   S: {
-    font: { fontSize: 30, lineHeight: 24 },
+    fontSize: 14,
     size: 32,
+    houseIcon: 15,
+    houseContainer: 20,
+    subscriptAfterSize: 7,
+    outerPadding: 3,
   },
   M: {
-    font: { fontSize: 30, lineHeight: 24 },
+    fontSize: 20,
     size: 48,
+    houseIcon: 20,
+    houseContainer: 28,
+    subscriptAfterSize: 10,
+    outerPadding: 4,
   },
   L: {
-    font: { fontSize: 30, lineHeight: 24 },
+    fontSize: 30,
     size: 64,
+    houseIcon: 24,
+    houseContainer: 32,
+    subscriptAfterSize: 16,
+    outerPadding: 4,
   },
+} as const;
+
+export type TClusterSizeKey = keyof typeof SIZES;
+export type TClusterSize = (typeof SIZES)[TClusterSizeKey];
+
+type TVariantStyle = {
+  borderColor: Colors;
+  fillColor: Colors;
+  textColor: Colors;
 };
+
+export const variantStyleMap: Record<TMapClusterMarkerVariant, TVariantStyle> =
+  {
+    primary: {
+      borderColor: Colors.WHITE,
+      fillColor: Colors.ORANGE,
+      textColor: Colors.WHITE,
+    },
+  };
