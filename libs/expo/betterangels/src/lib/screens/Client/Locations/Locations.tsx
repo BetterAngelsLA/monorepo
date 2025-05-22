c5446622e7264a871ef47e5fe60136ee7cc75086
Davit-BetterAngels <davit@betterangels.la>
2025-05-22 21:54:15 +0000

    move locateMeButton under map

--- libs/expo/betterangels/src/lib/screens/Client/Locations/Locations.tsx
+++ libs/expo/betterangels/src/lib/screens/Client/Locations/Locations.tsx
@@ -1,6 +1,6 @@
-import { LocateMeButton, MapView } from '@monorepo/expo/shared/ui-components';
+import { MapView } from '@monorepo/expo/shared/ui-components';
 import { useRef } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { TMapView } from '../../../maps';
 import { useUserLocation } from './useUserLocation';
 
@@ -8,46 +8,13 @@ export function Locations() {
   const userLocation = useUserLocation();
   const mapRef = useRef<TMapView>(null);
 
-  const goToUserLocation = () => {
-    if (userLocation && mapRef.current) {
-      mapRef.current.animateToRegion(
-        {
-          latitude: userLocation.coords.latitude,
-          longitude: userLocation.coords.longitude,
-          latitudeDelta: 0.005,
-          longitudeDelta: 0.005,
-        },
-        500
-      );
-    }
-  };
-
   return (
-    <View>
-      <MapView
-        ref={mapRef}
-        showsUserLocation={userLocation ? true : false}
-        style={styles.map}
-      />
-      <LocateMeButton
-        style={styles.locateMeButton}
-        onPress={goToUserLocation}
-      />
-    </View>
+    <MapView ref={mapRef} userLocation={userLocation} style={styles.map} />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    position: 'relative',
-  },
   map: {
     height: 650,
-  },
-  locateMeButton: {
-    position: 'absolute',
-    right: 16,
-    bottom: '25%',
   },
 });
--- libs/expo/shared/ui-components/src/lib/Map/Map.tsx
+++ libs/expo/shared/ui-components/src/lib/Map/Map.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
-import { StyleProp, StyleSheet, ViewStyle } from 'react-native';
+import * as Location from 'expo-location';
+import { forwardRef, useState } from 'react';
+import { StyleProp, StyleSheet, View, ViewStyle } from 'react-native';
 import { Region } from 'react-native-maps';
+import LocateMeButton from '../LocateMeButton';
 import { defaultMapRegion } from './constants';
 import { RNMapView, TRNMapView } from './mapLib';
 
@@ -8,41 +10,73 @@ type TMapProps = {
   provider?: 'google';
   initialRegion?: Region;
   style?: StyleProp<ViewStyle>;
-  showsUserLocation?: boolean;
-  ref: React.Ref<TRNMapView>;
+  userLocation?: Location.LocationObject | null;
 };
 
-export function MapView(props: TMapProps) {
-  const {
-    provider: mapPovider = 'google',
-    initialRegion,
-    style,
-    showsUserLocation,
-    ref,
-  } = props;
+export const MapView = forwardRef<TRNMapView, TMapProps>(
+  (props: TMapProps, ref) => {
+    const {
+      provider: mapPovider = 'google',
+      initialRegion,
+      style,
+      userLocation,
+    } = props;
 
-  const [_mapReady, setMapReady] = useState(false);
+    const [_mapReady, setMapReady] = useState(false);
 
-  return (
-    <RNMapView
-      showsUserLocation={showsUserLocation}
-      ref={ref}
-      provider={mapPovider}
-      showsMyLocationButton={false}
-      zoomEnabled
-      scrollEnabled
-      zoomControlEnabled
-      mapType="standard"
-      initialRegion={initialRegion || defaultMapRegion}
-      onMapReady={() => setMapReady(true)}
-      style={[styles.map, style]}
-    />
-  );
-}
+    const goToUserLocation = () => {
+      if (userLocation && ref && 'current' in ref && ref.current) {
+        ref.current.animateToRegion(
+          {
+            latitude: userLocation.coords.latitude,
+            longitude: userLocation.coords.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          },
+          500
+        );
+      }
+    };
+
+    return (
+      <View style={styles.wrapper}>
+        <RNMapView
+          showsUserLocation={userLocation ? true : false}
+          ref={ref}
+          provider={mapPovider}
+          showsMyLocationButton={false}
+          zoomEnabled
+          scrollEnabled
+          zoomControlEnabled={false}
+          mapType="standard"
+          initialRegion={initialRegion || defaultMapRegion}
+          onMapReady={() => setMapReady(true)}
+          style={[styles.map, style]}
+        />
+
+        <LocateMeButton
+          style={styles.locateMeButton}
+          onPress={goToUserLocation}
+        />
+      </View>
+    );
+  }
+);
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    position: 'relative',
+  },
   map: {
     width: '100%',
     height: '100%',
+    position: 'relative',
+  },
+  locateMeButton: {
+    position: 'absolute',
+    right: 16,
+    bottom: '15%',
+    zIndex: 10,
   },
 });
