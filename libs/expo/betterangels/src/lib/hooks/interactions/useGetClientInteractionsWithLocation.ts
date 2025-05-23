d3fe2c267693e529bc6b5ebe0c5020ed22b3e607
Tom Glaz <tgmessages@gmail.com>
2025-05-22 22:00:14 -0700

    load using useGetClientInteractionsWithLocation

---
+++ libs/expo/betterangels/src/lib/hooks/interactions/useGetClientInteractionsWithLocation.ts
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import { NotesQuery, Ordering, useNotesQuery } from '../../apollo';
+
+type TInteraction = NonNullable<
+  NonNullable<NotesQuery['notes']>['results']
+>[number];
+
+export function useGetClientInteractionsWithLocation(id: string) {
+  const { data, error, loading } = useNotesQuery({
+    variables: {
+      pagination: { limit: 1000, offset: 0 },
+      order: { interactedAt: Ordering.Desc, id: Ordering.Desc },
+      filters: {
+        clientProfile: id,
+      },
+    },
+    fetchPolicy: 'cache-and-network',
+    nextFetchPolicy: 'cache-first',
+  });
+
+  const [interactions, setInteractions] = useState<TInteraction[]>();
+
+  useEffect(() => {
+    const results = data?.notes.results;
+
+    if (!data || !('notes' in data) || !results?.length) {
+      setInteractions([]);
+
+      return;
+    }
+
+    const withLocation = results.filter((note) => !!note.location?.point);
+
+    setInteractions(withLocation);
+  }, [data]);
+
+  if (error) {
+    console.error('Error fetching interactions:', error);
+
+    return {};
+  }
+
+  return { interactions, loading };
+}
--- libs/expo/shared/ui-components/src/lib/Map/constants.ts
+++ libs/expo/shared/ui-components/src/lib/Map/constants.ts
@@ -5,14 +5,18 @@ import { TMapLatLng } from './types';
 export const LA_COUNTY_CENTER_LAT = 34.04499;
 export const LA_COUNTY_CENTER_LNG = -118.251601;
 
+export const defaultLatDelta = 0.25;
+export const defaultLngDelta = 0.25;
+export const defaultAnimationDuration = 300;
+
 export const LA_COUNTY_CENTER: TMapLatLng = {
   lat: LA_COUNTY_CENTER_LAT,
   lng: LA_COUNTY_CENTER_LNG,
 };
 
 export const defaultMapRegion: Region = {
-  longitudeDelta: 0.1,
-  latitudeDelta: 0.1,
+  longitudeDelta: defaultLatDelta,
+  latitudeDelta: defaultLngDelta,
   latitude: LA_COUNTY_CENTER.lat,
   longitude: LA_COUNTY_CENTER.lng,
 };
