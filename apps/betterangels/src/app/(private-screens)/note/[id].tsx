2f32175a3f96b6dbe3f156eb81939bb647f2f10b
Aaron <aaronipollock@gmail.com>
2025-05-29 12:22:55 -0700

    reset [id].tsx

--- apps/betterangels/src/app/(private-screens)/note/[id].tsx
+++ apps/betterangels/src/app/(private-screens)/note/[id].tsx
@@ -1,10 +1,7 @@
 import { Note } from '@monorepo/expo/betterangels';
-import { Button } from '@monorepo/expo/shared/ui-components';
-import { useLocalSearchParams, useRouter } from 'expo-router';
-import { View } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
 
 export default function NoteScreen() {
-  const router = useRouter();
   const { id, arrivedFrom } = useLocalSearchParams<{
     id: string;
     arrivedFrom?: string;
@@ -14,20 +11,5 @@ export default function NoteScreen() {
     throw new Error('Something went wrong. Please try again.');
   }
 
-  return (
-    <View style={{ flex: 1 }}>
-      {/* Note display */}
-      <Note id={id} arrivedFrom={arrivedFrom} />
-
-      {/* Proof-of-Concept Modal Trigger */}
-      <View style={{ padding: 16 }}>
-        <Button
-          title="Open Demo Modal"
-          onPress={() => router.push('/base-modal-screen?type=SERVICES')}
-          variant="primary"
-          accessibilityHint="Opens the demo modal screen"
-        />
-      </View>
-    </View>
-  );
+  return <Note id={id} arrivedFrom={arrivedFrom} />;
 }
