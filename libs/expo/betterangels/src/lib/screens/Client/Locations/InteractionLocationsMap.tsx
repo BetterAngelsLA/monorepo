f540e71f178477078dd9c547fc69689c1d0e21c3
Tom Glaz <tgmessages@gmail.com>
2025-05-29 16:18:43 -0700

    redefine interaction map region

--- libs/expo/betterangels/src/lib/screens/Client/Locations/InteractionLocationsMap.tsx
+++ libs/expo/betterangels/src/lib/screens/Client/Locations/InteractionLocationsMap.tsx
@@ -5,7 +5,10 @@ import {
   coordsToRegion,
   regionDeltaMap,
 } from '@monorepo/expo/shared/ui-components';
+import { defaultMapRegion } from 'libs/expo/shared/ui-components/src/lib/Map/constants';
 import { StyleSheet } from 'react-native';
+import { Region } from 'react-native-maps';
+import { NotesQuery } from '../../../apollo';
 import { useGetClientInteractionsWithLocation } from '../../../hooks/interactions/useGetClientInteractionsWithLocation';
 import { Marker } from '../../../maps';
 import { EmptyState } from './EmptyState';
@@ -25,7 +28,7 @@ export function InteractionLocationsMap(props: TProps) {
   }
 
   // unless loading, render nothing until interactions are defined
-  if (!loading && interactionsWithLocation === undefined) {
+  if (interactionsWithLocation === undefined) {
     return null;
   }
 
@@ -33,33 +36,14 @@ export function InteractionLocationsMap(props: TProps) {
     return <EmptyState />;
   }
 
-  const mostRecentInteraction = interactionsWithLocation.reduce(
-    (latest, current) =>
-      new Date(current.interactedAt) > new Date(latest.interactedAt)
-        ? current
-        : latest
-  );
-
-  const mostRecentPoint = mostRecentInteraction.location?.point;
-
-  if (!mostRecentPoint) {
-    return <EmptyState />;
-  }
-
-  const [longitude, latitude] = mostRecentPoint;
-
-  const mapRegion = coordsToRegion({
-    latitude,
-    longitude,
-    ...regionDeltaMap.M,
-  });
+  const mapRegion = getMapRegion(interactionsWithLocation);
 
   return (
     <MapView
       enableUserLocation={true}
       style={styles.map}
       provider="google"
-      initialRegion={mapRegion}
+      initialRegion={mapRegion || defaultMapRegion}
     >
       {interactionsWithLocation.map((interaction) => {
         const { id, location } = interaction;
@@ -87,3 +71,30 @@ const styles = StyleSheet.create({
     height: 650,
   },
 });
+
+type TInteraction = NonNullable<
+  NonNullable<NotesQuery['notes']>['results']
+>[number];
+
+function getMapRegion(interactionsWithLocation: TInteraction[]): Region | null {
+  const mostRecentInteraction = interactionsWithLocation.reduce(
+    (latest, current) =>
+      new Date(current.interactedAt) > new Date(latest.interactedAt)
+        ? current
+        : latest
+  );
+
+  const point = mostRecentInteraction.location?.point;
+
+  if (!point) {
+    return null;
+  }
+
+  const [longitude, latitude] = point;
+
+  return coordsToRegion({
+    latitude,
+    longitude,
+    ...regionDeltaMap.L,
+  });
+}
