670cf4c1d32bc2e02a0a286e61dc17ac49a7cb53
Tom Glaz <tgmessages@gmail.com>
2025-06-02 14:04:33 -0700

    interactions map default center (DEV-1643) (#1325)
    * update map style
    
    * load using useGetClientInteractionsWithLocation
    
    * render markers
    
    * set initialRegion to latest interaction point
    
    * delete unused
    
    * render nothing unless loading if interactions undefined
    
    * rename files
    
    * use regionDeltaMap
    
    * pass TMapDeltaLatLng
    
    * minor
    
    * update useGetClientInteractionsWithLocation
    
    * use Accuracy.Balanced for getUserLocation)
    
    * use try catch with useGetClientInteractionsWithLocation.ts
    
    * redefine interaction map region
    
    * import fix
    
    * simplify useGetClientInteractionsWithLocation.ts
    
    * use sorted interactions to get most recent

--- libs/expo/shared/ui-components/src/lib/Map/Map.tsx
+++ libs/expo/shared/ui-components/src/lib/Map/Map.tsx
@@ -1,51 +1,38 @@
-import * as Location from 'expo-location';
 import { ReactNode, useRef, useState } from 'react';
 import { StyleProp, StyleSheet, View, ViewStyle } from 'react-native';
 import { Region } from 'react-native-maps';
-import LocateMeButton from '../LocateMeButton';
 import { defaultMapRegion } from './constants';
 import { RNMapView, TRNMapView } from './mapLib';
+import { MapLocateMeBtn } from './mapUi/MapLocateMeBtn';
 
 type TMapProps = {
   provider?: 'google';
   initialRegion?: Region;
   style?: StyleProp<ViewStyle>;
-  userLocation?: Location.LocationObject | null;
+  mapStyle?: StyleProp<ViewStyle>;
+  enableUserLocation?: boolean;
   children?: ReactNode;
 };
 
 export function MapView(props: TMapProps) {
   const {
-    provider: mapPovider = 'google',
+    provider,
     initialRegion,
     style,
-    userLocation,
+    mapStyle,
+    enableUserLocation,
     children,
   } = props;
 
   const [_mapReady, setMapReady] = useState(false);
   const mapRef = useRef<TRNMapView>(null);
 
-  const goToUserLocation = () => {
-    if (userLocation && mapRef.current) {
-      mapRef.current.animateToRegion(
-        {
-          latitude: userLocation.coords.latitude,
-          longitude: userLocation.coords.longitude,
-          latitudeDelta: 0.005,
-          longitudeDelta: 0.005,
-        },
-        500
-      );
-    }
-  };
-
   return (
-    <View style={styles.wrapper}>
+    <View style={[styles.wrapper, style]}>
       <RNMapView
-        showsUserLocation={userLocation ? true : false}
         ref={mapRef}
-        provider={mapPovider}
+        provider={provider}
+        showsUserLocation={!!enableUserLocation}
         showsMyLocationButton={false}
         zoomEnabled
         scrollEnabled
@@ -53,32 +40,23 @@ export function MapView(props: TMapProps) {
         mapType="standard"
         initialRegion={initialRegion || defaultMapRegion}
         onMapReady={() => setMapReady(true)}
-        style={[styles.map, style]}
+        style={[styles.map, mapStyle]}
       >
         {children}
       </RNMapView>
-      <LocateMeButton
-        style={styles.locateMeButton}
-        onPress={goToUserLocation}
-      />
+
+      {!!enableUserLocation && <MapLocateMeBtn mapRef={mapRef} />}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   wrapper: {
-    flex: 1,
     position: 'relative',
   },
   map: {
     width: '100%',
     height: '100%',
     position: 'relative',
-  },
-  locateMeButton: {
-    position: 'absolute',
-    right: 16,
-    bottom: '15%',
-    zIndex: 10,
   },
 });
