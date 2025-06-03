1feeac9fd66310fc6951bef17534210080df57f2
Paul Vecchio <paul@betterangels.la>
2025-06-03 14:26:41 -0700

    Disable signup

--- apps/betterangels-backend/accounts/adapters.py
+++ apps/betterangels-backend/accounts/adapters.py
@@ -2,9 +2,13 @@ from typing import Any, Dict
 
 from allauth.account.adapter import DefaultAccountAdapter
 from django.conf import settings
+from django.http import HttpRequest
 
 
 class AccountAdapter(DefaultAccountAdapter):
+    def is_open_for_signup(self, request: HttpRequest) -> bool:
+        return False
+
     def send_mail(
         self,
         template_prefix: str,
