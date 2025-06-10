c63394aa216ce72b631309889ef0c5ed1ffdec8a
Mike Feldberg <mike@betterangels.la>
2025-06-10 12:46:10 -0700

    add quasi disabled button state

--- libs/react/shelter/src/lib/pages/shelter/Actions.tsx
+++ libs/react/shelter/src/lib/pages/shelter/Actions.tsx
@@ -1,4 +1,8 @@
-import { CallIcon, LocationIcon, ShareIcon } from '@monorepo/react/icons';
+import {
+  CallRegularIcon,
+  LocationIcon,
+  ShareIcon,
+} from '@monorepo/react/icons';
 
 interface ActionsProps {
   phone?: string | null;
@@ -8,8 +12,10 @@ export default function Actions({ phone }: ActionsProps) {
   return (
     <div className="flex items-center py-4 justify-between text-xs px-11 border-neutral-90 border-t border-b mt-4 -mx-4">
       <a href={phone ? `tel:${phone}` : undefined}>
-        <div className="flex flex-col items-center">
-          <CallIcon className="w-6 h-6 fill-primary-20" />
+        <div
+          className={`flex flex-col items-center ${phone ? 'opacity-50' : ''}`}
+        >
+          <CallRegularIcon className="w-6 h-6 fill-primary-20" />
           <span>Call</span>
         </div>
       </a>
