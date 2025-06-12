6c8d2beef0dd95d20146cfa38379e0e17a53786e
Paul Vecchio <paul@betterangels.la>
2025-06-11 18:31:58 -0700

    Replace admin magic link with otp (DEV-1878) (#1373)
    * Update login
    
    * Remove sesame
    
    * Cleanup
    
    * Add back comments
    
    * Remove the post workaround
    
    * Remove next

--- apps/betterangels-backend/betterangels_backend/admin.py
+++ apps/betterangels-backend/betterangels_backend/admin.py
@@ -1,6 +1,5 @@
 from typing import Any, Dict, Optional
 
-from accounts.services import send_magic_link
 from betterangels_backend import settings
 from django.contrib import admin
 from django.contrib.auth import get_user_model
@@ -10,20 +9,11 @@ from django.shortcuts import render
 UserModel = get_user_model()
 
 
-class MagicLinkAdminSite(admin.AdminSite):
+class CustomAdminSite(admin.AdminSite):
     def login(self, request: HttpRequest, extra_context: Optional[Dict[str, Any]] = None) -> HttpResponse:
         context = extra_context or {}
         is_local_dev = getattr(settings, "IS_LOCAL_DEV", False)
         context["is_local_dev"] = is_local_dev
-
-        if request.method == "POST" and "email" in request.POST:
-            email = request.POST["email"]
-            if UserModel.objects.filter(email=email, is_staff=True).exists():
-                send_magic_link(email, request)
-            context.update({"email_sent": True, "sent_email_address": email})
-
-            # Keep login form instead of trying to validate empty fields
-            request.method = "GET"
 
         if not is_local_dev:
             # In production, render the login page directly (disable username/password auth)
@@ -32,4 +22,4 @@ class MagicLinkAdminSite(admin.AdminSite):
         return super().login(request, context)
 
 
-magic_link_admin_site = MagicLinkAdminSite(name="admin")
+magic_link_admin_site = CustomAdminSite(name="admin")
