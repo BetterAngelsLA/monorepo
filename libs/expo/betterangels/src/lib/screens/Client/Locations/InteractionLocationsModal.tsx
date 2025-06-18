b98a4282e2603d7809d3354c7c36d85b6ee08320
Davit-BetterAngels <141072810+Davit-BetterAngels@users.noreply.github.com>
2025-06-17 22:32:55 -0700

    location marker click (DEV-1832) (#1380)
    * marker click
    
    * ui fixes

---
+++ libs/expo/betterangels/src/lib/screens/Client/Locations/InteractionLocationsModal.tsx
@@ -0,0 +1,56 @@
+import { Colors, Spacings } from '@monorepo/expo/shared/static';
+import { TextBold } from '@monorepo/expo/shared/ui-components';
+import { View } from 'react-native';
+import { NotesQuery } from '../../../apollo';
+import { Modal, NoteCard } from '../../../ui-components';
+
+interface InteractionLocationsModalProps {
+  selectedLocation: NotesQuery['notes']['results'][number] | null;
+  setSelectedLocation: (
+    location: NotesQuery['notes']['results'][number] | null
+  ) => void;
+}
+
+export function InteractionLocationsModal(
+  props: InteractionLocationsModalProps
+) {
+  const { selectedLocation, setSelectedLocation } = props;
+
+  if (!selectedLocation) {
+    return null;
+  }
+
+  return (
+    <Modal
+      propogateSwipe
+      vertical
+      isModalVisible={!!selectedLocation}
+      closeModal={() => setSelectedLocation(null)}
+    >
+      <View
+        style={{
+          borderBottomWidth: 1,
+          borderColor: Colors.NEUTRAL_LIGHT,
+          marginBottom: Spacings.sm,
+          paddingBottom: Spacings.xs,
+          paddingHorizontal: Spacings.sm,
+        }}
+      >
+        <TextBold>{selectedLocation.location?.address.street}</TextBold>
+      </View>
+      <View
+        style={{
+          paddingHorizontal: Spacings.sm,
+          gap: Spacings.sm,
+        }}
+      >
+        <NoteCard
+          onPress={() => setSelectedLocation(null)}
+          note={selectedLocation}
+          variant={'clientProfile'}
+          hasBorder
+        />
+      </View>
+    </Modal>
+  );
+}
