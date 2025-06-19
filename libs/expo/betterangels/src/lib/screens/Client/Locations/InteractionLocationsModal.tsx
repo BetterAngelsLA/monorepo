44a0c5df96577a895106d9cb0b95b8dc5dd3f536
Davit-BetterAngels <davit@betterangels.la>
2025-06-19 05:52:10 +0000

    bottomsheet modal

--- libs/expo/betterangels/src/lib/screens/Client/Locations/InteractionLocationsModal.tsx
+++ libs/expo/betterangels/src/lib/screens/Client/Locations/InteractionLocationsModal.tsx
@@ -1,8 +1,16 @@
-import { Colors, Spacings } from '@monorepo/expo/shared/static';
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
+import {
+  Colors,
+  Radiuses,
+  Shadow,
+  Spacings,
+} from '@monorepo/expo/shared/static';
 import { TextBold } from '@monorepo/expo/shared/ui-components';
-import { View } from 'react-native';
+import { useMemo, useRef, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { NotesQuery } from '../../../apollo';
-import { Modal, NoteCard } from '../../../ui-components';
+import { NoteCard } from '../../../ui-components';
 
 interface InteractionLocationsModalProps {
   selectedLocation: NotesQuery['notes']['results'][number] | null;
@@ -15,43 +23,72 @@ export function InteractionLocationsModal(
   props: InteractionLocationsModalProps
 ) {
   const { selectedLocation, setSelectedLocation } = props;
+  const [titleHeight, setTitleHeight] = useState<number>(1);
+
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  const snapPoints = useMemo(() => [titleHeight], [titleHeight]);
+  const insets = useSafeAreaInsets();
+  const bottomOffset = insets.bottom;
 
   if (!selectedLocation) {
     return null;
   }
 
   return (
-    <Modal
-      fullWidth={false}
-      propogateSwipe
-      vertical
-      isModalVisible={!!selectedLocation}
-      closeModal={() => setSelectedLocation(null)}
+    <BottomSheet
+      index={0}
+      handleIndicatorStyle={{
+        backgroundColor: Colors.NEUTRAL_LIGHT,
+        width: 54,
+        height: 5,
+        borderRadius: Radiuses.xxs,
+      }}
+      snapPoints={snapPoints}
+      enablePanDownToClose={false}
+      ref={bottomSheetRef}
+      enableDynamicSizing
+      style={styles.shadow}
     >
-      <View
-        style={{
-          borderBottomWidth: 1,
-          borderColor: Colors.NEUTRAL_LIGHT,
-          marginBottom: Spacings.sm,
-          paddingBottom: Spacings.xs,
-          paddingHorizontal: Spacings.sm,
-        }}
-      >
-        <TextBold>{selectedLocation.location?.address.street}</TextBold>
-      </View>
-      <View
-        style={{
-          paddingHorizontal: Spacings.sm,
-          gap: Spacings.sm,
-        }}
-      >
-        <NoteCard
-          onPress={() => setSelectedLocation(null)}
-          note={selectedLocation}
-          variant={'clientProfile'}
-          hasBorder
-        />
-      </View>
-    </Modal>
+      <BottomSheetView style={styles.container}>
+        <View
+          onLayout={(e) => {
+            const height = e.nativeEvent.layout.height;
+            setTitleHeight(height + 25);
+            console.log('Title height:', height);
+          }}
+          style={{
+            borderBottomWidth: 1,
+            borderColor: Colors.NEUTRAL_LIGHT,
+            marginBottom: Spacings.sm,
+            paddingBottom: bottomOffset + Spacings.xs,
+            paddingHorizontal: Spacings.sm,
+          }}
+        >
+          <TextBold>{selectedLocation.location?.address.street}</TextBold>
+        </View>
+        <View
+          style={{
+            paddingHorizontal: Spacings.sm,
+            gap: Spacings.sm,
+          }}
+        >
+          <NoteCard
+            onPress={() => setSelectedLocation(null)}
+            note={selectedLocation}
+            variant={'clientProfile'}
+            hasBorder
+          />
+        </View>
+      </BottomSheetView>
+    </BottomSheet>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingBottom: Spacings.md,
+  },
+  shadow: Shadow,
+});
