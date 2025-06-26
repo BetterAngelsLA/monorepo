019dc2d884c9c1b34dfd19d4d4193f296cb19947
Mike Feldberg <mike@betterangels.la>
2025-06-26 15:55:28 -0700

    bump version for release

--- apps/betterangels/app.config.js
+++ apps/betterangels/app.config.js
@@ -21,7 +21,7 @@ export default {
     name: IS_PRODUCTION ? 'BetterAngels' : 'BetterAngels (Dev)',
     slug: 'betterangels',
     scheme: IS_PRODUCTION ? 'betterangels' : 'betterangels-dev',
-    version: '1.0.46',
+    version: '1.0.47',
     orientation: 'portrait',
     icon: IS_PRODUCTION
       ? './src/app/assets/images/icon.png'
@@ -34,7 +34,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: BUNDLE_IDENTIFIER,
-      buildNumber: '1.0.57',
+      buildNumber: '1.0.58',
       associatedDomains: [`applinks:${HOSTNAME}`],
       config: {
         googleMapsApiKey: process.env.EXPO_PUBLIC_IOS_GOOGLEMAPS_APIKEY,
@@ -72,7 +72,7 @@ export default {
           apiKey: process.env.EXPO_PUBLIC_ANDROID_GOOGLEMAPS_APIKEY,
         },
       },
-      versionCode: 56,
+      versionCode: 57,
     },
     web: {
       favicon: './src/app/assets/images/favicon.png',
