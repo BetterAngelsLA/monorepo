5c6aa181f7c6aa7d61dfed79de8b3b6ee4ce4292
Tom Glaz <tgmessages@gmail.com>
2025-07-02 13:43:42 -0700

    copy mikes PhoneNumberInput

---
+++ libs/expo/shared/ui-components/src/lib/PhoneNumberInput/types.ts
@@ -0,0 +1,28 @@
+import { Control, RegisterOptions } from 'react-hook-form';
+import { ViewStyle } from 'react-native';
+
+type TRules = Omit<
+  RegisterOptions,
+  'disabled' | 'valueAsNumber' | 'valueAsDate' | 'setValueAs'
+>;
+
+export interface IPhoneNumberInputProps {
+  value?: string;
+  onChange?: (value: string) => void;
+  onClear?: () => void;
+  parseNumber?: (value: string) => [string, string | undefined];
+  formatValues?: (number: string, extension?: string) => string;
+  placeholderNumber?: string;
+  placeholderExt?: string;
+  disabled?: boolean;
+  errors?: string;
+  // errors?: {
+  //   phoneNumber?: string;
+  //   // extension?: string;
+  // };
+  label?: string;
+  style?: ViewStyle;
+  name?: string;
+  control?: Control<any>;
+  rules?: TRules;
+}
