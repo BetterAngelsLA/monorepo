f87ca7b32d7999460ad8f1ec2a1259774a3b2bee
jluna2000 <jesseluna2000@gmail.com>
2025-07-03 15:33:56 -0700

    Modified icon size

--- libs/react/shelter/src/lib/components/ImagePlaceholder/ImagePlaceholder.tsx
+++ libs/react/shelter/src/lib/components/ImagePlaceholder/ImagePlaceholder.tsx
@@ -20,7 +20,7 @@ export function ImagePlaceholder(props: TProps) {
 
   return (
     <div className={mergeCss(parentCss)}>
-      <ImageIcon className="text-primary-60 h-11" />
+      <ImageIcon className="text-primary-60 h-20" />
     </div>
   );
 }
