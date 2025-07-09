e6450dc7d722d13e3056a15d344aa69214adfd98
Paul Vecchio <paul@betterangels.la>
2025-07-09 16:07:36 -0700

    Add to apps (#1446)

--- apps/betterangels-admin/jest.config.ts
+++ apps/betterangels-admin/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
   displayName: 'betterangels-admin',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../coverage/betterangels-admin',
+  coverageDirectory: '../../coverage/apps/betterangels-admin',
 };
