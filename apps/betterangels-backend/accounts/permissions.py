70e918f6ffa423c2661cabb2d6f1de1ac30d23d8
Mike Feldberg <mike@betterangels.la>
2025-07-21 12:19:31 -0700

    add view members permission

--- apps/betterangels-backend/accounts/permissions.py
+++ apps/betterangels-backend/accounts/permissions.py
@@ -5,5 +5,6 @@ from django.utils.translation import gettext_lazy as _
 class UserOrganizationPermissions(models.TextChoices):
     ACCESS_ORG_PORTAL = "organizations.access_org_portal", _("Can access organization management portal")
     ADD_ORG_MEMBER = "organizations.add_org_member", _("Can add organization member")
-    REMOVE_ORG_MEMBER = "organizations.remove_org_member", _("Can remove organization member")
     CHANGE_ORG_MEMBER_ROLE = "organizations.change_org_member_role", _("Can change organization member role")
+    REMOVE_ORG_MEMBER = "organizations.remove_org_member", _("Can remove organization member")
+    VIEW_ORG_MEMBERS = "organizations.view_org_members", _("Can view organization members")
