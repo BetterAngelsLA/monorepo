9caa39aba2ad46b9d3ad156f747683cd5bdea489
Nayan Parmar <nparmar84@gmail.com>
2025-07-24 09:22:27 -0700

    Updated styling and links

--- apps/shelter-web/src/app/layout/footer.tsx
+++ apps/shelter-web/src/app/layout/footer.tsx
@@ -1,6 +1,7 @@
 import { BaShelterLogoIcon } from '@monorepo/react/icons';
 import { ReactElement } from 'react';
 import { Link } from 'react-router-dom';
+import { aboutUsPath, shelterHomePath } from '../routes/routePaths';
 
 type IParams = {
   className?: string;
@@ -22,18 +23,18 @@ export function Footer(props: IParams): ReactElement {
   return (
     <footer className={parentCss}>
       <div className="flex flex-col md:flex-row md:items-center justify-between">
-        <div className="flex items-center mb-11">
+        <div className="flex items-center mb-11 md:mt-3">
           <BaShelterLogoIcon className="h-8" />
           <div className="text-white flex ml-3 text-2xl">
             <div className="font-normal">Shelter</div>
             <div className="font-semibold">LA</div>
           </div>
         </div>
-        <div className="flex flex-col md:flex-row gap-6 text-sm">
-          <Link aria-label="navigate to Homepage" to="">
+        <div className="flex flex-col md:flex-row gap-6 text-[14px] md:mb-9">
+          <Link aria-label="navigate to Homepage" to={shelterHomePath}>
             Home
           </Link>
-          <Link aria-label="navigate to About page" to="">
+          <Link aria-label="navigate to About page" to={aboutUsPath}>
             About Us
           </Link>
           <Link aria-label="navigate to Terms of Service page" to="">
@@ -44,7 +45,7 @@ export function Footer(props: IParams): ReactElement {
           </Link>
         </div>
       </div>
-      <div className="mt-12 border-t-[0.5px] border-white pt-6 flex justify-start">
+      <div className="mt-12 border-t-[0.5px] border-white pt-6 flex justify-end">
         <div className="text-xs">
           © 2025 Better Angels Inc. All rights reserved.
         </div>
