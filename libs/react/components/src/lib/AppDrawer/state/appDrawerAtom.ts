65b874cac07e1968b390b7fe1cc9cfaf09df4fca
Tom Glaz <tgmessages@gmail.com>
2025-07-31 12:04:22 -0700

    simplify appDrawerAtom

--- libs/react/components/src/lib/AppDrawer/state/appDrawerAtom.ts
+++ libs/react/components/src/lib/AppDrawer/state/appDrawerAtom.ts
@@ -1,4 +1,4 @@
-import { WritableAtom, atom } from 'jotai';
+import { atom } from 'jotai';
 import { ReactNode } from 'react';
 
 export type TDrawerPlacement = 'left' | 'right';
@@ -6,41 +6,10 @@ export type TDrawerPlacement = 'left' | 'right';
 export type TAppDrawerAtomProps = {
   visible: boolean;
   content: ReactNode | null;
+  placement?: TDrawerPlacement;
   contentClassName?: string | null;
   header?: ReactNode | null;
   footer?: ReactNode | null;
-  placement?: TDrawerPlacement;
 };
 
-const defaultDrawerProps: Partial<TAppDrawerAtomProps> = {
-  placement: 'right',
-  visible: false,
-};
-
-const appDrawerAtomBase = atom<TAppDrawerAtomProps | null>(null);
-
-type drawerSetterFn = (
-  prev: TAppDrawerAtomProps | null
-) => TAppDrawerAtomProps | null;
-
-export const appDrawerAtom: WritableAtom<
-  TAppDrawerAtomProps | null, // Return read value
-  [TAppDrawerAtomProps | null | drawerSetterFn], // Write argument (tuple)
-  void // Return type of write function
-> = atom(
-  (get) => {
-    const drawer = get(appDrawerAtomBase);
-
-    if (!drawer) {
-      return null;
-    }
-
-    return { ...defaultDrawerProps, ...drawer };
-  },
-  (get, set, update) => {
-    const prev = get(appDrawerAtomBase);
-    const next = typeof update === 'function' ? update(prev) : update;
-
-    set(appDrawerAtomBase, next);
-  }
-);
+export const appDrawerAtom = atom<TAppDrawerAtomProps | null>(null);
