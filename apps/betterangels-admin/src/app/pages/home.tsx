01ac5da2bc7f7eabc0f0fd4bfd00650d4eabd460
Davit-BetterAngels <141072810+Davit-BetterAngels@users.noreply.github.com>
2025-07-31 09:33:08 -0700

    navbar (DEV-2009) (#1476)
    * navbar
    
    * navbar
    
    * update dropdown and navbar
    
    * removed .css file

--- apps/betterangels-admin/src/app/pages/home.tsx
+++ apps/betterangels-admin/src/app/pages/home.tsx
@@ -1,23 +1,11 @@
-import {
-  Dropdown,
-  useSignOut,
-  useUser,
-} from '@monorepo/react/betterangels-admin';
+import { useUser } from '@monorepo/react/betterangels-admin';
 
 export default function Home() {
   const { user } = useUser();
-  const { signOut } = useSignOut();
 
   return (
-    <div className="flex items-center justify-center h-screen flex-col">
+    <div className="flex items-center justify-center h-full bg-white flex-col">
       <div>Welcome to Home, {user?.email}!</div>
-      <button onClick={signOut}>Sign Out</button>
-      <Dropdown
-        title="Click me"
-        className="text-white"
-        options={['Option 1', 'Option 2']}
-        onSelect={(option) => console.log('Selected:', option)}
-      />
     </div>
   );
 }
