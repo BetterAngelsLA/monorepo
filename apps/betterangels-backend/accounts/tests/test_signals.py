19b1a0242da2e5b9560161dbef3d1c6e7f26675a
Mike Feldberg <mike@betterangels.la>
2025-08-01 10:59:43 -0700

    unused imports

--- apps/betterangels-backend/accounts/tests/test_signals.py
+++ apps/betterangels-backend/accounts/tests/test_signals.py
@@ -1,30 +1,32 @@
-from accounts.enums import OrgRoleEnum
 from accounts.groups import GroupTemplateNames
 from accounts.models import PermissionGroup, User
-from accounts.utils import OrgPermissionManager
 from django.test import TestCase
 from model_bakery import baker
-from unittest_parametrize import ParametrizedTestCase
 
 from .baker_recipes import organization_recipe
 
 
-class OrgSignalTestCase(ParametrizedTestCase, TestCase):
+class OrgSignalTestCase(TestCase):
     def setUp(self) -> None:
         super().setUp()
 
     def test_handle_caseworker_perm_group_created_signal(self) -> None:
-        org = organization_recipe.make()
+        org_1 = organization_recipe.make()
+        org_2 = organization_recipe.make()
         user = baker.make(User)
 
-        initial_org_perm_groups = PermissionGroup.objects.filter(organization=org)
-        self.assertEqual(initial_org_perm_groups.count(), 0)
+        initial_org_1_perm_groups = PermissionGroup.objects.filter(organization=org_1)
+        initial_org_2_perm_groups = PermissionGroup.objects.filter(organization=org_2)
+        self.assertEqual(initial_org_1_perm_groups.count(), 0)
+        self.assertEqual(initial_org_2_perm_groups.count(), 0)
 
-        org.add_user(user)
-        org_perm_groups = PermissionGroup.objects.filter(organization=org)
-        self.assertEqual(org_perm_groups.count(), 3)
+        org_1.add_user(user)
+        org_1_perm_groups = PermissionGroup.objects.filter(organization=org_1)
+        org_2_perm_groups = PermissionGroup.objects.filter(organization=org_2)
+        self.assertEqual(org_1_perm_groups.count(), 3)
+        self.assertEqual(org_2_perm_groups.count(), 0)
 
-        org_perm_group_names = [pg.name for pg in org_perm_groups]
+        org_perm_group_names = [pg.name for pg in org_1_perm_groups]
         expected_names = [
             GroupTemplateNames.CASEWORKER,
             GroupTemplateNames.ORG_ADMIN,
