18bd5dea85b14f567dcf4a6d1f35e2473ad473d7
Tom Glaz <tgmessages@gmail.com>
2025-08-01 12:07:28 -0700

    storybook page loading with Button

---
+++ libs/react/components/src/lib/Button/Button.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from './Button';
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  title: 'Example/Button',
+};
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {
+    children: 'Click Me',
+  },
+};
