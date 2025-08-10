b999225e5b07e72056465a69e21deb8009f22ebc
Tom Glaz <tgmessages@gmail.com>
2025-08-10 15:46:49 -0700

    add checkbox

---
+++ libs/react/components/src/lib/Form/CheckboxGroup.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+import { disableControls } from '../../storybook';
+import { CheckboxGroup as StoryComponent, TProps } from './CheckboxGroup';
+
+const meta: Meta<typeof StoryComponent> = {
+  title: 'Form/Checkbox',
+  component: StoryComponent,
+  argTypes: {
+    ...disableControls(['onClick']),
+  },
+};
+export default meta;
+
+type Story = StoryObj;
+
+const defaultArgs: Pick<TProps, 'options' | 'values' | 'selectAll'> = {
+  options: ['option 1', 'option 2', 'option 3'],
+  values: [],
+};
+
+export const CheckboxGroup: Story = {
+  render: (args) => {
+    const [selected, setSelected] = useState<string[]>([]);
+    const baseArgs: TProps = {
+      ...defaultArgs,
+      ...args,
+      values: selected,
+      onChange: setSelected,
+    };
+
+    return <StoryComponent {...baseArgs} className="max-w-80" />;
+  },
+};
