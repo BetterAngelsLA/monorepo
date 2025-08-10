e05a03e54a2dababd53468e0566f457b498ab079
Tom Glaz <tgmessages@gmail.com>
2025-08-10 16:09:13 -0700

    simplify as as StoryComponent

--- libs/react/components/src/lib/BetterAngelsLogoBadge/BetterAngelsLogoBadge.stories.tsx
+++ libs/react/components/src/lib/BetterAngelsLogoBadge/BetterAngelsLogoBadge.stories.tsx
@@ -1,14 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { BetterAngelsLogoBadge } from './BetterAngelsLogoBadge';
+import { BetterAngelsLogoBadge as StoryComponent } from './BetterAngelsLogoBadge';
 
-const meta: Meta<typeof BetterAngelsLogoBadge> = {
-  component: BetterAngelsLogoBadge,
+const meta: Meta<typeof StoryComponent> = {
+  component: StoryComponent,
   title: 'Components/BetterAngelsLogoBadge',
 };
 export default meta;
 
-type Story = StoryObj<typeof BetterAngelsLogoBadge>;
+type Story = StoryObj<typeof StoryComponent>;
 
-export const BALogoBadge: Story = {
-  render: (args) => <BetterAngelsLogoBadge {...args} />,
+export const BetterAngelsLogoBadge: Story = {
+  render: (args) => <StoryComponent {...args} />,
 };
