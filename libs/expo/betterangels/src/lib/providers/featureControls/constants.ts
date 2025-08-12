1261871a07e166c3d076022598a2714fcff98364
Tom Glaz <tgmessages@gmail.com>
2025-08-12 11:26:35 -0700

    add Tasks tab and update feature flags (#1505)

--- libs/expo/betterangels/src/lib/providers/featureControls/constants.ts
+++ libs/expo/betterangels/src/lib/providers/featureControls/constants.ts
@@ -1,6 +1,6 @@
 export const FeatureFlags = {
   APP_UPDATE_PROMPT_FF: 'ffAppUpdatePrompt',
   CLIENT_DEDUPE_FF: 'ffClientDedupe',
-  LOCATION_HISTORY_FF: 'ffLocationHistory',
   SHOW_DEBUG_INFO_FF: 'ffShowDebugInfo',
+  TASKS_FF: 'ffTasks',
 } as const;
