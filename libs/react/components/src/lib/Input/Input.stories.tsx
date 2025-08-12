899dbf88c732f6c5e34fcd5819b106dfc6b1d065
Tom Glaz <tgmessages@gmail.com>
2025-08-11 18:35:25 -0700

    add Input

---
+++ libs/react/components/src/lib/Input/Input.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { SbList } from '../../storybook';
+import { IInputProps, Input as StoryComponent } from './Input';
+
+const meta: Meta<typeof StoryComponent> = {
+  component: StoryComponent,
+  title: 'Form/Input',
+};
+export default meta;
+
+type Story = StoryObj<typeof StoryComponent>;
+
+const defaultArgs: IInputProps = {
+  label: 'Name',
+};
+
+export const Input: Story = {
+  render: (args) => {
+    const baseArgs = { ...defaultArgs, ...args };
+
+    return (
+      <SbList className="max-w-[400px] p-8 border-2 border-gray-100">
+        <StoryComponent {...baseArgs} />
+      </SbList>
+    );
+  },
+};
