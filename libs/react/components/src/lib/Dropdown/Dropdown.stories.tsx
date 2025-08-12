8431b08d0022dd6b36da1128bb9038f96d823a64
Tom Glaz <tgmessages@gmail.com>
2025-08-11 18:44:16 -0700

    add dropdown

---
+++ libs/react/components/src/lib/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { SbList } from '../../storybook';
+import { DropdownProps, Dropdown as StoryComponent } from './Dropdown';
+
+const meta: Meta<typeof StoryComponent> = {
+  component: StoryComponent,
+  title: 'Components/Dropdown',
+};
+export default meta;
+
+type Story = StoryObj<typeof StoryComponent>;
+
+const defaultArgs: DropdownProps<string> = {
+  title: 'hello dropdown',
+  options: ['option 1', 'option 2', 'longer option 3', 'even longer option 4'],
+  onSelect: () => {},
+};
+
+export const Dropdown: Story = {
+  render: (args) => {
+    const baseArgs = { ...defaultArgs, ...args };
+
+    return (
+      <SbList className="w-full h-[300px] px-24 py-12 bg-gray-100">
+        <StoryComponent {...baseArgs} />
+      </SbList>
+    );
+  },
+};
