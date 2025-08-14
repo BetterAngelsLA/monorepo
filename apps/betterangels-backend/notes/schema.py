77bedf55c83e46d895187c2a55e5f2d43370cc82
Mike Feldberg <mike@betterangels.la>
2025-08-14 12:29:29 -0700

    types

--- apps/betterangels-backend/notes/schema.py
+++ apps/betterangels-backend/notes/schema.py
@@ -284,6 +284,8 @@ class Mutation:
                 ServiceRequest,
                 {
                     **service_request_data,
+                    "service": service_request_data["service"] or service_request_data["service_enum"],
+                    "service_enum": service_request_data["service_enum"] or service_request_data["service"],
                     "status": (
                         ServiceRequestStatusEnum.TO_DO
                         if service_request_type == ServiceRequestTypeEnum.REQUESTED
