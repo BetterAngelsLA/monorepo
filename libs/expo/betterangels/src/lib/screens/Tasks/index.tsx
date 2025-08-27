c23ff606320b7b13c30a4336846f9f552343889c
Davit-BetterAngels <davit@betterangels.la>
2025-08-27 07:31:13 +0000

    removed mock

--- libs/expo/betterangels/src/lib/screens/Tasks/index.tsx
+++ libs/expo/betterangels/src/lib/screens/Tasks/index.tsx
@@ -1,28 +1,11 @@
 import { Colors } from '@monorepo/expo/shared/static';
 import { Text } from 'react-native';
-import { TaskStatusEnum } from '../../apollo';
-import { MainContainer, TaskCard } from '../../ui-components';
+import { MainContainer } from '../../ui-components';
 
 export default function Tasks() {
   return (
     <MainContainer pb={0} bg={Colors.NEUTRAL_EXTRA_LIGHT}>
       <Text>Tasks</Text>
-      <TaskCard
-        task={{
-          id: '1',
-          summary: 'This is a task',
-          description: 'This is a description',
-          status: TaskStatusEnum.ToDo,
-          createdAt: new Date().toISOString(),
-          updatedAt: new Date(),
-          createdBy: {
-            id: '1',
-            firstName: 'John',
-            lastName: 'Doe',
-            username: 'test',
-          },
-        }}
-      />
     </MainContainer>
   );
 }
