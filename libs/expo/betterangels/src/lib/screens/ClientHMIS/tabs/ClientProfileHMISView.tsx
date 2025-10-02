d26bba16ee22dcfb0d34e0c4dbc62c8da1825d18
Tom Glaz <tgmessages@gmail.com>
2025-10-02 11:40:27 -0700

    add tabs

--- libs/expo/betterangels/src/lib/screens/ClientHMIS/tabs/ClientProfileHMISView.tsx
+++ libs/expo/betterangels/src/lib/screens/ClientHMIS/tabs/ClientProfileHMISView.tsx
@@ -1,3 +1,19 @@
-export function ClientProfileHMISView() {
-  return null;
+import { TextBold } from '@monorepo/expo/shared/ui-components';
+import { View } from 'react-native';
+import { HmisClientType } from '../../../apollo';
+import { ClientProfileSectionEnum } from '../../../screenRouting';
+
+type TProps = {
+  client?: HmisClientType;
+  openCard?: ClientProfileSectionEnum | null;
+};
+
+export function ClientProfileHMISView(props: TProps) {
+  const { client, openCard } = props;
+
+  return (
+    <View>
+      <TextBold>HELLO PROFILE TAB</TextBold>
+    </View>
+  );
 }
