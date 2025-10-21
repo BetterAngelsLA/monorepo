0e209ffba7ff7c5d37b7e51dd3c52fa4d118f728
Grace Lee <jaeeungracelee@gmail.com>
2025-10-20 21:02:17 -0400

    feat: add a new GraphQL mutation that creates a Shelter in the database

--- apps/betterangels-backend/shelters/schema.py
+++ apps/betterangels-backend/shelters/schema.py
@@ -1,6 +1,12 @@
+from typing import cast
+
 import strawberry
 import strawberry_django
+from django.db import transaction
+from shelters.models import Shelter
 from shelters.types import ShelterType
+from strawberry.types import Info
+from strawberry_django.mutations import resolvers
 from strawberry_django.pagination import OffsetPaginated
 
 
@@ -8,3 +14,23 @@ from strawberry_django.pagination import OffsetPaginated
 class Query:
     shelter: ShelterType = strawberry_django.field()
     shelters: OffsetPaginated[ShelterType] = strawberry_django.offset_paginated()
+
+@strawberry.input
+class ShelterLocationInput:
+    place: str
+
+@strawberry.input
+class CreateShelterInput:
+    name: str
+    description: str
+
+
+@strawberry.type
+class Mutation:
+    @strawberry_django.mutation
+    def create_shelter(self, info: Info, input: CreateShelterInput) -> ShelterType:
+        with transaction.atomic():
+            shelter_data = strawberry.asdict(input)
+            shelter = resolvers.create(info, Shelter, shelter_data)
+
+        return cast(ShelterType, shelter)
