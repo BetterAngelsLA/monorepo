9f08584df97ea687f4f4dd9703dd9281c2f9444f
Paul Vecchio <paul@betterangels.la>
2025-11-12 12:07:22 -0800

    Fix apollo types (#1673)
    * Fix typing
    
    * Remove has rest directive
    
    * Fix resolutions
    
    * Set graphql as a subdependency

--- libs/expo/shared/clients/src/lib/apollo/client.ts
+++ libs/expo/shared/clients/src/lib/apollo/client.ts
@@ -1,11 +1,9 @@
 import { ApolloClient, ApolloLink, InMemoryCache } from '@apollo/client';
-import { RestLink } from 'apollo-link-rest';
 import UploadHttpLink from 'apollo-upload-client/UploadHttpLink.mjs';
 import { isReactNativeFileInstance } from './ReactNativeFile';
 import { createAuthLink } from './links/authLink';
 import { createErrorLink } from './links/errorLink';
 import { loggerLink } from './links/loggerLink';
-import { hasRestDirective } from './links/utils';
 
 type TArgs = {
   apiUrl: string;
@@ -26,11 +24,6 @@ export const createApolloClient = (args: TArgs) => {
 
   const authLink = createAuthLink({ apiUrl, csrfUrl });
 
-  const restLink = new RestLink({
-    uri: apiUrl,
-    credentials: 'include',
-  });
-
   const uploadHttpLink = new UploadHttpLink({
     uri: `${apiUrl}/graphql`,
     credentials: 'include',
@@ -42,14 +35,7 @@ export const createApolloClient = (args: TArgs) => {
     onUnauthenticated,
   });
 
-  // Split between REST operations and GraphQL operations
-  const restOrUploadLink = ApolloLink.split(
-    (operation) => hasRestDirective(operation.query),
-    restLink as unknown as ApolloLink, // if hasRestDirective=true
-    uploadHttpLink as unknown as ApolloLink // if hasRestDirective=false
-  );
-
-  const composedLinks: ApolloLink[] = [errorLink, authLink, restOrUploadLink];
+  const composedLinks = [errorLink, authLink, uploadHttpLink];
 
   // Debug only: logs GraphQL requests/responses
   if (
