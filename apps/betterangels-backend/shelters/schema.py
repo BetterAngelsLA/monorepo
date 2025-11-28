6d86d460988c1e86a2c03555f7963e7e31b842b6
Gordon06060 <gordonz0913@gmail.com>
2025-11-28 03:30:12 +0000

    perm cahnges

--- apps/betterangels-backend/shelters/schema.py
+++ apps/betterangels-backend/shelters/schema.py
@@ -1,27 +1,27 @@
 import strawberry
 import strawberry_django
-from common.permissions.utils import IsAuthenticated
 from django.db.models import QuerySet
-from notes.permissions import NotePermissions
+from common.permissions.utils import IsAuthenticated
 from shelters.models import Shelter
+from shelters.permissions import ShelterPermissions
 from shelters.types import ShelterType
+from strawberry.types import Info
 from strawberry_django.pagination import OffsetPaginated
 from strawberry_django.permissions import HasPerm
-
 
 @strawberry.type
 class Query:
     shelter: ShelterType = strawberry_django.field()
     shelters: OffsetPaginated[ShelterType] = strawberry_django.offset_paginated()
 
-    # modified code
     @strawberry_django.offset_paginated(
         OffsetPaginated[ShelterType],
         permission_classes=[IsAuthenticated],
-        extensions=[HasPerm(NotePermissions.ADD)],
+        extensions=[HasPerm(ShelterPermissions.VIEW)]
     )
-    def shelters_by_organization(self, organization_id: strawberry.ID) -> QuerySet[Shelter]:
-        queryset: QuerySet[Shelter] = Shelter.objects.filter(organization_id=organization_id).order_by("-created_at")
-        return queryset
-
-    #
+    def shelters_by_organization(self, info: Info) -> QuerySet[Shelter]:
+        user = info.context.request.user
+        organization_id = getattr(user, "organization_id", None)
+        if organization_id is None:
+            return Shelter.objects.none()
+        return Shelter.objects.filter(organization_id=organization_id).order_by("-created_at")
