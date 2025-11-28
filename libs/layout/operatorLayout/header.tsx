8386d742b23309de1e80c824daa125bfb6860903
Vrinda Joshi <vrinda.joshi@hotmail.com>
2025-11-28 03:28:34 +0000

    feat: layout refaction, distinct operator layout

---
+++ libs/layout/operatorLayout/header.tsx
@@ -0,0 +1,34 @@
+// import { BaShelterLogoIcon } from '@monorepo/react/icons';
+import { ReactElement } from 'react';
+// import { MenuBtnMobile } from '../shared/components/navBar/MenuBtnMobile';
+
+type IParams = {
+  className?: string;
+};
+
+export function Header(props: IParams): ReactElement {
+  const { className = '' } = props;
+
+  const parentCss = [
+    className,
+    'w-full',
+    'flex',
+    'items-center',
+    'justify-between',
+    'h-[42px]',
+    'text-white',
+  ].join(' ');
+
+  return (
+    <header className={parentCss}>
+      <div className="flex items-center">
+        {/* <BaShelterLogoIcon className="h-4" /> */}
+        <div className="text-white flex ml-2 text-sm">
+          <div className="font-normal">Shelter</div>
+          <div className="font-semibold">LA</div>
+        </div>
+      </div>
+      {/* <MenuBtnMobile /> */}
+    </header>
+  );
+}
---
+++ libs/layout/operatorLayout/operatorLayout.tsx
@@ -0,0 +1,43 @@
+import { HorizontalLayout } from '@monorepo/layout/horizontalLayout';
+import { mergeCss } from '@monorepo/layout/styles/mergeCss';
+import { ReactElement } from 'react';
+import { Outlet } from 'react-router-dom';
+// import { Flyoutcontainer } from '../shared/flyout/Flyoutcontainer';
+// import { ModalContainer } from '../shared/modal/modalContainer';
+import { Footer } from './footer';
+import { Header } from './header';
+
+type IParams = {
+  className?: string;
+};
+
+export function OperatorLayout(props: IParams): ReactElement {
+  const { className = '' } = props;
+
+  const parentCss = [
+    'w-full',
+    'max-w-screen',
+    'overflow-x-hidden',
+    'flex',
+    'flex-col',
+    'items-center',
+    'min-h-screen',
+    className,
+  ];
+
+  return (
+    <div className={mergeCss(parentCss)}>
+      <HorizontalLayout className="bg-steel-blue">
+        <Header />
+      </HorizontalLayout>
+      <HorizontalLayout className="mb-24">
+        <Outlet />
+      </HorizontalLayout>
+      <HorizontalLayout className="bg-dark-blue mt-auto">
+        <Footer />
+      </HorizontalLayout>
+      {/* <ModalContainer />
+      <Flyoutcontainer /> */}
+    </div>
+  );
+}
