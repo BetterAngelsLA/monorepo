099f7bb30e06abf5840e34ee239cc972f3bf47b5
Jaeeun Grace Lee <155155813+jaeeungracelee@users.noreply.github.com>
2025-12-03 22:20:21 -0500

    [BP] feat: shelter dashboard page (#1691)
    * feat: add shelter page under operator
    
    * feat: add operator shelter dashboard route
    
    * fix: remove appRoutes from shelter-operator
    
    * refactor: moving shared shelter query to shared directory
    
    Co-authored-by: julianchen606@gmail.com
    Co-authored-by: jaeeungracelee@gmail.com
    
    * refactor: removed unused elements from appRoutes
    
    Co-authored-by: Julian Chen <julianchen606@gmail.com>
    Co-authored-by: Grace Lee <jaeeungracelee@gmail.com>
    
    * fix: isolate operator routes under operatorApp.tsx
    
    Co-authored-by: Julian Chen julianchen606@gmail.com
    Co-authored-by: Grace Lee jaeeungracelee@gmail.com
    
    * Revert "refactor: moving shared shelter query to shared directory"
    
    This reverts commit 797e1b5e740631b9c61ca9b8e86b53d0999bc055.
    
    * fix: remove project.json for ci/cd check pass
    
    * fix: run yarn install to update dependencies
    
    ---------
    
    Co-authored-by: Julian Chen <julianchen606@gmail.com>

--- libs/react/shelter-operator/src/lib/OperatorApp.tsx
+++ libs/react/shelter-operator/src/lib/OperatorApp.tsx
@@ -1,18 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
-<<<<<<< HEAD
-=======
 import ShelterDashboardPage from './ShelterDashboardPage'
->>>>>>> origin/uw-blueprint
 
 export function OperatorApp() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
-<<<<<<< HEAD
-=======
       <Route path="/shelter/:id" element={<ShelterDashboardPage />} />
->>>>>>> origin/uw-blueprint
     </Routes>
   );
 }
