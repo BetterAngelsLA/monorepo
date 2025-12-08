2d8de410030200d53a92ba32bb07aac23e47e751
Paul Vecchio <paul@betterangels.la>
2025-12-08 12:52:35 -0800

    See if this works

--- apps/betterangels-backend/notes/admin.py
+++ apps/betterangels-backend/notes/admin.py
@@ -128,6 +128,12 @@ class NoteAdmin(AttachmentAdminMixin, ExportActionMixin, admin.ModelAdmin):
     def get_export_formats(self) -> list:
         return [CSV]
 
+    autocomplete_fields = (
+        "provided_services",
+        "requested_services",
+        "organization",
+    )
+
     list_display = (
         "note_purpose",
         "client_profile",
