ea4482e1a2422bcf55b0f697514a96669e957d9f
Paul Vecchio <paul@betterangels.la>
2025-12-10 08:54:40 -0800

    remove Auto Update Feature Flag

--- libs/expo/betterangels/src/lib/providers/featureControls/constants.ts
+++ libs/expo/betterangels/src/lib/providers/featureControls/constants.ts
@@ -1,7 +1,5 @@
 export const FeatureFlags = {
-  APP_UPDATE_PROMPT_FF: 'ffAppUpdatePrompt',
   CLIENT_DEDUPE_FF: 'ffClientDedupe',
   HMIS_FF: 'ffHMIS',
   SHOW_DEBUG_INFO_FF: 'ffShowDebugInfo',
-  TASKS_FF: 'ffTasks',
 } as const;
