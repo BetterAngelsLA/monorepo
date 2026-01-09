ea3c54f410b8d54cf8c9168235bf6bfe93a5ea30
Tom Glaz <tgmessages@gmail.com>
2026-01-08 16:20:24 -0800

    handle hmis client not found in view

--- apps/betterangels-backend/common/errors.py
+++ apps/betterangels-backend/common/errors.py
@@ -3,6 +3,9 @@ from graphql import GraphQLError
 
 class APIErrorCodes:
     UNAUTHENTICATED = "UNAUTHENTICATED"
+    NOT_FOUND = "NOT_FOUND"
+    HMIS_ENTITY_NOT_FOUND = "HMIS_ENTITY_NOT_FOUND"
+    # UNPROCESSABLE_ENTITY = "UNPROCESSABLE_ENTITY" ...
 
 
 class UnauthenticatedGQLError(GraphQLError):
@@ -14,3 +17,20 @@ class UnauthenticatedGQLError(GraphQLError):
                 "http": {"status": 401},
             },
         )
+
+
+class NotFoundGQLError(GraphQLError):
+    error_code = APIErrorCodes.NOT_FOUND
+
+    def __init__(self, message: str | None = None) -> None:
+        super().__init__(
+            message or "Not Found.",
+            extensions={
+                "code": self.error_code,
+                "http": {"status": 404},
+            },
+        )
+
+
+class HmisNotFoundGQLError(NotFoundGQLError):
+    error_code = APIErrorCodes.HMIS_ENTITY_NOT_FOUND
