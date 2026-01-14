aa67027816874bee9a265267474acff9ac602e74
Mike Feldberg <mike@betterangels.la>
2026-01-14 14:03:31 -0800

    resolve migration collision

--- apps/betterangels-backend/shelters/migrations/0023_alter_shelter_city_council_district_and_more.py
+++ apps/betterangels-backend/shelters/migrations/0023_alter_shelter_city_council_district_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("shelters", "0021_add_shelter_hero_image"),
+        ("shelters", "0022_city_overhaul"),
     ]
 
     operations = [
