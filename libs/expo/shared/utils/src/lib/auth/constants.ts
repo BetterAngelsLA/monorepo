c3785895147ef826eeae868f6991a7fedef9f2c7
Paul Vecchio <paul@betterangels.la>
2026-01-21 15:44:17 -0800

    Remove non-HMIS frontend changes

--- libs/expo/shared/utils/src/lib/auth/constants.ts
+++
@@ -1,3 +0,0 @@
-export const HMIS_AUTH_COOKIE_NAME = 'auth_token';
-export const HMIS_API_URL_COOKIE_NAME = 'api_url';
-export const HMIS_DOMAIN_KEY = 'hmis_domain';
--- libs/expo/shared/utils/src/lib/storage/types.ts
+++ libs/expo/shared/utils/src/lib/storage/types.ts
@@ -1,4 +1,4 @@
-export interface PersistentSynchronousStorageApi {
+export interface StateStorageSyncApi {
   get<T>(key: string): T | null;
   set<T>(key: string, value: T): void;
   remove(key: string): void;
