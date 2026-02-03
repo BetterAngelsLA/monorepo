ac9554475a3d5b59024da09be905eed4d08513ba
Mike Feldberg <mike@betterangels.la>
2026-02-03 20:36:47 +0000

    put back clientId param (#1851)

--- libs/expo/betterangels/src/lib/screens/ClientHMIS/tabs/ClientInteractionsHmisView/ClientInteractionsHmisView.tsx
+++ libs/expo/betterangels/src/lib/screens/ClientHMIS/tabs/ClientInteractionsHmisView/ClientInteractionsHmisView.tsx
@@ -22,6 +22,7 @@ export function ClientInteractionsHmisView(props: TProps) {
         onPress={() => {
           router.navigate({
             pathname: `/notes-hmis/${item.id}`,
+            params: { clientId: client?.id },
           });
         }}
         variant="clientProfile"
