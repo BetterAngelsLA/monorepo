9e83747e87bd245b3da15f02435483b6af951791
Vrinda Joshi <vrinda.joshi@hotmail.com>
2026-02-10 02:53:31 +0000

    refactoring button components into shelter operator app
    Co-authored-by: jennifer shi <jennifershi255@users.noreply.github.com>

--- apps/storybook-react/config/storiesWebApps.ts
+++ apps/storybook-react/config/storiesWebApps.ts
@@ -1,12 +1,18 @@
 import { dirname, join } from 'path';
-import { StoriesSpecifier } from 'storybook/internal/types';
 import { fileURLToPath } from 'url';
 import { storyFileTypes } from './constants.ts';
+
+export interface StoriesSpecifier {
+  directory: string;
+  files: string | string[];
+  titlePrefix?: string;
+}
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export const SHELTER_APP_LIB_DIR = '../../../libs/react/shelter/src/lib';
-export const SHELTER_WEB_APP_DIR = '../../../apps/shelter-web/src/app';
+export const SHELTER_OPR_APP_DIR =
+  '../../../libs/react/shelter-operator/src/lib/components';
 
 export const REACT_APP_LIB_STORIES: StoriesSpecifier[] = [
   {
@@ -15,8 +21,8 @@ export const REACT_APP_LIB_STORIES: StoriesSpecifier[] = [
     titlePrefix: 'Web/Apps/Shelter',
   },
   {
-    directory: join(__dirname, SHELTER_WEB_APP_DIR),
+    directory: join(__dirname, SHELTER_OPR_APP_DIR),
     files: storyFileTypes,
-    titlePrefix: 'Web/Apps/Shelter Web',
+    titlePrefix: 'Web/Apps/ShelterOperator',
   },
 ];
