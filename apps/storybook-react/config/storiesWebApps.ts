6c7d7a140828968cf8b8171d373367c1c59f8dfb
jennifershi255 <jennifershi255@gmail.com>
2026-02-10 01:50:23 +0000

    add shelter web storyboard

--- apps/storybook-react/config/storiesWebApps.ts
+++ apps/storybook-react/config/storiesWebApps.ts
@@ -6,11 +6,17 @@ import { storyFileTypes } from './constants.ts';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export const SHELTER_APP_LIB_DIR = '../../../libs/react/shelter/src/lib';
+export const SHELTER_WEB_APP_DIR = '../../../apps/shelter-web/src/app';
 
 export const REACT_APP_LIB_STORIES: StoriesSpecifier[] = [
   {
     directory: join(__dirname, SHELTER_APP_LIB_DIR),
     files: storyFileTypes,
     titlePrefix: 'Web/Apps/Shelter',
+  },
+  {
+    directory: join(__dirname, SHELTER_WEB_APP_DIR),
+    files: storyFileTypes,
+    titlePrefix: 'Web/Apps/Shelter Web',
   },
 ];
