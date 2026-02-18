67ba8fcbb5e9929907a3e2ac467e4fdd5df4dcfd
Paul Vecchio <paul@betterangels.la>
2026-02-18 06:54:58 -0800

    remove mood model and GraphQL mood operations

--- apps/betterangels-backend/notes/enums.py
+++ apps/betterangels-backend/notes/enums.py
@@ -2,28 +2,8 @@ import strawberry
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-
-class MoodEnum(models.TextChoices):
-    AGITATED = "agitated", _("Agitated")
-    AGREEABLE = "agreeable", _("Agreeable")
-    ANXIOUS = "anxious", _("Anxious")
-    DEPRESSED = "depressed", _("Depressed")
-    DETACHED = "detached", _("Detached")
-    DISORGANIZED_THOUGHT = "disorganized_thought", _("Disorganized Thought")
-    DISORIENTED = "disoriented", _("Disoriented")
-    ESCALATED = "escalated", _("Escalated")
-    EUTHYMIC = "euthymic", _("Euthymic")
-    FLAT_BLUNTED = "flat_blunted", _("Flat/blunted")
-    HAPPY = "happy", _("Happy")
-    HOPELESS = "hopeless", _("Hopeless")
-    INDIFFERENT = "indifferent", _("Indifferent")
-    MANIC = "manic", _("Manic")
-    MOTIVATED = "motivated", _("Motivated")
-    OPTIMISTIC = "optimistic", _("Optimistic")
-    PERSONABLE = "personable", _("Personable")
-    PLEASANT = "pleasant", _("Pleasant")
-    RESTLESS = "restless", _("Restless")
-    SUICIDAL = "suicidal", _("Suicidal")
+# Re-export for migration compatibility (migrations reference notes.enums.MoodEnum)
+from notes.deprecated.deprecated_enums import MoodEnum  # noqa: F401
 
 
 class ServiceRequestStatusEnum(models.TextChoices):
