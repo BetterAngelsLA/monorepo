d1ac7a61226f98db10d114e89256490cec568ae2
frank731 <frankl7315@gmail.com>
2026-02-24 06:17:18 +0000

    setup auth for bed tests

--- apps/betterangels-backend/shelters/tests/test_queries.py
+++ apps/betterangels-backend/shelters/tests/test_queries.py
@@ -56,6 +56,7 @@ from shelters.models import (
     TrainingService,
 )
 from shelters.tests.baker_recipes import shelter_contact_recipe, shelter_recipe
+from accounts.models import User
 from test_utils.mixins import GraphQLTestCaseMixin
 from unittest_parametrize import ParametrizedTestCase, parametrize
 
