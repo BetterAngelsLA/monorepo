b59efc518921bbb4a36599c19b0b895ef6f29548
Davit-BetterAngels <141072810+Davit-BetterAngels@users.noreply.github.com>
2026-02-26 10:37:30 -0800

    default location (OUT-190) (#1891)
    * select default location
    
    * update on note
    
    * default state update
    
    * change import
    
    * default location for hmis
    
    * cycle fix

--- libs/expo/betterangels/src/lib/screens/AppSettings/index.ts
+++ libs/expo/betterangels/src/lib/screens/AppSettings/index.ts
@@ -1,3 +1,4 @@
 export { AboutApp } from './AboutApp';
 export { AppSettings } from './AppSettings';
+export { DefaultLocation } from './DefaultLocation';
 export { UserTeamPreference } from './UserTeamPreference';
