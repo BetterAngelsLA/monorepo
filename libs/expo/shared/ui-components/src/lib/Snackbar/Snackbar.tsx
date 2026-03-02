aec22e6eb33c445fd6c1dc3a09f5f5eecb6ec08e
Davit-BetterAngels <davit@betterangels.la>
2026-03-02 08:57:41 +0000

    snackbar ui update

--- libs/expo/shared/ui-components/src/lib/Snackbar/Snackbar.tsx
+++ libs/expo/shared/ui-components/src/lib/Snackbar/Snackbar.tsx
@@ -1,9 +1,10 @@
-import { Colors } from '@monorepo/expo/shared/static';
+import { Colors, FontSizes, Radiuses } from '@monorepo/expo/shared/static';
 import { ReactElement } from 'react';
 import {
   Snackbar as RnpSnackbar,
   SnackbarProps as RnpSnackbarProps,
 } from 'react-native-paper';
+import TextRegular from '../TextRegular';
 
 export type TSnackbarType = 'default' | 'success' | 'error';
 
@@ -15,27 +16,81 @@ export interface ISnackbar extends RnpSnackbarProps {
 
 const typeStyles = {
   error: {
-    backgroundColor: Colors.ERROR,
+    container: {
+      backgroundColor: Colors.ERROR_EXTRA_LIGHT,
+      borderWidth: 1,
+      borderColor: Colors.ERROR,
+      borderRadius: Radiuses.xxs,
+    },
+    text: {
+      color: Colors.PRIMARY_EXTRA_DARK,
+    },
   },
   success: {
-    backgroundColor: Colors.SUCCESS,
+    container: {
+      backgroundColor: Colors.SUCCESS_EXTRA_LIGHT,
+      borderWidth: 1,
+      borderColor: Colors.SUCCESS,
+      borderRadius: Radiuses.xxs,
+    },
+    text: {
+      color: Colors.PRIMARY_EXTRA_DARK,
+    },
+  },
+  warning: {
+    container: {
+      backgroundColor: Colors.WARNING_EXTRA_LIGHT,
+      borderWidth: 1,
+      borderColor: Colors.WARNING,
+      borderRadius: Radiuses.xxs,
+    },
+    text: {
+      color: Colors.PRIMARY_EXTRA_DARK,
+    },
   },
   default: {
-    backgroundColor: Colors.PRIMARY,
+    container: {
+      backgroundColor: Colors.PRIMARY,
+      borderWidth: 0,
+    },
+    text: {
+      color: Colors.PRIMARY_EXTRA_DARK,
+    },
   },
-};
+} as const;
 
 export function Snackbar(props: ISnackbar): ReactElement {
-  const { style, onActionPress, type, actionLabel = 'Close', ...rest } = props;
+  const {
+    style,
+    contentStyle,
+    onActionPress,
+    type = 'default',
+    actionLabel = 'Close',
+    children,
+    ...rest
+  } = props;
+
+  const t = typeStyles[type];
 
   return (
     <RnpSnackbar
-      style={[style, typeStyles[type || 'default']]}
+      style={[t.container, style]}
+      contentStyle={contentStyle}
       action={{
         label: actionLabel,
         onPress: onActionPress,
+        textColor: t.text.color,
+        labelStyle: {
+          fontFamily: 'Poppins-SemiBold',
+          fontWeight: '700',
+          fontSize: FontSizes.sm.fontSize,
+        },
       }}
       {...rest}
-    />
+    >
+      <TextRegular size="sm" color={t.text.color}>
+        {children}
+      </TextRegular>
+    </RnpSnackbar>
   );
 }
