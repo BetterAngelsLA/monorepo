cb73b049b3d52d0766f06d81f3061b123ea0c07b
Paul Vecchio <paul@betterangels.la>
2026-03-03 13:45:38 -0800

    Use public type

--- apps/storybook-react/config/storiesPlatform.ts
+++ apps/storybook-react/config/storiesPlatform.ts
@@ -1,4 +1,4 @@
-import type { StoriesSpecifier } from 'storybook/internal/types';
+import type { StoriesSpecifier } from '@storybook/types';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { storyFileTypes } from './constants.ts';
