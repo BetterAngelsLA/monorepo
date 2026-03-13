8c89fbc246b8150378cbefb8210a45acaf44acdf
julianchen24 <julianchen606@gmail.com>
2026-03-12 21:04:05 -0400

    Merge shelters migration heads

---
+++ apps/betterangels-backend/shelters/migrations/0036_merge_20260313_0105.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("shelters", "0034_modify_shelter_group_permissions_add_schedule"),
+        ("shelters", "0035_merge_20260313_0035"),
+    ]
+
+    operations = []
