46800fd56b4beb400528f23da7d5dde35b976fad
Paul Vecchio <paul@betterangels.la>
2026-03-19 12:30:41 -0700

    Cleanup and fix typescript errors

--- apps/storybook-react/.storybook/preview.tsx
+++ apps/storybook-react/.storybook/preview.tsx
@@ -1,8 +1,8 @@
 import { CustomLayout } from '@monorepo/react/storybook';
 import type { Preview } from '@storybook/react';
+import '../../../libs/tailwind/src/css/base.css';
 import '../../../libs/tailwind/src/css/fonts-rn.css';
 import '../../../libs/tailwind/src/css/fonts.css';
-import '../../../libs/tailwind/src/css/base.css';
 
 const preview: Preview = {
   parameters: { controls: { expanded: false }, layout: 'fullscreen' },
