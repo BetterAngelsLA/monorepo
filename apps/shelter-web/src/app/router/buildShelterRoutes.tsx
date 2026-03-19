1581267c36d1283c997bc4e579826a78086bd63e
Paul Vecchio <paul@betterangels.la>
2026-03-19 13:40:28 -0700

    More cleanup

--- apps/shelter-web/src/app/router/buildShelterRoutes.tsx
+++ apps/shelter-web/src/app/router/buildShelterRoutes.tsx
@@ -6,35 +6,20 @@ import {
   shelterHomePath,
 } from '@monorepo/react/shelter';
 import { OperatorApp } from '@monorepo/react/shelter-operator';
-import { Route } from 'react-router-dom';
+import type { RouteObject } from 'react-router-dom';
 import { GalleryRoute } from '../routes/gallery.route';
 import { HomeRoute } from '../routes/home.route';
 import { PolicyRoute } from '../routes/policy.route';
 import { ShelterRoute } from '../routes/shelter.route';
 
-export const publicRoutes = [
-  <Route
-    key={shelterHomePath}
-    path={shelterHomePath}
-    element={<HomeRoute />}
-  />,
-  <Route
-    key={shelterDetailsPath}
-    path={shelterDetailsPath}
-    element={<ShelterRoute />}
-  />,
-  <Route
-    key={shelterGalleryPath}
-    path={shelterGalleryPath}
-    element={<GalleryRoute />}
-  />,
-  <Route
-    key={privacyPolicyPath}
-    path={privacyPolicyPath}
-    element={<PolicyRoute />}
-  />,
+export const publicRoutes: RouteObject[] = [
+  { path: shelterHomePath, element: <HomeRoute /> },
+  { path: shelterDetailsPath, element: <ShelterRoute /> },
+  { path: shelterGalleryPath, element: <GalleryRoute /> },
+  { path: privacyPolicyPath, element: <PolicyRoute /> },
 ];
 
-export const operatorRoute = (
-  <Route path={`${operatorPath}/*`} element={<OperatorApp />} />
-);
+export const operatorRoute: RouteObject = {
+  path: `${operatorPath}/*`,
+  element: <OperatorApp />,
+};
--- apps/shelter-web/src/app/router/useOperatorRoute.tsx
+++ apps/shelter-web/src/app/router/useOperatorRoute.tsx
@@ -1,8 +1,9 @@
 import { useFeatureFlagActive } from '@monorepo/react/shared';
 import { FeatureFlags } from '@monorepo/react/shelter';
+import type { RouteObject } from 'react-router-dom';
 import { operatorRoute } from './buildShelterRoutes';
 
-export const useOperatorRoute = () => {
+export const useOperatorRoute = (): RouteObject | null => {
   const operatorEnabled = useFeatureFlagActive(
     FeatureFlags.SHELTER_OPERATOR_APP
   );
