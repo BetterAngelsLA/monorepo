2ad82b9d3c9a25dfc4f3e60af3f0f95b6621461f
Paul Vecchio <paul@betterangels.la>
2026-03-19 12:19:34 -0700

    Cleanup

--- apps/storybook-react/.storybook/preview.tsx
+++ apps/storybook-react/.storybook/preview.tsx
@@ -2,7 +2,7 @@ import { CustomLayout } from '@monorepo/react/storybook';
 import type { Preview } from '@storybook/react';
 import '../../../libs/tailwind/src/css/fonts-rn.css';
 import '../../../libs/tailwind/src/css/fonts.css';
-import '../src/styles/tailwind.css';
+import '../../../libs/tailwind/src/css/base.css';
 
 const preview: Preview = {
   parameters: { controls: { expanded: false }, layout: 'fullscreen' },
