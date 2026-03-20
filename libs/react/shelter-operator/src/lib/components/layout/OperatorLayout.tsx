57c07390ee3613549a9f340f6ba8c4f6ab1febb9
jennifershi255 <jennifershi255@gmail.com>
2026-03-20 00:55:20 +0000

    fix header shelter name

--- libs/react/shelter-operator/src/lib/components/layout/OperatorLayout.tsx
+++ libs/react/shelter-operator/src/lib/components/layout/OperatorLayout.tsx
@@ -1,13 +1,23 @@
 import { Outlet, useLocation } from 'react-router-dom';
+import { useUser } from '@monorepo/react/shelter';
 import NavBar from '../NavBar';
 
 export function OperatorLayout() {
   const location = useLocation();
+  const { user } = useUser();
   const isDashboardPage = location.pathname === '/operator' || location.pathname === '/operator/';
+  
+  const isReservationPage = location.pathname.includes('/reservation');
+  const organizationName = user?.organization?.name;
+  const pageTitle = isReservationPage ? 'Shelter Reservation' : undefined;
 
   return (
     <div className="flex flex-col min-h-screen">
-      <NavBar showCreateButton={isDashboardPage} />
+      <NavBar
+        showCreateButton={isDashboardPage}
+        organizationName={organizationName}
+        pageTitle={pageTitle}
+      />
       <main className="flex-1">
         <Outlet />
       </main>
