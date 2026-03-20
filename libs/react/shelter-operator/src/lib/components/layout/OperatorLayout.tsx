206fbc3ead669b3c2114aa8aedddc32929692e7a
jennifershi255 <jennifershi255@gmail.com>
2026-03-20 01:24:46 +0000

    add shelter-level name to header

--- libs/react/shelter-operator/src/lib/components/layout/OperatorLayout.tsx
+++ libs/react/shelter-operator/src/lib/components/layout/OperatorLayout.tsx
@@ -1,14 +1,28 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { useQuery } from '@apollo/client/react';
+import { Outlet, useLocation, useParams } from 'react-router-dom';
 import { useUser } from '@monorepo/react/shelter';
 import NavBar from '../NavBar';
+import { GetShelterNameDocument } from '../../graphql/__generated__/shelters.generated';
 
 export function OperatorLayout() {
   const location = useLocation();
+  const params = useParams<{ id?: string; shelterId?: string }>();
   const { user } = useUser();
-  const isDashboardPage = location.pathname === '/operator' || location.pathname === '/operator/';
-  
+  const isDashboardPage =
+    location.pathname === '/operator' || location.pathname === '/operator/';
+
+  const shelterId = params.shelterId || params.id;
+  const isShelterPage =
+    !!shelterId && location.pathname.includes(`/shelter/${shelterId}`);
+
+  const { data: shelterData } = useQuery(GetShelterNameDocument, {
+    variables: { id: shelterId || '' },
+    skip: !isShelterPage || !shelterId,
+  });
+
   const isReservationPage = location.pathname.includes('/reservation');
   const organizationName = user?.organization?.name;
+  const shelterName = shelterData?.shelter?.name;
   const pageTitle = isReservationPage ? 'Shelter Reservation' : undefined;
 
   return (
@@ -16,6 +30,7 @@ export function OperatorLayout() {
       <NavBar
         showCreateButton={isDashboardPage}
         organizationName={organizationName}
+        shelterName={shelterName}
         pageTitle={pageTitle}
       />
       <main className="flex-1">
