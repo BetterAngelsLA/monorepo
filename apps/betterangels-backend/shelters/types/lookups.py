105886bee15c0902b8021013658ecb060bf05fc5
Mike Feldberg <mike@betterangels.la>
2026-03-25 12:53:19 -0700

    add and alphabetize types

--- apps/betterangels-backend/shelters/types/lookups.py
+++ apps/betterangels-backend/shelters/types/lookups.py
@@ -21,33 +21,39 @@ class ContactInfoType:
 # Enum-backed M2M lookup types.
 
 
-@strawberry_django.type(models.Demographic)
-class DemographicType:
-    name: auto
-
-
-@strawberry_django.type(models.SpecialSituationRestriction)
-class SpecialSituationRestrictionType:
-    name: auto
-
-
-@strawberry_django.type(models.ShelterType)
-class ShelterTypeType:
-    name: auto
-
-
-@strawberry_django.type(models.RoomStyle)
-class RoomStyleType:
-    name: auto
-
-
 @strawberry_django.type(models.Accessibility)
 class AccessibilityType:
     name: auto
 
 
-@strawberry_django.type(models.Storage)
-class StorageType:
+@strawberry_django.type(models.City)
+class CityType:
+    id: auto
+    name: auto
+
+
+@strawberry_django.type(models.Demographic)
+class DemographicType:
+    name: auto
+
+
+@strawberry_django.type(models.EntryRequirement)
+class EntryRequirementType:
+    name: auto
+
+
+@strawberry_django.type(models.ExitPolicy)
+class ExitPolicyType:
+    name: auto
+
+
+@strawberry_django.type(models.Funder)
+class FunderType:
+    name: auto
+
+
+@strawberry_django.type(models.Parking)
+class ParkingType:
     name: auto
 
 
@@ -56,9 +62,28 @@ class PetType:
     name: auto
 
 
-@strawberry_django.type(models.Parking)
-class ParkingType:
+@strawberry_django.type(models.ReferralRequirement)
+class ReferralRequirementType:
     name: auto
+
+
+@strawberry_django.type(models.RoomStyle)
+class RoomStyleType:
+    name: auto
+
+
+@strawberry_django.type(models.Schedule)
+class ScheduleType:
+    id: ID
+    schedule_type: auto
+    day: auto
+    start_time: auto
+    end_time: auto
+    start_date: auto
+    end_date: auto
+    condition: auto
+    demographic: DemographicType | None
+    is_exception: auto
 
 
 @strawberry_django.type(models.ServiceCategory)
@@ -80,14 +105,13 @@ class ServiceType:
     priority: auto
 
 
-@strawberry_django.type(models.EntryRequirement)
-class EntryRequirementType:
+@strawberry_django.type(models.ShelterProgram)
+class ShelterProgramType:
     name: auto
 
 
-@strawberry_django.type(models.City)
-class CityType:
-    id: auto
+@strawberry_django.type(models.ShelterType)
+class ShelterTypeType:
     name: auto
 
 
@@ -96,25 +120,11 @@ class SPAType:
     name: auto
 
 
-@strawberry_django.type(models.ShelterProgram)
-class ShelterProgramType:
+@strawberry_django.type(models.SpecialSituationRestriction)
+class SpecialSituationRestrictionType:
     name: auto
 
 
-@strawberry_django.type(models.Funder)
-class FunderType:
+@strawberry_django.type(models.Storage)
+class StorageType:
     name: auto
-
-
-@strawberry_django.type(models.Schedule)
-class ScheduleType:
-    id: ID
-    schedule_type: auto
-    day: auto
-    start_time: auto
-    end_time: auto
-    start_date: auto
-    end_date: auto
-    condition: auto
-    demographic: DemographicType | None
-    is_exception: auto
