872a4eed1797159386b4c68b17fa7bc3463dd873
Paul Vecchio <paul@betterangels.la>
2026-04-30 12:20:38 -0700

    Fix branch deploy redirect

--- libs/react/shelter/src/lib/providers/user/useSignOut.ts
+++ libs/react/shelter/src/lib/providers/user/useSignOut.ts
@@ -1,18 +1,25 @@
-import { useMutation } from '@apollo/client/react';
+import { useApolloClient, useMutation } from '@apollo/client/react';
 import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { LogoutDocument } from './__generated__/logout.generated';
+import { useUser } from './useUser';
 
 export function useSignOut() {
+  const client = useApolloClient();
+  const { setUser } = useUser();
+  const navigate = useNavigate();
   const [logout, { loading, error }] = useMutation(LogoutDocument);
 
   const signOut = useCallback(async () => {
     try {
       await logout();
-      window.location.href = '/';
+      setUser(undefined);
+      navigate('/');
+      await client.resetStore();
     } catch (err) {
       console.error(err);
     }
-  }, [logout]);
+  }, [logout, setUser, navigate, client]);
 
   return { signOut, loading, error };
 }
