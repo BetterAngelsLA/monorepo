83d2d60ceb9c546ec597f1d9740a6861559da22d
Davit-BetterAngels <davit@betterangels.la>
2026-06-10 16:05:31 +0000

    import from utils

--- apps/betterangels-backend/shelters/migrations/0007_add_shelteravailability_group_permissions.py
+++ apps/betterangels-backend/shelters/migrations/0007_add_shelteravailability_group_permissions.py
@@ -1,8 +1,8 @@
 from django.db import migrations
 
-from shelters.utils import (
-    assign_permissions_to_group_in_migration,
-    remove_permissions_from_group_in_migration,
+from common.migrations._utils import (
+    assign_permissions_to_group,
+    remove_permissions_from_group,
 )
 
 
@@ -10,16 +10,16 @@ def add_shelter_availability_permissions(apps, schema_editor):
     from shelters.models import ShelterAvailability
 
     permission_map = {ShelterAvailability: [ShelterAvailability.perms]}
-    assign_permissions_to_group_in_migration(apps, "Shelter Data Entry", permission_map)
-    assign_permissions_to_group_in_migration(apps, "Shelter Administration", permission_map)
+    assign_permissions_to_group(apps, "Shelter Data Entry", permission_map)
+    assign_permissions_to_group(apps, "Shelter Administration", permission_map)
 
 
 def remove_shelter_availability_permissions(apps, schema_editor):
     from shelters.models import ShelterAvailability
 
     permission_map = {ShelterAvailability: [ShelterAvailability.perms]}
-    remove_permissions_from_group_in_migration(apps, "Shelter Data Entry", permission_map)
-    remove_permissions_from_group_in_migration(apps, "Shelter Administration", permission_map)
+    remove_permissions_from_group(apps, "Shelter Data Entry", permission_map)
+    remove_permissions_from_group(apps, "Shelter Administration", permission_map)
 
 
 class Migration(migrations.Migration):
