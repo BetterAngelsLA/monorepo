a6c37ac9f66db8e45a7e8bac77b6ecbc43f6ced9
Mike Feldberg <mike@betterangels.la>
2026-06-10 16:02:27 -0700

    dropdown comment

--- libs/react/shelter-operator/src/lib/components/rooms/room-form/constants/roomFormOptions.ts
+++ libs/react/shelter-operator/src/lib/components/rooms/room-form/constants/roomFormOptions.ts
@@ -1,13 +1,5 @@
 import { RoomStatusChoices } from '../../../../apollo/graphql/__generated__/types';
-import type { DropdownOption } from '../../../base-ui/dropdown';
-
-const toOptions = <T extends string>(
-  labels: Record<T, string>
-): DropdownOption<T>[] =>
-  (Object.entries(labels) as [T, string][]).map(([value, label]) => ({
-    value,
-    label,
-  }));
+import { toDropdownOptions } from '../../../base-ui/dropdown';
 
 const ROOM_STATUS_LABELS: Record<RoomStatusChoices, string> = {
   [RoomStatusChoices.Available]: 'Available',
@@ -15,7 +7,7 @@ const ROOM_STATUS_LABELS: Record<RoomStatusChoices, string> = {
   [RoomStatusChoices.Reserved]: 'Reserved',
 };
 
-export const ROOM_STATUS_OPTIONS = toOptions(ROOM_STATUS_LABELS);
+export const ROOM_STATUS_OPTIONS = toDropdownOptions(ROOM_STATUS_LABELS);
 
 export const BOOLEAN_OPTIONS = [
   { value: true, label: 'Yes' },
