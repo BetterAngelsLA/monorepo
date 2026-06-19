be39c9a22305e4e58ad8871b4ea85434fd25a6e9
Paul Vecchio <paul@betterangels.la>
2026-06-19 03:51:03 +0000

    revert: restore original _add_shelter_view_permission

--- apps/betterangels-backend/shelters/tests/test_admin_queries.py
+++ apps/betterangels-backend/shelters/tests/test_admin_queries.py
@@ -32,16 +32,9 @@ class OperatorShelterQueryTestCase(GraphQLBaseTestCase):
         self.shelter = shelter_recipe.make(organization=self.org_1)
 
     def _add_shelter_view_permission(self) -> None:
-        """Grant view_shelter permission to org_1_case_manager_1 via their PermissionGroup."""
-        from accounts.models import PermissionGroup
-        from django.contrib.auth.models import Permission
-        from django.contrib.contenttypes.models import ContentType
-
-        ct = ContentType.objects.get_for_model(Shelter)
-        perm = Permission.objects.get(codename="view_shelter", content_type=ct)
-        pg = PermissionGroup.objects.filter(group__user=self.org_1_case_manager_1).first()
-        if pg:
-            pg.group.permissions.add(perm)
+        app_label, codename = Shelter.perms.VIEW.split(".")
+        perm = Permission.objects.get(codename=codename, content_type__app_label=app_label)
+        self.org_1_case_manager_1.user_permissions.add(perm)
 
     def test_operator_shelters_filter_by_organization(self) -> None:
         """Only shelters for the specified organization are returned."""
