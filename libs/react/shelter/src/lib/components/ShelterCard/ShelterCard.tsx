708d55d3d76005ae05e6c98cdc996439e6c698b4
Nayan Parmar <nparmar84@gmail.com>
2026-06-19 11:53:15 -0700

    updated shelter cards pressed state

--- libs/react/shelter/src/lib/components/ShelterCard/ShelterCard.tsx
+++ libs/react/shelter/src/lib/components/ShelterCard/ShelterCard.tsx
@@ -47,7 +47,15 @@ export function ShelterCard(props: TShelterCard) {
 
   const formattedAddress = location?.place.replace(/, USA$/, '');
 
-  const parentCss = ['flex', 'flex-col', 'cursor-pointer', className];
+  const parentCss = [
+    'flex',
+    'flex-col',
+    'cursor-pointer',
+    'rounded-lg',
+    'transition-colors',
+    'active:bg-[#E8ECF2]',
+    className,
+  ];
 
   const bodyCss = ['flex', 'flex-col', 'md:flex-row', 'overflow-hidden'];
 
