880a4c337b30a3ef50aa4dbc325bde69b5471b79
Paul Vecchio <paul@betterangels.la>
2026-06-25 02:45:30 +0000

    refactor: remove unnecessary orgLink re-exports from both app apollo barrels

--- libs/react/betterangels-admin/src/lib/apollo/index.ts
+++ libs/react/betterangels-admin/src/lib/apollo/index.ts
@@ -4,5 +4,3 @@ export {
   UserOrganizationPermissions,
 } from './graphql/__generated__/types';
 export * from './graphql/response';
-// Re-export orgLink from the shared ba-platform lib
-export { createOrgLink, orgLink } from '@monorepo/ba-platform';
--- libs/react/shelter-operator/src/lib/apollo/index.ts
+++ libs/react/shelter-operator/src/lib/apollo/index.ts
@@ -1,3 +1,1 @@
 export * from './graphql';
-// Re-export orgLink from the shared ba-platform lib
-export { createOrgLink, orgLink } from '@monorepo/ba-platform';
