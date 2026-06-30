c8f8b236849928df345fce264fac4998dc0e0a8a
tomek <tgmessages@gmail.com>
2026-06-30 15:25:12 -0700

    Fix shelter photo cache sync after upload (#2206)
    * export operator shelter gql typename from hook for safety
    
    * export shelter img upload typenames
    
    * adjust shelter cachePolicy to accept extra policies for so

--- libs/react/shelter-operator/src/lib/hooks/useShelterOperatorProfile/index.ts
+++ libs/react/shelter-operator/src/lib/hooks/useShelterOperatorProfile/index.ts
@@ -1,2 +1,3 @@
 export type { UseShelterOperatorProfileResultType } from './types';
+export { OPERATOR_SHELTER_TYPENAME } from './constants';
 export { useShelterOperatorProfile } from './useShelterOperatorProfile';
