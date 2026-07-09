eaaafc9eb5d1f8a5541de16c8dcda63cc0bc132b
tomek <tgmessages@gmail.com>
2026-07-09 00:42:37 -0700

    delete legacy shelter-create-form (#2222)

--- libs/react/shelter-operator/src/lib/routing/routePaths.ts
+++ libs/react/shelter-operator/src/lib/routing/routePaths.ts
@@ -16,7 +16,6 @@ export const paths = {
   signIn: '/operator/sign-in',
   createOrganization: '/operator/create-organization',
   users: '/operator/users',
-  dashboardCreate: '/operator/dashboard/create',
   shelter: '/operator/shelter/:shelterId',
   shelterCreate: '/operator/shelter/create',
   shelterManage: '/operator/shelter/:shelterId/manage',
