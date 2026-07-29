b7e75df2355ecef4c8bd0f74cad219204b3ab5d8
tomek <tgmessages@gmail.com>
2026-07-29 14:46:58 -0700

    Turn off expo dev client tools button via config (DEV-2430) (#2283)
    * add expo-dev-launcher options to app.config
    
    * bump app version
    
    * typo fix

--- apps/betterangels/app.config.js
+++ apps/betterangels/app.config.js
@@ -21,7 +21,7 @@ export default {
     name: IS_PRODUCTION ? 'BetterAngels' : 'BetterAngels (Dev)',
     slug: 'betterangels',
     scheme: IS_PRODUCTION ? 'betterangels' : 'betterangels-dev',
-    version: '1.2.6',
+    version: '1.2.7',
     orientation: 'portrait',
     icon: IS_PRODUCTION
       ? './src/app/assets/images/icon.png'
@@ -34,7 +34,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: BUNDLE_IDENTIFIER,
-      buildNumber: '1.2.6',
+      buildNumber: '1.2.7',
       associatedDomains: [`applinks:${HOSTNAME}`],
       config: {
         usesNonExemptEncryption: false,
@@ -70,7 +70,7 @@ export default {
         },
       ],
       config: {},
-      versionCode: 80,
+      versionCode: 81,
     },
     web: {
       favicon: './src/app/assets/images/favicon.png',
@@ -102,6 +102,10 @@ export default {
         'expo-dev-client',
         {
           launchMode: 'launcher',
+          toolsButton: false,
+          skipOnboarding: true,
+          showMenuAtLaunch: false,
+          // See: https://github.com/expo/expo/blob/main/packages/expo-dev-launcher/plugin/src/pluginConfig.ts
         },
       ],
       [
