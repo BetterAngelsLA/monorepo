4164e39f73d39dccf859ccc399dcc6adaa491dc9
c4bae <123281198+c4bae@users.noreply.github.com>
2026-08-07 01:08:06 +0000

    Update button size + right align

--- libs/react/shelter-operator/src/lib/pages/dashboard/components/ExportShelterModal.tsx
+++ libs/react/shelter-operator/src/lib/pages/dashboard/components/ExportShelterModal.tsx
@@ -253,10 +253,10 @@ export function ExportShelterModal({
         </div>
       </ModalBody>
 
-      <ModalFooter className="grid grid-cols-2 gap-5 px-7 pb-7 pt-8">
+      <ModalFooter className="flex justify-end gap-5 px-7 pb-7 pt-8">
         <Button
           variant="floating-inverse"
-          className="h-[54px] w-full justify-center border-0 bg-white text-[#6B7280] shadow-[0_10px_24px_rgba(17,24,39,0.08)] hover:bg-[#F9FAFB] [&>span]:font-normal"
+          className="h-[54px] w-[213px] justify-center border-0 bg-white text-[#6B7280] shadow-[0_10px_24px_rgba(17,24,39,0.08)] hover:bg-[#F9FAFB] [&>span]:font-normal"
           rightIcon={false}
           onClick={onClose}
         >
@@ -265,7 +265,7 @@ export function ExportShelterModal({
         <Button
           variant="floating"
           color="blue"
-          className="h-[54px] w-full justify-center shadow-none [&>span]:font-normal"
+          className="h-[54px] w-[213px] justify-center shadow-none [&>span]:font-normal"
           rightIcon={false}
         >
           Export
