b34a771d28092ecda824fd9beb42725a57603cbb
Mike Feldberg <mike@betterangels.la>
2026-08-25 10:46:54 -0700

    lint; codegen

--- libs/ba-platform/src/lib/apollo/graphql/__generated__/types.ts
+++ libs/ba-platform/src/lib/apollo/graphql/__generated__/types.ts
@@ -2617,7 +2617,7 @@ export type Query = {
   shelterCities: CityTypeOffsetPaginated;
   shelterMaxStay?: Maybe<Scalars['Int']['output']>;
   shelterOccupancyMetrics: ShelterOccupancyMetricsType;
-  shelterOperatorOrganizations: OrganizationTypeOffsetPaginated;
+  shelterOrganizations: OrganizationTypeOffsetPaginated;
   shelterServiceCategories: ServiceCategoryTypeOffsetPaginated;
   shelterSpas: SpaTypeOffsetPaginated;
   shelters: ShelterTypeOffsetPaginated;
@@ -2855,7 +2855,7 @@ export type QueryShelterOccupancyMetricsArgs = {
 };
 
 
-export type QueryShelterOperatorOrganizationsArgs = {
+export type QueryShelterOrganizationsArgs = {
   filters?: InputMaybe<OrganizationFilter>;
   ordering?: Array<OrganizationOrder>;
   pagination?: InputMaybe<OffsetPaginationInput>;
